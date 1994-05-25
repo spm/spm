95e22d89707ad127263e3542fdaf443790c22eec
John Ashburner <j.ashburner@ucl.ac.uk>
1994-05-25 15:39:05 +0000

    Initial revision

---
+++ spm_unmap_vol.c
@@ -0,0 +1,31 @@
+#ifndef lint
+static char sccsid[]="%W% John Ashburner %E%";
+#endif
+
+#include "cmex.h"
+#include "volume.h"
+
+
+#ifdef __STDC__
+void mexFunction(int nlhs, Matrix *plhs[], int nrhs, Matrix *prhs[])
+#else
+mexFunction(nlhs, plhs, nrhs, prhs)
+int nlhs, nrhs;
+Matrix *plhs[], *prhs[];
+#endif
+{
+	MAPTYPE *map;
+	int i, n;
+	double *dat;
+	if (nrhs != 1 || nlhs > 0)
+		mexErrMsgTxt("Inappropriate usage.");
+
+	map=get_map(prhs[0]);
+	(void)close(map->fd);
+	(void)munmap(map->map, map->off+map->len);
+
+	dat = mxGetPr(prhs[0]);
+	n = mxGetN(prhs[0])*mxGetM(prhs[0]);
+	for(i=0; i< n; i++)
+		dat[i] = 0.0;
+}
