b1c5e0feb3d5961daca53d6f1b34d5d54223e7e7
Tom Nichols <thomas.nichols@bdi.ox.ac.uk>
1995-08-11 20:07:56 +0000

    Initial revision

---
+++ spm_unmap.m
@@ -0,0 +1,14 @@
+function spm_unmap(V)
+% unmaps a memory mapped volume
+% FORMAT spm_unmap(V)
+% V    -  is a memory mapped volume
+%____________________________________________________________________________
+%
+% spm_unmap calls spm_unmap_vol
+%
+% see also spm_unmap_vol.m, spm_map.m, and spm_map_vol.m
+%
+% %W% %E%
+
+spm_unmap_vol(V)
+	
