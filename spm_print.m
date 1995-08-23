1878bbac9e9cb52aa9d0c0167f070828a286e10e
Andrew Holmes <andrew.holmes@veramed.co.uk>
1995-08-23 17:22:15 +0000

    Added default PRINTSTR, optional figure specification Default figure is now found as 'Graphics' 'Tag'ged figure - if not found then default to gcf.

--- spm_print.m
+++ spm_print.m
@@ -1,6 +1,9 @@
-function spm_print
+function spm_print(F)
 % SPM print function that executes PRINTSTR
-% FORMAT spm_print
+% FORMAT spm_print(F)
+% F	- [Optional] Figure to print
+%	- defaults to the figure 'Tag'ged as 'Graphics'
+%	- If no such figure is found, then CurrentFigure is used, if avaliable
 %___________________________________________________________________________
 %
 % spm_print creates a footnote with details of the current
@@ -9,21 +12,33 @@ function spm_print
 %__________________________________________________________________________
 % %W% %E%
 
-% create footnote
+
+%-Retrieve print command
 %---------------------------------------------------------------------------
 global PRINTSTR
+if isempty(PRINTSTR)
+	PrintCmd = 'print -dps2 fig.ps';
+else
+	PrintCmd = PRINTSTR;
+end
 
+%-Find window to print
+%---------------------------------------------------------------------------
+if (nargin<1), F = findobj(get(0,'Children'),'Tag','Graphics'); end
+if (isempty(F) & length(get(0,'Children'))), F = gcf; end
+figure(F)
+
+%-Create footnote
+%---------------------------------------------------------------------------
 NAME	        = getenv('USER');			% username
 GRAPHSTR  	= ['SPM analysis - date: ' date '  user: ' NAME];
 GRAPHSTR  	= GRAPHSTR(GRAPHSTR ~= 10);		% footnote
 
-figure(3)
-
-% create axis and text objects
+%-Create axis and text objects
 %---------------------------------------------------------------------------
 axes('position',[0.02 0.02 1 1],'Visible','off')
 text(0,0,GRAPHSTR,'FontSize',10);
 
-% print command
+%-Print command
 %---------------------------------------------------------------------------
-eval(PRINTSTR)
+eval(PrintCmd)
