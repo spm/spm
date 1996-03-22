47ac43ac451a558fb0deccc494dc67c911cfb380
Andrew Holmes <andrew.holmes@veramed.co.uk>
1996-03-22 15:03:39 +0000

    Added DefaultFigurePaperType

--- spm.m
+++ spm.m
@@ -1,24 +1,38 @@
 
 % SPM startup
 % FORMAT spm
-%____________________________________________________________________________
+%_______________________________________________________________________
 % spm prompts for PET or fMRI and calls the apropriate user interface
 % routine
 %
 % see also spm_ui.m and spm_fmri_ui.m
 %
-%__________________________________________________________________________
+%_______________________________________________________________________
 % %W% %E%
 
+%-Clean up workspace, print welcome banner
+%-----------------------------------------------------------------------
 close all
+clear all
+clc
+disp(' ___  ____  __  __                                                  ')
+disp('/ __)(  _ \(  \/  )  Statistical Parametric Mapping                 ')
+disp('\__ \ )___/ )    (   The Wellcome Department of Cognitive Neurology ')
+disp('(___/(__)  (_/\/\_)  Version SPMdevel:96                            ')
+fprintf('\n')
 
-% figure, frames and text
-%----------------------------------------------------------------------------
+%-Open startup window, set window defaults
+%-----------------------------------------------------------------------
 whitebg(0,'w')
 
 figure('Color',[1 1 1]*.8,'Name',' ',...
 	'NumberTitle','off','Position',[360 200 400 280],'Resize','off');
 
+set(0,'DefaultFigureColormap',gray);
+set(0,'DefaultFigurePaperType','a4letter')
+
+%-Frames and text
+%-----------------------------------------------------------------------
 uicontrol(1,'Style','Frame','Position',[10 120 380 140]);
 uicontrol(1,'Style','Frame','Position',[10 020 380 090]);
 
@@ -38,8 +52,8 @@ uicontrol(1,'Style','Text', 'Position',[10 140 380 16],'String',c)
 global MODALITY
 MODALITY = 'unknown';
 
-% objects with Callbacks - main spm_*_ui.m routines
-%----------------------------------------------------------------------------
+%-Objects with Callbacks - main spm_*_ui.m routines
+%-----------------------------------------------------------------------
 uicontrol(1,'String','PET and SPECT',         'Position',[040 066 150 30],...
 'CallBack','global MODALITY ; MODALITY = ''PET''; spm_ui','Interruptible','yes','ForegroundColor',[0 1 1]);
 
