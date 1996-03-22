db3fb0c717f877e5e5311b1e28738e4d421a1584
Andrew Holmes <andrew.holmes@veramed.co.uk>
1996-03-22 15:09:23 +0000

    Integrated with spm_figure.m spm_clf is now only a gateway to spm_figure('Clear') Improved to work on non-current figures

--- spm_clf.m
+++ spm_clf.m
@@ -1,19 +1,26 @@
-function spm_clf
+function spm_clf(F)
 % Selective deletion of objects in the current figure window
-% FORMAT spm_clf
+% FORMAT spm_clf(F)
+% F	- Figure number, or 'Tag' string of figure(s) to clear
 %___________________________________________________________________________
 %
-% deletes the children of the current figure other that objects
-% with 'Tag' attribute = 'NoDelete'.   This serves as a partial
-% clf, preventing some objects (e.g. menu bar objects) from being
-% deleted
+% Deletes the children of the specified figure, other than objects with
+% 'Tag' attribute = 'NoDelete'. This serves as a partial clf,
+% preventing some objects (e.g. menu bar objects) from being deleted.
 %
+% If the current window is 'Tag'ged interactive, then the figures name
+% is cleared and the pointer reset.
+%
+% F Defaults to the current figure, if there is one.
+%
+% This is just a gateway to spm_figure('Clear',F)
 %__________________________________________________________________________
 % %W% %E%
 
-
+%-Call spm_figure
 %---------------------------------------------------------------------------
-h = get(gcf,'Children');
-for i = 1:length(h)
-	if ~strcmp(get(h(i),'Tag'),'NoDelete'); delete(h(i)); end
+if nargin==0
+	spm_figure('Clear')
+else
+	spm_figure('Clear',F)
 end
