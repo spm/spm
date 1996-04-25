58f78745f06fc7f36b6dae94826b4f2ce987278a
Andrew Holmes <andrew.holmes@veramed.co.uk>
1996-04-25 18:29:19 +0000

    Added tagging of various graphics objects as "Empty", to act as data containers (via "UserData") for the new buttons in spm_projections_ui.m (v1.3).

--- spm_projections.m
+++ spm_projections.m
@@ -1,6 +1,6 @@
 function [t,XYZ] = spm_projections(t,XYZ,u,k,V,W,S,G,C,df)
 % display and analysis of SPM{Z} [SPM{t}]
-% FORMAT [Z XYZ] = spm_projections(Z,XYZ,u,k,V,W,S,G,C,df);
+% FORMAT [Z,XYZ] = spm_projections(Z,XYZ,u,k,V,W,S,G,C,df);
 %
 % Z   - row vector of {n} voxel values
 % XYZ - {3 x n} matrix of spatial locations {mm}
@@ -46,16 +46,18 @@ function [t,XYZ] = spm_projections(t,XYZ,u,k,V,W,S,G,C,df)
 % The secondary maxima are selected to be at least 8mm apart.
 %
 %__________________________________________________________________________
-% %W% %E%
+% %W% Karl Friston %E%
 
-%---------------------------------------------------------------------------
 global CWD
 
-% display, characterize and print SPM{t}
+%-Display, characterize and print SPM{t}
 %===========================================================================
-figure(3); spm_clf; set(3,'Units','Normalized');
+Fgraph = spm_figure('FindWin','Graphics');
+figure(Fgraph)
+spm_clf
+set(Fgraph,'Units','Normalized');
 
-% apply threshold {u}
+%-Apply height threshold {u}
 %---------------------------------------------------------------------------
 if size(C,1) > 1
 	Q = all(t > u);
@@ -63,15 +65,13 @@ if size(C,1) > 1
 	g = C*pinv(G'*G)*C';
 	r = inv(diag(sqrt(diag(g))))'*g*inv(diag(sqrt(diag(g))));
 	t = sum(t(:,Q))/sqrt(sum(r(:)));
-
 else
 	Q = t > u;
 	t = t(1,Q);
-
 end
 XYZ       = XYZ(:,Q);
 
-% return if there are no voxels
+%-Return if there are no voxels
 %---------------------------------------------------------------------------
 if sum(Q) == 0
 	axis off
@@ -80,18 +80,21 @@ if sum(Q) == 0
 	return
 end
 
-% apply threshold {k}
+%-Apply extent threshold {k}
 %---------------------------------------------------------------------------
-A         = spm_clusters(XYZ,V([4 5 6]));
-Q         = [];
-for i     = 1:max(A)
+A     = spm_clusters(XYZ,V([4 5 6]));
+Q     = [];
+for i = 1:max(A)
 	j = find(A == i);
 	if length(j) >= k
 		Q = [Q j];
 	end
 end
 
-% return if there are no voxels
+t     = t(Q);
+XYZ   = XYZ(:,Q);
+
+%-Return if there are no voxels
 %---------------------------------------------------------------------------
 if sum(Q) == 0
 	axis off
@@ -100,81 +103,81 @@ if sum(Q) == 0
 	return
 end
 
-t       = t(Q);
-XYZ     = XYZ(:,Q);
-
-
-
-% maximium intenisty projection of SPM{Z}
-%---------------------------------------------------------------------------
-axes('Position',[0.05 0.5 0.5 0.5])
+%-Maximium intenisty projection of SPM{Z}
+%===========================================================================
+axes('Position',[0.05 0.5 0.5 0.5],'Tag','Empty');
 spm_mip(t,XYZ,V(1:6)); axis image
 title('SPM{Z}','FontSize',16,'Fontweight','Bold')
 
-axes('Position',[0.65 0.6 0.2 0.2])
+%-Show design matrix & contrast
+%===========================================================================
+axes('Position',[0.65 0.6 0.2 0.2],'Tag','Empty')
 imagesc(spm_DesMtxSca(G))
 xlabel 'design matrix'
-axes('Position',[0.65 0.8 0.2 0.1])
+axes('Position',[0.65 0.8 0.2 0.1],'Tag','Empty')
 bar(C'); hold on
 [i j]   = bar(C(1,:));
 fill(i,j,[1 1 1]*.8);
 set(gca,'Xlim',[min(i) max(i)]) 
 title 'contrast'; axis off; hold off
 
-	
-% characterize excursion set in terms of maxima %---------------------------------------------------------------------------
-[N Z M A] = spm_max(t,XYZ,V([4 5 6]));
-
-% display (sorted on Z values and grouped by regions)
+%-Display (sorted on Z values and grouped by regions)
 %===========================================================================
 
+%-Characterize excursion set in terms of maxima 
+%---------------------------------------------------------------------------
+[N Z M A] = spm_max(t,XYZ,V([4 5 6]));
 
-% table headings
+
+%-Table headings
 %---------------------------------------------------------------------------
 y   = 24;
-axes('Position',[0.1 0.06 0.8 0.46]); axis off
+axes('Position',[0.1 0.06 0.8 0.46],'Tag','Empty'); axis off
 text(0,y,['P values & statistics:   ' CWD],'FontSize',12,'FontWeight','Bold');
 y   = y - 1;
 line([0 1],[y y],'LineWidth',3,'Color',[0 0 0])
 y   = y - 1;
 
-% Construct tables
-%===========================================================================
-text(0.00,y,'set-level {c}'      ,'FontSize',10);
-text(0.18,y,'cluster-level {k,Z}','FontSize',10);
-text(0.42,y,'voxel-level {Z}'    ,'FontSize',10);
-text(0.64,y,'uncorrected'        ,'FontSize',10);
-text(0.84,y,'location {mm}'      ,'FontSize',10);
+%-Construct tables
+%---------------------------------------------------------------------------
+text(0.00,y,'set-level {c}'      ,'FontSize',10,'Tag','Empty');
+text(0.18,y,'cluster-level {k,Z}','FontSize',10,'Tag','Empty');
+text(0.42,y,'voxel-level {Z}'    ,'FontSize',10,'Tag','Empty');
+text(0.64,y,'uncorrected'        ,'FontSize',10,'Tag','Empty');
+text(0.84,y,'location {mm}'      ,'FontSize',10,'Tag','Empty');
 y  = y - 1;
 
 line([0 1],[y y],'LineWidth',3,'Color',[0 0 0])
 y  = y - 1;
 
-% set-level p values {c}
+% -Set-level p values {c}
 %-----------------------------------------------------------------------
 c      = max(A);				% number of clusters
 Pc     = spm_P(c,W,u,k,S);			% set-level p value
 
-% print set
+%-Print set-level p-value
 %-----------------------------------------------------------------------
 str    = sprintf('%-0.3f   (%i)',Pc,c);
 text(0.00,y,str,'FontSize',8,'FontWeight','Bold')
 
-while max(Z) & (y > 0)
 
-    	% cluster-level p values {k}
-    	%--------------------------------------------------------------------
+while max(Z) & (y > 0)
+    	%-Find largest remaining local maximum
+    	%---------------------------------------------------------------
 	[U i] = max(Z);				% largest Z value
 	j     = find(A == A(i));		% maxima in cluster
-	Pkn   = spm_Pkn(N(i),U-u,W,u,S);
+
+    	%-Compute cluster {k} and voxel-level p values for this cluster
+    	%---------------------------------------------------------------
+	Pkn   = spm_Pkn(N(i),U-u,W,u,S);	% Bivariate cluster-height p
 %	Pk    = spm_P(1,W,u,N(i),S);		% cluster-level p value
 	Pu    = spm_P(1,W,U,0,S);		% voxel-level p value
 	Pz    = 1 - spm_Ncdf(U);		% uncorrected p value
 
         if N(i) < k; break; end
 
-	% print cluster and maximum voxel-level p values {Z}
-	%-------------------------------------------------------------------
+	%-Print cluster and maximum voxel-level p values {Z}
+    	%---------------------------------------------------------------
         str   = sprintf('%-0.3f   (%i, %0.2f)',Pkn,N(i),U);
 	text(0.18,y,str,'FontSize',8,'FontWeight','Bold')
         str   = sprintf('%-0.3f   (%-0.2f)',Pu,U);
@@ -186,8 +189,8 @@ while max(Z) & (y > 0)
  
 	y     = y - 1;
 
-	% print 2 secondary maxima (> 8mm apart)
-	%-------------------------------------------------------------------
+	%-Print 2 secondary maxima (> 8mm apart)
+    	%---------------------------------------------------------------
 	[l q] = sort(-Z(j));				% sort on Z value
 	D     = i;
 	for i = 1:length(q)
@@ -196,7 +199,7 @@ while max(Z) & (y > 0)
 		    if length(D) < 3
 			
 			% voxel-level p values {Z}
-			%----------------------------------------------------
+			%-----------------------------------------------
 			Pu  = spm_P(1,W,Z(d),0,S);	% voxel-level p value
 			Pz  = 1 - spm_Ncdf(Z(d));	% uncorrected p value
 
@@ -211,8 +214,9 @@ while max(Z) & (y > 0)
 		    end
 		end
 	end
-	Z(j) = Z(j)*0;
+	Z(j) = Z(j)*0;			% Zero local maxima for this cluster
 end					% end region
+
 if size(C,1) > 1
 	str = 'NB multiple contrasts - use voxel-level inferences';
 	text(0,y,str,'Color',[1 0 0])
@@ -222,16 +226,16 @@ end
 line([0 1],[y y],'LineWidth',1,'Color',[0 0 0])
 y     = y - 0.5;
 
-% volume, resels and smoothness 
-%---------------------------------------------------------------------------
+%-Volume, resels and smoothness 
+%=======================================================================
 D               = length(W);					% dimension
 FWHM            = sqrt(8*log(2))*W.*V(([1:D] + 3),1)'; 		% FWHM {mm}
 RESEL           = S*prod(V([1:D] + 3))/prod(FWHM);		% RESELS
 [P,EN,Em,En,Pk] = spm_P(1,W,u,k,S);
 
 
-% footnote with SPM parameters
-%---------------------------------------------------------------------------
+%-Footnote with SPM parameters
+%-----------------------------------------------------------------------
 str = sprintf('Height threshold {u} = %0.2f, p = %0.3f',u,1 - spm_Ncdf(u));
 text(0,y,str,'FontSize',8);
 y  = y - 1;
