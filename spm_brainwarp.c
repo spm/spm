5cba869ba7797f90e7dc88e688373d6072dae332
John Ashburner <j.ashburner@ucl.ac.uk>
1996-07-08 16:10:28 +0000

    Got rid of "resels" stuff

--- spm_brainwarp.c
+++ spm_brainwarp.c
@@ -2,15 +2,15 @@
 static char sccsid[] = "%W% (c) John Ashburner MRCCU/FIL %E%";
 #endif lint
 
-#include <math.h>
 #include "cmex.h"
 #include "volume.h"
-double fabs();
+#include <math.h>
+extern double floor(), fabs();
 
 
 /*
 INPUTS
-T[3*nz*ny*nx + ni] - current transform
+T[3*nz*ny*nx + ni*4] - current transform
 
 dt2           - data type of image to normalize
 scale2        - scalefactor of image to normalize
@@ -21,7 +21,7 @@ ni            - number of templates
 dt1[ni]       - data types of templates
 scale1[ni]    - scalefactors of templates
 dim1[3]       - x, y & z dimensions of templates
-dat1[ni][dim1[2]*dim1[1]*dim1[0]] - voxels of templates
+dat1[ni*dim1[2]*dim1[1]*dim1[0]] - voxels of templates
 
 nx            - number of basis functions in x
 BX[dim1[0]*nx]- basis functions in x
@@ -35,12 +35,13 @@ samp[3]       - frequency of sampling template.
 
 
 OUTPUTS
-alpha[(3*nz*ny*nx+ni)^2] - A'A
-beta[(3*nz*ny*nx+ni)]    - A'B
-
-on each iteration, T is updated by T = T + alpha\beta;
+alpha[(3*nz*ny*nx+ni*4)^2] - A'A
+ beta[(3*nz*ny*nx+ni*4)]   - A'b
+pss[1*1]                   - sum of squares difference
+pnsamp[1*1]                - number of voxels sampled
 
 */
+
 
 void mrqcof(T, alpha, beta, pss, dt2,scale2,dim2,dat2, ni,dt1,scale1,dim1,dat1, nx,BX, ny,BY, nz,BZ, M, samp, pnsamp)
 double T[], alpha[], beta[], pss[], BX[], BY[], BZ[], M[], scale2, scale1[];
@@ -48,12 +49,15 @@ int dt2, dim2[], dt1[], dim1[], ni, nx, ny, nz, samp[], *pnsamp;
 unsigned char *dat1[], dat2[];
 {
 	int i1,i2, s0[3], x1,x2, y1,y2, z1,z2, m1, m2, nsamp = 0, ni4;
-	double dvds0[3],dvds1[3], *dvdt, s2[3], *ptr1, *ptr2, *ptr3, *ptr4, *Tz, *Ty, tmp,dtmp,
+	double dvds0[3],dvds1[3], *dvdt, s2[3], *ptr1, *ptr2, *Tz, *Ty, tmp,dtmp,
 		*betaxy, *betax, *alphaxy, *alphax, ss=0.0, *scale1a;
-	extern double floor();
+
 	ni4 = ni*4;
 
+	/* rate of change of voxel with respect to change in parameters */
 	dvdt    = (double *)mxCalloc( 3*nx    + ni4                 , sizeof(double));
+
+	/* Intermediate storage arrays */
 	Tz      = (double *)mxCalloc( 2*3*nx*ny                     , sizeof(double));
 	Ty      = (double *)mxCalloc( 2*3*nx                        , sizeof(double));
 	betax   = (double *)mxCalloc( 3*nx    + ni4                 , sizeof(double));
@@ -61,6 +65,7 @@ unsigned char *dat1[], dat2[];
 	alphax  = (double *)mxCalloc((3*nx    + ni4)*(3*nx    + ni4), sizeof(double));
 	alphaxy = (double *)mxCalloc((3*nx*ny + ni4)*(3*nx*ny + ni4), sizeof(double));
 
+	/* pointer to scales for each of the template images */
 	scale1a = T + 3*nx*ny*nz;
 
 	/* only zero half the matrix */
@@ -83,9 +88,11 @@ unsigned char *dat1[], dat2[];
 				{
 					tmp  += ptr1[x1+z1*ny*nx] *  BZ[dim1[2]*z1+s0[2]];
 					if (i1==2)
-						dtmp += ptr1[x1+z1*ny*nx] * (BZ[dim1[2]*z1+s0[2]]*0.9+BZ[dim1[2]*z1+s0[2]+1]*0.1);					
+						dtmp += ptr1[x1+z1*ny*nx] * BZ[dim1[2]*z1+s0[2]+1];					
 				}
+
 				ptr2[x1] = tmp;
+
 				if (i1==2)
 					ptr2[x1 + 3*nx*ny] = dtmp;
 				else
@@ -103,7 +110,7 @@ unsigned char *dat1[], dat2[];
 
 		for(s0[1]=0; s0[1]<dim1[1]-1; s0[1]+=samp[1]) /* For each row of the template images plane */
 		{
-			/* build up the deformation field from it's seperable form */
+			/* build up the deformation field (and gradients) from it's seperable form */
 			for(i1=0, ptr1=Tz, ptr2=Ty; i1<3; i1++, ptr1+=ny*nx, ptr2+=nx)
 			{
 				for(x1=0; x1<nx; x1++)
@@ -113,9 +120,9 @@ unsigned char *dat1[], dat2[];
 					{
 						tmp  += ptr1[x1+y1*nx] *  BY[dim1[1]*y1+s0[1]];
 						if (i1==1)
-							dtmp += ptr1[x1+nx*(y1+3*ny)] * (BY[dim1[1]*y1+s0[1]]*0.9+BY[dim1[1]*y1+s0[1]+1]*0.1);
+							dtmp += ptr1[x1+nx*(y1+3*ny)] * BY[dim1[1]*y1+s0[1]+1];
 						else
-							dtmp += ptr1[x1+nx*(y1+3*ny)] *  BY[dim1[1]*y1+s0[1]];
+							dtmp += ptr1[x1+nx*(y1+3*ny)] * BY[dim1[1]*y1+s0[1]  ];
 
 					}
 					ptr2[x1     ] = tmp;
@@ -134,9 +141,9 @@ unsigned char *dat1[], dat2[];
 
 			for(s0[0]=0; s0[0]<dim1[0]-1; s0[0]+=samp[0]) /* For each pixel in the row */
 			{
-				double trans[3], dtrans[3], dtrans1[3], volume = 1;
+				double trans[3], dtrans[3];
 
-				/* nonlinear deformation of the atlas space, followed by the affine transform */
+				/* nonlinear deformation of the template space, followed by the affine transform */
 				for(i1=0, ptr1 = Ty; i1<3; i1++, ptr1 += nx)
 				{
 					dtmp = tmp = 1.0;
@@ -144,14 +151,15 @@ unsigned char *dat1[], dat2[];
 					{
 						tmp  += ptr1[x1] * BX[dim1[0]*x1+s0[0]];
 						if (i1==0)
-							dtmp += ptr1[x1+3*nx] * (BX[dim1[0]*x1+s0[0]]*0.9+BX[dim1[0]*x1+s0[0]+1]*0.1);
+							dtmp += ptr1[x1+3*nx] * BX[dim1[0]*x1+s0[0]+1];
 						else
-							dtmp += ptr1[x1+3*nx] *  BX[dim1[0]*x1+s0[0]];
+							dtmp += ptr1[x1+3*nx] * BX[dim1[0]*x1+s0[0]  ];
 					}
 					 trans[i1] = tmp + s0[i1];
-					dtrans[i1] = 1 + (dtmp-tmp)/0.1;
+					dtrans[i1] = 1 + dtmp - tmp; /* derivative */
 				}
 
+				/* Affine component */
 				s2[0] = M[0+4*0]*trans[0] + M[0+4*1]*trans[1] + M[0+4*2]*trans[2] + M[0+4*3];
 				s2[1] = M[1+4*0]*trans[0] + M[1+4*1]*trans[1] + M[1+4*2]*trans[2] + M[1+4*3];
 				s2[2] = M[2+4*0]*trans[0] + M[2+4*1]*trans[1] + M[2+4*2]*trans[2] + M[2+4*3];
@@ -180,7 +188,6 @@ unsigned char *dat1[], dat2[];
 
 					/* get pixel values of 8 nearest neighbours */
 					off1 = xcoord-1 + dim2[0]*(ycoord-1  + dim2[1]*(zcoord-1));
-
 					switch (dt2)
 					{
 					case UNSIGNED_CHAR:
@@ -231,25 +238,27 @@ unsigned char *dat1[], dat2[];
 					default:
 						mexErrMsgTxt("Bad data type.");
 					}
+
+					/* resampled pixel value */
 					v =     (((val111*dx2 + val011*dx1)*dy2
 						+ (val101*dx2 + val001*dx1)*dy1)*dz2
 						+((val110*dx2 + val010*dx1)*dy2
 						+ (val100*dx2 + val000*dx1)*dy1)*dz1)*scale2;
 
-					/* local gradients accross resampled pixel */
+					/* local gradients accross resampled pixel (in space of object image) */
 					dvds0[0] = ((dy2*(val011-val111) + dy1*(val001-val101))*dz2
-						 + (dy2*(val010-val110) + dy1*(val000-val100))*dz1)*scale2*dtrans[0];
+						 + (dy2*(val010-val110) + dy1*(val000-val100))*dz1)*scale2;
 
 					dvds0[1] = ((dx2*(val101-val111) + dx1*(val001-val011))*dz2
-						 + (dx2*(val100-val110) + dx1*(val000-val010))*dz1)*scale2*dtrans[1];
+						 + (dx2*(val100-val110) + dx1*(val000-val010))*dz1)*scale2;
 
 					dvds0[2] = ((dx2*(val110-val111) + dx1*(val010-val011))*dy2
-						 + (dx2*(val100-val101) + dx1*(val000-val001))*dy1)*scale2*dtrans[2];
+						 + (dx2*(val100-val101) + dx1*(val000-val001))*dy1)*scale2;
 
-					/* transform the gradients to the same space as the atlas */
-					dvds1[0] = (M[0+4*0]*dvds0[0] + M[1+4*0]*dvds0[1] + M[2+4*0]*dvds0[2]);
-					dvds1[1] = (M[0+4*1]*dvds0[0] + M[1+4*1]*dvds0[1] + M[2+4*1]*dvds0[2]);
-					dvds1[2] = (M[0+4*2]*dvds0[0] + M[1+4*2]*dvds0[1] + M[2+4*2]*dvds0[2]);
+					/* transform the gradients to the same space as the template */
+					dvds1[0] = (M[0+4*0]*dvds0[0] + M[1+4*0]*dvds0[1] + M[2+4*0]*dvds0[2])*dtrans[0];
+					dvds1[1] = (M[0+4*1]*dvds0[0] + M[1+4*1]*dvds0[1] + M[2+4*1]*dvds0[2])*dtrans[1];
+					dvds1[2] = (M[0+4*2]*dvds0[0] + M[1+4*2]*dvds0[1] + M[2+4*2]*dvds0[2])*dtrans[2];
 
 
 					/* there is no change in the contribution from BY and BZ, so only
@@ -260,7 +269,11 @@ unsigned char *dat1[], dat2[];
 							dvdt[i1*nx+x1] = -dvds1[i1] * BX[dim1[0]*x1+s0[0]];
 					}
 
+					/* coordinate of template voxel to sample */
 					off2 = s0[0] + dim1[0]*(s0[1] + dim1[1]*s0[2]);
+
+					/* dv will be the difference between the object image
+					   and the linear combination of templates */
 					dv = v;
 					for(i1=0; i1<ni; i1++)
 					{
@@ -285,6 +298,9 @@ unsigned char *dat1[], dat2[];
 						default:
 							mexErrMsgTxt("Bad data type.");
 						}
+
+						/* linear combination of image and image modulated by constant
+						   gradients in x, y and z */
 						dvdt[i1*4  +3*nx] = tmp;
 						dvdt[i1*4+1+3*nx] = tmp*s2[0];
 						dvdt[i1*4+2+3*nx] = tmp*s2[1];
@@ -296,8 +312,8 @@ unsigned char *dat1[], dat2[];
 						dv -= dvdt[i1*4+3+3*nx]*scale1a[i1+3];
 					}
 
-					ss += dv*dv;
 
+					/* cf Numerical Recipies "mrqcof.c" routine */
 					m1 = 3*nx+ni4;
 					for(x1=0; x1<m1; x1++)
 					{
@@ -305,91 +321,120 @@ unsigned char *dat1[], dat2[];
 							alphax[m1*x1+x2] += dvdt[x1]*dvdt[x2];
 						betax[x1] += dvdt[x1]*dv;
 					}
+
+					/* sum of squares */
+					ss += dv*dv;
 				}
 			}
 
-			/* Kronecker tensor products with BY'*BY */
 			m1 = 3*nx*ny+ni4;
 			m2 = 3*nx+ni4;
-			for(i1=0; i1<3; i1++)
+
+			/* Kronecker tensor products */
+			for(y1=0; y1<ny; y1++)
 			{
-				for(y1=0; y1<ny; y1++)
+				double wt = BY[dim1[1]*y1+s0[1]];
+
+				for(i1=0; i1<3; i1++)	/* loop over deformations in x, y and z */
 				{
-					for(i2=0; i2<=i1; i2++)
+					/* spatial-spatial covariances */
+					for(i2=0; i2<=i1; i2++)	/* symmetric matrixes - so only work on half */
 					{
 						for(y2=0; y2<=y1; y2++)
 						{
-							double wt = BY[dim1[1]*y1+s0[1]] * BY[dim1[1]*y2+s0[1]];
+							/* Kronecker tensor products with BY'*BY */
+							double wt2 = wt * BY[dim1[1]*y2+s0[1]];
+
 							ptr1 = alphaxy + nx*(m1*(ny*i1 + y1) + ny*i2 + y2);
 							ptr2 = alphax  + nx*(m2*i1 + i2);
+
 							for(x1=0; x1<nx; x1++)
 							{
 								for (x2=0;x2<=x1;x2++)
-									ptr1[m1*x1+x2] += ptr2[m2*x1+x2]*wt;
+									ptr1[m1*x1+x2] += wt2 * ptr2[m2*x1+x2];
 							}
 						}
 					}
+
+					/* spatial-intensity covariances */
 					ptr1 = alphaxy + nx*(m1*ny*3 + ny*i1 + y1);
 					ptr2 = alphax  + nx*(m2*3 + i1);
-
 					for(x1=0; x1<ni4; x1++)
 					{
 						for (x2=0;x2<nx;x2++)
-							ptr1[m1*x1+x2] += ptr2[m2*x1+x2] * BY[dim1[1]*y1+s0[1]];
+							ptr1[m1*x1+x2] += wt * ptr2[m2*x1+x2];
 					}
+
+					/* spatial component of beta */
 					for(x1=0; x1<nx; x1++)
-						betaxy[x1+nx*(ny*i1 + y1)] += betax[x1 + nx*i1] * BY[dim1[1]*y1+s0[1]];
+						betaxy[x1+nx*(ny*i1 + y1)] += wt * betax[x1 + nx*i1];
 				}
 			}
 			ptr1 = alphaxy + nx*(m1*ny*3 + ny*3);
 			ptr2 = alphax  + nx*(m2*3 + 3);
 			for(x1=0; x1<ni4; x1++)
 			{
+				/* intensity-intensity covariances  */
 				for (x2=0; x2<=x1; x2++)
 					ptr1[m1*x1 + x2] += ptr2[m2*x1 + x2];
+
+				/* intensity component of beta */
 				betaxy[nx*ny*3 + x1] += betax[nx*3 + x1];
 			}
 		}
 
-		/* Kronecker tensor products with BZ'*BZ */
 		m1 = 3*nx*ny*nz+ni4;
 		m2 = 3*nx*ny+ni4;
+
+		/* Kronecker tensor products */
 		for(z1=0; z1<nz; z1++)
 		{
-			for(i1=0; i1<3; i1++)
+			double wt = BZ[dim1[2]*z1+s0[2]];
+
+			for(i1=0; i1<3; i1++)	/* loop over deformations in x, y and z */
 			{
-				for(i2=0; i2<=i1; i2++)
+				/* spatial-spatial covariances */
+				for(i2=0; i2<=i1; i2++)	/* symmetric matrixes - so only work on half */
 				{
 					for(z2=0; z2<=z1; z2++)
 					{
-						double wt = BZ[dim1[2]*z1+s0[2]] * BZ[dim1[2]*z2+s0[2]];
+						/* Kronecker tensor products with BZ'*BZ */
+						double wt2 = wt * BZ[dim1[2]*z2+s0[2]];
+
 						ptr1 = alpha   + nx*ny*(m1*(nz*i1 + z1) + nz*i2 + z2);
 						ptr2 = alphaxy + nx*ny*(m2*i1 + i2);
 						for(y1=0; y1<ny*nx; y1++)
 						{
 							for (y2=0;y2<=y1;y2++)
-								ptr1[m1*y1+y2] += ptr2[m2*y1+y2]*wt;
+								ptr1[m1*y1+y2] += wt2 * ptr2[m2*y1+y2];
 						}
 					}
 				}
+
+				/* spatial-intensity covariances */
 				ptr1 = alpha   + nx*ny*(m1*nz*3 + nz*i1 + z1);
 				ptr2 = alphaxy + nx*ny*(m2*3 + i1);
-
 				for(y1=0; y1<ni4; y1++)
 				{
 					for (y2=0;y2<ny*nx;y2++)
-						ptr1[m1*y1+y2] += ptr2[m2*y1+y2]*BZ[dim1[2]*z1+s0[2]];
+						ptr1[m1*y1+y2] += wt * ptr2[m2*y1+y2];
 				}
+
+				/* spatial component of beta */
 				for(y1=0; y1<ny*nx; y1++)
-					beta[y1 + nx*ny*(nz*i1 + z1)] += betaxy[y1 + nx*ny*i1]*BZ[dim1[2]*z1+s0[2]];
+					beta[y1 + nx*ny*(nz*i1 + z1)] += wt * betaxy[y1 + nx*ny*i1];
 			}
 		}
+
 		ptr1 = alpha   + nx*ny*(m1*nz*3 + nz*3);
 		ptr2 = alphaxy + nx*ny*(m2*3 + 3);
 		for(y1=0; y1<ni4; y1++)
 		{
+			/* intensity-intensity covariances */
 			for(y2=0;y2<=y1;y2++)
 				ptr1[m1*y1 + y2] += ptr2[m2*y1 + y2];
+
+			/* intensity component of beta */
 			beta[nx*ny*nz*3 + y1] += betaxy[nx*ny*3 + y1];
 		}
 
@@ -402,7 +447,7 @@ unsigned char *dat1[], dat2[];
 	   - OK I know some bits are done more than once - but it shouldn't matter. */
 
 	m1 = 3*nx*ny*nz+ni4;
-	for(i1=0; i1<3; i1++)
+	for(i1=0; i1<3; i1++)	
 	{
 		double *ptrz, *ptry, *ptrx;
 		for(i2=0; i2<=i1; i2++)
@@ -472,7 +517,7 @@ Matrix *plhs[], *prhs[];
 	MAPTYPE **map1, *map2;
 	int i, nx,ny,nz,ni=1, samp[3];
 	int dim1[3], dim2[3], dt1[32], dt2, nsamp;
-        double *M, *BX, *BY, *BZ, *T, scale1[32], scale2, fwhm, resels;
+        double *M, *BX, *BY, *BZ, *T, scale1[32], scale2, fwhm;
 	unsigned char *dat1[32], *dat2;
 
         if (nrhs != 8 || nlhs > 3)
@@ -548,9 +593,7 @@ Matrix *plhs[], *prhs[];
 		ni,dt1,scale1,dim1,dat1,
 		nx, BX, ny, BY, nz, BZ, M, samp, &nsamp);
 
-	resels = nsamp * map1[0]->xpixdim*samp[0] * map1[0]->ypixdim*samp[1] * map1[0]->ypixdim*samp[1]
-		/ fwhm*fwhm*fwhm;
-	mxGetPr(plhs[2])[0] = mxGetPr(plhs[2])[0] / (resels - (3*nx*ny*nz + ni*4));
+	mxGetPr(plhs[2])[0] = mxGetPr(plhs[2])[0] / (nsamp - (3*nx*ny*nz + ni*4));
 	free_maps(map1);
 }
 
