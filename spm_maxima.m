81324dbf483f6b5f031d17fb0a17e30a19399af3
Karl Friston <k.friston@ucl.ac.uk>
1996-07-31 19:05:30 +0000

    revised to handle F statistics

--- spm_maxima.m
+++ spm_maxima.m
@@ -8,9 +8,11 @@
 % and its associated maxima are characterized in terms of its cluster,
 % and voxel-level p values (corrected and uncorrected).
 %
-% Upto 16 maxima are displayed that are all at least 8mm apart.  If the
-% maximum selected corresponds to one of these maxima, its location is
-% displayed in red
+% The maxima displayed are all at least 8mm apart.  If the maximum
+% selected corresponds to one of these maxima, its location is
+% displayed in red.  Selecting the voxel coordinates of other 
+% maxima causes a green pointer to appear in the appropriate place on
+% maximum intensity projection.
 %
 %_______________________________________________________________________
 % %W% %E%
@@ -62,7 +64,7 @@ subplot(2,1,2); delete(gca), spm_figure('DeletePageControls')
 subplot(2,1,2); axis off
 
 
-% display (sorted on Z values)
+% display (sorted on Z)
 %=======================================================================
 
 
@@ -75,45 +77,72 @@ line([0 1],[23 23],'LineWidth',3,'Color',[0 0 0])
 
 % Construct tables
 %=======================================================================
-text(0.18,22,'cluster-level {k,Z}','FontSize',10);
-text(0.42,22,'voxel-level {Z}'    ,'FontSize',10);
-text(0.64,22,'uncorrected'        ,'FontSize',10);
-text(0.84,22,'location {mm}'      ,'FontSize',10);
+if SPMZ
+
+	text(0.18,22,'cluster-level {k,Z}','FontSize',10);
+	text(0.42,22,'voxel-level {Z}'    ,'FontSize',10);
+	text(0.62,22,'uncorrected k & Z'  ,'FontSize',10);
+	text(0.86,22,'x,y,z {mm}'         ,'FontSize',10);
+
+elseif SPMF
+
+	text(0.18,22,'cluster-level {k,F}','FontSize',10);
+	text(0.42,22,'voxel-level {F}'    ,'FontSize',10);
+	text(0.62,22,'uncorrected k & F'  ,'FontSize',10);
+	text(0.86,22,'x,y,z {mm}'         ,'FontSize',10);
+
+
+end
 
 line([0 1],[21 21],'LineWidth',3,'Color',[0 0 0])
 
 % Pagination variables
 %-----------------------------------------------------------------------
-y = 20;
+y     = 20;
 hPage = [];
 nPage = 1;
-Vis = 'on';
+Vis   = 'on';
 
 % cluster-level p values {k}
 %-----------------------------------------------------------------------
 [u i] = max(Z);				% largest Z value
-j     = find(A == A(i));		% maxima in cluster
+
+if SPMZ
+
 Pk    = spm_P(1,W,U,N(i),S);		% cluster-level p value
+Pkn   = spm_Pkn(N(i),(u - U),W,u,S);	% cluster-level (Bivariate)
 Pu    = spm_P(1,W,u,0,S);		% voxel-level p value
-Pz    = 1 - spm_Ncdf(u);		% uncorrected p value
+Pn    = 1 - spm_kcdf(N(i),U,W);		% uncorrected p value (k)
+Pz    = 1 - spm_Ncdf(u);		% uncorrected p value (Z)
+
+elseif SPMF
+
+Pk    = NaN;				% cluster-level p value
+Pkn   = NaN;				% cluster-level (Bivariate)
+Pu    = spm_pF(S,W,df,u);		% voxel-level p value
+Pn    = NaN;				% uncorrected p value (k)
+Pz    = 1 - spm_Fcdf(u,df);		% uncorrected p value (F)
+
+end
 
 % print cluster and maximum voxel-level p values {Z}
 %-----------------------------------------------------------------------
-str   = sprintf('%-0.3f   (%i, %0.2f)',Pk,N(i),u);
-h = text(0.18,y,str,'FontSize',8,'FontWeight','Bold');
+str   = sprintf('%-0.3f   (%i, %0.2f)',Pkn,N(i),u);
+h     = text(0.18,y,str,'FontSize',8,'FontWeight','Bold');
 hPage = [hPage, h];
 str   = sprintf('%-0.3f   (%-0.2f)',Pu,u);
-h = text(0.44,y,str,'FontSize',8,'FontWeight','Bold');
+h     = text(0.44,y,str,'FontSize',8,'FontWeight','Bold');
+hPage = [hPage, h];
+str   = sprintf('%-0.3f',Pn);
+h     = text(0.64,y,str,'FontSize',8,'FontWeight','Bold');
 hPage = [hPage, h];
 str   = sprintf('%-0.3f',Pz);
-h = text(0.68,y,str,'FontSize',8,'FontWeight','Bold');
+h     = text(0.74,y,str,'FontSize',8,'FontWeight','Bold');
 hPage = [hPage, h];
- 
-h = text(0.82,y,sprintf('%-6.0f',M(:,i)),...
-	'Fontsize',10,'FontWeight','Bold',...
-	'UserData',M(:,i));
+h     = text(0.84,y,sprintf('%-6.0f',M(:,i)),...
+	'Fontsize',8,'FontWeight','Bold','UserData',M(:,i));
+hPage = [hPage, h];
 if all(~(M(:,i) - L)), set(h,'Color','r','FontAngle','Italic'); end
-hPage = [hPage, h];
 
 % Callback for voxel co-ordinates
 %-----------------------------------------------------------------------
@@ -145,7 +174,7 @@ for i = 2:length(q)
 		% Paginate if necessary
 		%-------------------------------------------------------
 		if y < 2
-			h = text(0.5,-2,['Page ',num2str(nPage)],...
+			h     = text(0.5,-2,['Page ',num2str(nPage)],...
 				'FontSize',8,'FontAngle','Italic',...
 				'Visible',Vis);
 			hPage = [hPage, h];
@@ -153,49 +182,49 @@ for i = 2:length(q)
 			hPage = [];
 			nPage = nPage + 1;
 			Vis   = 'off';
-			y = 20;
+			y     = 20;
 		end
 
 		% voxel-level p values {Z}
 		%-------------------------------------------------------
-		Pu  = spm_P(1,W,Z(q(i)),0,S);	% voxel-level p value
-		Pz  = 1 - spm_Ncdf(Z(q(i)));	% uncorrected p value
+		if SPMZ
 
-		str = sprintf('%-0.3f   (%-0.2f)',Pu,Z(q(i)));
-		h = text(0.44,y,str,'FontSize',8,'Visible',Vis);
+		Pu    = spm_P(1,W,Z(q(i)),0,S);		% voxel-level p value
+		Pz    = 1 - spm_Ncdf(Z(q(i)));		% uncorrected p value
+
+		elseif SPMF
+
+		Pu    = spm_pF(S,W,df,Z(q(i)));		% voxel-level p value
+		Pz    = 1 - spm_Fcdf(Z(q(i)),df);	% uncorrected p value
+
+		end
+
+		str   = sprintf('%-0.3f   (%-0.2f)',Pu,Z(q(i)));
+		h     = text(0.44,y,str,'FontSize',8,'Visible',Vis);
 		hPage = [hPage, h];
-		str = sprintf('%-0.3f',Pz);
-		h = text(0.68,y,str,'FontSize',8,'Visible',Vis);
+		str   = sprintf('%-0.3f',Pz);
+		h     = text(0.74,y,str,'FontSize',8,'Visible',Vis);
 		hPage = [hPage, h];
-		str = sprintf('%-6.0f',M(:,q(i)));
-		h = text(0.84,y,str,'FontSize',8,'Visible',Vis,...
+		str   = sprintf('%-6.0f',M(:,q(i)));
+		h     = text(0.84,y,str,'FontSize',8,'Visible',Vis,...
 			'UserData',M(:,q(i)));
 		set(h,'ButtonDownFcn',tmp,'Interruptible','no')
 		if all(~(M(:,q(i)) - L))
 			set(h,'Color','r','FontAngle','Italic'); end
 		hPage = [hPage, h];
-		D   = [D q(i)];
-		y   = y - 1;
+		D     = [D q(i)];
+		y     = y - 1;
 	end
 end
 
 % number and paginate last page (if pagination was required)
+%-----------------------------------------------------------------------
 if strcmp(Vis,'off')
-	%-Label last page
-	h = text(0.5,-2,['Page ',num2str(nPage)],...
+	h     = text(0.5,-2,['Page ',num2str(nPage)],...
 		'FontSize',8,'FontAngle','Italic',...
 		'Visible',Vis);
 	hPage = [hPage, h];
 	spm_figure('NewPage',hPage);
 end
-
 line([0 1],[1 1],'LineWidth',1,'Color',[0 0 0])
-
-% footnote with SPM parameters
-%-----------------------------------------------------------------------
-str = sprintf('Height threshold {u} = %0.2f, p = %0.5f',U,1 - spm_Ncdf(U));
-text(0,0,str,'FontSize',8);
-str = sprintf('Extent threshold {k} = %i voxels',k);
-text(0,-1,str,'FontSize',8);
-
 
