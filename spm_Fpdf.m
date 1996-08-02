3fd3d370bf3f0418803f15fd59c9b865d0e7f4f5
Karl Friston <k.friston@ucl.ac.uk>
1996-08-02 19:03:07 +0000

    restriction to integer df removed

--- spm_Fpdf.m
+++ spm_Fpdf.m
@@ -18,40 +18,40 @@ function pdf = spm_Fpdf(f,df)
 %-Argument range and size checks
 %---------------------------------------------------------------------------
 if nargin<2 error('insufficient arguments'), end
-
 if any(df(:)<=0) error('df must be strictly positive'), end
-if any(floor(df(:))~=ceil(df(:))) error('df must be integer'), end
 
 %-re-orient df to 2 x n size, if n x 2 with n~=2.
-if (size(df,1)~=2)&(size(df,2)==2) df=df'; end
-if size(df,1)~=2 error('df must have 2 rows or 2 columns'), end
+%---------------------------------------------------------------------------
+if (size(df,1) ~= 2) & (size(df,2) == 2) df = df'; end
+if  size(df,1) ~= 2 error('df must have 2 rows or 2 columns'), end
 
 %-check sizes of arguments
-if prod(size(f))==size(df,2)
-elseif length(f)==1
-	f=f*ones(1,size(df,2));
-elseif size(df,2)==1
-	df=meshgrid(df,1:prod(size(f)))';
+%---------------------------------------------------------------------------
+if prod(size(f)) == size(df,2)
+elseif length(f) == 1
+	f  = f*ones(1,size(df,2));
+elseif size(df,2) == 1
+	df = meshgrid(df,1:prod(size(f)))';
 else
 	error('f and df not of compatible size'), end
 end % if (size)
 
 %-Store sizes-reshape arguments to rows
 %---------------------------------------------------------------------------
-[rdim,cdim]=size(f);
-f=f(:)';
-n=length(f);
+[rdim,cdim] = size(f);
+f   = f(:)';
+n   = length(f);
 
-%-Computation
+%-Computation - avoid cases f<=0 where pdf=0
+
 %---------------------------------------------------------------------------
-%-avoid cases f<=0 where pdf=0
-pdf=zeros(n,1);
-K=find(f>0);
+pdf = zeros(n,1);
+K   = find(f>0);
 if length(K)>0;
-	df1K=df(1,K); df2K=df(2,K);
-	scK=1./beta(df2K/2,df1K/2);
-	pdf(K)=scK.*(df1K./df2K).^(df1K/2).*f(K).^(df1K/2-1).*...
+	df1K   = df(1,K); df2K=df(2,K);
+	scK    = 1./beta(df2K/2,df1K/2);
+	pdf(K) = scK.*(df1K./df2K).^(df1K/2).*f(K).^(df1K/2-1).*...
 		(1+(df1K./df2K).*f(K)).^(-(df1K+df2K)/2);
 end % f>0 cases
 
-pdf=reshape(pdf,rdim,cdim);
+pdf = reshape(pdf,rdim,cdim);
