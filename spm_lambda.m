905e4634e4b55e654cd1249163d72367d0ce59f7
Karl Friston <k.friston@ucl.ac.uk>
1996-08-13 21:57:05 +0000

    df forced to be integers for spm_t2z - table added for low df

--- spm_lambda.m
+++ spm_lambda.m
@@ -15,18 +15,23 @@ function [Lc2z,Lc2t,Lt2z] = spm_lambda(df)
 % spatial partial derivatives of Z.
 %
 % ref:  Worsley KJ et al (1992) JCBFM 12 p917
+%       Holmes AP (1994) PhD Thesis p255 G. Smoothness of t fields
 
 % pdfs and Probability integral transform (PIT)
 %-----------------------------------------------------------------------
+df    = round(df);
 dt    = 0.01;
 t     = -12:dt:12;
 pdf   = spm_Tpdf(t,df);
 T     = spm_t2z(t,df);
 
 % var(dt/dx) following transformation = var(dT(t)/dx)/var(dt/dx)
-%----------------------------------------------------------------------------
+%-----------------------------------------------------------------------
 Lc2z  = sum((t.^2 + df).^2/(df*(df - 1)).*pdf.^3./(spm_Npdf(T).^2)*dt);
 Lc2t  = sum((t.^2 + df).^2/(df*(df - 1)).*pdf*dt);
 Lt2z  = Lc2z/Lc2t;
 
-
+% for low df
+%-----------------------------------------------------------------------
+LC2T  = [Inf Inf 20.98 4.37 2.66 2.09 1.80];
+if df < length(LC2T); Lc2t = LC2T(df); end
