fb580e441270eee4b5026fc81d5ed20dd414f585
Jean-Baptiste Poline <jbpoline@gmail.com>
1996-08-26 22:38:09 +0000

    replaced spm_Pz by spm_P

--- spm_min_Pz.m
+++ spm_min_Pz.m
@@ -1,8 +1,14 @@
-function x = min_Pz(z, p,s,V)
-% A JB special
+function x = spm_min_Pz(z, p,s,S)
+% used to find the value of z which gives probability p
+% used by spm_Pkn
 % FORMAT
+% z	: hight
+% p	: probability to match
+% s 	: smoothness
+% S	: volume of the search
 %_______________________________________________________________________
-% %W% Jean-Baptiste Poline %E%
+% @(#)spm_min_Pz.m	1.1 Jean-Baptiste Poline 96/08/22
 
-x = (spm_Pz(s,z,V) - p).^2;
+x = (spm_P(1,s,z,0,S) - p).^2;
+
 
