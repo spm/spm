7f1e4d2e8b7ec22d2fe18eabf4d4faf6c53b72ae
Jean-Baptiste Poline <jbpoline@gmail.com>
1996-08-26 22:39:01 +0000

    replaced spm_Pn by spm_P

--- spm_min_Pn.m
+++ spm_min_Pn.m
@@ -1,8 +1,15 @@
-function x = min_Pn(n, p,s,u,V)
-% A JB special
+function x = min_Pn(n, p,s,u,S)
+% used to find the value of n which gives probability p
+% used by spm_Pkn
 % FORMAT
+% n	: spatial extent of the cluster
+% u	: threshold
+% p	: probability to match
+% s 	: smoothness
+% S	: volume of the search
 %_______________________________________________________________________
-% %W% Jean-Baptiste Poline %E%
+% @(#)spm_min_Pn.m	1.1 Jean-Baptiste Poline 96/08/22
 
-x = (spm_Pn(n,s,u,V) - p).^2;
+x = (spm_P(1,s,u,n,S) - p).^2;
+
 
