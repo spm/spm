0dfc723f2ee6173b7340683dda33ebb6c0c39066
Karl Friston <k.friston@ucl.ac.uk>
1996-08-28 16:55:42 +0000

    Initial revision

---
+++ spm_load.m
@@ -0,0 +1,10 @@
+function [x] = spm_load(f)
+% function to load ascii file data
+% FORMAT [x] = spm_load(f)
+% f  - file {ascii file containing a regular array of numbers
+% x  - corresponding data matrix
+%___________________________________________________________________________
+% %E% Karl Friston %W%
+
+eval(['load ' f ' -ascii; x = ' f ';']);
+
