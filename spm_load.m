d3336c85eff8fd0cb7a85551e1305826fc606be6
Andrew Holmes <andrew.holmes@veramed.co.uk>
1996-09-20 13:44:44 +0000

    Offers file selection using spm_get if called with no file arg

--- spm_load.m
+++ spm_load.m
@@ -4,7 +4,7 @@ function [x] = spm_load(f)
 % f  - file {ascii file containing a regular array of numbers
 % x  - corresponding data matrix
 %___________________________________________________________________________
-% %E% Karl Friston %W%
+% %E% Karl Friston, Andrew Holmes %W%
 
+if nargin==0, f=spm_get(1,'*','Select ASCII data file'); end
 eval(['load ' f ' -ascii; x = ' spm_str_manip(spm_str_manip(f,'t'),'r') ';']);
-
