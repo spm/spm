bc1e0fef84023efe3752981fd03c5ceb3222ae19
Andrew Holmes <andrew.holmes@veramed.co.uk>
1996-09-23 20:33:32 +0000

    Amended high value approximation to kick in for both tails at *standardised* variate of 6.

--- spm_Ncdf.m
+++ spm_Ncdf.m
@@ -1,8 +1,8 @@
-function p = spm_Ncdf(z,Mu,SigmaSq)
+function p = spm_Ncdf(x,Mu,SigmaSq)
 % Cumulative Distribution Function (CDF) for univariate normal distributions
-% FORMAT p = spm_Ncdf(z,Mu,Var)
+% FORMAT p = spm_Ncdf(x,Mu,Var)
 %
-% z       - N(Mu,SigmaSq) variates
+% x       - N(Mu,SigmaSq) variates
 % Mu      - Mean of the univariate Normal distribution
 % SigmaSq - Variance of the univariate Normal distribution
 % p       - lower tail probability
@@ -19,11 +19,16 @@ function p = spm_Ncdf(z,Mu,SigmaSq)
 % related to the error function, and MatLab's implementation of the error
 % function is used for computation. See "Numerical Recipies in C"
 %
+% For extreme variates with abs(z)>6 where z=(x-Mu)/sqrt(SigmaSq), the
+% approximation \phi(z) \approx exp(-z^2/2)/(z*sqrt(2*pi)) is used
+%
 %---------------------------------------------------------------------------
 % %W% Andrew Holmes %E%
 
-% - Andrew Holmes - V1 - 09/92 - ah_phi, for standard Normal variates
-%                   V2 - 02/95 - Rewritten for SPM, including Mu and SigmaSq
+%-Parameters - (Absolute) threshold above which the high value
+%		to \phi approximation is used
+%-----------------------------------------------------------------------
+z1 = 6;
 
 %-Condition arguments
 %---------------------------------------------------------------------------
@@ -34,14 +39,26 @@ if nargin < 1, z       = []; end
 if SigmaSq <=0 , error('SigmaSq must be strictly positive'), end
 
 %-Computation
-%---------------------------------------------------------------------------
-p  = zeros(size(z));
-h  = find(z > 6);
-l  = find(z <= 6);
-u  = (z - Mu)/sqrt(SigmaSq);
-if length(h)
-	p(h) = 1 - exp(-(u(h).^2)/2)./(u(h)*sqrt(2*pi));
-end
+%=======================================================================
+p  = zeros(size(x));
+
+%-Center and scale to standard Gaussian variates
+%-----------------------------------------------------------------------
+z  = (x - Mu)/sqrt(SigmaSq);
+
+%-Find really extreme variates
+%-----------------------------------------------------------------------
+h  = find(abs(z) >  z1);
+l  = find(abs(z) <= z1);
+
+%-Standard Ncdf
+%-----------------------------------------------------------------------
 if length(l)
-	p(l) = 0.5 + 0.5*erf(u(l)/sqrt(2));
+	p(l) = 0.5 + 0.5*erf(z(l)/sqrt(2));
+end
+
+%-High value approximation to Ncdf
+%-----------------------------------------------------------------------
+if length(h)
+	p(h) = (z(h)>0) - exp(-(z(h).^2)/2)./(z(h)*sqrt(2*pi));
 end
