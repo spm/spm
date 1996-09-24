c946fb8dfcecbcbc5ea837cf545241fbc02cb966
Andrew Holmes <andrew.holmes@veramed.co.uk>
1996-09-24 15:39:15 +0000

    Mods to write in current directory. Commented.

--- spm_mean_ui.m
+++ spm_mean_ui.m
@@ -1,24 +1,28 @@
 function spm_average
 % promts for a series of images and averages them
 % FORMAT spm_average
-%____________________________________________________________________________
+%_______________________________________________________________________
 %
 % spm_average simply averages a set of images to produce an mean image that
-% is written to "average.img" (in the same subdirectory)
+% is written to "average.img" (in the current directory)
 %
 % It is assumed that the image and voxels sizes and data format are the same. 
 %
-%__________________________________________________________________________
-% %W% %E%
+%_______________________________________________________________________
+% %W% John Ashburner %E%
 
+%-Select images
 %----------------------------------------------------------------------------
-P    = spm_get(Inf,'.img','Select images to be averaged');
-Q    = P(1,:);
-Q    = [Q([1:max(find(Q == '/'))]) 'average.img'];
+P = spm_get(Inf,'.img','Select images to be averaged');
+%Q = P(1,:); Q = [Q([1:max(find(Q == '/'))]) 'average.img'];
+Q = 'average.img';
 
+%-Get scalefactors and compute weights
+%-----------------------------------------------------------------------
 scales = zeros(size(P,1),1);
 for i=1:size(P,1)
-	[DIM VOX SCALE TYPE OFFSET ORIGIN DESCRIP] = spm_hread(deblank(P(i,:)));
+	[DIM VOX SCALE TYPE OFFSET ORIGIN DESCRIP] = ...
+					spm_hread(deblank(P(i,:)));
 	scales(i) = SCALE;
 	if (i==1)
 		DIM1 = DIM;
@@ -29,6 +33,8 @@ for i=1:size(P,1)
 	end
 end
 
+%-Compute mean and write headers etc.
+%-----------------------------------------------------------------------
 scale = spm_mean(prod(DIM),TYPE,Q,P,scales/length(scales));
 spm_hwrite(Q,DIM,VOX,scale,TYPE,OFFSET,ORIGIN,DESCRIP);
 spm_get_space(Q,spm_get_space(P(1,:)));
