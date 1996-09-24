7f3c3a09db06f06ac105f4f77f1309eb8be7f349
Andrew Holmes <andrew.holmes@veramed.co.uk>
1996-09-24 15:09:23 +0000

    Only computes group variances if requested. Returns row vector arguments as row vectors.

--- spm_meanby.m
+++ spm_meanby.m
@@ -2,7 +2,7 @@ function [M,Mi,V,Vi,iVals]=spm_meanby(X,i)
 % Mean (& variances) of data in columns by group
 % FORMAT [M,Mi,V,Vi,iVals]=spm_meanby(X,i)
 % X	- Data matrix, with data in columns (row vector X is transposed)
-% i	- Column of indicatir vectors, indicating group membership of
+% i	- Column of indicator vectors, indicating group membership of
 %	  observations in rows of X
 % Mi	- Mean for observations in each group, for each column
 % Vi    - (Sample) variance for observations in each group
@@ -18,12 +18,17 @@ function [M,Mi,V,Vi,iVals]=spm_meanby(X,i)
 % %E% Andrew Holmes %W%
 
 if nargin<2, i=ones(size(X,1),1); end
+M = zeros(size(X));
+V = zeros(size(X));
 if size(X,1)==1, X=X'; end
 if min(size(i))~=1, error('vector i'), end
 i=i(:);
 
 if ~all(floor(i)==ceil(i)), error('Non-integer indicator vector'), end
 if size(X,1)~=length(i), error('X must have row dimension the length of i'), end
+
+%-Computation
+%=======================================================================
 
 %-Sort out unique indicator levels
 tmp    = sort(i);
@@ -41,16 +46,22 @@ Ni = sum(I)'*ones(1,size(X,2));
 %-Work out sums by index
 Si = I'*X;
 
-%-Work out sum of squares by index
-SSi = I'*X.^2;
-
 %-Work out means by index
 Mi = Si ./ Ni;
+
+%-Combine into matrix same size as X:
+% (Effectively replacing each observation by its group mean)
+M(:) = Mi(i,:);
+
+%-Compute group variances if requested
+%-----------------------------------------------------------------------
+if nargout<3, return, end
+
+%-Work out sum of squares by index
+SSi = I'*X.^2;
 
 %-Work out variances by index
 Vi = ( SSi - Si.^2./Ni ) ./ (Ni -1);
 
 %-Combine into matrix same size as X:
-% Effectively replacing each observation by its group mean
-M = Mi(i,:);
-V = Vi(i,:);
+V(:) = Vi(i,:);
