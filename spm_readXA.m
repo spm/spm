98d0b00dd2a9ca6532f5abc9fef857b68b57c0b8
John Ashburner <j.ashburner@ucl.ac.uk>
1997-02-07 11:57:35 +0000

    can now read BETA.mat files

--- spm_readXA.m
+++ spm_readXA.m
@@ -5,6 +5,10 @@ function xa = spm_readXA(element, filename)
 % element  - columns to read.
 % filename - name of the XA.mat file
 % xa       - the data read from XA.mat
+%
+% Note: that if the file is not an XA.mat file - then columns from the
+% first variable stored in the file `filename' will be read.
+% The data must be stored as real, full, etc...
 %_______________________________________________________________________
 % %W% John Ashburner %E%
 
@@ -34,19 +38,16 @@ if any(element>dim(2) | element<1)
 	error('Elements out of range');
 end
 
-%-Check that it is (probably) in the right format
+%-Need to skip over the variable name
 %-----------------------------------------------------------------------
-fseek(fp,20,'bof');
-nam = fread(fp,3,'char');
-if ~all(nam == [88 65 0]')
-	error('This is not a proper XA.mat');
-end
+fseek(fp,16,'bof');
+nlen=fread(fp,1,'int32');
 
 %-Read the data
 %-----------------------------------------------------------------------
 xa = zeros(dim(1),length(element));
 for i=1:length(element)
-	fseek(fp,23+(element(i)-1)*8*dim(1),'bof');
+	fseek(fp,20+nlen+(element(i)-1)*8*dim(1),'bof');
 	xa(:,i) = fread(fp,dim(1),'double');
 end
 
