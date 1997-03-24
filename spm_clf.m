abf2fdd3bb94d1d244bb25def3a9c4023db8c0d9
Andrew Holmes <andrew.holmes@veramed.co.uk>
1997-03-24 13:57:18 +0000

    Updated comments for spm_figurev1.20 - matlab5

--- spm_clf.m
+++ spm_clf.m
@@ -1,21 +1,20 @@
 function spm_clf(F)
-% Selective deletion of objects in the current figure window
+% Clear specified figure of objects with visible handles
 % FORMAT spm_clf(F)
 % F	- Figure number, or 'Tag' string of figure(s) to clear
 %___________________________________________________________________________
 %
-% Deletes the children of the specified figure, other than objects with
-% 'Tag' attribute = 'NoDelete'. This serves as a partial clf,
-% preventing some objects (e.g. menu bar objects) from being deleted.
+% Clears the specified figure, deleting all objects with visible handles.
+% ('HandleVisibility'=='on').
 %
 % If the current window is 'Tag'ged interactive, then the figures name
 % is cleared and the pointer reset.
 %
 % F Defaults to the current figure, if there is one.
 %
-% This is just a gateway to spm_figure('Clear',F)
+% This is just a gateway to spm_figure('Clear',F).
 %__________________________________________________________________________
-% %W% %E%
+% %W% Andrew Holmes %E%
 
 %-Call spm_figure
 %---------------------------------------------------------------------------
