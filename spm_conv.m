3f3f7c163979400590379f26aed5528cad456099
Andrew Holmes <andrew.holmes@veramed.co.uk>
1997-04-08 12:45:45 +0000

    (Originally updated by Karl, 97/01/02, but he forgot to include SCCS keyowrds! He didn't make any SCCS comments either!

--- spm_conv.m
+++ spm_conv.m
@@ -1,6 +1,6 @@
-function [O] = spm_conv(X,sx,sy)
+function [X] = spm_conv(X,sx,sy)
 % Gaussian convolution
-% FORMAT [O] = spm_conv(X,sx,[sy]);
+% FORMAT [X] = spm_conv(X,sx[,sy]);
 % X    - matrix
 % sx   - kernel width (FWHM) in pixels
 % sy   - optional non-isomorphic smoothing
@@ -13,23 +13,45 @@ function [O] = spm_conv(X,sx,sy)
 % restricted to non near-zero values
 %
 %__________________________________________________________________________
-% %W% %E%
+% %W% Karl Friston %E%
 
 % assume isomorphic smoothing
 %---------------------------------------------------------------------------
 if nargin < 3; sy = sx; end
+[lx ly] = size(X);
+
 
 % FWHM -> sigma
 %---------------------------------------------------------------------------
-sx    = sx/sqrt(8*log(2));
-sy    = sy/sqrt(8*log(2));
+sx    = sx/sqrt(8*log(2)) + eps;
+sy    = sy/sqrt(8*log(2)) + eps;
 
-% create and mulitply by convolution matrices
+% kernels
 %---------------------------------------------------------------------------
-[lx ly] = size(X);
-K       = spm_sptop(sx,lx);
-O       = K*X;
-if (ly ~= lx) | (sy ~= sx)
-	K  = spm_sptop(sy,ly);
+Ex    = min([ceil(3*sx) lx]);
+x     = [-Ex:Ex];
+kx    = exp(-x.^2/(2*sx^2));
+kx    = kx/sum(kx);
+Ey    = min([ceil(3*sy) ly]);
+y     = [-Ey:Ey];
+ky    = exp(-y.^2/(2*sy^2));
+ky    = ky/sum(ky);
+
+% convolve
+%---------------------------------------------------------------------------
+if lx > 1;
+	for i = 1:ly
+		u      = X(:,i);
+		u      = [flipud(u(1:Ex)); u; flipud(u([1:Ex] + lx - Ex))];
+		U      = conv(u,kx);
+		X(:,i) = U([1:lx] + 2*Ex);
+	end
 end
-O       = full(K*O')';
+if ly > 1;
+	for i = 1:lx
+		u      = X(i,:);
+		u      = [fliplr(u(1:Ey)) u fliplr(u([1:Ey] + ly - Ey))];
+		U      = conv(u,ky);
+		X(i,:) = U([1:ly] + 2*Ey);
+	end
+end
