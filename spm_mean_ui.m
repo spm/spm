df7489ee20af7b068f618e7725e10ee46fbb8416
John Ashburner <j.ashburner@ucl.ac.uk>
1997-08-08 15:42:57 +0000

    Uses a new spm_mean (done with memory mapping)

--- spm_mean_ui.m
+++ spm_mean_ui.m
@@ -17,25 +17,20 @@ P = spm_get(Inf,'.img','Select images to be averaged');
 %Q = P(1,:); Q = [Q([1:max(find(Q == '/'))]) 'average.img'];
 Q = 'average.img';
 
-%-Get scalefactors and compute weights
-%-----------------------------------------------------------------------
-scales = zeros(size(P,1),1);
+drawnow;
+
+[DIM VOX SCALE TYPE OFFSET ORIGIN DESCRIP] = spm_hread(deblank(P(1,:)));
+V=zeros(12,size(P,1));
 for i=1:size(P,1)
-	[DIM VOX SCALE TYPE OFFSET ORIGIN DESCRIP] = ...
-					spm_hread(deblank(P(i,:)));
-	scales(i) = SCALE;
-	if (i==1)
-		DIM1 = DIM;
-		TYPE1 = TYPE;
-	end
-	if DIM ~= DIM1 | TYPE ~= TYPE1 | OFFSET ~= 0
-		error(['spm_mean can not cope with image "' deblank(P(i,:)) '".']);
-	end
+	V(:,i)=spm_map(P(i,:));
+	V(7,i)=V(7,i);
 end
 
 %-Compute mean and write headers etc.
 %-----------------------------------------------------------------------
-scale = spm_mean(prod(DIM),TYPE,Q,P,scales/length(scales));
-spm_hwrite(Q,DIM,VOX,scale,TYPE,OFFSET,ORIGIN,DESCRIP);
+scale = spm_mean(V,Q)/size(P,1);
+
+spm_hwrite(Q,DIM,VOX,scale,4,0,ORIGIN,'Mean image');
 spm_get_space(Q,spm_get_space(P(1,:)));
 
+for v=V, spm_unmap(v); end;
