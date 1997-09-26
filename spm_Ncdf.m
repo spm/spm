3057d6f8f464d3272b30548fedfc39921d02225b
Andrew Holmes <andrew.holmes@veramed.co.uk>
1997-09-26 17:05:14 +0000

    Upgraded for ML5: n-dim matrices, better comments.

--- spm_Ncdf.m
+++ spm_Ncdf.m
@@ -1,64 +1,86 @@
-function p = spm_Ncdf(x,Mu,SigmaSq)
-% Cumulative Distribution Function (CDF) for univariate normal distributions
-% FORMAT p = spm_Ncdf(x,Mu,Var)
+function F = spm_Ncdf(x,u,v)
+% Cumulative Distribution Function (CDF) for univariate Normal distributions
+% FORMAT F = spm_Ncdf(x,u,v)
 %
-% x       - N(Mu,SigmaSq) variates
-% Mu      - Mean of the univariate Normal distribution
-% SigmaSq - Variance of the univariate Normal distribution
-% p       - lower tail probability
+% x - ordinates
+% u - mean		[Defaults to 0]
+% v - variance	(v>0)	[Defaults to 1]
+% F - pdf of N(u,v) at x (Lower tail probability)
 %__________________________________________________________________________
 %
 % spm_Ncdf implements the Cumulative Distribution Function (CDF) for
 % the Normal (Gaussian) family of distributions.
 %
-% Returns the probability p, that a variate from a Normal distribution with
-% mean Mu and variance SigmaSq is less than z.
-% p = Pr(Z <= z) for Z ~ N(Mu,SigmaSq).
+% Definition:
+%-----------------------------------------------------------------------
+% The CDF F(x) of a Normal distribution with mean u and variance v is
+% the probability that a random realisation X from this distribution
+% will be less than x. F(x)=Pr(X<=x) for X~N(u,v). See Evans et al.,
+% Ch29 for further definitions and variate relationships.
 %
-% The CDF for a standard N(0,1) Normal distribution (known as \Phi), is
-% related to the error function, and MatLab's implementation of the error
-% function is used for computation. See "Numerical Recipies in C"
+% If X~N(u,v), then Z=(Z-u)/sqrt(v) has a standard normal distribution,
+% Z~N(0,1). The CDF of the standard normal distribution is known as \Phi(z).
 %
-% For extreme variates with abs(z)>6 where z=(x-Mu)/sqrt(SigmaSq), the
-% approximation \phi(z) \approx exp(-z^2/2)/(z*sqrt(2*pi)) is used
+% (KWorsley) For extreme variates with abs(z)>6 where z=(x-u)/sqrt(v), the
+% approximation \Phi(z) \approx exp(-z^2/2)/(z*sqrt(2*pi)) may be useful.
 %
-%---------------------------------------------------------------------------
+% Algorithm:
+%-----------------------------------------------------------------------
+% The CDF for a standard N(0,1) Normal distribution, \Phi(z), is
+% related to the error function by: (Abramowitz & Stegun, 26.2.29)
+%
+%	\Phi(z) = 0.5 + erf(z/sqrt(2))/2
+%
+% MatLab's implementation of the error function is used for computation.
+%
+% References:
+%-----------------------------------------------------------------------
+% Evans M, Hastings N, Peacock B (1993)
+%	"Statistical Distributions"
+%	 2nd Ed. Wiley, New York
+%
+% Abramowitz M, Stegun IA, (1964)
+%	"Handbook of Mathematical Functions"
+%	 US Government Printing Office
+%
+% Press WH, Teukolsky SA, Vetterling AT, Flannery BP (1992)
+%	"Numerical Recipes in C"
+%	 Cambridge
+%
+%__________________________________________________________________________
 % %W% Andrew Holmes %E%
 
-%-Parameters - (Absolute) threshold above which the high value
-%		to \phi approximation is used
+%-Format arguments, note & check sizes
 %-----------------------------------------------------------------------
-z1 = 6;
-
-%-Condition arguments
-%---------------------------------------------------------------------------
-if nargin < 3, SigmaSq = 1;  end
-if nargin < 2, Mu      = 0;  end
-if nargin < 1, z       = []; end
-
-if SigmaSq <=0 , error('SigmaSq must be strictly positive'), end
+if nargin<3, v=1; end
+if nargin<2, u=0; end
+if nargin<1, F=[]; return, end
+ad = [ndims(x);ndims(u);ndims(v)];
+rd = max(ad);
+as = [	[size(x),ones(1,rd-ad(1))];...
+	[size(u),ones(1,rd-ad(2))];...
+	[size(v),ones(1,rd-ad(3))]     ];
+rs = max(as);
+xa = prod(as,2)>1;
+if sum(xa)>1 & any(any(diff(as(xa,:)),1))
+	error('non-scalar args must match in size'), end
 
 %-Computation
-%=======================================================================
-p  = zeros(size(x));
-
-%-Center and scale to standard Gaussian variates
 %-----------------------------------------------------------------------
-z  = (x - Mu)/sqrt(SigmaSq);
+%-Initialise result to zeros
+F = zeros(rs);
 
-%-Find really extreme variates
-%-----------------------------------------------------------------------
-h  = find(abs(z) >  z1);
-l  = find(abs(z) <= z1);
+%-Only defined for strictly positive variance v. Return NaN if undefined.
+md = ( ones(size(x))  &  ones(size(u))  &  v>0 );
+if any(~md(:)), F(~md) = NaN;
+	warning('Returning NaN for out of range arguments'), end
 
-%-Standard Ncdf
-%-----------------------------------------------------------------------
-if length(l)
-	p(l) = 0.5 + 0.5*erf(z(l)/sqrt(2));
-end
+%-Non-zero where defined
+Q  = find( md );
+if isempty(Q), return, end
+if xa(1), Qx=Q; else Qx=1; end
+if xa(2), Qu=Q; else Qu=1; end
+if xa(3), Qv=Q; else Qv=1; end
 
-%-High value approximation to Ncdf
-%-----------------------------------------------------------------------
-if length(h)
-	p(h) = (z(h)>0) - exp(-(z(h).^2)/2)./(z(h)*sqrt(2*pi));
-end
+%-Compute
+F(Q) = 0.5 + 0.5*erf((x(Qx)-u(Qu))./sqrt(2*v(Qv)));
--- spm_invNcdf.m
+++ spm_invNcdf.m
@@ -1,38 +1,99 @@
-function z = spm_invNcdf(p,Mu,SigmaSq)
-% Inverse CDF for univariate normal distributions
-% FORMAT z = spm_invNcdf(p,Mu,SigmaSq)
+function x = spm_invNcdf(F,u,v)
+% Inverse Cumulative Distribution Function (CDF) for univariate Normal
+% FORMAT x = spm_invNcdf(F,u,v)
 %
-% p       - lower tail probability
-% Mu      - Mean of the univariate Normal distribution [defaults to 0]
-% SigmaSq - Variance of the univariate Normal distribution [defaults to 1]
-% z       - N(Mu,SigmaSq) variates
+% F - CDF (lower tail p-value)
+% u - mean		[Defaults to 0]
+% v - variance	(v>0)	[Defaults to 1]
+% x - ordinates of N(u,v) at which CDF F(x)=F
 %_______________________________________________________________________
 %
-% ah_invNcdf implements the inverse of the Cumulative Distribution
+% spm_invNcdf implements the inverse of the Cumulative Distribution
 % Function (CDF) for the Normal (Gaussian) family of distributions.
 %
-% Returns the variate z, such that Pr(Z <= z) = p for Z ~ N(Mu,SigmaSq), a
-% univariate random variable distributed Normally with mean Mu and
-% variance SigmaSq.
+% Returns the variate x, such that Pr{X<x} = F for X~N(u,v), a
+% univariate random variable distributed Normally with mean u and
+% variance v.
 %
-% The inverse CDF for a standard N(0,1) Normal distribution (known as
-% \Phi^{-1}), is related to the inverse error function, and MatLab's
-% implementation of the inverse error function is used for computation.
-% See "Numerical Recipies in C"
+% Definition:
+%-----------------------------------------------------------------------
+% The CDF F(x) of a Normal distribution with mean u and variance v is
+% the probability that a random realisation X from this distribution
+% will be less than x. F(x)=Pr(X<=x) for X~N(u,v). The inverse CDF
+% returns the normal ordinate x for which the CDF is F. See Evans et
+% al., Ch29 for further definitions and variate relationships.
+%
+% If X~N(u,v), then Z=(Z-u)/sqrt(v) has a standard normal distribution,
+% Z~N(0,1). The CDF of the standard normal distribution is known as \Phi(z),
+% its inverse as \Phi^{-1}(F).
+%
+% Algorithm:
+%-----------------------------------------------------------------------
+% The CDF for a standard N(0,1) Normal distribution, \Phi(z), is
+% related to the error function by: (Abramowitz & Stegun, 26.2.29)
+%
+%	\Phi(z)      = 0.5 + erf(z/sqrt(2))/2
+% so
+%
+%	\Phi^{-1}(p) = sqrt(2) * erfinv(2*p-1)
+%
+% where erfinv(.) is the inverse error function.
+%
+% MatLab's implementation of the inverse error function is used for
+% computation of z=\Phi^{-1}(F), the corresponding standard normal
+% variate, which converted to a variate x from a N(u,v) distribution by:
+% 	x = u+z*sqrt(v)
+%
+% References:
+%-----------------------------------------------------------------------
+% Evans M, Hastings N, Peacock B (1993)
+%	"Statistical Distributions"
+%	 2nd Ed. Wiley, New York
+%
+% Abramowitz M, Stegun IA, (1964)
+%	"Handbook of Mathematical Functions"
+%	 US Government Printing Office
+%
+% Press WH, Teukolsky SA, Vetterling AT, Flannery BP (1992)
+%	"Numerical Recipes in C"
+%	 Cambridge
 %
 %_______________________________________________________________________
 % %W% Andrew Holmes %E%
 
 
-%-Condition arguments
+%-Format arguments, note & check sizes
 %-----------------------------------------------------------------------
-if nargin<3, SigmaSq=1; end
-if nargin<2, Mu=0; end
-if nargin<1, p=[]; end
-
-if any( abs(p(:)-0.5) > 0.5), error('p must be in [0,1]!'), end
+if nargin<3, v=1; end
+if nargin<2, u=0; end
+if nargin<1, x=[]; return, end
+ad = [ndims(F);ndims(u);ndims(v)];
+rd = max(ad);
+as = [	[size(F),ones(1,rd-ad(1))];...
+	[size(u),ones(1,rd-ad(2))];...
+	[size(v),ones(1,rd-ad(3))]     ];
+rs = max(as);
+xa = prod(as,2)>1;
+if sum(xa)>1 & any(any(diff(as(xa,:)),1))
+	error('non-scalar args must match in size'), end
 
 %-Computation
-%=======================================================================
-if isempty(p), z=[]; return, end
-z = ( sqrt(2)*erfinv(2*p-1) .* SigmaSq ) + Mu;
+%-----------------------------------------------------------------------
+%-Initialise result to zeros
+x = zeros(rs);
+
+%-Only defined for F in [0,1], & strictly positive variance v.
+% Return NaN if undefined.
+md = ( F>=0  &  F<=1  &  ones(size(u))  &  v>0 );
+if any(~md(:)), x(~md) = NaN;
+	warning('Returning NaN for out of range arguments'), end
+
+%-Compute where defined
+Q  = find( md );
+if isempty(Q), return, end
+if xa(1), QF=Q; else QF=1; end
+if xa(2), Qu=Q; else Qu=1; end
+if xa(3), Qv=Q; else Qv=1; end
+
+%-Compute
+x(Q) = ( sqrt(2)*erfinv(2*F(QF)-1) .* v(Qv) ) + u(Qu);
