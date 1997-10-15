5cbf9ae5b9511cd16e214629de8287376608f8da
John Ashburner <j.ashburner@ucl.ac.uk>
1997-10-15 16:34:52 +0000

    Bug fixes

--- spm_unmap_vol.c
+++ spm_unmap_vol.c
@@ -8,24 +8,22 @@ static char sccsid[]="%W% John Ashburner %E%";
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-	mxArray *matrix_ptr;
 	MAP *current_map;
 	int j, n;
 
 	if (nrhs != 1 || nlhs > 0)
 		mexErrMsgTxt("Inappropriate usage.");
 
-	matrix_ptr = (mxArray *)prhs[0];
-	if ((!mxIsNumeric(matrix_ptr) || mxIsComplex(matrix_ptr) ||
-		mxIsSparse(matrix_ptr) || !mxIsDouble(matrix_ptr) ||
-		mxGetM(matrix_ptr) != (sizeof(MAP)+sizeof(double)-1)/sizeof(double)))
+	if (!mxIsNumeric(prhs[0]) || mxIsComplex(prhs[0]) ||
+		mxIsSparse(prhs[0]) || !mxIsDouble(prhs[0]) ||
+		mxGetM(prhs[0]) != (sizeof(MAP)+sizeof(double)-1)/sizeof(double))
+		mexErrMsgTxt("Wrong sized matrix/vector.");
 
-	n = mxGetN(matrix_ptr);
+	n = mxGetN(prhs[0]);
 
 	for (j=0; j<n; j++)
 	{
-		current_map = (MAP *)(mxGetPr(matrix_ptr)
-			+ j*((sizeof(MAP)+sizeof(double)-1)/sizeof(double)));
+		current_map = (MAP *)mxGetPr(prhs[0]) +j;
 		if (current_map->magic != MAGIC)
 			mexErrMsgTxt("Bad magic number in image handle.");
 		if (current_map->pid != getpid())
