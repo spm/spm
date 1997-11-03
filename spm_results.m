cf4ee24e7717d222ad01ad63b13502387a022929
Andrew Holmes <andrew.holmes@veramed.co.uk>
1997-11-03 17:59:22 +0000

    Rewrote GUI using callbacks in spm_results_ui.m (v1.1), thereby using am XYZ registry (see spm_XYZreg.m) to synch locations across various controls. Preperations for integration of Chloe's MultiPlanar imaging toolbox, and other

--- spm_results.m
+++ spm_results.m
@@ -3,38 +3,105 @@
 % FORMAT spm_results
 %_______________________________________________________________________
 %
-% spm_results prompts for details of a SPM{Z} or SPM{F} that is then
-% displayed in the results window.  By moving (dragging) the pointer on
-% the maximum intensity projection one can select a particular
-% voxel (or planes passing though that voxel) for further display and
-% analysis:
+% The SPM results section is for the interactive exploration and
+% characterisation of the results of a statistical analysis.
+% 
+% The user is prompted to select a SPM{Z} or SPM{F}, which is
+% thresholded at user specified levels. The specification of the
+% contrasts to use and the height and size thresholds are the same as
+% that described in spm_projections_ui.m (for SPM{Z}) &
+% spm_projectionsF_ui.m (fpr SPM{F}). The resulting SPM is then
+% displayed in the graphics window as a maximum intensity projection,
+% alongside the design matrix.
 %
-% 1) plot the activities at the point selected
-% 2) render the SPM{Z/F} on transverse slices at the level selected
-% 3) render the SPM{Z/F} through 3 orthogonal sections selected
-% 4) list and characterize all the maxima in a selected region
-% 5) write out the filtered image as an Analyze volume
+% The cursors in the MIP can be moved (dragged) to select a particular
+% voxel. The three mouse buttons give different drag and drop behavoir:
+% Button 1 - point & drop; Button 2 - "dynamic" drag & drop with
+% co-ordinate & SPM value updating; Button 3 - "magnetic" drag & drop,
+% where the cursor jumps to the nearest suprathreshold voxel in the
+% MIP, and shows the value there. See spm_mip_ui.m, the MIP GUI handling
+% function.
 %
-% The SPM{Z/F} is thresholded at a specified threshold and displayed
-% on top of a background image.  This background image can be arbitrary
-% but should have ORIGIN correctly specified in its header.  ORIGIN
-% is the [i j k] voxel corresponding to [0 0 0] mm in XYZ [typically
-% the anterior commissure in the space defined by the atlas of Talairach
-% and Tournoux (1988)].
+% The current voxel specifies the voxel, suprathreshold cluster, or
+% orthogonal planes (planes passing through that voxel) for subsequent
+% localised utilities. Only voxels saved by the statistical analysis
+% which survives the specified thresholds are available for analysis.
 %
-% The specification of the contrasts to use and the height and size
-% thresholds are the same as that described in spm_projections_ui.
+% A control panel in the interactive window enables interactive
+% exploration of the results, divided into volume, cluster, and voxel
+% level utilities:
+%
+% Volume level:
+%	  (i) Multiplanar - Launches a MultiPlanar lightbox viewing window
+%                                                       - see spm_????.m
+%	 (ii)   
+%	(iii) Write - write out thresholded statistic image
+%	 (iv) 
+%	  (v) 
+% Cluster level:
+%         (i) Maxima - tables all local maxima in the suprathreshold region
+%             containing the cursor, going over onto multiple pages if necessary.
+%             The table lists all local maxima, their locations, statistic
+%             values and p-values. (Note that the cursor is repositioned to the
+%             nearest local maxima by this option.)
+%                                                       - see spm_maxima.m
+%	 (ii)   
+%	(iii) 
+%	 (iv) 
+%	  (v) 
+% Voxel level:
+%         (i) Plot - Graphs of adjusted and fitted activity against various
+%             ordinates. (Note that the cursor is repositioned to the nearest
+%             voxel with data by this option.) Additionally, writes out adjusted
+%             data to the MatLab command window.
+%                                                       - see spm_graph.m
+%
+%	 (ii) Slices - slices of the thresholded statistic image overlaid
+%             on a secondary image chosen by the user. Three transverse
+%             slices are shown, being those at the level of the cursor
+%             in the z-axis and the two adjacent to it.
+%                                                       - see spm_transverse.m
+%
+%	(iii) Sections - orthogonal sections of the thresholded statistic image
+%             overlaid on a secondary image chosen by the user. The sections
+%             are through the cursor position.
+%                                                       - see spm_sections.m
+%	 (iv) 
+%	  (v) 
+%
+% Graphics appear in the bottom half of the graphics window, additional
+% controls and questions in the interactive window.
+%
+%                           ----------------
+%
+% The MIP uses a template outline in Talairach space. Consequently for
+% the results section to display properly the input images to the
+% statistics section should either be in Talairach space (with the
+% ORIGIN correctly specified), or the ORIGIN header fields should be
+% set to the voxel coordinates of the anterior commissure in the input
+% images. See spm_format.man ("Data Format" in the help facility) for
+% further details of the Analyze image format used by SPM.
+%
+% Similarly, secondary images should be aligned with the input images
+% used for the statistical analysis. In particular the ORIGIN must
+% correspond to (0,0,0) in XYZ, the vector of locations.
+%
+%-----------------------------------------------------------------------
+%
+% spm_results is an M-file, a script rather than a function, so that
+% all the results variables (location, p-values, cluster definitions
+% etc.) are available to the (advanced) user in the MatLab workspace.
+% Many of the spm_*.m routines invoked are also M-files.
+%
 %_______________________________________________________________________
 % %W% Karl Friston, Andrew Holmes %E%
 
 %-Programers notes
 %-----------------------------------------------------------------------
-% spm_results is not a function so that data and results can be
-% held in the base workspace. Thus the spm_*.m routines invoked are
-% also M-files. This scenario enables the user easy access to the data
-% structures.
+% The GUI is handled by spm_results_ui, which uses an XYZ registry handled
+% by spm_XYZreg to synchronise the various GUI location controls.
 %
-% The interactive MIP is handled by spm_mip_ui
+% The interactive MIP is handled by spm_mip_ui.
 %_______________________________________________________________________
 
 % Initialise 
@@ -42,7 +109,9 @@
 clear
 WS     = spm('GetWinScale');
 Fgraph = spm_figure('FindWin','Graphics');
+if isempty(Fgraph), Fgraph=spm_figure('Create','Graphics','Graphics'); end
 Finter = spm_figure('FindWin','Interactive');
+if isempty(Finter), Finter=spm('CreateIntWin'); end
 spm_clf(Fgraph)
 spm_clf(Finter)
 set(Finter,'Name','SPM results')
@@ -56,16 +125,12 @@ SPMF     = ~SPMZ;
 % Get thresholded data, thresholds and parameters
 %-----------------------------------------------------------------------
 if SPMZ
-
 	[t,XYZ,QQ,U,k,s,w] = spm_projections_ui('Results');
-
 elseif SPMF
-
 	[t,XYZ,QQ,U,k,s,w] = spm_projectionsF_ui('Results');
-
 end
 
-% proceed if there are voxels
+%-Proceed only if there are voxels
 %-----------------------------------------------------------------------
 if ~length(t); return; end
 set(Finter,'Name','SPM results','Pointer','Watch')
@@ -76,70 +141,74 @@ set(Finter,'Name','SPM results','Pointer','Watch')
 load([CWD,'/SPM'])
 S        = s;
 W        = w;
+M        = [ [diag(V(4:6)), -(V(7:9).*V(4:6))]; [zeros(1,3) ,1]];
+DIM      = V(1:3);
 
 if SPMF, df = Fdf; end
 
 %-Load ER.mat (event-related) file if it exists
 %-----------------------------------------------------------------------
 str      = [CWD,'/ER.mat'];
-if exist(str); load(str); end
+if exist(str,'file'); load(str); end
 
 
-% Make description strings
+
+%-Setup Resuts User Interface; Display MIP, design matrix & parameters
+%=======================================================================
+%-Setup results GUI
+hReg = spm_results_ui('SetupGUI',M,DIM);
+figure(Finter)
+
+%-Setup MIP & register
 %-----------------------------------------------------------------------
+figure(Fgraph)
+hMIPax = axes('Position',[0.05 0.55 0.55 0.4],'Visible','off');
+hMIPax = spm_mip_ui(t,XYZ,V,hMIPax);
+spm_XYZreg('XReg',hReg,hMIPax,'spm_mip_ui');
+
+%-Design matrix
+%-----------------------------------------------------------------------
+hDax = axes('Position',[0.625 0.55 0.35 0.2]);
+image((spm_DesMtx('sca',[H C B G],Dnames)+1)*32)
+xlabel 'design matrix'
+%if SPMZ
+%	dy = 0.1/size(CONTRAST,1);
+%	for i = 1:size(CONTRAST,1)
+%		axes('Position',[0.625 (0.75 +dy*(i-1)) 0.35 dy])
+%		h = bar(CONTRAST(i,:),1);
+%		set(h,'FaceColor',[1 1 1]*.8)
+%		set(gca,'XLim',[0.5,size(CONTRAST,2)+0.5],'Visible','off')
+%		text(size(CONTRAST,2)+.55,0,sprintf('%d',i),...
+%			'FontSize',10,'Color','r')
+%	end
+%	axes('Position',[0.6 0.75 0.025 0.1],'Visible','off')
+%	text(0.5,0.5,'contrast',...
+%		'FontSize',10,'Rotation',90,...
+%		'HorizontalAlignment','Center','VerticalAlignment','middle')
+%end
+
+%-Text
+%-----------------------------------------------------------------------
+hTax = axes('Position',[0.6 0.9 0.4 0.05],'Visible','off');
 if SPMZ
+	SPMdist = 'Z';
 	tmp = 1 - spm_Ncdf(U);
-elseif SPMF
+else
+	SPMdist = 'F';
 	tmp = 1 - spm_Fcdf(U,df);
 end
-
-Descrip = spm_str_manip(CWD,'a30');
-Descrip = str2mat(Descrip,...
-	sprintf('Height threshold {u} = %0.2f, p = %0.3f',U,tmp));
-Descrip = str2mat(Descrip,...
-	sprintf('Extent threshold {k} = %0.0f voxels',k));
-
-% Display SPM{Z/F} & setup buttons
-%=======================================================================
-spm_mip_ui(t,XYZ,V,Descrip)
-
-% Objects with calls to spm_*.m routines
-%-----------------------------------------------------------------------
-uicontrol(Fgraph,'Style','Frame','Position',[20 470 100 185].*WS);
-
-tmp = 'L = spm_mip_ui(''GetCoords'')'';';
-
-uicontrol(Fgraph,'Style','Text',...
-	'String','Results',...
-	'Position',[40 630 60 20].*WS,...
-	'HorizontalAlignment','Center',...
-	'ForegroundColor','w')
-uicontrol(Fgraph,'Style','Pushbutton','String','plot',...
-	'Callback',[tmp 'spm_graph;'],...
-	'Position',[40 610 60 20].*WS,...
-	'Interruptible','on');
-uicontrol(Fgraph,'Style','Pushbutton','String','maxima',...
-	'Callback',[tmp 'spm_maxima;'],...
-	'Position',[40 550 60 20].*WS,...
-	'Interruptible','on');
-uicontrol(Fgraph,'Style','Pushbutton','String','slices',...
-	'Callback',[tmp 'spm_transverse;'],...
-	'Position',[40 580 60 20].*WS,...
-	'Interruptible', 'on');
-uicontrol(Fgraph,'Style','Pushbutton','String','write',...
-	'Callback','spm_write_filtered(t,XYZ,U,k,V,SPMZ);',...
-	'Position',[40 520 60 20].*WS,...
-	'Interruptible', 'on');
-if V(3) > 1
-	uicontrol(Fgraph,'Style','Pushbutton','String','sections',... 
-		'Callback',[tmp 'spm_sections;'],...
-		'Position',[40 490 60 20].*WS,...
-		'Interruptible','on');
-end
+text(0.5,1,['SPM{',SPMdist,'}'],...
+	'FontSize',16,'Fontweight','Bold','HorizontalAlignment','Center')
+text(0,0.6,spm_str_manip(CWD,'a30'),...
+	'FontSize',10,'FontWeight','Bold')
+text(0,0.3,sprintf('Height threshold {u} = %0.2f, p = %0.3f',U,tmp),...
+	'FontSize',10)
+text(0,0,sprintf('Extent threshold {k} = %0.0f voxels',k),...
+	'FontSize',10)
 
 
 % Finished
-%-----------------------------------------------------------------------
+%=======================================================================
 set(Finter,'Pointer','Arrow')
 
 
