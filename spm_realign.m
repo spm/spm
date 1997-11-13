24148c32660d7b5e39ad8d6ac14f242b989ffa9b
John Ashburner <j.ashburner@ucl.ac.uk>
1997-11-13 16:41:57 +0000

    Bug fix

--- spm_realign.m
+++ spm_realign.m
@@ -275,10 +275,10 @@ elseif nargin >= 1 & strcmp(arg1,'Reslice'),
 	P = arg2;
 	Flags = 'n';
 	sessions = size(P,1);
-	if nargin<4,
-		sessions = arg4;
-		if nargin<3,
-			Flags = arg3;
+	if nargin>=3,
+		Flags = arg3;
+		if nargin>=4,
+			sessions = arg4;
 		end;
 	end;
 	reslice_images(P,Flags,Sessions);
