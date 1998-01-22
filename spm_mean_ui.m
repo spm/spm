6dee45c2f4628d2a8795cf17ecf6763a5cdc0c02
John Ashburner <j.ashburner@ucl.ac.uk>
1998-01-22 17:07:56 +0000

    Now uses spm_vol instead of spm_map

--- spm_mean_ui.m
+++ spm_mean_ui.m
@@ -3,11 +3,9 @@ function spm_average
 % FORMAT spm_average
 %_______________________________________________________________________
 %
-% spm_average simply averages a set of images to produce an mean image that
-% is written to "average.img" (in the current directory)
-%
-% It is assumed that the image and voxels sizes and data format are the same. 
-%
+% spm_average simply averages a set of images to produce an mean image
+% that is written to "average.img" (in the current directory).
+% Any spatial differences described by the `mat' files are ignored.
 %_______________________________________________________________________
 % %W% John Ashburner %E%
 
@@ -15,22 +13,17 @@ function spm_average
 %----------------------------------------------------------------------------
 P = spm_get(Inf,'.img','Select images to be averaged');
 %Q = P(1,:); Q = [Q([1:max(find(Q == '/'))]) 'average.img'];
-Q = 'average.img';
+Q = 'test_average.img';
 
 drawnow;
 
-[DIM VOX SCALE TYPE OFFSET ORIGIN DESCRIP] = spm_hread(deblank(P(1,:)));
-V=zeros(12,size(P,1));
-for i=1:size(P,1)
-	V(:,i)=spm_map(P(i,:));
-	V(7,i)=V(7,i);
-end
+V=spm_vol(P);
 
 %-Compute mean and write headers etc.
 %-----------------------------------------------------------------------
-scale = spm_mean(V,Q)/size(P,1);
-
-spm_hwrite(Q,DIM,VOX,scale,4,0,ORIGIN,'Mean image');
-spm_get_space(Q,spm_get_space(P(1,:)));
-
-for v=V, spm_unmap(v); end;
+scale  = spm_mean(V,Q)/size(P,1);
+VOX    = sqrt(sum(V(1).mat(1:3,1:3).^2));
+ORIGIN = (V(1).mat\[0 0 0 1]')';
+ORIGIN = round(ORIGIN(1:3));
+spm_hwrite(Q,V(1).dim(1:3),VOX,scale,4,0,ORIGIN,'Mean image');
+spm_get_space(Q,V(1).mat);
