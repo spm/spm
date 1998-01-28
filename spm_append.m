ca73e80937b63e09bc920fee83b1943d6a4507fe
John Ashburner <j.ashburner@ucl.ac.uk>
1998-01-28 15:32:26 +0000

    Completely reworked in order to save disk space

--- spm_append.m
+++ spm_append.m
@@ -1,52 +1,108 @@
-function spm_append(MAT,X)
-% appends a matrix to a MAT-file (Level 1 format)
-% FORMAT spm_append(MAT,X);
-% MAT  - name of .mat file
-% X    - matrix
-%____________________________________________________________________________
+function spm_append(fname,data)
+% appends columns to a matrix file stored on disk
+% FORMAT spm_append(fname,data)
+% fname	- name of data file.
+% data  - the data to be appended.
 %
-% spm_append is used to append variables to a matrix in a MAT- file
-% without loading the matrix into working memory.  This saves memory
-% and time.  spm_append is used by spm_spm as the latter cycles over
-% planes saving data for subsequent analysis
-%
-% The name of the matrix is set to MAT ensuring only one matrix per
-% MAT-file.  MAT.mat is created if necessary.
-%
-% NOTE: this routine will require updating for Level 2 MAT-file format
-% and forwards compatibility with new versions of MATLAB spm_append
-% will only append to MAT-files in pwd
-%
-%__________________________________________________________________________
-% %W% Karl Friston %E%
+% See also spm_extract.
+%___________________________________________________________________________
+% %W% John Ashburner %E%
+
+if isempty(data), return; end;
+
+hdrlen = 256;
+MGC = 270198;
+
+fp = my_fopen(fname,'r+','silent');
+if fp.ptr==-1,
+	fp = my_fopen(fname,'w+','error');
+	
+	my_fwrite(fp,MGC,'uint32');
+	my_fwrite(fp,size(data), 'uint32');
+	my_fwrite(fp,zeros(hdrlen-3*4,1), 'uchar');
+	dim = [size(data,1) 0];
+else,
+	mgc = my_fread(fp,1,'uint32');
+	if mgc ~= MGC,
+		my_fclose(fp);
+		error(['" fname "' appears to be the wrong kind of file.']);
+	end;
+	dim = my_fread(fp,2,'uint32');
+	if dim(1) ~= size(data,1),
+		my_fclose(fp);
+		error('Incompatible data size.');
+	end;
+end;
+
+mx = max(data,[],1);
+mn = min(data,[],1);
+scale = (mx-mn)/(2^16-1);
+off = mn;
+
+len    = 8*ceil(dim(1)*2/8);
+rmndr  = rem(dim(1),4);
+for i=1:size(data,2),
+	ofset = hdrlen + (len + 2*8)*(dim(2)+i-1);
+	my_fseek(fp,ofset,'bof');
+	my_fwrite(fp,[scale(i) off(i)], 'float64');
+	dt = round((data(:,i)-off(i))/scale(i));
+	my_fwrite(fp,dt, 'uint16');
+	if rmndr ~= 0,
+		my_fwrite(fp,zeros(rmndr,1), 'uint16');
+	end;
+end;
+
+my_fseek(fp,4*2,'bof');
+my_fwrite(fp,dim(2)+size(data,2), 'integer*4');
+my_fclose(fp);
+return;
 
 
-%----------------------------------------------------------------------------
-if ~length(X); return; end
-X     = real(X);
+function fp = my_fopen(fname,perm,flag)
+if nargin < 2,
+	flag = 'error';
+end;
+fp = struct('ptr',-1,'fname',fname,'perm',perm);
+fp.ptr = fopen(fp.fname,fp.perm); %,'ieee-be');
+if (fp.ptr == -1) & (strcmp(flag,'error')),
+	error(sprintf('Can''t open "%s" (perm "%s")\n%s\n', fname, perm,...
+		'Check that you have permission.'));
+end;
+return;
 
-% create if necessary
-%----------------------------------------------------------------------------
-fid   = fopen([pwd '/' MAT '.mat'],'r');
-if fid < 0
-        eval([MAT ' = X;']);
-	save([MAT '.mat'],'-v4',MAT);
-        return
-end
+function my_fwrite(fp,a,prec)
+count = fwrite(fp.ptr,a,prec);
+if count ~= prod(size(a)),
+	er=ferror(fp.ptr);
+	my_fclose(fp);
+	error(sprintf(...
+		'Problems writing to file "%s"\nThe error was:\n"%s"\n%s\n',...
+		fp.fname,er, 'Check your disk space.'));
+end;
+return;
 
-% check for number of rows compatibility
-%----------------------------------------------------------------------------
-[m n] = size(X);
-HDR   = fread(fid,5,'int32');
-if m ~= HDR(2);  error('  Incompatible sizes');  end
-fclose(fid);
+function a = my_fread(fp,count,prec)
+a = fread(fp.ptr,count,prec);
+if count ~= prod(size(a)),
+	er=ferror(fp.ptr);
+	my_fclose(fp);
+	error(sprintf(...
+		'Problems reading from file "%s"\nThe error was:\n"%s"\n%s\n',...
+		fp.fname,er, 'Check what has already gone wrong.'));
+end;
+return;
 
-% append and update nunmber of columns
-%----------------------------------------------------------------------------
-fid   = fopen([MAT '.mat'],'a');
-fwrite(fid,X(:),'double');
-fclose(fid);
-fid   = fopen([MAT '.mat'],'r+');
-fseek(fid,8,'bof');
-fwrite(fid,(HDR(3) + n),'int32');
-fclose(fid);
+function my_fseek(fp,offset,origin)
+sts=fseek(fp.ptr,offset,origin);
+if sts == -1,
+	er=ferror(fp.ptr);
+	my_fclose(fp);
+	error(sprintf(...
+		'Problems seeking through "%s".  The error was:\n"%s"\n%s\n',...
+		fp.fname,er, 'Check what has already gone wrong.'));
+end;
+return;
+
+function my_fclose(fp)
+fclose(fp.ptr);
+return;
