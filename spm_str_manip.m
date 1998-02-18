844ba0a8a5e7e2aa40e2b7c2c4e9c4c9f617dd91
Andrew Holmes <andrew.holmes@veramed.co.uk>
1998-02-18 18:17:43 +0000

    Upgraded to ML5: Accepts cell arrays of strings, added 'v' option.

--- spm_str_manip.m
+++ spm_str_manip.m
@@ -1,10 +1,10 @@
-function strout = spm_str_manip(strin, options)
+function [strout,R2] = spm_str_manip(strin,options)
 % miscellaneous string manipulation options
 % FORMAT string_out = spm_str_manip(string_in,options)
-% string_in  - input string (or string matrix)
-% string_out - output sring (or string matrix)
+% string_in  - input string, string matrix, or cell array of strings
+% string_out - output sring, string matrix, or cell array of strings
 % options    - a string of options flags
-%____________________________________________________________________________
+%_______________________________________________________________________
 % Each of the options is performed from left to right.
 % The options are:
 % 	'r'              - remove trailing suffix
@@ -18,32 +18,34 @@ function strout = spm_str_manip(strin, options)
 %	['k' num2str(n)] - produce a string of at most n characters long.
 %			   If the input string is longer than n, then
 %			   it is prefixed with '..' and the last n-2 characters
-%	['a' num2str(n)] - similar to above - except the leading directory is
-%			   replaced by '../'.
+%	['a' num2str(n)] - similar to above - except the leading directories
+%			   are replaced by './'.
 %			   eg. spm_str_manip('/dir1/dir2/file.img','a16') would
 %			   produce '../dir2/file.img'.
 %			   are returned.
-% 	'c'		 - remove leading components which are common to
-%			   all rows of string-matrix
-%	'd'		 - deblank
+%       'v'              - delete non valid filename characters
+%                          Valid are './abc...xyzAbc...XYZ0123456789_-'
+% 	'c'		 - remove leading components common to all strings
+%                          returns leading component as a second output argument
+%	'd'		 - deblank - this is always done!
 %
-%____________________________________________________________________________
+%_______________________________________________________________________
 % %W% John Ashburner %E%
 
-strout = strin;
+if nargin<2, options=''; end
+if nargin<1, strout=[]; R2=''; return, end
+
+strout = cellstr(strin);
+R2     = '';
 
 while (~isempty(options))
-	m = size(strout,1);
-	n = size(strout,2);
 	o = 2;
 
-
 	% Read in optional numeric argument c
+	%---------------------------------------------------------------
 	opt = options(2:length(options));
 	c = 0;
-	b = '-';
-	if (~isempty(opt))b = opt(1);end
-
+	if (~isempty(opt)), b = opt(1); else, b = '-'; end
 	while(b >= '0'+0 & b <= '9'+0)
 		c = c * 10 + opt(1)-'0';
 		opt = opt(2:length(opt));
@@ -56,114 +58,104 @@ while (~isempty(options))
 	end
 
 
-	for i=1:m
-		opt = options;
-		stri = deblank(strout(i,:));
-		if (opt(1)=='r')
-			% Remove a trailing suffix of the form `.xxx',
-			% leaving the basename.
-			d1 = max([find(stri == '/') 0]);
-			d2 = max([find(stri == '.') 0]);
-			if (d2>d1), stri = stri(1:(d2-1)); end
+	for i=1:prod(size(strout))
+		str = deblank(strout{i});
+		switch options(1)
+		
+		case 'r'	% Remove a trailing suffix of the form `.xxx',
+				% leaving the basename.
+			d1 = max([find(str == '/') 0]);
+			d2 = max([find(str == '.') 0]);
+			if (d2>d1), str = str(1:(d2-1)); end
 
-		elseif (opt(1)=='e')
-			% Remove all but the suffix.
-			d1 = max([find(stri == '/') 0]);
-			d2 = max([find(stri == '.') 0]);
+		case 'e'	% Remove all but the suffix.
+			d1 = max([find(str == '/') 0]);
+			d2 = max([find(str == '.') 0]);
 			if (d2>d1)
-				stri = stri((d2+1):length(stri));
+				str = str((d2+1):length(str));
 			else
-				stri = '';
+				str = '';
 			end
 
-		elseif (opt(1)=='h')
-			% Remove a trailing pathname component,
-			% leaving the head.
-			d1 = max([find(stri == '/') 0]);
+		case 'h'	% Remove a trailing pathname component,
+				% leaving the head.
+			d1 = max([find(str == '/') 0]);
 			if (d1>0)
-				stri = stri(1:(d1-1));
+				str = str(1:(d1-1));
 			end
 
-		elseif (opt(1)=='t')
-			% Remove all leading  pathname  components,
-			% leaving the tail.
-			d1 = max([find(stri == '/') 0]);
+		case 't'	% Remove all leading  pathname  components,
+				% leaving the tail.
+			d1 = max([find(str == '/') 0]);
 			if (d1>0)
-				stri = stri((d1+1):length(stri));
+				str = str((d1+1):length(str));
 			end
 
-		elseif (opt(1)=='f')
-			% First few characters
-			stri = stri(1:min([length(stri) c]));
+		case 'f'	% First few characters
+			str = str(1:min([length(str) c]));
 
-		elseif (opt(1)=='l')
-			% Last few characters
-			l = length(stri);
-			stri = stri(l-min([length(stri) c])+1:l);
+		case 'l'	% Last few characters
+			l = length(str);
+			str = str(l-min([length(str) c])+1:l);
 
-		elseif (opt(1)=='k')
-			% Last few characters
-			l = length(stri);
+		case 'k'	% Last few characters
+			l = length(str);
 			if (l>c)
-				stri = ['..' stri(l-c+2:l)];
+				str = ['..' str(l-c+2:l)];
 			end
 
-		elseif (opt(1)=='a')
-			% Last few characters
-			stri = deblank(stri);
-			m1   = find(stri == '/');
-			l    = length(stri);
+		case 'a'	% Last few characters
+			m1   = find(str == '/');
+			l    = length(str);
 			if (c < l)
 				m2   = find(l-m1+1+2 <= c);
 				if ~isempty(m2)
-					stri = ['..' stri(m1(min(m2)):l)];
+					str = ['.' str(m1(min(m2)):l)];
 				else
-					stri = ['..' stri(max(m1):l)];
+					str = ['.' str(max(m1):l)];
 				end
 			end
 
-		elseif (opt(1)=='s')
-			% Strip off '.img', '.hdr' or '.mat' suffixes only
-			l = length(stri);
+		case 's'	% Strip off '.img', '.hdr' or '.mat' suffixes
+			l = length(str);
 			if (l > 4)
-				if (strcmp(stri((l-3):l),'.img') | ...
-				    strcmp(stri((l-3):l),'.hdr') | ...
-				    strcmp(stri((l-3):l),'.mat'))
-					stri = spm_str_manip(stri, 'r');
+				if (strcmp(str((l-3):l),'.img') | ...
+				    strcmp(str((l-3):l),'.hdr') | ...
+				    strcmp(str((l-3):l),'.mat'))
+					str = spm_str_manip(str, 'r');
 				end
 			end
-		end
-		l = length(stri);
-		if (l<n)
-			strout(i,:) = [stri zeros(1,(n-l))+' '];
-		else
-			strout(i,:) = stri;
-		end
 
+		case 'v'
+			tmp = find(...
+				( str>='a' & str<='z' ) | ...
+				( str>='A' & str<='Z' ) | ...
+				( str>='0' & str<='9' ) | ...
+				  str=='-' | str=='_' | ...
+				  str=='.' | str=='/' );
+			str = str(tmp);
+
+		end
+		strout{i} = str;
 	end
 		
-	if (options(1) == 'c')
-		% Remove common path components..
-		if (m>1)
-			msk = diff(strout+0) ~=0;
-			d1 = min(find(sum(msk)));
-			d1 = max([find(strout(1,1:d1) == '/') 0])+1;
-			strout = [strout(:,d1:n) zeros(m,(d1-1))+' '];
+	if options(1)=='c'	% Remove common path components
+		if length(strout)>1
+			tmp    = size(strout);
+			strout = char(strout(:));
+			msk    = diff(strout+0)~=0;
+			d1     = min(find(sum(msk)));
+			d1     = max([find(strout(1,1:d1) == '/') 0]);
+			R2     = strout(1,1:d1);
+			strout = reshape(cellstr(strout(:,d1+1:end)),tmp);
 		end
 
-
-	elseif (options(1) == 'd')
-		% Deblank
-		if (m<=1)
-			strout = deblank(strout);
-		else
-			msk = fliplr(sum(strout~=' '));
-			d1  = min(find(msk));
-			strout = strout(:,1:(n-d1+1));
-		end
+	elseif options(1)=='d'	% Deblanking is always done by cellstr above
+		% strout = deblank(strout);
 	end
 
-	options = options(2:length(options));	
+	options = options(o:length(options));	
 
 end
 
+if ischar(strin), strout=char(strout); end
