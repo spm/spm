827455d9a9a64ef6ed42afca6820cf8bb0a5f546
John Ashburner <j.ashburner@ucl.ac.uk>
1998-04-01 13:58:08 +0000

    changed documentation to describe the different strategy for writing images.

--- spm_conv_vol.m
+++ spm_conv_vol.m
@@ -1,14 +1,19 @@
 
 % Convolves a mapped volume with a three dimensional separable function
 % FORMAT spm_conv_vol(V,Q,fx,fy,fz,offsets)
-% V        -  is the memory mapped volume
-% Q        -  is the output filename
+% V        -  the input volume
+%             - can be a 3D Matlab array, or an image handle
+%               obtained by spm_vol
+% Q        -  the output volume
+%             - can be a 3D Matlab array (should probably be
+%               a lhs argument in this case), or an image
+%               handle describing the format of the output
+%               image.
 % fx       -  the separable form of the function in x
 % fy       -  the separable form of the function in y
 % fz       -  the separable form of the function in z
 % offsets  - [i j k] contains the x, y and z shifts to reposition
 %             the output
-% iA       -  Image array (in memory) to convolve
 %_______________________________________________________________________
 %
 % spm_conv_vol is a compiled function (see spm_conv_vol.c).
@@ -19,13 +24,11 @@
 % in z.
 %
 % If Q is an array with the same number of elements as the volume, the
-% convolved volume will be store there instead of on disk.
+% convolved volume will be store there instead of on disk.  When Q 
+% describes an output image, it is passed to the function spm_write_plane
+% to write out each plane of the image.
 %
-% If iA is specified, the image dimensions are gotten from the first 3
-% members of V and (if iA has the corresponding number of elements) the
-% image data is read from it.
-% 
-% See also spm_conv.m and spm_smooth.m
+% See also spm_conv.m and spm_smooth.m spm_write_plane.m
 %
 %_______________________________________________________________________
 % %W% John Ashburner, Tom Nichols %E%
