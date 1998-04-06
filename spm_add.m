8e766dd433b4e78502923e21b3c2de22a7009070
John Ashburner <j.ashburner@ucl.ac.uk>
1998-04-06 17:51:15 +0000

    Updated help information

--- spm_add.m
+++ spm_add.m
@@ -1,24 +1,22 @@
 
 % add a series of images - a compiled routine
-% FORMAT s = spm_add(V,Q,flags)
-% V     - Vector of mapped volumes (from spm_map or spm_vol).
-% Q     - Filename for averaged image
+% FORMAT s = spm_add(VI,VO,flags)
+% VI    - Vector of mapped volumes (from spm_map or spm_vol).
+% VO    - Description of output volume that gets passed to
+%         spm_write_plane.m
 % flags - Flags can be:
-%               'f' - writes floating point output image.
 %               'm' - masks the mean to zero or NaN wherever
 %                     a zero occurs in the input images.
 % s     - Scalefactor for output image.
 %_______________________________________________________________________
 %
 % spm_add computes a sum of a set of image volumes to produce an
-% integral image that is written to a named file (Q).
+% integral image that is written to a named file (VI.fname).
 %
-% The image is written as signed short (16 bit) unless the `f' flag
-% is specified.
+% A mean can be effected by modifying the scalefactors (and offsets) of
+% VI (see spm_mean for an example). A weighted sum can be effected by
+% using different weightings for image scalefactors.
 %
-% A mean can be effected by scaling the output image via it's
-% scalefactor (see spm_mean for an example). A weighted sum can be
-% effected by weighting the image scalefactors appropriately.
-%
+% See also, spm_res2.m
 %_______________________________________________________________________
 % %W% John Ashburner (from JB's original spm_mean.c) %E%
