d567c51b27c0bfe49346011586b3551d04278837
Andrew Holmes <andrew.holmes@veramed.co.uk>
1998-04-10 15:43:41 +0000

    Removed use of global variables in favour of additional arguments. ...work in progress - just a checkpoint delta.

--- spm_AnCova.m
+++ spm_AnCova.m
@@ -1,92 +1,96 @@
-function [Fdf,F,BETA,T,RES,BCOV] = spm_AnCova(C,G,sigma,X,CON)
-% AnCova for temporally correlated variables
-% FORMAT [Fdf,F,BETA,T,RES,BCOV] = spm_AnCova(C,G,sigma,[X,[CON]]);
+function varargout = spm_AnCova(C,G,sigma,Y,CON,Xd)
+% General Linear Model & inference for serially correlated regression
 %
-% C     - Design matrix partition of interest (K*c)
-% C     - Design matrix partition of confounds (K*g)
-% sigma - SD of Gaussian kernel K (or kernel itself)
-% X     - {m x n} matrix of response {m x 1} variables (K*x)
+% FORMAT [Fdf,F,BETA,T,RES,BCOV,Xd] = spm_AnCova(C,G,sigma,Y,CON,Xd);
+% FORMAT [Fdf,Xd] = spm_AnCova(C,G,sigma);
+%
+% C     - Design matrix partition of interest (K*C)
+% G     - Design matrix partition of confounds (K*G)
+% sigma - SD of Gaussian kernel K (or kernel itself) [default 0]
+% Y     - {m x n} matrix of response {m x 1} variables (K*Y)
 % CON   - {t x p} matrix of contrasts p = size([C G],2)
+% Xd    - Data structure containing working design matrix df and pseudoinverses
+%         fieldnames(Xd) = {'C';'G';'sigma';'df';'trRV';'trR0V';'PG';'PH';'PHV'}
+%         (See  body of code for definitions.)
 %
 % Fdf   - {1 x 2} vector of degrees of freedom
 % F     - {1 x n} vector of F statistics
 % BETA  - {p x n} matrix of parameter estimates
 % T     - {t x n} matrix of T values
-% RES   - {1 x n} matrix of residual SSQ
-% BCOV  - {p x p} matrix such that cov{BETA) = RES*BCOV
+% RES   - {1 x n} matrix of residual sums of squares (ResSS)
+% BCOV  - {p x p} matrix such that cov(BETA) = RES*BCOV
 %_______________________________________________________________________
 %
-% spm_AnCova uses a General[ized] Least quares Model of the form
+% spm_AnCova uses a General Linear Model of the form:
 %
-%	K*x  = K*[c g]*BETA + K*e
+%	K*Y  = K*[c g]*BETA + K*e
 %
 % to compute the parameter estimates (BETA) and make inferences (F)
 % where K is a convolution matrix corresponding to a Gaussian kernel
-% of parameter sigma.  sigma = 0 corresponds to the case of independent
-% observations.
+% of parameter sigma.  sigma = 0 corresponds to the standard case of
+% independent observations.
 %
-% spm_AnCova checks to see whether previously computed global variables
-% are appropriate for the current computation and uses them is they are.
-% This facility is expedient when spm_AnCova is called repeatedly with
-% the same design matrix and sigma.
+% spm_AnCova checks to see whether the previously computed df and
+% pseudoinverses held in the data structure Xd are appropriate for the
+% current computation, and uses them is they are. This facility is
+% expedient when spm_AnCova is called repeatedly with the same design
+% matrix and sigma.
 % 
 % ref: Seber GAF (1977) Linear regression analysis, Wiley New York
 % ref: Worsley KJ & Friston KJ (1995) NeuroImage 2:173-181
 %
-%---------------------------------------------------------------------------
-% %W% Karl Friston %E%
+%_______________________________________________________________________
+% %W% Karl Friston, Andrew Holmes %E%
 
+%-Format arguments
+%-----------------------------------------------------------------------
+if nargin<6, bXd=0; else, bXd=1; end
+if nargin<3, sigma=0; end
+if nargin<2, error('Insufficient arguments'), end
 
-% output arguments
-%---------------------------------------------------------------------------
-global DF trRV trR0V PG PH PHV G_sig G_C G_G
-T     = [];
+%-Output arguments
+%-----------------------------------------------------------------------
 F     = [];
 BETA  = [];
+T     = [];
 RES   = [];
+BCOV  = [];
 
-% check whether global variables should be used
-%---------------------------------------------------------------------------
-GLOB  = all([ size(G_C) == size(C) size(G_G) == size(G) ]);
-if GLOB
-   GLOB = (G_sig == sigma) & all(all(G_C == C)) & all(all(G_G == G));
-end
-G_sig = sigma;
-G_C   = C;
-G_G   = G;
+%-Check whether variables in design matrix data structure Xd should be used
+%-----------------------------------------------------------------------
+if bXd, bXd = all([size(Xd.C)==size(C),size(Xd.G)==size(G)]); end
+if bXd, bXd = all(all(Xd.C==C)) & all(all(Xd.G==G)) & (Xd.sigma==sigma); end
+bXd
 
-
-% check design matrix.  If C = [] then assume statistics are required
+%-Check design matrix.  If C = [] then assume statistics are required
 % for the effect of confounds
-%--------------------------------------------------------------------------
+%-----------------------------------------------------------------------
 q     = size([C G],1);
 if size(C,2) == 0; C = G; G = [];  end
 H     = [C G];
 if size(G,2) == 0; G = zeros(q,1); end
 
-% compute df and psuedoinverses if necessary
-%---------------------------------------------------------------------------
-if ~GLOB
 
-
-	% temporal convolution kernel
-	%-------------------------------------------------------------------
+%-Compute & stote / unpack df and psuedoinverses
+%-----------------------------------------------------------------------
+if ~bXd
+	%-Temporal convolution kernel - A sparse Toeplitz matrix
+	%---------------------------------------------------------------
 	K      = spm_sptop(sigma,q);
 	V      = K*K';
 
-
-	% cunning piece of code to compute expectations of variances and 
+	%-Cunning piece of code to compute expectations of variances and 
 	% effective degrees of freedom by capitalizing on the sparsity
 	% structure of the problem
-	%-------------------------------------------------------------------
+	%---------------------------------------------------------------
 	PH     = pinv(H);
 	PHV    = PH*V;
 	PG     = pinv(G);
 	PGV    = PG*V;
 	PHVV   = PHV*V;
 
-	% traces
-	%--------------------------------------------------------------------
+	%-Traces
+	%---------------------------------------------------------------
 	trV    = trace(V);
 	trVV   = sum(sum(V.*V));
 
@@ -103,35 +107,49 @@ if ~GLOB
 	trR0V  = trQ - trP;
 	trR0V2 = trQQ - 2*trPQ + trPP;
 
+	%-The [effective] degrees of freedom
+	%---------------------------------------------------------------
+	df     = [trR0V^2/trR0V2 trRV^2/trRV2];
 
-	% the [effective] degrees of freedom
-	%-------------------------------------------------------------------
-	DF     = [trR0V^2/trR0V2 trRV^2/trRV2];
+	%-Save design matrix parameters in data structure Xd
+	%---------------------------------------------------------------
+	Xd     = struct('C',C,'G',G,'sigma',sigma,'df',df,...
+			'trRV',trRV,'trR0V',trR0V,'PG',PG,'PH',PH,'PHV',PHV);
+else
+	%-Unpack variables from data structure Xd
+	%---------------------------------------------------------------
+	df     = Xd.df;
+	trRV   = Xd.trRV;
+	trR0V  = Xd.trR0V;
+	PG     = Xd.PG;
+	PH     = Xd.PH;
+	PHV    = Xd.PHV;
 end
 
-Fdf   = DF;
+Fdf   = df;
 
-% if a response variable is specified
-%---------------------------------------------------------------------------
-if nargin > 3
 
-	% estimate parameters and sum of squares due to error
-	%-------------------------------------------------------------------
-	BETA  = (PH*X);					% parameter estimates
+%-If a response variable is specified
+%-----------------------------------------------------------------------
+if nargin>3
 
-	% SSQ
-	%-------------------------------------------------------------------
-	R     = X - H*BETA;				% residuals under Ha
-	N     = X - G*(PG*X);				% residuals under Ho
+	%-Estimate parameters and sum of squares due to error
+	%---------------------------------------------------------------
+	BETA  = (PH*Y);					% parameter estimates
+
+	%-SS
+	%---------------------------------------------------------------
+	R     = Y - H*BETA;				% residuals under Ha
+	N     = Y - G*(PG*Y);				% residuals under Ho
 	RES   = sum(R.^2);				% SSQ under Ha
 	NUL   = sum(N.^2);				% SSQ under Ho
 	
-	% test for effects with he F ratio of variances
-	%-----------------------------------------------------------------
+	%-Test for effects with the F ratio of variances
+	%---------------------------------------------------------------
 	F     = trRV/trR0V*(NUL - RES)./RES;		% F statistic
 
-	% compute t statistics if contrasts are specified
-	%-----------------------------------------------------------------
+	%-Compute t statistics if contrasts are specified
+	%---------------------------------------------------------------
 	if nargin > 4
 		T     = zeros(size(CON,1),size(BETA,2));
 		for i = 1:size(CON,1)
@@ -142,8 +160,15 @@ if nargin > 3
 	end
 end
 
-% matrices for computing the standard error of the paramter estimates
-%-------------------------------------------------------------------------
-if nargout > 5
-	BCOV  = PHV*PH'/trRV;
+%-Matrix for computing the standard error of the paramter estimates
+%-----------------------------------------------------------------------
+if nargout>5, BCOV = PHV*PH'/trRV; end
+
+
+%-Sort output
+%-----------------------------------------------------------------------
+if nargin<=3
+	varargout = {Fdf,Xd};
+else
+	varargout = {Fdf,F,BETA,T,RES,BCOV,Xd};
 end
