6fafa05f367e76f035956a085b5f90cce4177cff
Karl Friston <k.friston@ucl.ac.uk>
1998-05-12 14:21:54 +0000

    modified to cope with very high df using gammaln

--- spm_pF.m
+++ spm_pF.m
@@ -11,46 +11,47 @@ function CPF = spm_pF(S,W,df,F)
 % Reference : Worsley 1994, Adv Appl Prob 26, 13-42.
 %
 %---------------------------------------------------------------------------
-% %W% JBP %E%
+% %W% JBP/KJF %E%
 
 % check on df
 %---------------------------------------------------------------------------
 n      = df(1);
 m      = df(2);
-m      = min([m (340 - n)]);
 if (size(W,2) == 3 & (m < 4)) | (size(W,2) == 2 & (m < 3))
 	error('df too small'); end
 
 if size(W,1) > 1
-	W   = mean(W); end
+	W  = mean(W); end
 
 
 % parameters
 %---------------------------------------------------------------------------
-SqrtDetLmda = prod(1./(W*sqrt(2)));
-Lm2 	    = gamma(m/2);
-Ln2 	    = gamma(n/2);
-f	    = n*F/m;
+SDL     = prod(1./(W*sqrt(2)));
+LG      = gammaln(m/2) + gammaln(n/2);
+f       = n*F/m;
 
 % compute expection {2D}
 %---------------------------------------------------------------------------
 if size(W,2) == 2
-	CPF	= S * SqrtDetLmda*gamma((m + n - 2)/2)/(2*pi*Lm2*Ln2) *...
-	(f^(n/2 -1)) * ((1 + f)^(-(m + n - 2)/2)) * ((m - 1)*f - n + 1);
+
+	G   = exp(gammaln((m + n - 2)/2) - LG);
+	CPF = G*S*SDL/(2*pi)*(f^(n/2 -1))*((1 + f)^(-(m + n - 2)/2))*...
+	((m - 1)*f - n + 1);
 end
 
 % compute expection {3D}
 %---------------------------------------------------------------------------
 if size(W,2) == 3
-	CPF	= S * SqrtDetLmda*gamma((m + n - 3)/2)*...
-	f^(n/2 - 3/2) * ((1+f)^(-(m+n-2)/2) ) * ...
+
+	G   = exp(gammaln((m + n - 3)/2) - LG);
+	CPF = G*S*SDL*f^(n/2 - 3/2)*((1 + f)^(-(m + n - 2)/2))*...
 	((m - 1)*(m - 2)*(f^2) - (2*m*n - m - n - 1)*f + (n - 1)*(n - 2))/...
-	((2*pi)^(3/2)*sqrt(2)*Lm2*Ln2);
+	((2*pi)^(3/2)*sqrt(2));
 
 end
 
 % p value assuming Poisson behaviour
 %---------------------------------------------------------------------------
-CPF = 1 - exp(-CPF); 
+CPF     = 1 - exp(-CPF); 
 
 
