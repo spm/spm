d4a090586fbd4c2b06dcee45bd00079e3bb36637
John Ashburner <j.ashburner@ucl.ac.uk>
1998-07-28 16:47:55 +0000

    Added optional threshold

--- spm_mask.m
+++ spm_mask.m
@@ -1,15 +1,18 @@
-function spm_mask(P1,P2)
+function spm_mask(P1,P2, thresh)
 % Masks Images.
-% FORMAT spm_mask(P1,P2)
+% FORMAT spm_mask(P1,P2, thresh)
 % P1     - matrix of input image filenames from which
 %          to compute the mask.
 % P2     - matrix of input image filenames on which
 %          to apply the mask.
+% thresh - optional threshold(s) for defining the mask.
 % The masked images are prepended with the prefix `m'.
 %
 % If any voxel in the series of images is zero or does
 % not have a finite value, then that voxel is set to NaN
-% or zero in all the images.
+% or zero in all the images.  If a threshold, or vector of
+% thresholds is passed, then the masking is mased on voxels
+% whos values are above all the thresholds.
 %
 % Images sampled in different orientations and positions can
 % be passed to the routine.  Providing the `.mat' files are
@@ -23,6 +26,16 @@ if nargin==0,
 end;
 if nargin==1,
 	P2 = P1;
+end;
+if nargin==4,
+	if prod(size(thresh))==1,
+		thresh = ones(prod(size(P1)),1)*thresh;
+	else,
+		if prod(size(P1)) ~= prod(size(thresh)),
+			error('thresh vector is wrong size.');
+		end;
+		thresh=thresh(:);
+	end;
 end;
 
 V1=spm_vol(P1);
@@ -57,7 +70,11 @@ for j=1:dim(3),
 	for i=1:m1
 		M1  = M\V1(i).mat\Mi;
 		img = spm_slice_vol(V1(i),M1,dim(1:2),[1 0]);
-		msk = msk + (img~=0 & finite(img));
+		if nargin<3
+			msk = msk + (img~=0 & finite(img));
+		else,
+			msk = msk + (img>=thresh & finite(img));
+		end;
 	end;
 
 	msk = find(msk~=m1);
