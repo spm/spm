b2d37fccc703743bbceae95d75aca5f458ac85c6
Andrew Holmes <andrew.holmes@veramed.co.uk>
1998-08-12 19:00:17 +0000

    Cosmetic re-write for MatLab 5

--- spm_type.m
+++ spm_type.m
@@ -1,16 +1,15 @@
-function [T] = spm_type(x)
-% translates data type specifiers within SPM 
-% FORMAT [T] = spm_type(x)
-% x    - specifier {strings must be at least 5 characters}
+function T = spm_type(x)
+% translates data type specifiers between SPM & Matlab representations
+% FORMAT T = spm_type(x)
+% x    - specifier
 % T    - type
-%___________________________________________________________________________
+%_______________________________________________________________________
 %
 % Format specifiers are based on ANALYZE.  If the input is
 % a number then the corresponding matlab string is returned
 % If the input is a string then the appropriate TYPE is
 % returned
 %
-%---------------------------------------------------------------------------
 % TYPE  bits    bytes   MATLAB  ANALYZE                 UNIX
 % 0     0       -       -       DT_NONE                 -
 % 1     1       -       uint1   DT_BINARY               -
@@ -19,27 +18,32 @@ function [T] = spm_type(x)
 % 8     32      4       int32   DT_SIGNED_INT           (int)
 % 16    32      4       float   DT_FLOAT                (float)
 % 64    64      8       double  DT_DOUBLE               (double)
-%---------------------------------------------------------------------------
 %
-%___________________________________________________________________________
-% %W% %E%
+%_______________________________________________________________________
+% %W% John Ashburner, Andrew Holmes %E%
 
+if nargin==0, error('insufficient arguments'), end
 
-if isstr(x)
-	T = 0;
-	x = x(1:5);
-	if all(x == 'uint1');  T = 1;  end
-	if all(x == 'uint8');  T = 2;  end
-	if all(x == 'int16');  T = 4;  end
-	if all(x == 'int32');  T = 8;  end
-	if all(x == 'float');  T = 16; end
-	if all(x == 'doubl');  T = 64; end
-else
-	T = 'unknown';
-	if (x == 1);  T = 'uint1';  end
-	if (x == 2);  T = 'uint8';  end
-	if (x == 4);  T = 'int16';  end
-	if (x == 8);  T = 'int32';  end
-	if (x == 16); T = 'float';  end
-	if (x == 64); T = 'double'; end
+if nargin==1
+	if ischar(x)
+		switch lower(x)
+		case 'uint1',  T =  1;
+		case 'uint8',  T =  2;
+		case 'int16',  T =  4;
+		case 'int32',  T =  8;
+		case 'float',  T = 16;
+		case 'double', T = 64;
+		otherwise,     T =  0;
+		end
+	else
+		switch x
+		case  1,   T = 'uint1';
+		case  2,   T = 'uint8';
+		case  4,   T = 'int16';
+		case  8,   T = 'int32';
+		case 16,   T = 'float';
+		case 64,   T = 'double';
+		otherwise, T= 'unknown';
+		end
+	end
 end
