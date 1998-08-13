62d0bf42b27fbe46817315b80ed3184511fa59a4
Andrew Holmes <andrew.holmes@veramed.co.uk>
1998-08-13 10:37:59 +0000

    Removed "See also spm_res2" comment.

--- spm_add.m
+++ spm_add.m
@@ -16,7 +16,5 @@
 % A mean can be effected by modifying the scalefactors (and offsets) of
 % VI (see spm_mean for an example). A weighted sum can be effected by
 % using different weightings for image scalefactors.
-%
-% See also, spm_res2.m
 %_______________________________________________________________________
 % %W% John Ashburner (from JB's original spm_mean.c) %E%
