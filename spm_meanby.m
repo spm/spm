26457c98e2a6760bad3db9d46da6b1bc28138e67
Andrew Holmes <andrew.holmes@veramed.co.uk>
1998-08-20 11:22:23 +0000

    Streamlined to use spm_DesMtx(v2.4+) for the donkey work, which permits interactions (via matrix I), and overcomes previous dependence on indexes being 1:n. Dropped varby stuff (maybe write a seperate function?) (Originally modified as v1.7 on 98/05/07 18:25:15 andrew but re-SCCSed to v2.1)

--- spm_meanby.m
+++ spm_meanby.m
@@ -1,70 +1,38 @@
-function [M,Mi,V,Vi,iVals]=spm_meanby(X,i)
-% Mean (& variances) of data in columns by group
-% FORMAT [M,Mi,V,Vi,iVals]=spm_meanby(X,i)
-% X	- Data matrix, with data in columns (row vector X is transposed)
-% i	- Column of indicator vectors, indicating group membership of
-%	  observations in rows of X
-% Mi	- Mean for observations in each group, for each column
-% Vi    - (Sample) variance for observations in each group
-% iVals	- Group indicator values corresponding to rows of Mi
-% M	- Matrix of same size as X, with observations replaced by the
+function [M,Mi,i] = spm_meanby(Y,I)
+% Means of data in columns by group
+% FORMAT [M,Mi,i] = spm_meanby(Y,I)
+% Y  - Data matrix, data in columns. (Row vector Y also accepted.)
+% I  - Column of indicator vectors, indicating group membership of rows of Y
+%    - Multi-column I are treated as multiple factors to be interacted, 
+%      and means are computed within each unique combination of the factor levels
+% M  - Matrix of same size as Y, with observations replaced by the
 %	  appropriate group mean
-% V     - Matrix of same size as X, with observations replaced by the
-%	  appropriate group sample variance
+% Mi - Mean for observations in each group, one column for each column of Y,
+%      one row for each group (or unique factor level combination)
+% i  - Group indicator values corresponding to rows of Mi
 %_______________________________________________________________________
 %
 % spm_meanby computes means for grouped data presented as columns of
 % data with a vector of group indicators
 %_______________________________________________________________________
-% %E% Andrew Holmes %W%
+% %W% Andrew Holmes %E%
+
 
 %-Check arguments
 %=======================================================================
-if nargin<2, i=ones(size(X,1),1); end
-M = zeros(size(X));
-V = zeros(size(X));
-if size(X,1)==1, X=X'; end
-if min(size(i))~=1, error('vector i'), end
-i=i(:);
+if nargin<0, error('insufficient arguments'), end
+if  size(Y,1)==1, bT=1; Y=Y'; else, bT=0; end
+if nargin<2, I=ones(size(Y,1),1); end
+if  size(I,1)~=size(Y,1), I=I'; end
+if  size(I,1)~=size(Y,1), error('row dimension mismatch between inputs'), end
 
-if ~all(floor(i)==ceil(i)), error('Non-integer indicator vector'), end
-if size(X,1)~=length(i), error('X must have row dimension the length of i'), end
 
-%-Computation
+%-Computation - using spm_DesMtx
 %=======================================================================
+[X,null,i,idx,jdx] = spm_DesMtx(I);
 
-%-Sort out unique indicator levels
-tmp    = sort(i);
-iVals  = tmp(logical([1;diff(tmp)>0]));
+i  = i';
+Mi = (X'*Y)./repmat(sum(X,1)',1,size(Y,2));
+M  = Mi(jdx,:);
 
-%-Compute indicator matrix
-I       = zeros(size(X,1),length(iVals));
-for p_i = 1:length(iVals)
-	I(:,p_i)=i==iVals(p_i);
-end
-
-%-Work out number of elements by index
-Ni = sum(I)'*ones(1,size(X,2));
-
-%-Work out sums by index
-Si = I'*X;
-
-%-Work out means by index
-Mi = Si ./ Ni;
-
-%-Combine into matrix same size as X:
-% (Effectively replacing each observation by its group mean)
-M(:) = Mi(i,:);
-
-%-Compute group variances if requested
-%-----------------------------------------------------------------------
-if nargout<3, return, end
-
-%-Work out sum of squares by index
-SSi = I'*X.^2;
-
-%-Work out variances by index
-Vi = ( SSi - Si.^2./Ni ) ./ (Ni -1);
-
-%-Combine into matrix same size as X:
-V(:) = Vi(i,:);
+if bT, M=M'; end
