fe032e818abc682a98b7a14a3569ecd409a1db4b
Andrew Holmes <andrew.holmes@veramed.co.uk>
1998-10-21 12:58:17 +0000

    Renamed from spm_mean.m Added command window progress reporting. Added image dimension & orientation consistency checking code.

--- spm_mean_ui.m
+++ spm_mean_ui.m
@@ -1,27 +1,66 @@
-function spm_mean
+function spm_mean_ui
 % promts for a series of images and averages them
-% FORMAT spm_mean
+% FORMAT spm_mean_ui
 %_______________________________________________________________________
 %
-% spm_mean simply averages a set of images to produce a mean image
-% that is written to "mean.img" (in the current directory).
-% Any spatial differences described by the `mat' files are ignored, and
-% the resultant mean is given the spatial orientation of the first image.
+% spm_mean_ui simply averages a set of images to produce a mean image
+% that is written as type int16 to "mean.img" (in the current directory).
+%
+% The images must have the same dimensions, orientations (as defined by
+% the Origin header field or any associated *.mat files), and the same
+% voxel sizes.
+%
+% This is not a "softmean" - zero voxels are treated as zero.
 %_______________________________________________________________________
-% %W% John Ashburner %E%
+% %W% John Ashburner, Andrew Holmes %E%
+SCCSid = '%I%';
 
-%-Select images
+
+%-Say hello
 %-----------------------------------------------------------------------
-V=spm_vol(spm_get(Inf,'.img','Select images to be averaged'));
+SPMid = spm('FnBanner',mfilename,SCCSid);
+
+
+%-Select images & check dimensions, orientations and voxel sizes
+%-----------------------------------------------------------------------
+fprintf('\t...select files')
+P = spm_get(Inf,'.img','Select images to be averaged');
+fprintf(' ...mapping & checking files')
+Vi = spm_vol(P);
+
+n  = prod(size(Vi));
+if n==0, fprintf('\t%s : no images selected\n\n',mfilename), return, end
+
+if n>1 & any(any(diff(cat(1,Vi.dim),1,1),1)&[1,1,1,0])
+	error('images don''t all have same dimensions'), end
+if any(any(any(diff(cat(3,Vi.mat),1,3),3)))
+	error('images don''t all have same orientation & voxel size'), end
+
 
 %-Compute mean and write headers etc.
 %-----------------------------------------------------------------------
-VO         = V(1);
-VO.fname   = 'mean.img';
-VO.descrip = 'Mean image';
-VO.pinfo   = [1.0 0 0]';
-VO.dim(4)  = 4;
-spm_create_image(VO);
-for i=1:prod(size(V)), V(i).pinfo(1:2,:) = V(i).pinfo(1:2,:)/prod(size(V)); end;
-VO.pinfo(1,1) = spm_add(V,VO);
-spm_create_image(VO);
+fprintf(' ...computing')
+Vo = struct(	'fname',	'mean.img',...
+		'dim',		[Vi(1).dim(1:3),4],...
+		'mat',		Vi(1).mat,...
+		'pinfo',	[1.0,0,0]',...
+		'descrip',	'spm - mean image');
+
+%-Adjust scalefactors by 1/n to effect mean by summing
+for i=1:prod(size(Vi))
+	Vi(i).pinfo(1:2,:) = Vi(i).pinfo(1:2,:)/n; end;
+
+%-Write basic header
+spm_create_image(Vo);
+
+%-Use spm_add to do the donkey work
+Vo.pinfo(1,1) = spm_add(Vi,Vo);
+
+%-Write header (complete with scaling information)
+spm_create_image(Vo);
+
+
+%-End - report back
+%-----------------------------------------------------------------------
+fprintf(' ...done\n')
+fprintf('\tMean image written to file ''%s'' in current directory\n\n',Vo.fname)
