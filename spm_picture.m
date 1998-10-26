7559de5f644c0962f3e831eccf8282cba7a2998c
Andrew Holmes <andrew.holmes@veramed.co.uk>
1998-10-26 16:14:40 +0000

    Obsoleted by spm_render, which uses an actual segmented image for rendering. Now just prints obsolesence notice.

--- spm_picture.m
+++ spm_picture.m
@@ -1,7 +1,11 @@
 
-% rendering of regional effects [SPM{Z}] on drawing of the cortical surface
+% Rendering of regional effects [SPM{Z}] on drawings of the cortical surface
 % FORMAT spm_picture
 %____________________________________________________________________________
+%
+% spm_picture has been replaced by the SPM rendering utility - spm_render.m
+%
+%                           ----------------
 %
 % spm_picture is called after spm_results_ui, and uses variables in working
 % memory to load images/pictures of the cortical surface.  Regional
@@ -14,103 +18,10 @@
 %__________________________________________________________________________
 % %W% Karl Friston %E%
 
-Finter = spm_figure('FindWin','Interactive');
-Fgraph = spm_figure('FindWin','Graphics');
+%-Print warning of obsolescence
+%-----------------------------------------------------------------------
+warning('spm_picture is obsolete: use spm_render instead')
 
-set(Finter,'Pointer','Watch');
 
-load Split
-colormap default
-colormap gray
-split = [colormap; ones(64,1)*[1 0 0]];
-colormap(split)
-
-%----------------------------------------------------------------------------
-load spm_rctx
-load spm_lctx
-
-T     = t - min(t);
-T     = t*length(colormap)/max(t)/2;
-T     = T + length(colormap)/2;
-sr    = sr*length(colormap)/300;
-sl    = sl*length(colormap)/300;
-
-% right medial
-%----------------------------------------------------------------------------
-d     = XYZ(1,:) > 0 & XYZ(1,:) < 24;
-D     = T(d);
-P     = round(XYZ(:,d));
-for n = 1:length(D)
-      y = P(2,n);
-      z = P(3,n);
-      q = ones(2,4)*D(n);
-      if abs(D(n)) > sr(96 - y,228  +  z)
-              sr((96 - y):(97 - y),(228 + z):(231 + z)) = q;
-      end
-end
-
-% right lateral
-%----------------------------------------------------------------------------
-d     = XYZ(1,:) >= 24;
-D     = T(d);
-P     = XYZ(:,d);
-for n = 1:length(D)
-      y = P(2,n);
-      z = P(3,n);
-      q = ones(2,4)*D(n);
-      if abs(D(n))>sr(134 + y,82 + z)
-              sr(134 + y:135 + y,82 + z:85 + z) = q;
-      end
-end
-
-% left medial
-%----------------------------------------------------------------------------
-d = XYZ(1,:) <= 0 & XYZ(1,:) > (-24);
-D = T(d);
-P = XYZ(:,d);
-
-for n = 1:length(D)
-      y = P(2,n);
-      z = P(3,n);
-      q = ones(2,4)*D(n);
-      if abs(D(n)) > sl(134 + y,228 + z)
-              sl(134 + y:135 + y,228 + z:231 + z) = q;
-      end
-end
-
-% left lateral
-%----------------------------------------------------------------------------
-d = XYZ(1,:) <= ( -24);
-D = T(d);
-P = XYZ(:,d);
-
-for n = 1:length(D)
-      y = P(2,n);
-      z = P(3,n);
-      q = ones(2,4)*D(n);
-      if abs(D(n)) > sl(96 - y,82 + z)
-              sl(96 - y:97 - y,82 + z:85 + z) = q;
-      end
-end
-
-% delete previous axis
-%----------------------------------------------------------------------------
-figure(Fgraph)
-subplot(2,1,2); delete(gca)
-subplot(2,2,4); image(sr');axis xy; axis image; axis off
-subplot(2,2,3); image(sl');axis xy; axis image; axis off
-
-% unmap and reset pointer (and x locations is necessary)
-%----------------------------------------------------------------------------
-set(Finter,'Pointer','Arrow')
-
-if spm_input('write to disk ?',6,'b','yes|no',[1 0]);
-	d = spm_input('filename',6,'s');
-	d = [CWD '/' d '.img'];
-	A = [sl; sr];
-	fid = fopen(d,'w');
-	fwrite(fid,A,'uint8');
-	fclose(fid);
-	spm_hwrite(d,[size(A) 1],[1 1 1],1,spm_type('uint8'),0);
-end
-
+%-NB: Right & left hemisphere cortex & lateral surface drawings are
+% stored in the MAT files spm_rctx.mat & spm_lctx.mat
