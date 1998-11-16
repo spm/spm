fe6ecc19348e606abb5d72efc80e8bf3903c05c7
Karl Friston <k.friston@ucl.ac.uk>
1998-11-16 17:00:20 +0000

    Initial revision.

---
+++ spm_uc_RF.m
@@ -0,0 +1,47 @@
+function [u] = spm_U(a,df,STAT,R,n)
+% corrected critical height threshold at a specified significance level
+% FORMAT [u] = spm_U(a,df,STAT,R,n)
+% a     - critical probability - {alpha}
+% df    - [df{interest} df{residuals}]
+% STAT  - Statisical feild
+%		'Z' - Gaussian feild
+%		'T' - T - feild
+%		'X' - Chi squared feild
+%		'F' - F - feild
+% R     - RESEL Count {defining search volume}
+% n     - number of conjoint SPMs
+%
+% u     - critical height {corrected}
+%
+%___________________________________________________________________________
+%
+% spm_U returns the corrected critical threshold at a specified significance
+% level (a). If n > 1 a conjunction the probability over the n values of the 
+% statistic is returned.
+%___________________________________________________________________________
+% 98/11/01 Karl Friston @(#)spm_U.m	1.1
+
+% find approximate value
+%---------------------------------------------------------------------------
+u     = spm_u((a/sum(R))^(1/n),df,STAT);
+du    = 1e-6;
+
+% approximate estimate using E{m}
+%---------------------------------------------------------------------------
+d     = 1;
+while abs(d) > 1e-6
+	[P P p] = spm_P(1,0,u,df,STAT,R,n);
+	[P P q] = spm_P(1,0,u + du,df,STAT,R,n);
+        d       = (a - p)/((q - p)/du);
+        u       = u + d;
+end
+
+% refined estimate using 1 - exp(-E{m})
+%---------------------------------------------------------------------------
+d     = 1;
+while abs(d) > 1e-6
+        p       = spm_P(1,0,u,df,STAT,R,n);
+	q       = spm_P(1,0,u + du,df,STAT,R,n);
+        d       = (a - p)/((q - p)/du);
+        u       = u + d;
+end
