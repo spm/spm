62da5dcf68174ec14c84ef20d9d2aa475106683a
Andrew Holmes <andrew.holmes@veramed.co.uk>
1998-11-17 08:38:41 +0000

    Simplified code to use new features of load in ML5.

--- spm_load.m
+++ spm_load.m
@@ -1,10 +1,19 @@
 function [x] = spm_load(f)
-% function to load ascii file data
+% function to load ascii file data as matrix
 % FORMAT [x] = spm_load(f)
 % f  - file {ascii file containing a regular array of numbers
 % x  - corresponding data matrix
-%___________________________________________________________________________
+%_______________________________________________________________________
 % %E% Karl Friston, Andrew Holmes %W%
 
-if nargin==0, f=spm_get(1,'*','Select ASCII data file'); end
-eval(['load ' f ' -ascii; x = ' spm_str_manip(spm_str_manip(f,'t'),'r') ';']);
+
+%-Get a filename if none was passed
+%-----------------------------------------------------------------------
+if nargin==0
+	f = spm_get(1,'*','Select ASCII data file');
+end
+
+
+%-Load the data file into double precision matrix x
+%-----------------------------------------------------------------------
+x = load(f,'-ascii');
