33b470305738a937c358553dee0f1633d69a3849
Karl Friston <k.friston@ucl.ac.uk>
1998-11-26 18:36:20 +0000

    now handles empty matrices

--- spm_detrend.m
+++ spm_detrend.m
@@ -10,19 +10,31 @@ function [y] = spm_detrend(x,p);
 % y   - detrended data matrix
 %
 %__________________________________________________________________________
-% %W% %E%
+% %W% Karl Friston %E%
 
+% defaults
 %---------------------------------------------------------------------------
 [m n] = size(x);
-if nargin == 1, p = 0; end
+if ~n
+	y = [];
+	return
+end
+if nargin == 1
+	p = 0;
+end
+
+% centre columns
+%---------------------------------------------------------------------------
 if ~p
 	y = x - ones(m,1)*mean(x);
 	return
 end
+
+% polynomial adjustment
+%---------------------------------------------------------------------------
 G     = [];
 for i = 0:p
 	d = [1:m].^i;
 	G = [G d(:)];
 end
-
 y     = x - G*(pinv(G)*x);
