375d7182d834f7762ea068add53a2fb4d7b15026
John Ashburner <j.ashburner@ucl.ac.uk>
1998-12-01 16:48:47 +0000

    Initial revision.

---
+++ spm_resels_vol.m
@@ -0,0 +1,21 @@
+
+% computes the number of resels in a volume
+% FORMAT R = spm_resels_vol(V,W)
+% V      -  is a memory mapped image volume.
+%           Finite and non-zero values are considered to be part of
+%           the search volume.
+% W      -  smoothness of the component fields {FWHM in voxels}.
+% R      - Resel counts, where:
+%          R(1) - Euler Characteristic of the volume (number of connected
+%                 components - number of holes).
+%          R(2) - Resel Diameter (average over all rotations of the
+%                 distance between two parallel planes tangent to the
+%                 volume in resel space).
+%          R(3) - Resel Surface Area (half the surface area of the
+%                 volume in resel space).
+%          R(4) - Resel Volume (the volume in resel space).
+%_______________________________________________________________________
+%
+% Reference : Worsley KJ et al 1996, Hum Brain Mapp. 4:58-73
+%_______________________________________________________________________
+% %W% John Ashburner %E%
