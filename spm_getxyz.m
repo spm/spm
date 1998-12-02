0e5dcd2617bcbc4f7f11302842682fa58ec486a5
John Ashburner <j.ashburner@ucl.ac.uk>
1998-12-02 15:22:02 +0000

    Initial revision.

---
+++ spm_getxyz.m
@@ -0,0 +1,9 @@
+
+% extracts the finite non-zero coordinates from a volume
+% FORMAT XYZ = spm_getxyz(V)
+% V      -  is a memory mapped image volume.
+%           Finite and non-zero values are considered to be part of
+%           the search volume.
+% XYZ    - The co-ordinates (in millimeters) of the voxels.
+%_______________________________________________________________________
+% %W% John Ashburner %E%
