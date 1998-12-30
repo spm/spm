307c086e3193bd79efc1d90fed43984639db4264
Karl Friston <k.friston@ucl.ac.uk>
1998-12-30 17:54:11 +0000

    *** empty log message ***

--- spm_hrf.m
+++ spm_hrf.m
@@ -1,4 +1,4 @@
-function [hrf,p] = spm_hrf(RT,p);
+function [hrf,p] = spm_hrf(RT,P);
 % returns a hemodynamic response function
 % FORMAT [hrf,p] = spm_hrf(RT,[p]);
 % RT   - scan repeat time
@@ -12,6 +12,7 @@ function [hrf,p] = spm_hrf(RT,p);
 %	p(4) - dispersion of undershoot			   1
 %	p(5) - ratio of response to undershoot		   6
 %	p(6) - onset (seconds)				   0
+%	p(7) - length of kernel (seconds)		  32
 %
 % hrf  - hemodynamic response function
 % p    - parameters of the response function
@@ -20,15 +21,16 @@ function [hrf,p] = spm_hrf(RT,p);
 
 % default parameters
 %-----------------------------------------------------------------------
-if nargin < 2
-	p = [6 16 1 1 6 0];
+p     = [6 16 1 1 6 0 32];
+if nargin > 1
+      p(1:length(P)) = P
 end
 
 % modelled hemodynamic response function - {mixture of Gammas}
 %-----------------------------------------------------------------------
 dt    = RT/8;
-u     = [0:(32 + p(6))/dt] - p(6)/dt;
+u     = [0:(p(7)/dt)] - p(6)/dt;
 hrf   = spm_Gpdf(u,p(1)/p(3),dt/p(3)) - spm_Gpdf(u,p(2)/p(4),dt/p(4))/p(5);
 hrf   = spm_conv(hrf,8);
-hrf   = hrf([1:(32 + p(6))/RT]*8);
+hrf   = hrf([1:(p(7)/RT)]*8);
 hrf   = hrf'/sum(hrf);
