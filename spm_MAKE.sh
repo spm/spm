cfa5e246c07417299b04a829906e15f6d8fc529a
John Ashburner <j.ashburner@ucl.ac.uk>
1999-01-15 15:33:24 +0000

    Matthew Brett's port to Windows

--- spm_MAKE.sh
+++ spm_MAKE.sh
@@ -1,62 +1,105 @@
-#!/bin/csh -v
+#!/bin/sh
 #
-#     %W% John Ashburner %E%
+#     %W% JA, mods by Matthew Brett %E%
 #
-# spm_MAKE will compile spm*.c scripts in a platform specific fashion
-# see cmex
-alias CC    "cc -xO5"
-alias cmex5 "cmex     COPTIMFLAGS=-xO5"
-alias cmex4 "cmex -V4 COPTIMFLAGS=-xO5"
+# spm_MAKE.sh will compile spm*.c scripts in a platform specific fashion
+# see mex
+#
+# default compilation is for Unix
+# pass string "windows" to script for Windows compile
+# ie ./spm_MAKE.sh windows
 
-CC -c -o utils_uchar.o		spm_vol_utils.c -DUNSIGNED_CHAR
-CC -c -o utils_short.o		spm_vol_utils.c -DSIGNED_SHORT
-CC -c -o utils_int.o		spm_vol_utils.c -DSIGNED_INT
-CC -c -o utils_schar.o		spm_vol_utils.c -DSIGNED_CHAR
-CC -c -o utils_ushort.o	spm_vol_utils.c -DUNSIGNED_SHORT
-CC -c -o utils_uint.o		spm_vol_utils.c -DUNSIGNED_INT
-CC -c -o utils_float.o		spm_vol_utils.c -DFLOAT
-CC -c -o utils_double.o	spm_vol_utils.c -DDOUBLE
+if [ $# = 0 ]; then
+	arch="unix";
+else
+	arch=$1;
+fi
+
+echo "SPM mex file compile for $arch"
+echo 
+
+case $arch in
+
+    unix)
+	# (default) unix compile
+	CC="gcc -Wall -O2"
+	cmex5="mex     COPTIMFLAGS=-O2"
+	cmex4="mex -V4 COPTIMFLAGS=-O2"
+	added_objs="spm_mapping.o";;
+    windows)
+	# set options for windows compile with gcc/mingw32
+	deff=-DSPM_WIN32
+	CC="gcc -mno-cygwin -Wall $deff"
+	cmex5="cmd /c mex $deff "
+	cmex4="cmd /c mex $deff -V4 "
+
+	# Windows added utility files
+	$CC -c -o win32mmap.o win32mmap.c
+	$CC -c -o rint.o rint.c
+
+	added_objs="rint.o win32mmap.o spm_mapping.obj";;
+
+   *)
+	echo "Sorry, not set up for architecture $arch"
+	exit;;
+esac
+
+echo "Compiling volume utilities..."
+$CC -c -o utils_uchar.o		spm_vol_utils.c -DSPM_UNSIGNED_CHAR
+$CC -c -o utils_short.o		spm_vol_utils.c -DSPM_SIGNED_SHORT
+$CC -c -o utils_int.o		spm_vol_utils.c -DSPM_SIGNED_INT
+$CC -c -o utils_schar.o		spm_vol_utils.c -DSPM_SIGNED_CHAR
+$CC -c -o utils_ushort.o	spm_vol_utils.c -DSPM_UNSIGNED_SHORT
+$CC -c -o utils_uint.o		spm_vol_utils.c -DSPM_UNSIGNED_INT
+$CC -c -o utils_float.o		spm_vol_utils.c -DSPM_FLOAT
+$CC -c -o utils_double.o	spm_vol_utils.c -DSPM_DOUBLE
 
 # Byteswapped images
-CC -c -o utils_short_s.o	spm_vol_utils.c -DSIGNED_SHORT -DBYTESWAP
-CC -c -o utils_int_s.o		spm_vol_utils.c -DSIGNED_INT -DBYTESWAP
-CC -c -o utils_ushort_s.o	spm_vol_utils.c -DUNSIGNED_SHORT -DBYTESWAP
-CC -c -o utils_uint_s.o	spm_vol_utils.c -DUNSIGNED_INT -DBYTESWAP
-CC -c -o utils_float_s.o	spm_vol_utils.c -DFLOAT -DBYTESWAP
-CC -c -o utils_double_s.o	spm_vol_utils.c -DDOUBLE -DBYTESWAP
+$CC -c -o utils_short_s.o	spm_vol_utils.c -DSPM_SIGNED_SHORT -DSPM_BYTESWAP
+$CC -c -o utils_int_s.o		spm_vol_utils.c -DSPM_SIGNED_INT -DSPM_BYTESWAP
+$CC -c -o utils_ushort_s.o	spm_vol_utils.c -DSPM_UNSIGNED_SHORT -DSPM_BYTESWAP
+$CC -c -o utils_uint_s.o	spm_vol_utils.c -DSPM_UNSIGNED_INT -DSPM_BYTESWAP
+$CC -c -o utils_float_s.o	spm_vol_utils.c -DSPM_FLOAT -DSPM_BYTESWAP
+$CC -c -o utils_double_s.o	spm_vol_utils.c -DSPM_DOUBLE -DSPM_BYTESWAP
 
-CC -c -o spm_make_lookup.o spm_make_lookup.c
-CC -c -o spm_vol_utils2.o  spm_vol_utils2.c 
+$CC -c -o spm_make_lookup.o spm_make_lookup.c
+$CC -c -o spm_getdata.o spm_getdata.c
+$CC -c -o spm_vol_access.o  spm_vol_access.c 
 
-set vol_utils = (utils_uchar.o utils_short.o utils_int.o utils_float.o utils_double.o \
+# utility routine
+$cmex5 -c spm_mapping.c
+
+vol_utils="utils_uchar.o utils_short.o utils_int.o utils_float.o utils_double.o \
 	utils_schar.o utils_ushort.o utils_uint.o \
 	utils_short_s.o utils_int_s.o utils_float_s.o utils_double_s.o \
 	utils_ushort_s.o utils_uint_s.o \
-	spm_vol_utils2.o spm_make_lookup.o)
+	spm_vol_access.o spm_make_lookup.o spm_getdata.o $added_objs"
 
+echo "Adding to archive library spm_vol_utils.a..."
+\rm spm_vol_utils.a
 ar rcv spm_vol_utils.a $vol_utils
 \rm $vol_utils
 
-cmex5 spm_sample_vol.c	spm_vol_utils.a spm_mapping.c
-cmex5 spm_slice_vol.c	spm_vol_utils.a spm_mapping.c
-cmex5 spm_brainwarp.c	spm_vol_utils.a spm_mapping.c
-cmex5 spm_map_vol.c	spm_vol_utils.a spm_mapping.c
-cmex5 spm_unmap_vol.c	spm_vol_utils.a spm_mapping.c
-cmex5 spm_add.c	spm_vol_utils.a spm_mapping.c
-cmex5 spm_conv_vol.c	spm_vol_utils.a spm_mapping.c
-cmex5 spm_render_vol.c	spm_vol_utils.a spm_mapping.c
-cmex5 spm_global.c	spm_vol_utils.a spm_mapping.c
-cmex5 spm_resels_vol.c	spm_vol_utils.a spm_mapping.c
-cmex5 spm_getxyz.c	spm_vol_utils.a spm_mapping.c
+echo "Compiling mex files..."
+$cmex5 spm_sample_vol.c	spm_vol_utils.a 
+$cmex5 spm_slice_vol.c	spm_vol_utils.a 
+$cmex5 spm_brainwarp.c	spm_vol_utils.a 
+$cmex5 spm_map_vol.c	spm_vol_utils.a 
+$cmex5 spm_unmap_vol.c	spm_vol_utils.a 
+$cmex5 spm_add.c	spm_vol_utils.a 
+$cmex5 spm_conv_vol.c	spm_vol_utils.a 
+$cmex5 spm_render_vol.c	spm_vol_utils.a 
+$cmex5 spm_global.c	spm_vol_utils.a 
+$cmex5 spm_resels_vol.c	spm_vol_utils.a 
+$cmex5 spm_getxyz.c	spm_vol_utils.a 
 
-cmex5 spm_atranspa.c
-cmex5 spm_list_files.c
-cmex5 spm_unlink.c
+$cmex5 spm_atranspa.c
+$cmex5 spm_list_files.c
+$cmex5 spm_unlink.c
+$cmex5 spm_kronutil.c
+$cmex5 spm_project.c
 
-cmex5 spm_map_vol.c    spm_mapping.c
-cmex5 spm_unmap_vol.c  spm_mapping.c
+$cmex4 spm_max.c
+$cmex4 spm_clusters.c
 
-cmex4 spm_kronutil.c
-cmex4 spm_project.c
-cmex4 spm_max.c
-cmex4 spm_clusters.c
+echo "Done."
