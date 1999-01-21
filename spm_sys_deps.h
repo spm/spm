938b673ac4a28dc21f75fee2a410af4ee674f75d
John Ashburner <j.ashburner@ucl.ac.uk>
1999-01-21 12:14:56 +0000

    Initial revision.

---
+++ spm_sys_deps.h
@@ -0,0 +1,15 @@
+/* %W% John Ashburner - from Matthew Brett's work %E% */
+
+#ifndef SYS_DEP
+	#define SYS_DEP
+	#ifdef SPM_WIN32
+		#define rint(x) floor((x)+0.5)  /* round to nearest int */
+		#define finite(x) mxIsFinite(x) /* finite */
+		#define MAXNAMLEN 1024          /* maximum filename length */
+		#define SEPS      "\\"          /* directory separator */
+		#include <process.h>
+	#else /* SPM_WIN32 */
+		#define SEPS      "/"           /* directory separator */
+	#endif /* SPM_WIN32 */
+#endif /* SYS_DEP */
+
