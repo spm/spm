ea262fa61f66f4b5d5cfdb8703b17bd7bfef581c
John Ashburner <j.ashburner@ucl.ac.uk>
1999-02-10 15:27:28 +0000

    Sets the origin to the true image centre rather than 0.866 voxels away (if it is [0 0 0] in the header).  - Thanks Stefan

--- spm_get_space.m
+++ spm_get_space.m
@@ -39,7 +39,7 @@ if (nargin == 1)
 
 		% If origin hasn't been set, then assume
 		% it is the centre of the image.
-		if all(origin == 0), origin = dim(1:3)/2; end;
+		if all(origin == 0), origin = (dim(1:3)+1)/2; end;
 		if all(vox == 0), vox = [1 1 1]; end;
 		off = -vox.*origin;
 		M   = [vox(1) 0 0 off(1) ; 0 vox(2) 0 off(2) ; 0 0 vox(3) off(3) ; 0 0 0 1];
@@ -72,7 +72,7 @@ elseif (nargin == 2)
 		if isempty(dim),
 			error(sprintf('Can''t read header for "%s"\n', [spm_str_manip(imagename,'sd') '.img']));
 		end
-		if all(origin == 0), origin = dim(1:3)/2; end;
+		if all(origin == 0), origin = (dim(1:3)+1)/2; end;
 		if all(vox == 0), vox = [1 1 1]; end;
 		off = -vox.*origin;
 		mt  = [vox(1) 0 0 off(1) ; 0 vox(2) 0 off(2) ; 0 0 vox(3) off(3) ; 0 0 0 1];
