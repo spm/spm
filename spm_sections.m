357ac03cbafd5e345f01ddda383395f1236bede6
Andrew Holmes <andrew.holmes@veramed.co.uk>
1999-03-12 13:46:06 +0000

    Removed 'RNP' argument from call to spm_results_ui('ClearPane',...

--- spm_sections.m
+++ spm_sections.m
@@ -18,7 +18,7 @@ function spm_sections(SPM,VOL,hReg)
 
 Fgraph = spm_figure('FindWin','Graphics');
 spms   = spm_get(1,'.img','select an image for rendering');
-spm_results_ui('ClearPane',Fgraph,'RNP');
+spm_results_ui('ClearPane',Fgraph);
 spm_orthviews('Image',spms,[0.05 0.05 0.9 0.4]);
 spm_orthviews('register',hReg);
 spm_orthviews('addblobs',1,VOL.XYZ,SPM.Z,VOL.M);
