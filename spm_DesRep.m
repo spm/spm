ee64b9a311ec94a03d92bfe69f400321e161c8bd
Andrew Holmes <andrew.holmes@veramed.co.uk>
1999-03-19 14:53:02 +0000

    Updated 'DesMtx' reporting to use xX space structures efficiently. Looks first for xX.nKX, then xX.xKXs.X, then xX.X. Added CallBacks for ButtonDown & WindowButtonMotion for surfing design matrix pictures and parameter estimability pictures.

--- spm_DesRep.m
+++ spm_DesRep.m
@@ -28,12 +28,18 @@ function varargout = spm_DesRep(varargin)
 %
 %                           ----------------
 %
-% FORMAT spm_DesRep('DesMtx',X,Xnames,P,xs)
+% FORMAT spm_DesRep('DesMtx',xX,fnames,xs)
 % Produces a one-page graphical summary of the design matrix
-% X      - nxp Design matrix
-% Xnames - px1 CellStr of parameter names
-% P      - [optional] nx1 CellStr of filenames (i.e. {V.fname}')
-% xs     - [optional] structure of extra strings containing descriptive
+% xX      - Structure containing design matrix information
+%         - the first of {xX.nX, xKXs.X, xX.X} is used for display
+% .nX     - Desgin matrix already scaled for display
+% .xKXs.X - temporally filtered design matrix (within space structure)
+% .X      - "raw" design matrix (as setup by spm{,_fmri}_ui routines)
+%
+% .Xnames - [optional] px1 CellStr of parameter names
+%
+% P       - [optional] nx1 CellStr of filenames (i.e. {V.fname}')
+% xs      - [optional] structure of extra strings containing descriptive
 %          information which is printed at the foot of the page.
 %          The field names are used as sub-headings, the field values
 %          (which must be strings or CellStr) printed alongside.
@@ -43,13 +49,19 @@ function varargout = spm_DesRep(varargin)
 % spm_DesMtx('sca',...) such that zero is mid-grey, -1 is black, and +1
 % is white. Covariates exceeding this randge are scaled to fit.
 %
-% The design matrix is "clickable": Clicking in the design matrix image
-% results in the value of the design matrix at that point being
-% displayed, for as long as the mouse button is depressed.
+% The design matrix is "clickable": Clicking (and dragging) around the
+% design matrix image reports the corresponding value of the design
+% matrix ('normal' click - "left" mouse button usually), the image
+% filename ('extend' mouse click - "middle" mouse), or parameter name
+% ('alt' click - "right" mouse). (spm_DesRep('SurfDesMtx_CB') handles
+% the CallBacks)
 %
 % Under the design matrix the parameter estimability is displayed as a
 % 1xp matrix of grey and white squares. Parameters that are not
-% uniquely specified by the model are shown with a grey patch.
+% uniquely specified by the model are shown with a grey patch. CLicking
+% on (and dragging around) the estimability image reports the parameter
+% names and their estimability. (spm_DesRep('SurfEstIm_CB') handles
+% the CallBacks)
 % 
 %                           ----------------
 %
@@ -78,8 +90,45 @@ function varargout = spm_DesRep(varargin)
 %
 %                           ----------------
 %
-% FORMAT spm_DesRep('cb_DesMtxIm')
-% CallBack for revealing DesMtx values from clickable DesMtx image
+% FORMAT spm_DesRep('SurfDesMtx_CB')
+% 'ButtonDownFcn' CallBack for surfing clickable design matrix images
+% FORMAT spm_DesRep('SurfDesMtxMo_CB')
+% 'WindowButtonMotionFcn' CallBack for surfing clickable design matrix images
+%
+% Setting spm_DesRep('SurfDesMtx_CB') as the 'ButtonDownFcn' on a
+% design matrix image (and saving design info in it's UserData) allows
+% the user to "surf" the design matrix.
+%
+% "Surfing" (i.e. clicking on the image and dragging around it) with
+% the 'normal' mouse button (usually the "left" button) reports desgin
+% matrix values, the 'extend' ("middle" button) reports the
+% corresponding image file name, and the 'alt' ("right" button" reports
+% the parameter names.
+%
+% The design matrix, parameter names and image file names should be
+% saved in the UserData of the image object as a structure with fields
+% 'X','Xnames' & 'fnames' respectively. The code is robust to any of
+% these being empty or mis-specified - surfing simply reports "no
+% cached data".
+%
+%                           ----------------
+%
+% FORMAT spm_DesRep('SurfEstIm_CB')
+% 'ButtonDownFcn' CallBack for surfing clickable parameter estimability images
+% FORMAT spm_DesRep('surfEstImMo_CB')
+% 'WindowButtonMotionFcn' CallBack for surfing parameter estimability images
+%
+% Setting spm_DesRep('SurfEstIm_CB') as the 'ButtonDownFcn' on a
+% parameter estimability image (and saving design info in it's UserData) allows
+% the user to "surf" the parameter estimability image, with the
+% parameter at the current point and it's estimability being reported
+% continuously.
+%
+% The binary parameter estimability matrix and parameter names should
+% be saved in the UserData of the image object as a structure with
+% fields 'est' & 'Xnames' respectively. The code is robust to any of
+% these being empty or mis-specified - surfing simply reports "no
+% cached data".
 %
 %_______________________________________________________________________
 % %W% Andrew Holmes %E%
@@ -87,12 +136,11 @@ function varargout = spm_DesRep(varargin)
 
 %-Format arguments
 %-----------------------------------------------------------------------
-if nargin==0, error('do what? no arguments given...')
-	else, action = varargin{1}; end
+if nargin==0, error('do what? no arguments given...'), end
 
+switch lower(varargin{1})
 
-
-switch lower(action), case 'files&factors'   %-Summarise files & factors
+case 'files&factors'                         %-Summarise files & factors
 %=======================================================================
 % spm_DesRep('Files&Factors',P,I,xC,sF,xs)
 if nargin<4, error('insufficient arguments'), end
@@ -226,79 +274,116 @@ end
 
 case 'desmtx'                                    %-Display design matrix
 %=======================================================================
-% spm_DesRep('DesMtx',X,Xnames,P,xs)
-if nargin<3, error('insufficient arguments'), end
-X      = varargin{2};
-Xnames = varargin{3};
-if nargin<4, P={}; else, P=varargin{4}; end
-if nargin<5, xs=[]; else, xs=varargin{5}; end
+% spm_DesRep('DesMtx',xX,fnames,xs)
+if nargin<2, error('insufficient arguments'), end
+if ~isstruct(varargin{2}), error('design matrix structure required'), end
+if nargin<3, fnames={}; else, fnames=varargin{3}; end
+if nargin<4, xs=[]; else, xs=varargin{4}; end
 
-%-Get graphics window & window scaling
+
+%-Locate DesMtx (X), scaled DesMtx (nX) & get parameter names (Xnames)
+%-----------------------------------------------------------------------
+if isfield(varargin{2},'xKXs') & ...
+		~isempty(varargin{2}.xKXs) & isstruct(varargin{2}.xKXs)
+	iX = 1;
+	[nScan,nPar] = size(varargin{2}.xKXs.X);
+elseif isfield(varargin{2},'X') & ~isempty(varargin{2}.X)
+	iX = 0;
+	[nScan,nPar] = size(varargin{2}.X);
+else
+	error('Can''t find DesMtx in this structure!')
+end
+
+if isfield(varargin{2},'nKX') & ~isempty(varargin{2}.nKX)
+	inX = 1; else, inX = 0; end
+
+if isfield(varargin{2},'Xnames') & ~isempty(varargin{2}.Xnames)
+	Xnames = varargin{2}.Xnames; else, Xnames = {}; end
+
+
+%-Get graphics window & FontSizes
+%-----------------------------------------------------------------------
 Fgraph = spm_figure('GetWin','Graphics');
-spm_figure('Clear',Fgraph)
+spm_results_ui('Clear',Fgraph,0)
 FS = spm_figure('FontSizes');
 
 %-Title
 %-----------------------------------------------------------------------
 hTax = axes('Position',[0.03,0,0.94,1],...
-	'DefaultTextFontSize',FS(2),...
+	'DefaultTextFontSize',FS(9),...
 	'XLim',[0,1],'YLim',[0,1],...
 	'Visible','off');
 
 text(0.5,0.95,'Statistical analysis: Design',...
-	'Fontsize',FS(5),'Fontweight','Bold',...
+	'Fontsize',FS(14),'Fontweight','Bold',...
 	'HorizontalAlignment','center')
 
 line('XData',[0.3 0.7],'YData',[0.92 0.92],'LineWidth',3,'Color','r')
 line('XData',[0.3 0.7],'YData',[0.28 0.28],'LineWidth',3,'Color','r')
 
 
-%-Design matrix, parameter names & files
+%-Display
 %-----------------------------------------------------------------------
-%-Design matrix
-nX = spm_DesMtx('sca',X,Xnames);
-hDesMtx = axes('Position',[.07 .4 .6 .4]);
-hDesMtxIm = image((nX + 1)*32);
+hDesMtx   = axes('Position',[.07 .4 .6 .4]);
+if inX		%-Got a scaled DesMtx
+	hDesMtxIm = image((varargin{2}.nKX + 1)*32);
+elseif iX	%-No scaled DesMtx, DesMtx in .xKXs structure
+	hDesMtxIm = image((spm_DesMtx('sca',varargin{2}.xKXs.X,Xnames) + 1)*32);
+else		%-No scaled DesMtx, no .xKXs, DesMtx in .X
+	hDesMtxIm = image((spm_DesMtx('sca',varargin{2}.X,     Xnames) + 1)*32);
+end
 set(hDesMtx,'TickDir','out')
 ylabel('images')
 xlabel('parameters')
 
 %-Parameter names
-axes('Position',[.07 .8 .6 .1],'Visible','off',...
-	'DefaultTextFontSize',FS(1),'DefaultTextInterpreter','TeX',...
-	'XLim',[0,size(X,2)]+0.5)
-for i = 1:size(nX,2), text(i,.05,Xnames{i},'Rotation',90), end
+if ~isempty(Xnames)
+	axes('Position',[.07 .8 .6 .1],'Visible','off',...
+		'DefaultTextFontSize',FS(8),'DefaultTextInterpreter','TeX',...
+		'XLim',[0,nPar]+0.5)
+	for i = 1:nPar, text(i,.05,Xnames{i},'Rotation',90), end
+end
 
 %-Filenames
 % ( Show at most 32, showing every 2nd/3rd/4th/... as necessary to pair )
-% ( down to <32 items. Always show last item so #images is indicated.    )     
-if ~isempty(P)
-	nScan = size(X,1);
+% ( down to <32 items. Always show last item so #images is indicated.   )     
+if ~isempty(fnames)
 	s = spm_DesRep('ScanTick',nScan,32);
 	set(hDesMtx,'YTick',s)
 	axes('Position',[.68 .4 .3 .4],'Visible','off',...
-		'DefaultTextFontSize',FS(1),...
+		'DefaultTextFontSize',FS(8),...
 		'YLim',[0,nScan]+0.5,'YDir','Reverse')
-	for i=s, text(0,i,spm_str_manip(P{i},'a40')), end
+	for i=s, text(0,i,spm_str_manip(fnames{i},'a40')), end
 end
 
 %-Setup callbacks to allow interrogation of design matrix
 %-----------------------------------------------------------------------
-set(hDesMtxIm,'UserData',X)
-set(hDesMtxIm,'ButtonDownFcn','spm_DesRep(''cb_DesMtxIm'')')
+if iX, 	set(hDesMtxIm,'UserData',...
+	struct('X',varargin{2}.xKXs.X,'Xnames',{Xnames},'fnames',{fnames}))
+else, 	set(hDesMtxIm,'UserData',...
+	struct('X',varargin{2}.X,     'Xnames',{Xnames},'fnames',{fnames}))
+end
+set(hDesMtxIm,'ButtonDownFcn','spm_DesRep(''SurfDesMtx_CB'')')
 
 %-Parameter estimability/uniqueness
 %-----------------------------------------------------------------------
-hPEstAx = axes('Position',[.07 .325 .6 .025]);
-image((spm_SpUtil('IsCon',X)+1)'*32)
+tmp = get(Fgraph,'DefaultTextInterpreter');
+set(Fgraph,'DefaultTextInterpreter','TeX');
+hPEstAx   = axes('Position',[.07 .325 .6 .025],'DefaultTextInterpreter','TeX');
+if iX,	est = spm_SpUtil('IsCon',varargin{2}.xKXs);
+else,	est = spm_SpUtil('IsCon',varargin{2}.X); end
+hParEstIm = image((est+1)*32);
 set(hPEstAx,...
-	'XLim',[0,size(X,2)]+.5,'XTick',[1:size(X,2)-1]+.5,'XTickLabel','',...
+	'XLim',[0,nPar]+.5,'XTick',[1:nPar-1]+.5,'XTickLabel','',...
 	'YLim',[0,1]+.5,'YDir','reverse','YTick',[],...
 	'Box','on','TickDir','in','XGrid','on','GridLineStyle','-');
 xlabel('paremeter estimability')
-text((size(X,2)+0.5 + size(X,2)/30),1,...
+text((nPar+0.5 + nPar/30),1,...
 	'(gray \rightarrow \beta not uniquely specified)',...
 	'Interpreter','TeX','FontSize',FS(1))
+set(hParEstIm,'UserData',struct('est',est,'Xnames',{Xnames}))
+set(hParEstIm,'ButtonDownFcn','spm_DesRep(''SurfEstIm_CB'')')
+set(Fgraph,'DefaultTextInterpreter',tmp);
 
 
 %-Design descriptions
@@ -503,26 +588,87 @@ s = 1:p:nScan; s(end)=nScan;
 varargout = {s,lim};
 
 
-case 'cb_desmtxim'
+case {'surfdesmtx_cb','surfdesmtxmo_cb'}  %-Callbacks for surfing DesMtx
 %=======================================================================
-% spm_DesRep('cb_DesMtxIm')
-hDesMtxIm = gco;
-hDesMtx   = get(hDesMtxIm,'Parent');
-ij = get(hDesMtx,'CurrentPoint'); ij = round(ij(1,2:-1:1));
-X = get(hDesMtxIm,'UserData');
-if isempty(X)
-	warning('Design matrix not saved under image - using imaged values!')
-	X = get(hDesMtxIm,'CData');
+% spm_DesRep('SurfDesMtx_CB')
+% spm_DesRep('SurfDesMtxMo_CB')
+
+if strcmp(lower(varargin{1}),'surfdesmtx_cb')
+	set(0,'UserData',get(get(gca,'XLabel'),'String'))
+	set(gcbf,'WindowButtonMotionFcn','spm_DesRep(''SurfDesMtxMo_CB'')')
+	set(gcbf,'WindowButtonUpFcn',[...
+		'set(get(gca,''XLabel''),''String'',get(0,''UserData'')),',...
+		'set(0,''UserData'',[]),',...
+		'set(gcbf,''WindowButtonMotionFcn'',''''),',...
+		'set(gcbf,''WindowButtonUpFcn'','''')'])
 end
-str = sprintf('X(%d,%d) = %g',ij(1),ij(2),X(ij(1),ij(2)));
-set(get(hDesMtx,'XLabel'),'String',str)
 
-set(gcbf,'WindowButtonUpFcn',[...
-	'set(get(gca,''XLabel''),''String'',''parameters''),',...
-	'set(gcbf,''WindowButtonUpFcn'','''')'])
+mm  = [get(gca,'YLim')',get(gca,'XLim')']+[.5,.5;-.5,-.5];
+ij  = get(gca,'CurrentPoint');
+ij  = round(min(max(ij(1,[2,1]),mm(1,:)),mm(2,:)));
+
+switch get(gcbf,'SelectionType')
+case 'normal'
+	try, str = sprintf('X(%d,%d) = %g',ij(1),ij(2),...
+		subsref(get(gco,'UserData'),...
+		struct('type',{'.','()'},'subs',{'X',{ij(1),ij(2)}})));
+	catch, str='(no cached design matrix to surf)'; end
+case 'extend'
+	try, str = sprintf('Image %d: %s',ij(1),...
+		spm_str_manip(...
+		subsref(get(gco,'UserData'),...
+		struct('type',{'.','{}'},'subs',{'fnames',{ij(1)}})),'a40'));
+	catch, str='(no cached image filenames to surf)'; end
+case 'alt'
+	try, str = sprintf('Parameter %d: %s',ij(2),...
+		subsref(get(gco,'UserData'),...
+		struct('type',{'.','{}'},'subs',{'Xnames',{ij(2)}})));
+	catch, str='(no cached parameter names to surf)'; end
+case 'open'
+	return
+end
+
+set(get(gca,'XLabel'),'String',str)
 
 
-otherwise
+case {'surfestim_cb','surfestimmo_cb'}  %-Callbacks for surfing ParEstIm
+%=======================================================================
+% spm_DesRep('SurfEstIm_CB')
+% spm_DesRep('surfEstImMo_CB')
+
+if strcmp(lower(varargin{1}),'surfestim_cb')
+	set(0,'UserData',get(get(gca,'XLabel'),'String'))
+	set(gcbf,'WindowButtonMotionFcn','spm_DesRep(''surfEstImMo_CB'')')
+	set(gcbf,'WindowButtonUpFcn',[...
+		'set(get(gca,''XLabel''),''String'',get(0,''UserData'')),',...
+		'set(0,''UserData'',[]),',...
+		'set(gcbf,''WindowButtonMotionFcn'',''''),',...
+		'set(gcbf,''WindowButtonUpFcn'','''')'])
+end
+
+mm  = [get(gca,'XLim')]+[.5,-.5];
+i   = get(gca,'CurrentPoint');
+i   = round(min(max(i(1,1),mm(1)),mm(2)));
+
+switch get(gcbf,'SelectionType')
+case 'normal'
+	try, tmp = {' (not unique)',' (unique)'};
+	str = sprintf('Parameter %d : %s%s',...
+		i,...
+		subsref(get(gco,'UserData'),...
+			struct('type',{'.','{}'},'subs',{'Xnames',{i}})),...
+		tmp{subsref(get(gco,'UserData'),...
+			struct('type',{'.','()'},'subs',{'est',{i}}))+1});
+	catch, str='(no cached data to surf)'; end
+case {'extend','alt','open'}
+	return
+end
+
+set(get(gca,'XLabel'),'String',str)
+
+
+
+otherwise                                        %-Unknown action string
 %=======================================================================
 error('Unknown action string')
 
