1cb3521d7cc3d53754ec43de9ea35ba988f1c4f3
Karl Friston <k.friston@ucl.ac.uk>
1999-03-29 21:33:12 +0000

    upgrade for SPM99

--- spm_Vintrinsic.m
+++ spm_Vintrinsic.m
@@ -1,35 +1,44 @@
-function  [V] = spm_Vintrinsic(n,RT,form,Vpar)
-%--------------------------------------------------------------------------
-% Compute an assumed intrinsic autocorelation function
-% FORMAT [V] = spm_Vintrinsic(nscan,form,Vparam)
-% nscan		- sessions number of scans
-% RT		- Repetition time in seconds
-% form		- '1/f' 
-% Vpar		- Parameters pertaining to the form
+function [V] = spm_Vintrinsic(n,RT,form,Vpar)
+% Computes an assumed intrinsic autocorelation function
+% FORMAT [V] = spm_Vintrinsic(n,form,Vparam)
 %
-% V		- (intrinsic) autoccorrelation matrix
+% n      - sessions number of scans
+% RT	 - Repetition time in seconds
+% form	 - '1/f' 
+% Vpar	 - Parameters pertaining to the form
+%
+% V	 - (intrinsic) autoccorrelation matrix
 %__________________________________________________________________________
+% %W% Jean-Baptiste Poline, Karl Friston %E%
 
-if nargin < 4 error('not enough argument in spm_Vintrinsic'); end;
+%--------------------------------------------------------------------------
+if nargin < 4 error('not enough arguments in spm_Vintrinsic'); end;
 
 
-
+% frequencies
+%--------------------------------------------------------------------------
 if rem(n,2) == 0
- 	f=1/(n*RT):1/(n*RT):1/(2*RT)+1/(n*RT);
+	dF  = 1/(2*n*RT);
 else 
-	f=1/((n-1)*RT):1/((n-1)*RT):1/(2*RT)+1/((n-1)*RT);
+	dF  = 1/(2*(n - 1)*RT);
+end
+F    = [1:n]*dF;
+
+
+% Spectral density assumed
+%--------------------------------------------------------------------------
+switch form
+
+	case('1/f')
+	%------------------------------------------------------------------
+	G   = (Vpar(1)./F + 1).^2;
+
+	otherwise, error('unrecognised type')
 end
 
-
-
-switch form
-  case('1/f')
-   SD	= (Vpar(1)./f + 1).^2;	% Spectral density form
-   SDS= [fliplr(SD(2:length(SD))) SD];
-  otherwise, error('unrecognised type')
-end % switch form
-
-R	= real(ifft(SDS));
-R	= R/max(R); 
-V  	= toeplitz(R(1:n));
+% autocorrelation functional and matrix
+%--------------------------------------------------------------------------
+R    = fftshift(real(ifft(G([1:n n:-1:2]))));
+R    = R/max(R);
+V    = toeplitz(R([1:n] + n - 1));
 
