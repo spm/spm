4732e489b0f3e656320255283a78b690ef56e14a
Andrew Holmes <andrew.holmes@veramed.co.uk>
1999-04-27 14:16:41 +0000

    Grandfathered: Functionality absorbed by spm_list.m

--- spm_maxima.m
+++ spm_maxima.m
@@ -1,72 +1,28 @@
 function spm_maxima(SPM,VOL,hReg)
-% Tabular display of maxima in selected cluster
+% List of local maxima for a single suprathreshold cluster
 % FORMAT spm_maxima(SPM,VOL,hReg)
 %
 % SPM    - structure containing SPM, distribution & filtering details
-%        - required fields are:
-% .swd   - SPM working directory - directory containing current SPM.mat
-% .Z     - minimum of n Statistics {filtered on u and k}
-% .n     - number of conjoint tests        
-% .STAT  - distribution {Z, T, X or F}     
-% .df    - degrees of freedom [df{interest}, df{residual}]
-% .u     - height threshold
-% .k     - extent threshold {resels}
-% .XYZ   - location of voxels {voxel coords}
-% .XYZmm - location of voxels {mm}
+%        - See spm_list('ListCluster',... for list of required fields.
 %
 % VOL    - structure containing details of volume analysed
-%        - required fields are:
-% .S     - search Volume {voxels}
-% .R     - search Volume {resels}
-% .FWHM  - smoothness {voxels}     
-% .M     - voxels - > mm matrix
-% .VOX   - voxel dimensions {mm}
+%        - See spm_list('ListCluster',... for list of required fields.
 %
 % hReg   - handle of MIP XYZ registry object (see spm_XYZreg for details)
 %_______________________________________________________________________
 %
-% spm_maxima is called by the SPM results section, and takes variables
-% in the structures SPM and VOL to produce a table of maxima within the
-% selected region.  The region and its associated maxima are
-% characterized in terms of its cluster, and voxel-level p values
-% (corrected and uncorrected).
+% spm_maxima functionality has been absorbed into spm_list.
 %
-% The maxima displayed are all at least 4mm apart. Selecting the voxel
-% coordinates of a maxima causes a green pointer to appear in the
-% appropriate place on maximum intensity projection.
-%
-% See also: spm_list
-%
+% See spm_list('ListCluster',SPM,VOL)
 %_______________________________________________________________________
 % %W% Karl Friston, Andrew Holmes %E%
 
 
-%-Get voxel nearest current location
+%-Print warning of obsolescence
 %-----------------------------------------------------------------------
-if ~length(SPM.Z)
-	msgbox('No voxels survive masking & threshold(s)!',...
-		sprintf('%s%s: %s...',spm('ver'),...
-		spm('GetUser',' (%s)'),mfilename),'help','modal')
-	return
-end
-[xyzmm,i] = spm_XYZreg('NearestXYZ',spm_XYZreg('GetCoords',hReg),SPM.XYZmm);
+warning('spm_maxima is grandfathered, use spm_list(''ListCluster'',... instead')
 
-%-Find selected cluster
+
+%-Pass arguments on to spm_list
 %-----------------------------------------------------------------------
-A         = spm_clusters(SPM.XYZ);
-j         = find(A == A(i));
-SPM.Z     = SPM.Z(j);
-SPM.XYZ   = SPM.XYZ(:,j);
-SPM.XYZmm = SPM.XYZmm(:,j);
-
-
-%-Update GUI to cluster's maximum
-%-----------------------------------------------------------------------
-[i,j]   = max(SPM.Z);
-spm_XYZreg('SetCoords',SPM.XYZmm(:,j),hReg);
-
-
-%-Tabulate p values
-%-----------------------------------------------------------------------
-str = 'single cluster list (p-values corrected for entire volume)';
-spm_list(SPM,VOL,4,16,str)
+spm_list('ListCluster',SPM,VOL);
