75837fa243879920de9e9a05d4aae29b49d4f0e5
Karl Friston <k.friston@ucl.ac.uk>
1999-05-12 17:53:16 +0000

    F_iX0 elaborated to include trial-specific effects

--- spm_fmri_spm_ui.m
+++ spm_fmri_spm_ui.m
@@ -459,10 +459,39 @@ xX.xVi = xVi;
 
 
 
-%-Effects designated "of interest" - constuct an F-contrast
+%-Effects designated "of interest" - constuct F-contrast structure array
 %-----------------------------------------------------------------------
-F_iX0 = xX.iB;
+F_iX0  = struct(	'iX0',		xX.iB,...
+			'name',		'effects of interest');
 
+%-trial-specifc effects specified by Sess
+%-----------------------------------------------------------------------
+i      = length(F_iX0) + 1;
+if (Sess{1}.rep)
+	for t = 1:length(Sess{1}.name)
+		u     = [];
+		for s = 1:length(Sess)
+			u = [u Sess{s}.col(Sess{s}.ind{t})];
+		end
+		q             = 1:size(xX.X,2);
+		q(u)          = [];
+		F_iX0(i).iX0  = q;
+		F_iX0(i).name = Sess{s}.name{t};
+		i             = i + 1;
+	end
+else
+	for s = 1:length(Sess)
+		str   = sprintf('Session %d: ',s);
+		for t = 1:length(Sess{s}.name)
+			q             = 1:size(xX.X,2);
+			q(Sess{s}.col(Sess{s}.ind{t})) = [];
+			F_iX0(i).iX0  = q;
+			F_iX0(i).name = [str Sess{s}.name{t}];
+			i             = i + 1;
+		end
+	end
+end
+		
 
 %-Design description (an nx2 cellstr) - for saving and display
 %=======================================================================
