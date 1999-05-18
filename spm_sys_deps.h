b97c31a3fc749f106fe9dc341faa5571a0e62842
John Ashburner <j.ashburner@ucl.ac.uk>
1999-05-18 11:19:09 +0000

    indenting of preprocessor commands in spm_sys_deps.h really wrecked my preprocessor - Joe Devlin

--- spm_sys_deps.h
+++ spm_sys_deps.h
@@ -1,19 +1,18 @@
 /* %W% John Ashburner - from Matthew Brett's work %E% */
 
 #ifndef SYS_DEP
-	#define SYS_DEP
-	#ifdef SPM_WIN32
-		#define rint(x) floor((x)+0.5)  /* round to nearest int */
-		#define finite(x) mxIsFinite(x) /* finite */
-		#define SEPCHAR      '\\'          /* directory separator */
-		#include <process.h>
-	#else /* SPM_WIN32 */
-		#define SEPCHAR      '/'           /* directory separator */
-		extern double rint(double);
-	#endif /* SPM_WIN32 */
-	#ifndef MAXNAMLEN
-		/* default (very long) maximum filename length */
-		#define MAXNAMLEN 1024
-	#endif
+#define SYS_DEP
+#ifdef SPM_WIN32
+#define rint(x) floor((x)+0.5)  /* round to nearest int */
+#define finite(x) mxIsFinite(x) /* finite */
+#define SEPCHAR      '\\'          /* directory separator */
+#include <process.h>
+#else /* SPM_WIN32 */
+#define SEPCHAR      '/'           /* directory separator */
+extern double rint(double);
+#endif /* SPM_WIN32 */
+#ifndef MAXNAMLEN
+/* default (very long) maximum filename length */
+#define MAXNAMLEN 1024
+#endif
 #endif /* SYS_DEP */
-
