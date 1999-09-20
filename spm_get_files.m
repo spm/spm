af87601426b868d353b68533e3ac2a663065abac
Jean-Baptiste Poline <jbpoline@gmail.com>
1999-09-20 16:52:07 +0000

    *** empty log message ***

--- spm_get_files.m
+++ spm_get_files.m
@@ -1,8 +1,13 @@
 function Q = spm_get_files(dir,fil)
+% FORMAT Q spm_get_files(dir,fil)
+% dir    - string : the directory name
+% fil    - string : filter, like 'sn*.img'
 %
+% Based on spm_list_files.m
 % used in m-files for batch mode to help the users
-% in entering the file names
-
+% in entering the file names.
+% Not used by any spm routine.
+% %W% %E%
 
 [P,tmp] = spm_list_files(dir,fil);
 for k = 1:size(P,1)
