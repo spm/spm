2c65ed94fd3d015258ae0aeb241ba78e5ca313a3
John Ashburner <j.ashburner@ucl.ac.uk>
1999-10-29 16:08:55 +0000

    updated documentation

--- spm_check_registration.m
+++ spm_check_registration.m
@@ -1,8 +1,12 @@
 function spm_check_registration(images)
 % A visual check of image registration quality.
 % FORMAT spm_check_registration
-% Orthogonal views of two images are displayed.  Clicking in any image
-% moves the centre of the orthogonal views.
+% Orthogonal views of one or more images are displayed.  Clicking in
+% any image moves the centre of the orthogonal views.  Images are
+% shown in orientations relative to that of the first selected image.
+% The first specified image is shown at the top-left, and the last at
+% the bottom right.  The fastest increment is in the left-to-right
+% direction (the same as you are reading this).
 %_______________________________________________________________________
 % %W% John Ashburner %E%
 
