482af0e10ead1c05a9eb468cfd98d1855841032c
Andrew Holmes <andrew.holmes@veramed.co.uk>
1999-10-29 12:36:08 +0000

    Updated for new handling of .X0 & .X1o in xCon. Also tidied the indenting to match spm_getSPM.m

--- spm_bch_DoCont.m
+++ spm_bch_DoCont.m
@@ -10,6 +10,7 @@ function status = spm_bch_DoCont
 %
 %_______________________________________________________________________
 % %W% Jean-Baptiste Poline & Stephanie Rouquette %E%
+% Derived from spm_getSPM.m
 
 %- initialise status
 %-----------------------------------------------------------------------
@@ -80,7 +81,8 @@ dim	= Vbeta(1).dim(1:3);
 for i = 1:length(xCon)
 
     if ~isfield(xCon(i),'eidf') | isempty(xCon(i).eidf)
-	[trMV,trMVMV] = spm_SpUtil('trMV',xCon(i).X1o,xX.V);
+	[trMV,trMVMV] = spm_SpUtil('trMV',...
+				spm_FcUtil('X1o',xCon(i),xX.xKXs),xX.V);
         xCon(i).eidf  = trMV^2/trMVMV;
     else
         trMV = []; trMVMV = [];
@@ -89,123 +91,125 @@ for i = 1:length(xCon)
     %-Write contrast/ESS images?
     %-------------------------------------------------------------------
     if ~isfield(xCon(i),'Vcon') | isempty(xCon(i).Vcon) | ...
-       ~exist(fullfile('.',xCon(i).Vcon),'file')
+        ~exist(fullfile('.',xCon(i).Vcon),'file')
         	
-		switch(xCon(i).STAT)
-	
-		case 'T'       %-Implement contrast as sum of scaled beta images
-		%---------------------------------------------------------------
-	       fprintf('\t%-32s: %-10s%20s',sprintf('contrast image %2d',i),...
-					      '(spm_add)','...initialising') %-#
-	
-		    Q     = find(abs(xCon(i).c) > 0);
-		    V     = Vbeta(Q);
-		    for j = 1:length(Q)
-			V(j).pinfo(1,:) = V(j).pinfo(1,:)*xCon(i).c(Q(j));
-		    end
-		    
-		    %-Prepare handle for contrast image
-		    %-----------------------------------------------------------
-		    xCon(i).Vcon = struct(...
-		       'fname',  sprintf('con_%04d.img',i),...
-		       'dim',    [dim,16],...
-		       'mat',    M,...
-		       'pinfo',  [1,0,0]',...
-		       'descrip',sprintf('SPM contrast - %d: %s',i,xCon(i).name));
-	
-		    %-Write image
-		    %-----------------------------------------------------------
-		    fprintf('%s%20s',sprintf('\b')*ones(1,20),'...computing')%-#
-		    xCon(i).Vcon            = spm_create_image(xCon(i).Vcon);
-		    xCon(i).Vcon.pinfo(1,1) = spm_add(V,xCon(i).Vcon);
-		    xCon(i).Vcon            = spm_create_image(xCon(i).Vcon);
-	
-		    fprintf('%s%30s\n',sprintf('\b')*ones(1,30),...
-				sprintf('...written %s',xCon(i).Vcon.fname)) %-#
-	
-		case 'F'  %-Implement ESS as sum of squared weighted beta images
-		%---------------------------------------------------------------
-		    fprintf('\t%-32s: %30s',sprintf('ESS image %2d',i),...
-							     '...computing') %-#
-	
-	       %-Residual (in parameter space) forming mtx
-		    %-----------------------------------------------------------
-		    h       = spm_FcUtil('Hsqr',xCon(i),xX.xKXs);
-	
-		    %-Prepare handle for ESS image
-		    %-----------------------------------------------------------
-		    xCon(i).Vcon = struct(...
-			'fname',  sprintf('ess_%04d.img',i),...
-			'dim',    [dim,16],...
-			'mat',    M,...
-			'pinfo',  [1,0,0]',...
-			'descrip',sprintf('SPM ESS - contrast %d: %s',i,xCon(i).name));
-	
-		    %-Write image
-		    %-----------------------------------------------------------
-		    fprintf('%s',sprintf('\b')*ones(1,30))                   %-#
-		    xCon(i).Vcon  = spm_create_image(xCon(i).Vcon);
-		    xCon(i).Vcon  = spm_resss(Vbeta,xCon(i).Vcon,h);
-		    xCon(i).Vcon  = spm_create_image(xCon(i).Vcon);
-	
-		otherwise
-		    error(['unknown STAT "',xCon(i).STAT,'"'])
-	
-		end % switch(xCon(i).STAT)
+	switch(xCon(i).STAT)
 
-	end % if ~isfield(xCon(i)
+	case 'T'       %-Implement contrast as sum of scaled beta images
+	%---------------------------------------------------------------
+            fprintf('\t%-32s: %-10s%20s',sprintf('contrast image %2d',i),...
+				      '(spm_add)','...initialising') %-#
 
-   %-Write statistic image(s)
-   %-------------------------------------------------------------------
-   if ~isfield(xCon(i),'Vspm') | isempty(xCon(i).Vspm) | ...
-      ~exist(fullfile('.',xCon(i).Vspm),'file')
+	    Q     = find(abs(xCon(i).c) > 0);
+	    V     = Vbeta(Q);
+	    for j = 1:length(Q)
+		V(j).pinfo(1,:) = V(j).pinfo(1,:)*xCon(i).c(Q(j));
+	    end
+	    
+	    %-Prepare handle for contrast image
+	    %-----------------------------------------------------------
+	    xCon(i).Vcon = struct(...
+	        'fname',  sprintf('con_%04d.img',i),...
+	        'dim',    [dim,16],...
+	        'mat',    M,...
+	        'pinfo',  [1,0,0]',...
+	        'descrip',sprintf('SPM contrast - %d: %s',i,xCon(i).name));
 
-      fprintf('\t%-32s: %30s',sprintf('spm{%c} image %2d',xCon(i).STAT,i),...
+	    %-Write image
+	    %-----------------------------------------------------------
+	    fprintf('%s%20s',sprintf('\b')*ones(1,20),'...computing')%-#
+	    xCon(i).Vcon            = spm_create_image(xCon(i).Vcon);
+	    xCon(i).Vcon.pinfo(1,1) = spm_add(V,xCon(i).Vcon);
+	    xCon(i).Vcon            = spm_create_image(xCon(i).Vcon);
+
+	    fprintf('%s%30s\n',sprintf('\b')*ones(1,30),...
+			sprintf('...written %s',xCon(i).Vcon.fname)) %-#
+
+	case 'F'  %-Implement ESS as sum of squared weighted beta images
+	%---------------------------------------------------------------
+	    fprintf('\t%-32s: %30s',sprintf('ESS image %2d',i),...
+						     '...computing') %-#
+
+            %-Residual (in parameter space) forming mtx
+	    %-----------------------------------------------------------
+	    h       = spm_FcUtil('Hsqr',xCon(i),xX.xKXs);
+
+	    %-Prepare handle for ESS image
+	    %-----------------------------------------------------------
+	    xCon(i).Vcon = struct(...
+		'fname',  sprintf('ess_%04d.img',i),...
+		'dim',    [dim,16],...
+		'mat',    M,...
+		'pinfo',  [1,0,0]',...
+		'descrip',sprintf('SPM ESS - contrast %d: %s',i,xCon(i).name));
+
+	    %-Write image
+	    %-----------------------------------------------------------
+	    fprintf('%s',sprintf('\b')*ones(1,30))                   %-#
+	    xCon(i).Vcon  = spm_create_image(xCon(i).Vcon);
+	    xCon(i).Vcon  = spm_resss(Vbeta,xCon(i).Vcon,h);
+	    xCon(i).Vcon  = spm_create_image(xCon(i).Vcon);
+
+	otherwise
+	    error(['unknown STAT "',xCon(i).STAT,'"'])
+
+	end % switch(xCon(i).STAT)
+
+    end % if ~isfield(xCon(i)
+
+    %-Write statistic image(s)
+    %-------------------------------------------------------------------
+    if ~isfield(xCon(i),'Vspm') | isempty(xCon(i).Vspm) | ...
+        ~exist(fullfile('.',xCon(i).Vspm),'file')
+
+	fprintf('\t%-32s: %30s',sprintf('spm{%c} image %2d',xCon(i).STAT,i),...
                                                     '...computing')  %-#
 
-      switch(xCon(i).STAT)
+	switch(xCon(i).STAT)
 	case 'T'                                  %-Compute SPM{t} image
 	%---------------------------------------------------------------
-	   Z   = spm_sample_vol(xCon(i).Vcon, XYZ(1,:),XYZ(2,:),XYZ(3,:),0)./...
+	Z   = spm_sample_vol(xCon(i).Vcon, XYZ(1,:),XYZ(2,:),XYZ(3,:),0)./...
 		(sqrt(spm_sample_vol(VResMS,  XYZ(1,:),XYZ(2,:),XYZ(3,:),0)*...
-						(xCon(i).c'*xX.Bcov*xCon(i).c) ));
-	   str = sprintf('[%.2g]',xX.erdf);
+					(xCon(i).c'*xX.Bcov*xCon(i).c) ));
+	str = sprintf('[%.2g]',xX.erdf);
 	
 	case 'F'                                  %-Compute SPM{F} image
 	%---------------------------------------------------------------
-	   if isempty(trMV), trMV = spm_SpUtil('trMV',xCon(i).X1o,xX.V); end
-	   Z =(spm_sample_vol(xCon(i).Vcon,XYZ(1,:),XYZ(2,:),XYZ(3,:),0)/trMV)./...
-              (spm_sample_vol(VResMS, XYZ(1,:),XYZ(2,:),XYZ(3,:),0));
+	if isempty(trMV)
+	   	trMV = spm_SpUtil('trMV',spm_FcUtil('X1o',xCon(i),xX.xKXs),xX.V);
+	end
+	Z =(spm_sample_vol(xCon(i).Vcon,XYZ(1,:),XYZ(2,:),XYZ(3,:),0)/trMV)./...
+           (spm_sample_vol(VResMS, XYZ(1,:),XYZ(2,:),XYZ(3,:),0));
 	
-	   str = sprintf('[%.2g,%.2g]',xCon(i).eidf,xX.erdf);
+	str = sprintf('[%.2g,%.2g]',xCon(i).eidf,xX.erdf);
 	
 	otherwise
 	%---------------------------------------------------------------
-           error(['unknown STAT "',xCon(i).STAT,'"'])
+            error(['unknown STAT "',xCon(i).STAT,'"'])
 
-      end %- switch(xCon(i).STAT)
+        end %- switch(xCon(i).STAT)
 
-      %-Write full statistic image
-      %---------------------------------------------------------------
-      fprintf('%s%30s',sprintf('\b')*ones(1,30),'...writing')      %-#
+        %-Write full statistic image
+        %---------------------------------------------------------------
+        fprintf('%s%30s',sprintf('\b')*ones(1,30),'...writing')      %-#
 
-      xCon(i).Vspm = struct(...
-	'fname',  sprintf('spm%c_%04d.img',xCon(i).STAT,i),...
-	'dim',    [dim,16],...
-	'mat',    M,...
-	'pinfo',  [1,0,0]',...
-	'descrip',sprintf('SPM{%c_%s} - contrast %d: %s',...
+        xCon(i).Vspm = struct(...
+	    'fname',  sprintf('spm%c_%04d.img',xCon(i).STAT,i),...
+	    'dim',    [dim,16],...
+	    'mat',    M,...
+	    'pinfo',  [1,0,0]',...
+	    'descrip',sprintf('SPM{%c_%s} - contrast %d: %s',...
 	                       xCon(i).STAT,str,i,xCon(i).name));
 
-      tmp = zeros(dim);
-      tmp(cumprod([1,dim(1:2)])*XYZ - sum(cumprod(dim(1:2)))) = Z;
+	tmp = zeros(dim);
+	tmp(cumprod([1,dim(1:2)])*XYZ - sum(cumprod(dim(1:2)))) = Z;
 
-      xCon(i).Vspm  = spm_write_vol(xCon(i).Vspm,tmp);
+	xCon(i).Vspm  = spm_write_vol(xCon(i).Vspm,tmp);
 
-      fprintf('%s%30s\n',sprintf('\b')*ones(1,30),...
+	fprintf('%s%30s\n',sprintf('\b')*ones(1,30),...
           sprintf('...written %s',xCon(i).Vspm.fname)) %-#
 
-   end %- if ~isfield(xCon(i),'Vspm')
+    end %- if ~isfield(xCon(i),'Vspm')
 
 end %- for i = 1:length(xCon)
 
