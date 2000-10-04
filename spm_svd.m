2902ccb156b57556e5b6b268dc93ec9b228fc916
Karl Friston <k.friston@ucl.ac.uk>
2000-10-04 15:18:59 +0000

    replaced by an efficient svd routine

--- spm_svd.m
+++ spm_svd.m
@@ -1,74 +1,87 @@
-
-% eigenimage display
-% FORMAT spm_svd
-% requires the following to be in working memory
-% u    - eigenimages or spatial modes
-% v    - component scores
-% s    - eigenvalues
-% e    - selected mode
-% XYZ  - voxel locations in mm
-%____________________________________________________________________________
+function [U,S,V] = spm_svd(X,U)
+% computationally efficient SVD (that can handle sparse arguments)
+% FORMAT [U,S,V] = spm_svd(X,u);
+% X    - {m x n} matrix
+% u    - threshold for normalized eigenvalues (default = 1e-6)
 %
-% spm_svd displays the positive and negative parts of an eigenimage
-% as maximum intensity projections.  The eigenvalue spectrum and component
-% scores are also displayed.  spm_svd is essentially a script invoked by
-% spm_svd_ui.m
-%
-%__________________________________________________________________________
+% U    - {m x p} singular vectors
+% V    - {m x p} singular variates
+% S    - {p x p} singular values
+%___________________________________________________________________________
 % %W% Karl Friston %E%
 
 
+% default threshold
 %---------------------------------------------------------------------------
-global CWD
-
-% find and clear Interactive window
-%---------------------------------------------------------------------------
-Fgraph = spm_figure('FindWin','Graphics');
-if isempty(Fgraph), Fgraph=spm_figure('Create','Graphics'); end
-figure(Fgraph), spm_clf(Fgraph)
-
-
-% eigenimages
-%---------------------------------------------------------------------------
-if V(3) == 1						% 2-dimensional data
-	subplot(2,1,1)
-	spm_mip(u(:,e),XYZ,V);
-	title(sprintf('eigenimage %0.0f',e));
-else
-	axes('Position',[0.05 0.5 0.45 0.4])
-	spm_mip(u(:,e),XYZ,V);
-	title(sprintf('eigenimage %0.0f {+ve}',e));
-
-	axes('Position',[0.50 0.5 0.45 0.4])
-	spm_mip(-u(:,e),XYZ,V);
-	title(sprintf('eigenimage %0.0f {-ve}',e));
+if nargin < 2
+	U = 1e-6;
 end
 
-axes('Position',[0 0.5 1 1]); axis off
-text(0.1,0,'Eigenimage analysis:','Fontsize',16,'FontWeight','Bold')
-text(0.42,0,CWD)
-
-
-% eigenvalue spectrum
+% deal with sparse matrices
 %---------------------------------------------------------------------------
-subplot(2,2,3)
-d     = zeros(size(s)); d(e) = s(e);
-[x y] = bar(s);
-fill(x,y,[1 1 1]*0.9); hold on
-[x y] = bar(d);
-fill(x,y,[1 0 0]); hold off
-axis square; set(gca,'Xlim',[0 size(u,2)])
-xlabel eigenimage
-ylabel eigenvalue
-title(sprintf('%0.1f percent of variance',s(e)/length(s)*100))
+[M N] = size(X);
+p     = find(any(X,2));
+q     = find(any(X,1));
+X     = X(p,q);
 
-% component scores
+% SVD
 %---------------------------------------------------------------------------
-subplot(2,2,4)
-[x y] = bar(v(:,e));
-fill(x,y,[1 1 1]*0.9);
-axis square; set(gca,'Xlim',[0 (size(v,1) + 1)])
-xlabel 'observation'
-ylabel 'component score'
+[i j s] = find(X);
+[m n]   = size(X);
+if any(i - j)
 
+	% off-leading diagonal elements - full SVD
+	%-------------------------------------------------------------------
+	X     = full(X);
+	if m > n
 
+		[v S v] = svd(spm_atranspa(X),0);
+		S       = sparse(S);
+		s       = diag(S);
+		s       = s*length(s)/sum(s);
+		j       = find(s >= U);
+		v       = v(:,j);
+		u       = spm_en(X*v);
+		S       = sqrt(S(j,j));
+
+	elseif m < n
+
+		[u S u] = svd(spm_atranspa(X'),0);
+		S       = sparse(S);
+		s       = diag(S);
+		s       = s*length(s)/sum(s);
+		j       = find(s >= U);
+		u       = u(:,j);
+		v       = spm_en(X'*u);
+		S       = sqrt(S(j,j));
+
+	else
+
+		[u S v] = svd(X,0);
+		S       = sparse(S);
+		s       = diag(S).^2;
+		s       = s*length(s)/sum(s);
+		j       = find(s >= U);
+		v       = v(:,j);
+		u       = u(:,j);
+		S       = S(j,j);
+	end
+
+else
+	[i j] = sort(-s);
+	i     = 1:n;
+	S     = sparse(i,i,s(j),M,N);
+	s     = s*length(s)/sum(s);
+	j     = find(s >= U);
+	u     = speye(m,length(j));
+	v     = speye(m,length(j));
+
+end
+
+% replace in full matrices
+%---------------------------------------------------------------------------
+j      = length(j);
+U      = sparse(M,j);
+V      = sparse(N,j);
+U(p,:) = u;
+V(q,:) = v;
