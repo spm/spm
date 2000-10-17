3ae3d0abb7c55426398bd448b7440c126952ceee
Karl Friston <k.friston@ucl.ac.uk>
2000-10-17 18:00:58 +0000

    further upgrades reversion to EM

--- spm_PEB.m
+++ spm_PEB.m
@@ -44,6 +44,7 @@ function [C,P] = spm_PEB(y,P)
 M     = 32;				% maximum number of iterations
 p     = length(P);
 
+
 % If full Bayes - only estimate hyperparameters upto the penultimate level
 %---------------------------------------------------------------------------
 if ~iscell(P{end}.C)
@@ -142,8 +143,10 @@ if ~isfield(P{1},'Q')
 		m     = length(P{i}.C);
 		for j = 1:m
 			h                 = [h; any(diag(P{i}.C{j}))];
-			Q{end + 1}        = sparse(q,q);
-			Q{end}(I{i},I{i}) = P{i}.C{j};
+			[u v s]           = find(P{i}.C{j});
+			u                 = u + I{i}(1) - 1;
+			v                 = v + I{i}(1) - 1;
+			Q{end + 1}        = sparse(u,v,s,q,q);
 		end
 
 		% indices for ith level hyperparameters
@@ -154,67 +157,87 @@ if ~isfield(P{1},'Q')
 	P{1}.h = h;
 	P{1}.K = K;
 
+	% estimation matrix for hyperparameters
+	%-------------------------------------------------------------------
+	m      = length(Q);
+	T      = sparse(m,m);
+	for  i = 1:m
+		for  j = 1:m
+
+			% trace(Q{j}*Q{i}) = 
+			%--------------------------------------------------
+			T(i,j) = sum(sum(Q{j}.*Q{i}));
+		end
+	end
+	P{1}.iT = inv(T'*T)*T';
+	P{1}.XQX = XQX;
+
 end
 Q     = P{1}.Q;
 h     = P{1}.h;
 K     = P{1}.K;
+iT    = P{1}.iT;
+m     = length(Q);
+T     = sparse(m,1);
 H     = h;
 
 % initial estimate of Ce
 %---------------------------------------------------------------------------
-q     = length(Q);
 Ce    = Cb;
-for i = 1:q
+for i = 1:m
 	Ce = Ce + h(i)*Q{i};
 end
 iCe   = inv(Ce);
-u     = sparse(q,1);
-W     = sparse(q,q);
+
+% pre-comutation of XQX
+%---------------------------------------------------------------------------
+for  i = 1:m
+	XQX{i} = XX'*Q{i}*XX;
+enf
+
 
 % Iterative EM estimation
 %---------------------------------------------------------------------------
-for k = 1:M
+for j = 1:M
 
-	% E-step: conditional covariance cov(B|y)
+	% E-step: conditional mean E{B|y} and covariance cov(B|y)
 	%===================================================================
-	iCeX  = iCe*XX;
-	Cby   = inv(XX'*iCeX);
-	R     = iCe - iCeX*Cby*iCeX';
+        iCeX  = iCe*XX;
+        Cby   = inv(XX'*iCeX);
+	B     = Cby*(iCeX'*y);
 
-
-	% M-step: REML estimate of hyperparameters
+	% M-step: ML estimate of hyperparameters
 	%===================================================================
-	r     = R*y;
-	for i = 1:q
-		RQ{i}  = R*Q{i};
-		u(i)   = r'*Q{i}*r;
+
+	% Ce:  <r*r'> + <X*Cov{b|y}*X'> = Cov{e} = <h(1)*Ce{1} + ...>
+	%-------------------------------------------------------------------
+	r     = y - XX*B;
+	for i = 1:m
+
+		% trace((r*r' + XX*Cny*XX')*Q{i}) = 
+		%-----------------------------------------------------------
+		T(i) = r'*Q{i}*r + sum(sum(Cby.*XQX{i}));
+
 	end
-	for i = 1:q
-	for j = 1:q
-		W(i,j) = sum(sum(RQ{i}.*RQ{j}'));
-	end
-	end
-	h     = W\u;
+	h     = iT*T;
 
 	% assemble new estimate
 	%-------------------------------------------------------------------
 	Ce    = Cb;
-	for i = 1:q
+	for i = 1:m
 		Ce = Ce + h(i)*Q{i};
 	end
 	iCe   = inv(Ce);
 
-
 	% Convergence
 	%===================================================================
-	w     = full(sum(H - h)^2);
+	w     = sum((H - h).^2);
 	if iscell(P{end}.C)
-		fprintf('%-30s: %i %30s%e\n','PEB Iteration',k,'...',w);
+		fprintf('%-30s: %i %30s%e\n','PEB Iteration',j,'...',full(w));
 	end
 	if w < 1e-16, break, end
 	H     = h;
 end
-
 
 % place hyperparameters in P{1}
 %---------------------------------------------------------------------------
@@ -225,16 +248,17 @@ P{1}.h     = h;
 B          = Cby*iCeX'*y;
 C{p + 1}.E = B(J{p}) + C{p + 1}.E;
 for i = p:-1:2
-	C{i}.E     = B(J{i - 1}) + P{i}.X*C{i + 1}.E;
+        C{i}.E     = B(J{i - 1}) + P{i}.X*C{i + 1}.E;
 end
 
-% conditional covariances Cov{b|y} and REML esimtates of Ce{i) = Cb{i - 1}
+% conditional covariances Cov{b|y} and ML esimtates of Ce{i) = Cb{i - 1}
 %---------------------------------------------------------------------------
 for i = 1:p
-	C{i + 1}.C = Cby(J{i},J{i});
-	C{i}.M     = Ce(I{i},I{i});
-	C{i}.h     = h(K{i});
+        C{i + 1}.C = Cby(J{i},J{i});
+        C{i}.M     = Ce(I{i},I{i});
+        C{i}.h     = h(K{i});
 end
+
 
 % warning
 %---------------------------------------------------------------------------
