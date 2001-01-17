9a5630bf6511384d5c5601b29034894124cc0554
Karl Friston <k.friston@ucl.ac.uk>
2001-01-17 18:27:07 +0000

    confounds entered into estimation

--- spm_nlsi_GN.m
+++ spm_nlsi_GN.m
@@ -9,9 +9,9 @@ function [Ep,Cp,Ce] = spm_nlsi_GN(M,U,Y,Ce)
 %
 % Model Parameter estimates
 %---------------------------------------------------------------------------
-% Ep    - (p x 1)         	conditional expectation  E{P|y}
-% Cp    - (p x p)        	conditional covariance   Cov{P|y}
-% Ce    - (v x v)        	ML estimate of           Cov{e}
+% Ep  - (p x 1)        	conditional expectation  E{P|y}
+% Cp  - (p x p)        	conditional covariance   Cov{P|y}
+% Ce  - (v x v)        	ML estimate of           Cov{e}
 %
 %___________________________________________________________________________
 % Returns the moments of the posterior p.d.f. of the parameters of a 
@@ -30,81 +30,64 @@ function [Ep,Cp,Ce] = spm_nlsi_GN(M,U,Y,Ce)
 % repsonse variable and confounds if applicable
 %---------------------------------------------------------------------------
 y      = Y.y;
-u      = length(M.pE);
-v      = size(y,1);
+[v u]  = size(y);
 if isfield(Y,'X0')
-	X0  = Y.X0;
-	pX0 = inv(X0'*X0)*X0';
+	X0  = kron(speye(u,u),Y.X0);
 else
-	X0  = 0;
-	pX0 = 0;
+	X0  = [];
 end
 
 % SVD of prior covariances
 %---------------------------------------------------------------------------
-if iscell(M.pC)
-	Q     = sparse(u,u);
-	for i = 1:length(M.pC);
-		if any(diag(M.pC{i}))
-			Q = Q + M.pC{i};
-		end
-	end
-else
-	Q     = M.pC;
-end
-V      = spm_svd(Q,1e-16);
-p      = M.pE(:);
+V      = spm_svd(M.pC,1e-16);
 pE     = V'*M.pE(:);
-if iscell(M.pC)
-	for i = 1:length(M.pC);
-		pC{i} = V'*M.pC{i}*V;
-	end
-else
-	pC    = V'*M.pC*V;
-end
+pC     = V'*M.pC*V;
 
+% treat confounds as fixed effects
+%---------------------------------------------------------------------------
+u      = size(X0,2);
+E0     = sparse(u,1);
+C0     = speye(u,u)*1e8;
+pC     = blkdiag(pC,C0);
 P{1}.C = Ce;
 P{2}.C = pC;
 
-
 % Gauss-Newton search 
 %===========================================================================
-dp     = 1e-4;
+p      = M.pE(:);
+dv     = 1e-4;
 for  j = 1:32
 
-	% f(P(x)) - for new expansion point (x)
+	% f(p) - for new expansion point (p)
 	%-------------------------------------------------------------------
-	fx    = spm_nlsi_int(p,M,U,v);
+	fx     = spm_nlsi_int(p,M,U,v);
 
-	% compute partial derivatives df(x)/dx and Jacobian
+	% compute partial derivatives df(x)/dx {Jacobian}
 	%-------------------------------------------------------------------
-	for i = 1:size(V,2)
-		pi     = p + V(:,i)*dp;
+	for  i = 1:size(V,2)
+		pi     = p + V(:,i)*dv;
 		dfx    = spm_nlsi_int(pi,M,U,v) - fx;
-
-		% remove confounds from Jacobian
-		%-----------------------------------------------------------
-		dfx    = dfx - X0*(pX0*dfx);
-		J(:,i) = dfx(:)/dp;
+		J(:,i) = dfx(:)/dv;
 	end
 
 	% Bayesian estimator of new expansion point
 	%-------------------------------------------------------------------
-	P{1}.X = J;
-	P{2}.X = pE - V'*p;
+	P{1}.X = [J X0];
+	P{2}.X = [(pE - V'*p); E0];
 	[C P]  = spm_PEB(y(:) - fx(:),P);
-	p      = p + V*C{2}.E;
+	dp     = V*C{2}.E(1:size(V,2));
+	p      = p + dp;
 
 	% convergence
 	%-------------------------------------------------------------------
-	e      = full(C{2}.E'*C{2}.E);
-	fprintf('%-30s: %i %20s%e\n','GNS Iteration',j,'...',e);
-	if e < 1e-6, break, end
+	w      = dp'*dp;
+	fprintf('%-30s: %i %20s%e\n','GNS Iteration',j,'...',full(w));
+	if w < 1e-6, break, end
 	
 end
 
 % rotate back into native parameter space
 %---------------------------------------------------------------------------
 Ep     = p;
-Cp     = V*C{2}.C*V';
+Cp     = V*C{2}.C(1:size(V,2),1:size(V,2))*V';
 Ce     = C{1}.M;
