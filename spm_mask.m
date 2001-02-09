714127dce8591130538ec528d6dfffc0349410dd
John Ashburner <j.ashburner@ucl.ac.uk>
2001-02-09 11:05:38 +0000

    It never worked, but hopefully it will now

--- spm_mask.m
+++ spm_mask.m
@@ -48,7 +48,7 @@ m2=prod(size(V2));
 % Create headers
 VO=V2;
 for i=1:m2,
-	[pth,nm,xt,vr] = fileparts(deblank(p));
+	[pth,nm,xt,vr] = fileparts(deblank(VO(i).fname));
 	VO(i).fname    = fullfile(pth,['m' nm xt vr]);
 	VO(i).descrip  = 'Masked';
 	VO(i).mat      = VO(1).mat;
