c6d7ab17986dfac6ce0c432b5d7346ffb6150895
John Ashburner <j.ashburner@ucl.ac.uk>
2001-08-28 11:00:59 +0000

    Added 'diff2' option

--- spm_dctmtx.m
+++ spm_dctmtx.m
@@ -20,22 +20,26 @@ function C = spm_dctmtx(N,K,n,f)
 
 d = 0;
 
-if (nargin == 2)
+if nargin == 1, K = N; end;
+
+if any(nargin == [1 2]),
 	n = (0:(N-1))';
-	if (nargin == 3)
+elseif nargin == 3,
+	if strcmp(n,'diff'),
 		d = 1;
-	end
-elseif (nargin == 3)
-	if (strcmp(n,'diff'))
-		d = 1;
+		n = (0:(N-1))';
+	elseif strcmp(n,'diff2'),
+		d = 2;
 		n = (0:(N-1))';
 	else
 		n = n(:);
 	end
-elseif (nargin == 4)
+elseif nargin == 4,
 	n = n(:);
-	if (strcmp(f,'diff'))
+	if strcmp(f,'diff'),
 		d = 1;
+	elseif strcmp(n,'diff2'),
+		d = 2;
 	else
 		error('Incorrect Usage');
 	end
@@ -45,15 +49,21 @@ end
 
 C = zeros(size(n,1),K);
 
-if (d == 0)
+if d == 0,
 	C(:,1)=ones(size(n,1),1)/sqrt(N);
 	for k=2:K
 		C(:,k) = sqrt(2/N)*cos(pi*(2*n+1)*(k-1)/(2*N));
 	end
-else
+elseif d == 1,
 	for k=2:K
 		C(:,k) = -2^(1/2)*(1/N)^(1/2)*sin(1/2*pi*(2*n*k-2*n+k-1)/N)*pi*(k-1)/N;
 	end
+elseif d == 2,
+	for k=2:K,
+		C(:,k) = -2^(1/2)*(1/N)^(1/2)*cos(1/2*pi*(2*n+1)*(k-1)/N)*pi^2*(k-1)^2/N^2;
+	end;
+else,
+	error('Can''t do this');
 end
 
 
