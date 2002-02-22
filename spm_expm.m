d4c19ec47b099af2bc463387428ee52147364f1c
Karl Friston <k.friston@ucl.ac.uk>
2002-02-22 21:11:10 +0000

    SPM02 upgrade - more accurate tolerance and simplified

--- spm_expm.m
+++ spm_expm.m
@@ -1,9 +1,7 @@
-function [x] = spm_expm(J,x,n)
+function [x] = spm_expm(J,x)
 % approximate matrix exponential using a Taylor expansion
-% FORMAT [y] = spm_expm(J,x,n)
+% FORMAT [y] = spm_expm(J,x)
 % y          = expm(J)*x:
-%
-% (n - optional parameter to deal with oveflow situations)
 %
 % This routine covers and extends expm  functionality  by  using  a
 % comoutationally  expedient  approximation  that can handle sparse
@@ -13,40 +11,19 @@ function [x] = spm_expm(J,x,n)
 % %W% Karl Friston %E%
 
 
-% compute expm(J/n) {expm(J)*x = expm(J/n).....expm(J/n)*x
+% compute y  = expm(J)*x
+%            = (1 + J + J*J/2! + J*J*J/3!  + ...)*x
 %---------------------------------------------------------------------------
-if nargin == 2
-	n = 1;
-end
-J     = sparse(J/n);
+J     = sparse(J);
 x     = sparse(x);
-for i = 1:n
+fx    = J*x;
+j     = 1;
+while norm(fx,1) > 1e-16
 
-	% compute y  = expm(J)*x
-	%            = (1 + J + J*J/2! + J*J*J/3!  + ...)*x
-        %-------------------------------------------------------------------
-	fx    = J*x;
-	for j = 1:1024
+	% acculuate high order terms
+	%-------------------------------------------------------------------
+	j  = j + 1;
+	x  = x + fx;
+	fx = J*fx/j;
 
-		% acculuate high order terms
-		%-----------------------------------------------------------
-                x  = x + fx;
-                fx = J*fx/(j + 1);
-
-		% break if converging
-		%-----------------------------------------------------------
-		m  = max(abs(fx));
-		if m > 1e+8
-			warning('results may be inaccurate')
-		end
-		if m < 1e-8
-			break
-		end
-	end
-
-	% break if converging
-	%-----------------------------------------------------------
-	if j == 1024
-		warning('number of iterations exceeded')
-	end
 end
