df70482b09fcda98cf4be1d8eab1cb45d200bd9f
Karl Friston <k.friston@ucl.ac.uk>
2002-04-19 14:34:32 +0000

    *** empty log message ***

--- spm_expm.m
+++ spm_expm.m
@@ -1,7 +1,9 @@
 function [x] = spm_expm(J,x)
 % approximate matrix exponential using a Taylor expansion
 % FORMAT [y] = spm_expm(J,x)
+% FORMAT [y] = spm_expm(J)
 % y          = expm(J)*x:
+% y          = expm(J);
 %
 % This routine covers and extends expm  functionality  by  using  a
 % comoutationally  expedient  approximation  that can handle sparse
@@ -10,20 +12,24 @@ function [x] = spm_expm(J,x)
 %___________________________________________________________________________
 % %W% Karl Friston %E%
 
+% assume expm(J) is required
+%---------------------------------------------------------------------------
+if nargin < 2
+	x = speye(length(J));
+end
 
-% compute y  = expm(J)*x
-%            = (1 + J + J*J/2! + J*J*J/3!  + ...)*x
+% compute y = expm(J)*x = (1 + J + J*J/2! + J*J*J/3!  + ...)*x
 %---------------------------------------------------------------------------
 J     = sparse(J);
 x     = sparse(x);
 fx    = J*x;
 j     = 1;
+
 while norm(fx,1) > 1e-16
 
-	% acculuate high order terms
+	% accumulate high-order terms until convergence
 	%-------------------------------------------------------------------
 	j  = j + 1;
 	x  = x + fx;
 	fx = J*fx/j;
-
 end
