5be519bd94a5380f59755a254d6ed009429cccd3
John Ashburner <j.ashburner@ucl.ac.uk>
2002-09-12 18:09:23 +0000

    Initial revision.

---
+++ spm_dicom_convert.m
@@ -0,0 +1,440 @@
+function spm_dicom_convert(hdr,opts)
+% Convert DICOM images into something that SPM can use
+% FORMAT spm_dicom_convert(hdr,opts)
+% hdr  - a cell array of DICOM headers from spm_dicom_headers
+% opts - options
+%        'all'      - all DICOM files (default)
+%        'mosaic'   - the mosaic images
+%        'standard' - standard DICOM files
+%        'raw'      - convert raw FIDs (not implemented)
+%
+% Converted files are written to the current directory
+%_______________________________________________________________________
+% %W% John Ashburner %E%
+if nargin<2, opts = 'all'; end;
+
+[images,guff]     = select_tomographic_images(hdr);
+[mosaic,standard] = select_mosaic_images(images);
+
+if (strcmp(opts,'all') | strcmp(opts,'mosaic')) & ~isempty(mosaic),
+	convert_mosaic(mosaic);
+end;
+if (strcmp(opts,'all') | strcmp(opts,'standard')) & ~isempty(standard),
+	convert_standard(standard);
+end;
+return;
+%_______________________________________________________________________
+
+%_______________________________________________________________________
+function convert_raw(hdr)
+disp('*** Ignoring Raw Data DICOM Files ***');
+return;
+%_______________________________________________________________________
+
+%_______________________________________________________________________
+function convert_mosaic(hdr)
+spm_progress_bar('Init',length(hdr),['Writing Mosaic'], 'Files written');
+
+for i=1:length(hdr),
+
+	% Output filename
+	%-------------------------------------------------------------------
+	fname = sprintf('f%s-%.4d-%.5d-%.6d.img', strip_unwanted(hdr{i}.StudyID),...
+		hdr{i}.SeriesNumber, hdr{i}.AcquisitionNumber, hdr{i}.InstanceNumber);
+
+
+	% Image dimensions and data
+	%-------------------------------------------------------------------
+	nc = hdr{i}.Columns;
+	nr = hdr{i}.Rows;
+	np = read_AcquisitionMatrixText(hdr{i});
+
+	if rem(nc, np(1)) | rem(nr, np(2)),
+		warning(sprintf('%s: %dx%d wont fit into %dx%d.',hdr{i}.Filename,...
+			np(1), np(2), nc,nr));
+		return;
+	end;
+	dim    = [np read_NumberOfImagesInMosaic(hdr{i})];
+	mosaic = read_image_data(hdr{i});
+	volume = zeros(dim);
+	for j=1:dim(3),
+		img = mosaic((1:np(1))+np(1)*rem(j-1,nc/np(1)), (np(2):-1:1)+np(2)*floor((j-1)/(nc/np(1))));
+		if ~any(img(:)),
+			volume = volume(:,:,1:(j-1));
+			break;
+		end;
+		volume(:,:,j) = img;
+	end;
+	dim = [size(volume) spm_type('int16')];
+
+	% Orientation information
+	%-------------------------------------------------------------------
+	% Axial Analyze voxel co-ordinate system:
+	% x increases     right to left
+	% y increases posterior to anterior
+	% z increases  inferior to superior
+
+	% DICOM patient co-ordinate system:
+	% x increases     right to left
+	% y increases  anterior to posterior
+	% z increases  inferior to superior
+
+	% T&T co-ordinate system:
+	% x increases      left to right
+	% y increases posterior to anterior
+	% z increases  inferior to superior
+
+	analyze_to_dicom = [diag([1 -1 1]) [0 (dim(2)+1) 0]'; 0 0 0 1];
+
+	vox    = [hdr{i}.PixelSpacing hdr{i}.SpacingBetweenSlices];
+	pos    = hdr{i}.ImagePositionPatient';
+	orient = reshape(hdr{i}.ImageOrientationPatient,[3 2]);
+	orient(:,3) = null(orient');
+	if det(orient)<0, orient(:,3) = -orient(:,3); end;
+
+	% The image position vector is not correct. In dicom this vector points to
+	% the upper left corner of the image. Perhaps it is unlucky that this is
+	% calculated in the syngo software from the vector pointing to the center of
+	% the slice (keep in mind: upper left slice) with the enlarged FoV.
+	dicom_to_patient = [diag(vox)*orient pos ; 0 0 0 1];
+	truepos          = dicom_to_patient *[(size(mosaic)-dim(1:2))/2 1 1]';
+	dicom_to_patient = [diag(vox)*orient truepos(1:3) ; 0 0 0 1];
+	patient_to_tal   = diag([-1 -1 1 1]);
+	mat              = patient_to_tal*dicom_to_patient*analyze_to_dicom;
+
+
+
+	% Maybe flip the image depending on SliceNormalVector from 0029,1010
+	%-------------------------------------------------------------------
+	SliceNormalVector = read_SliceNormalVector(hdr{i});
+	if det([reshape(hdr{i}.ImageOrientationPatient,[3 2]) SliceNormalVector(:)])<0;
+		volume = volume(:,:,end:-1:1);
+		mat    = mat*[eye(3) [0 0 -(dim(3)-1)]'; 0 0 0 1];
+	end;
+
+
+	% Possibly useful information
+	%-------------------------------------------------------------------
+	tim = datevec(hdr{i}.AcquisitionTime/(24*60*60));
+	descrip = sprintf('%gT %s %s TR=%gms/TE=%gms/FA=%gdeg %s %d:%d:%.5g Mosaic',...
+		hdr{i}.MagneticFieldStrength, hdr{i}.MRAcquisitionType,...
+		deblank(hdr{i}.ScanningSequence),...
+		hdr{i}.RepetitionTime,hdr{i}.EchoTime,hdr{i}.FlipAngle,...
+		datestr(hdr{i}.AcquisitionDate),tim(4),tim(5),tim(6));
+
+	% descrip = [deblank(descrip) '   ' hdr{i}.PatientsName];
+
+	V = struct('fname',fname,'dim',dim,'mat',mat,'descrip',descrip);
+	V = spm_write_vol(V,volume);
+	spm_progress_bar('Set',i);
+end;
+spm_progress_bar('Clear');
+return;
+%_______________________________________________________________________
+
+%_______________________________________________________________________
+function convert_standard(hdr)
+hdr = sort_into_volumes(hdr);
+for i=1:length(hdr),
+	write_volume(hdr{i});
+end;
+return;
+%_______________________________________________________________________
+
+%_______________________________________________________________________
+function vol = sort_into_volumes(hdr)
+
+vol{1}{1} = hdr{1};
+for i=2:length(hdr),
+	orient = reshape(hdr{i}.ImageOrientationPatient,[3 2]);
+	xy1    = hdr{i}.ImagePositionPatient*orient;
+	match  = 0;
+	for j=1:length(vol),
+		orient = reshape(vol{j}{1}.ImageOrientationPatient,[3 2]);
+		xy2    = vol{j}{1}.ImagePositionPatient*orient;
+		dist2  = sum((xy1-xy2).^2);
+		if        hdr{i}.SeriesNumber            == vol{j}{1}.SeriesNumber &...
+		          hdr{i}.AcquisitionNumber       == vol{j}{1}.AcquisitionNumber &...
+		   strcmp(hdr{i}.SeriesInstanceUID,         vol{j}{1}.SeriesInstanceUID) &...
+		          hdr{i}.Rows                    == vol{j}{1}.Rows &...
+		          hdr{i}.Columns                 == vol{j}{1}.Columns &...
+		      all(hdr{i}.ImageOrientationPatient == vol{j}{1}.ImageOrientationPatient) &...
+		      all(hdr{i}.PixelSpacing            == vol{j}{1}.PixelSpacing & dist2<0.00001) &...
+	        (~isfield(hdr{i},'EchoNumber')   | ~isfield(vol{j}{1},'EchoNumber') | ...
+		          hdr{i}.EchoNumber              == vol{j}{1}.EchoNumber),
+			vol{j}{end+1} = hdr{i};
+			match = 1;
+			break;
+		end;
+	end;
+	if ~match,
+		vol{end+1}{1} = hdr{i};
+	end;
+end;
+
+for j=1:length(vol),
+	orient = reshape(hdr{j}.ImageOrientationPatient,[3 2]);
+	proj   = null(orient');
+	if det([orient proj])<0, proj = -proj; end;
+
+	z         = zeros(length(vol{j}),1);
+	for i=1:length(vol{j}),
+		z(i)  = vol{j}{i}.ImagePositionPatient*proj;
+	end;
+	[z,index] = sort(z);
+	vol{j}    = vol{j}(index);
+	if length(vol{j})>1,
+		dist      = diff(z);
+		if any(diff(z)==0),
+			warning('Looks like there is something wrong with the conversion software.');
+		end;
+		if sum((dist-mean(dist)).^2)/length(dist)>0.001,
+			warning('Variable slice spacing');
+		end;
+	end;
+end;
+return;
+%_______________________________________________________________________
+
+%_______________________________________________________________________
+function write_volume(hdr)
+
+% Output filename
+%-------------------------------------------------------------------
+fname = sprintf('s%s-%.4d-%.5d-%.6d.img', strip_unwanted(hdr{1}.StudyID),...
+	hdr{1}.SeriesNumber, hdr{1}.AcquisitionNumber, hdr{1}.InstanceNumber);
+
+% Image dimensions
+%-------------------------------------------------------------------
+nc = hdr{1}.Columns;
+nr = hdr{1}.Rows;
+
+dim    = [nc nr length(hdr) spm_type('int16')];
+
+% Orientation information
+%-------------------------------------------------------------------
+% Axial Analyze voxel co-ordinate system:
+% x increases     right to left
+% y increases posterior to anterior
+% z increases  inferior to superior
+
+% DICOM patient co-ordinate system:
+% x increases     right to left
+% y increases  anterior to posterior
+% z increases  inferior to superior
+
+% T&T co-ordinate system:
+% x increases      left to right
+% y increases posterior to anterior
+% z increases  inferior to superior
+
+analyze_to_dicom = [diag([1 -1 1]) [0 (dim(2)+1) 0]'; 0 0 0 1];
+orient           = reshape(hdr{1}.ImageOrientationPatient,[3 2]);
+orient(:,3)      = null(orient');
+if det(orient)<0, orient(:,3) = -orient(:,3); end;
+if length(hdr)>1,
+	z            = zeros(length(hdr),1);
+	for i=1:length(hdr),
+		z(i)     = hdr{i}.ImagePositionPatient*orient(:,3);
+	end;
+	z            = mean(diff(z));
+else,
+	if checkfields(hdr(1),'SliceThickness'),
+		z = hdr{1}.SliceThickness;
+	else,
+		z = 1;
+	end;
+end;
+vox              = [hdr{1}.PixelSpacing z];
+pos              = hdr{1}.ImagePositionPatient';
+dicom_to_patient = [orient*diag(vox) pos ; 0 0 0 1];
+patient_to_tal   = diag([-1 -1 1 1]);
+mat              = patient_to_tal*dicom_to_patient*analyze_to_dicom;
+
+% Possibly useful information
+%-------------------------------------------------------------------
+if checkfields(hdr{1},'AcquisitionTime','MagneticFieldStrength','MRAcquisitionType',...
+	'ScanningSequence','RepetitionTime','EchoTime','FlipAngle',...
+	'AcquisitionDate'),
+	tim = datevec(hdr{1}.AcquisitionTime/(24*60*60));
+	descrip = sprintf('%gT %s %s TR=%gms/TE=%gms/FA=%gdeg %s %d:%d:%.5g',...
+		hdr{1}.MagneticFieldStrength, hdr{1}.MRAcquisitionType,...
+		deblank(hdr{1}.ScanningSequence),...
+		hdr{1}.RepetitionTime,hdr{1}.EchoTime,hdr{1}.FlipAngle,...
+		datestr(hdr{1}.AcquisitionDate),tim(4),tim(5),tim(6));
+else,
+	descrip = hdr{1}.Modality;
+end;
+
+% Write the image volume
+%-------------------------------------------------------------------
+spm_progress_bar('Init',length(hdr),['Writing ' fname], 'Planes written');
+V = struct('fname',fname, 'dim',dim, 'pinfo',[1 0 0]', 'mat',mat, 'descrip',descrip);
+V = spm_create_vol(V);
+for i=1:length(hdr),
+	plane = read_image_data(hdr{i});
+	plane = fliplr(plane);
+	V     = spm_write_plane(V,plane,i);
+	spm_progress_bar('Set',i);
+end;
+V = spm_close_vol(V);
+spm_progress_bar('Clear');
+return;
+%_______________________________________________________________________
+
+%_______________________________________________________________________
+function [images,guff] = select_tomographic_images(hdr)
+images = {};
+guff   = {};
+for i=1:length(hdr),
+	if ~checkfields(hdr{i},'Modality') | ~(strcmp(hdr{i}.Modality,'MR') |...
+		strcmp(hdr{i}.Modality,'PT') | strcmp(hdr{i}.Modality,'CT'))
+		disp(['Cant find appropriate modality information for "' hdr{i}.Filename '".']);
+		guff = {guff{:},hdr{i}};
+	elseif ~checkfields(hdr{i},'StartOfPixelData','SamplesperPixel',...
+		'Rows','Columns','BitsAllocated','BitsStored','HighBit','PixelRepresentation'),
+		disp(['Cant find "Image Pixel" information for "' hdr{i}.Filename '".']);
+		guff = {guff{:},hdr{i}};
+	elseif ~checkfields(hdr{i},'PixelSpacing','ImagePositionPatient','ImageOrientationPatient'),
+		disp(['Cant find "Image Plane" information for "' hdr{i}.Filename '".']);
+		guff = {guff{:},hdr{i}};
+	elseif ~checkfields(hdr{i},'StudyID','SeriesNumber','AcquisitionNumber','InstanceNumber'),
+		disp(['Cant find suitable filename info for "' hdr{i}.Filename '".']);
+		guff = {guff{:},hdr{i}};
+	else,
+		images = {images{:},hdr{i}};
+	end;
+end;
+return;
+%_______________________________________________________________________
+
+function [mosaic,standard] = select_mosaic_images(hdr)
+mosaic   = {};
+standard = {};
+for i=1:length(hdr),
+	if ~checkfields(hdr{i},'ImageType','CSAImageHeaderInfo') |...
+		~strcmp(deblank(hdr{i}.ImageType),'ORIGINAL\PRIMARY\M\MOSAIC'),
+		standard = {standard{:},hdr{i}};
+	else,
+		mosaic = {mosaic{:},hdr{i}};
+	end;
+end;
+return;
+%_______________________________________________________________________
+function ok = checkfields(hdr,varargin)
+ok = 1;
+for i=1:(nargin-1),
+	if ~isfield(hdr,varargin{i}),
+		ok = 0;
+		break;
+	end;
+end;
+return;
+%_______________________________________________________________________
+
+%_______________________________________________________________________
+function clean = strip_unwanted(dirty)
+msk = find((dirty>='a'&dirty<='z') | (dirty>='A'&dirty<='Z') |...
+           (dirty>='0'&dirty<='9') | dirty=='_');
+clean = dirty(msk);
+return;
+%_______________________________________________________________________
+
+%_______________________________________________________________________
+function img = read_image_data(hdr)
+img = [];
+if hdr.SamplesperPixel ~= 1,
+	warning([hdr.Filename ': SamplesperPixel = ' num2str(hdr.SamplesperPixel) ' - cant be an MRI']);
+	return;
+end;
+
+prec = ['ubit' num2str(hdr.BitsAllocated) '=>' 'uint32'];
+
+fp = fopen(hdr.Filename,'r','ieee-le');
+if fp==-1,
+	warning([hdr.Filename ': cant open file']);
+	return;
+end;
+
+fseek(fp,hdr.StartOfPixelData,'bof');
+img = fread(fp,hdr.Rows*hdr.Columns,prec);
+fclose(fp);
+if length(img)~=hdr.Rows*hdr.Columns,
+	error([hdr.Filename ': cant read whole image']);
+end;
+
+img = bitshift(img,hdr.BitsStored-hdr.HighBit-1);
+
+if hdr.PixelRepresentation,
+	% Signed data - done this way because bitshift only
+	% works with signed data.  Negative values are stored
+	% as 2s complement.
+	neg      = logical(bitand(img,uint32(2^hdr.HighBit)));
+	msk      = (2^hdr.HighBit - 1);
+	img      = double(bitand(img,msk));
+	img(neg) = img(neg)-2^(hdr.HighBit);
+else,
+	% Unsigned data
+	msk      = (2^(hdr.HighBit+1) - 1);
+	img      = double(bitand(img,msk));
+end;
+
+img = reshape(img,hdr.Columns,hdr.Rows);
+return;
+%_______________________________________________________________________
+
+%_______________________________________________________________________
+function nrm = read_SliceNormalVector(hdr)
+val = get_numaris4_val(hdr.CSAImageHeaderInfo,'SliceNormalVector');
+for i=1:3,
+	nrm(i,1) = sscanf(val(i,:),'%g');
+end;
+return;
+%_______________________________________________________________________
+
+%_______________________________________________________________________
+function n = read_NumberOfImagesInMosaic(hdr)
+str = hdr.CSAImageHeaderInfo;
+val = get_numaris4_val(hdr.CSAImageHeaderInfo,'NumberOfImagesInMosaic');
+n   = sscanf(val,'%d');
+return;
+%_______________________________________________________________________
+
+%_______________________________________________________________________
+function dim = read_AcquisitionMatrixText(hdr)
+str = hdr.CSAImageHeaderInfo;
+val = get_numaris4_val(hdr.CSAImageHeaderInfo,'AcquisitionMatrixText');
+dim = sscanf(val,'%d*%d')';
+return;
+%_______________________________________________________________________
+
+%_______________________________________________________________________
+function val = get_numaris4_val(str,name)
+name = deblank(name);
+val  = {};
+for i=1:length(str),
+	if strcmp(deblank(str(i).name),name),
+		for j=1:str(i).nitems,
+			val{j} = str(i).item(j).val;
+		end;
+		break;
+	end;
+end;
+val = str2mat(val{:});
+return;
+%_______________________________________________________________________
+
+%_______________________________________________________________________
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ spm_dicom_headers.m
@@ -0,0 +1,293 @@
+function hdr = spm_dicom_headers(P)
+% Read header information from DICOM files
+% FORMAT hdr = spm_dicom_headers(P)
+% P   - array of filenames
+% hdr - cell array of headers, one element for each file.
+%
+% Contents of headers are explained in:
+% http://medical.nema.org/dicom/2001.html
+%
+%_______________________________________________________________________
+% %W% John Ashburner %E%
+
+dict = readdict;
+j    = 0;
+hdr  = {};
+spm_progress_bar('Init',size(P,1),'Reading DICOM headers','Files complete');
+for i=1:size(P,1),
+	tmp = readdicomfile(P(i,:),dict);
+	if ~isempty(tmp),
+		j      = j + 1;
+		hdr{j} = tmp;
+	end;
+	spm_progress_bar('Set',i);
+end;
+spm_progress_bar('Clear');
+return;
+
+function ret = readdicomfile(P,dict)
+ret = [];
+P   = deblank(P);
+fp  = fopen(P,'r','ieee-le');
+if fp==-1, warning(['Cant open "' P '".']); return; end;
+
+fseek(fp,128,'bof');
+dcm = char(fread(fp,4,'char'))';
+if ~strcmp(dcm,'DICM'),
+	fclose(fp);
+	warning(['"' P '" is not a DICOM file.']);
+	return;
+end;
+ret = read_dicom(fp, 'e',dict);
+fclose(fp);
+return;
+
+
+function ret = read_dicom(fp, flg, dict,lim)
+if nargin<4, lim=Inf; end;
+len = 0;
+ret = struct('Filename',fopen(fp));
+tag = read_tag(fp,flg,dict);
+while ~isempty(tag),
+	if tag.length>0,
+		switch tag.name,
+		case {'GroupLength'},
+			% Ignore it
+			fseek(fp,tag.length,'cof');
+		case {'PixelData'},
+			ret.StartOfPixelData = ftell(fp);
+			ret.SizeOfPixelData  = tag.length;
+			fseek(fp,tag.length,'cof');
+		case {'CSAData'}, % raw data
+			ret.StartOfCSAData = ftell(fp);
+			ret.SizeOfCSAData = tag.length;
+			fseek(fp,tag.length,'cof');
+		case {'CSAImageHeaderInfo','CSASeriesHeaderInfo'},
+			dat  = read_numaris4_stuff(fp,tag.length);
+			ret  = setfield(ret,tag.name,dat);
+		case {'TransferSyntaxUID'},
+			dat = char(fread(fp,tag.length,'char'))';
+			dat = deblank(dat);
+			ret  = setfield(ret,tag.name,dat);
+			switch dat,
+			case {'1.2.840.10008.1.2'},      % Implicit VR Little Endian
+				flg = 'i';
+			case {'1.2.840.10008.1.2.1'},    % Explicit VR Little Endian
+				flg = 'e';
+			case {'1.2.840.10008.1.2.1.99'}, % Deflated Explicit VR Little Endian
+				warning(['Cant read Deflated Explicit VR Little Endian file "' fopen(fp) '".']);
+				return;
+			case {'1.2.840.10008.1.2.2'},    % Explicit VR Big Endian
+				warning(['Cant read Explicit VR Big Endian file "' fopen(fp) '".']);
+				return;
+			otherwise,
+				warning(['Unknown Transfer Syntax UID for "' fopen(fp) '".']);
+				return;
+			end;
+		otherwise,
+			switch tag.vr,
+			case {'UN'},
+				% Unknown - read as char
+				dat = fread(fp,tag.length,'char')';
+			case {'AE', 'AS', 'CS', 'DA', 'DS', 'DT', 'IS', 'LO', 'LT',...
+				  'PN', 'SH', 'ST', 'TM', 'UI', 'UT'},
+				% Character strings
+				dat = fread(fp,tag.length,'char')';
+				if any(dat<0 | dat>127),
+					disp(dat);
+					disp(tag.vr);
+					disp(tag.name);
+				end;
+				dat = char(dat);
+
+				switch tag.vr,
+				case {'UI','ST'},
+					dat = deblank(dat);
+				case {'DS'},
+					dat = strread(dat,'%f','delimiter','\\')';
+				case {'IS'},
+					dat = strread(dat,'%d','delimiter','\\')';
+				case {'DA'},
+					[y,m,d] = strread(dat,'%4d%2d%2d');
+					dat     = datenum(y,m,d);
+				case {'TM'},
+					if any(dat==':'),
+						[h,m,s] = strread(dat,'%d:%d:%f');
+					else,
+						[h,m,s] = strread(dat,'%2d%2d%f');
+					end;
+					dat = s+60*(m+60*h);
+				otherwise,
+				end;
+			case {'OB'},
+				% dont know if this should be signed or unsigned
+				dat = fread(fp,tag.length,'char')';
+			case {'US', 'AT', 'OW'},
+				dat = fread(fp,tag.length/2,'uint16')';
+			case {'SS'},
+				dat = fread(fp,tag.length/2,'int16')';
+			case {'UL'},
+				dat = fread(fp,tag.length/4,'uint32')';
+			case {'SL'},
+				dat = fread(fp,tag.length/4,'int32')';
+			case {'FL'},
+				dat = fread(fp,tag.length/4,'float')';
+			case {'FD'},
+				dat = fread(fp,tag.length/8,'double')';
+			case {'SQ'},
+				dat = read_sq(fp, flg,dict,tag.length);
+			otherwise,
+				dat = '';
+				fseek(fp,tag.length,'cof');
+				warning(['Unknown VR "' tag.vr '" in "' fopen(fp) '".']);
+			end;
+			ret  = setfield(ret,tag.name,dat);
+		end;
+	end;
+	len = len + tag.le + tag.length;
+	if len>=lim, break; end;
+	tag = read_tag(fp,flg,dict);
+end;
+return;
+
+
+function ret = read_sq(fp, flg, dict,lim);
+ret = {};
+n   = 0;
+len = 0;
+while len<lim,
+	tag.group   = fread(fp,1,'ushort');
+	tag.element = fread(fp,1,'ushort');
+	tag.length  = fread(fp,1,'uint');
+	if tag.length==13, tag.length=10; end;
+	len         = len + 8 + tag.length;
+	if (tag.group == 65534) & (tag.element == 57344),
+		Item   = read_dicom(fp, flg, dict, tag.length);
+		Item   = rmfield(Item,'Filename');
+		n      = n + 1;
+		ret{n} = Item;
+	elseif (tag.group == 65534) & (tag.element == 57565),
+		break;
+	else,
+		error([num2str(tag.group) '/' num2str(tag.element) ' unexpected.']);
+	end;
+end;
+return;
+
+function tag = read_tag(fp,flg,dict)
+tag.group   = fread(fp,1,'ushort');
+tag.element = fread(fp,1,'ushort');
+if isempty(tag.element), tag=[]; return; end;
+if tag.group == 2, flg = 'e'; end;
+t           = dict.tags(tag.group+1,tag.element+1);
+if t>0,
+	tag.name = dict.values(t).name;
+	tag.vr   = dict.values(t).vr{1};
+else,
+	tag.name = sprintf('Private_%.4x_%.4x',tag.group,tag.element);
+	tag.vr   = 'UN';
+end;
+
+if flg=='e',
+	tag.vr      = char(fread(fp,2,'char'))';
+	tag.le      = 6;
+	switch tag.vr,
+	case {'OB','OW','SQ','UN','UT'}
+		res        = fread(fp,1,'ushort');
+		tag.length = double(fread(fp,1,'uint'));
+		% should check for length of FFFFFFFFH
+		tag.le     = tag.le + 6;
+	otherwise,
+		tag.length = double(fread(fp,1,'ushort'));
+		tag.le     = tag.le + 2;
+	end;
+else,
+	tag.le =  8;
+	tag.length = double(fread(fp,1,'uint'));
+	% should check for length of FFFFFFFFH
+end;
+if isempty(tag.vr) | isempty(tag.length)
+	tag = [];
+	return;
+end;
+if tag.length==13,
+	% disp(['Whichever manufacturer created "' fopen(fp) '" is taking the p***!']);
+	% For some bizarre reason, known only to themselves, they confuse lengths of
+	% 13 with lengths of 10.
+	tag.length = 10;
+end;
+if rem(tag.length,2),
+	warning(['Unknown odd numbered Value Length (' num2str(tag.length) ') in "' fopen(fp) '".']);
+	tag = [];
+end;
+return;
+
+function dict = readdict
+file = textread('spm_dicom_dict.txt','%s','delimiter','\n','whitespace','');
+clear values
+for i=1:length(file),
+	words = strread(file{i},'%s','delimiter','\t');
+	if length(words)>=5,
+		grp = sscanf(words{1},'%x');
+		ele = sscanf(words{2},'%x');
+		if ~isempty(grp) & ~isempty(ele),
+			group(i)   = grp;
+			element(i) = ele;
+			vr         = {};
+			for j=1:length(words{4})/2,
+				vr{j}  = words{4}(2*(j-1)+1:2*(j-1)+2);
+			end;
+			name       = words{3};
+			msk        = find(~(name>='a' & name<='z') & ~(name>='A' & name<='Z') &...
+			                  ~(name>='0' & name<='9') & ~(name=='_'));
+			name(msk)  = '';
+			values(i)  = struct('name',name,'vr',{vr},'vm',words{5});
+		end;
+	end;
+end;
+
+tags = sparse(group+1,element+1,1:length(group));
+dict = struct('values',values,'tags',tags);
+return;
+
+function t = read_numaris4_stuff(fp,lim)
+% Decode shadow information (0029,1010) and (0029,1020)
+n   = fread(fp,1,'uint32')';
+if n>128 | n < 0,
+	fseek(fp,lim-4,'cof');
+	t = struct('junk','Don''t know how to read this damned file format');
+	return;
+end;
+xx  = fread(fp,1,'uint32')'; % "M" or 77 for some reason
+tot = 2*4;
+for i=1:n,
+	t(i).name    = fread(fp,64,'char')';
+	msk          = find(~t(i).name);
+	if ~isempty(msk),
+		t(i).name    = char(t(i).name(1:msk(1)));
+	else,
+		t(i).name    = char(t(i).name);
+	end;
+	t(i).vm      = fread(fp,1,'int32')';
+	t(i).vr      = fread(fp,4,'char')';
+	t(i).vr      = char(t(i).vr(1:3));
+	t(i).syngodt = fread(fp,1,'int32')';
+	t(i).nitems  = fread(fp,1,'int32')';
+	t(i).xx      = fread(fp,1,'int32')'; % 77 or 205
+	tot          = tot + 64+4+4+4+4+4;
+	for j=1:t(i).nitems
+		% This bit is just wierd
+		t(i).item(j).xx  = fread(fp,4,'int32')'; % [x x 77 x]
+		len              = t(i).item(j).xx(1)-t(1).nitems;
+		if len<0 | len+tot+4*4>lim,
+			t(i).item(j).val = '';
+			tot              = tot + 4*4;
+			break;
+		end;
+		t(i).item(j).val = char(fread(fp,len,'char'))';
+		fread(fp,4-rem(len,4),'char')';
+		tot              = tot + 4*4+len+(4-rem(len,4));
+	end;
+end;
+fseek(fp,lim-tot,'cof');
+return;
