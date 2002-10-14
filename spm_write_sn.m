fedcc0ecb62ea4cfe09ae07e47ee054e71d99297
John Ashburner <j.ashburner@ucl.ac.uk>
2002-10-14 14:54:19 +0000

    Fixed possible problems when working with size(T).  size(T,3) was not guarunteed to exist

--- spm_write_sn.m
+++ spm_write_sn.m
@@ -338,7 +338,7 @@ return;
 
 %_______________________________________________________________________
 function T2 = get_2Dtrans(T3,B,j)
-d   = size(T3);
+d   = [size(T3) 1 1 1];
 tmp = reshape(T3,d(1)*d(2),d(3));
 T2  = reshape(tmp*B(j,:)',d(1),d(2));
 return;
