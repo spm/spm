f001361cfee60571d97a31407d853da7e1b5d66b
Karl Friston <k.friston@ucl.ac.uk>
2003-02-18 22:30:59 +0000

    simplification to cope with static models

--- spm_nlsi_GN.m
+++ spm_nlsi_GN.m
@@ -4,8 +4,8 @@ function [ p,Cp,Ce] = spm_nlsi_GN(M,U,Y)
 %
 % Dynamical MIMO models
 %___________________________________________________________________________
-% M.fx - f(x,u,P)
-% M.lx - l(x,P)
+% M.f  - f(x,u,P)
+% M.g  - g(x,u,P)
 %	x - states variables
 %	u - inputs or causes
 %	P - free parameters
@@ -21,15 +21,6 @@ function [ p,Cp,Ce] = spm_nlsi_GN(M,U,Y)
 % Y.dt - sampling interval for outputs
 % Y.Ce - error covariance contraints
 %
-% Static nonlinear models
-%___________________________________________________________________________
-% M.fp - f(P,u)
-%	u - inputs or causes (c.f. design matrix)
-%	P - free parameters
-% M.pE - prior expectation  - E{P}
-% M.pC - prior covariance   - Cov{P}
-% U    - u 
-% Y    - output structure
 %
 % Parameter estimates
 %---------------------------------------------------------------------------
@@ -42,19 +33,22 @@ function [ p,Cp,Ce] = spm_nlsi_GN(M,U,Y)
 % dynamical MIMO input-state-ouput model under Gaussian assumptions
 %
 %  		 	dx/dt = f(x,u,P)
-%   		 	y(t)  = l(x(t),P) + e
+%   		 	y     = g(x,u,P) + e
 %
-% if inputs are specified, otherwise static a nonlinear observation 
-% model with fixed input or causes u
+% A static nonlinear observation model with fixed input or causes u
+% obtains x = []. i.e.
 %
-%                       y(t)  = f(P,u) + e
+%   		 	y     = g(u,P) + e
 %
-% Priors on the free parameters P specified in terms of expectation pE
+% Priors on the free parameters P are specified in terms of expectation pE
 % and covariance pC. The estimation uses a Gauss-Newton method with MAP
 % point estimators at each iteration.  The covariance of e is a maximum
-% likelihood estimator based on the residuals (c.f. the ReML algorithm).
+% likelihood estimator based on the residuals (ReML estimators).
 % This corresponds to a Gauss-Newton ascent on the conditional probabilty
 % p{P|y}
+%
+% SEE NOTES AT THE END OF THIS SCRIPT FOT EXAMPLES
+%
 %---------------------------------------------------------------------------
 % %W% Karl Friston %E%
 
@@ -114,46 +108,37 @@ end
 
 % Gauss-Newton search 
 %===========================================================================
-a      = 0.8;						     % learning rate
 p      = pE;
+pi     = pE;
+dp     = pE;
 dv     = 1e-6;
 for  j = 1:32
 
-	% y(t) = f(p) - for new expansion point (p) in parameter space 
+	% y = f(p) - for new expansion point (p) in parameter space 
 	%-------------------------------------------------------------------
-	if isfield(M,'fp')
-		fp      = feval(M.fp,p,U);
-	else
-		fp      = spm_int(p,M,U,v);
-	end
+	fp     = spm_int(p,M,U,v);
 
 	% compute partial derivatives [Jp] dy(t)/dp*Vp {p = parameters}
 	%-------------------------------------------------------------------
 	for  i = 1:size(Vp,2)
 
-		pi      = p + Vp(:,i)*dv;
-
-		if isfield(M,'fp')
-			dfp = feval(M.fp,pi,U)  - fp;
-		else
-			dfp = spm_int(pi,M,U,v) - fp;
-		end
-
+		pi(:)   = p(:) + Vp(:,i)*dv;
+		dfp     = spm_int(pi,M,U,v) - fp;
 		Jp(:,i) = dfp(:)/dv;
 	end
 
 	% Bayesian [conditional] estimator of new expansion point E{p|y}
 	%-------------------------------------------------------------------
 	P{1}.X = [Jp Ju];
-	P{2}.X = [Vp'*(pE - p); uE];
-	[C P]  = spm_PEB(y(:) - fp(:),P);
+	P{2}.X = [Vp'*( pE(:) - p(:));uE];
+	[C P]  = spm_PEB(y(:) - fp(:),P );
 
 
-	% update - project condotional esitmates onto parameter space
+	% update - project conditional esitmates onto parameter space
 	%-------------------------------------------------------------------
-	dp     = Vp*C{2}.E(ip)*a;
+	dp(:)  = Vp*C{2}.E(ip);
 
-	% update - ensure the system is dissipative
+	% update - ensuring the system is dissipative
 	%-------------------------------------------------------------------
 	for  i = 1:8
 		A    = spm_bi_reduce(M,p + dp);
@@ -169,7 +154,7 @@ for  j = 1:32
 
 	% convergence
 	%-------------------------------------------------------------------
-	w      = dp'*dp;
+	w      = dp(:)'*dp(:);
 	fprintf('%-30s: %i %20s%e\n','GNS Iteration',j,'...',full(w));
 	if w < 1e-6, break, end
 
@@ -191,3 +176,80 @@ end
 Ep     = p;
 Cp     = Vp*C{2}.C(ip,ip)*Vp';
 Ce     = C{1}.M;
+
+
+
+
+
+
+
+
+
+
+return
+
+% NOTES ON USE - DYNAMIC SYSTEMS
+%===========================================================================
+% Consider the dynamic system (c.f. spm_nlsi)
+%
+%              dx/dt  = 1./(1 + exp(-P*x)) + u
+%                y    = x + e
+%
+% Specify a model structure:
+%---------------------------------------------------------------------------
+M.f  = inline('1./(1 + exp(-P*x)) + [u; 0]','x','u','P');
+M.g  = inline('x','x','u','P');
+M.pE = [-1 .3;.5 -1];			% Prior expectation of parameters
+M.pC = speye(4,4);			% Prior covariance for parameters
+M.x  = zeros(2,1)			% intial state x(0)
+M.m  = 1;				% number of inputs
+M.n  = 2;				% number of states
+M.l  = 2;				% number of outputs
+
+% create inputs
+%---------------------------------------------------------------------------
+U.name = 'input'
+U.u    = randn(128,M.m);
+U.dt   = 1;
+
+% and outputs
+%---------------------------------------------------------------------------
+Y.name = 'response';
+y      = spm_int(M.pE,M,U,64);
+Y.y    = y + randn(size(y))/32;
+Y.dt   = U.dt*length(U.u)/length(Y.y);
+
+% estimate
+%---------------------------------------------------------------------------
+[Ep,Cp,Ce] = spm_nlsi_GN(M,U,Y);
+
+
+% STATIC SYSTEMS
+%===========================================================================
+% Consider the nonlinear static system where we want to estimate P
+%
+%                y    = 1./(1 + exp(-P*u)) + e
+%
+% Note that the inputs are explanatory variables (c.f. regressors
+% in a design matrix)
+clear % dynamic model and specify a model structure
+%---------------------------------------------------------------------------
+
+M.g  = inline('1./(1 + exp(-P*u(:)))','x','u','P');
+M.pE = [-1 .3;.5 -1];			% Prior expectation of parameters
+M.pC = speye(4,4);			% Prior covariance for parameters
+M.m  = 2;				% number of inputs
+M.l  = 2;				% number of outputs
+
+% create inputs
+%---------------------------------------------------------------------------
+u    = randn(128,M.m);
+
+% and outputs with observation error
+%---------------------------------------------------------------------------
+y    = spm_int(M.pE,M,u);
+Y.y  = y + randn(size(y))/32;
+
+% estimate
+%---------------------------------------------------------------------------
+[Ep,Cp,Ce] = spm_nlsi_GN(M,u,Y);
