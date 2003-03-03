f7e54d640cfdb5fa201ef982815ea0bddf1a7034
Karl Friston <k.friston@ucl.ac.uk>
2003-03-03 20:28:04 +0000

    restricted to confound removal and constant added

--- spm_filter.m
+++ spm_filter.m
@@ -1,70 +1,53 @@
 function [vargout] = spm_filter(K,Y)
-% High and low-pass filtering
+% Removes low frequency confounds X0
 % FORMAT [Y] = spm_filter(K,Y)
 % FORMAT [K] = spm_filter(K)
 %
 % K         - filter matrix or:
 % K(s)      - struct array containing partition-specific specifications
 %
-% K(s).RT       - observation interval in seconds
-% K(s).row      - row of Y constituting block/partition s
-% K(s).LParam   - Gaussian parameter in seconds
-% K(s).HParam   - cut-off period in seconds
+% K(s).RT   - observation interval in seconds
+% K(s).row  - row of Y constituting block/partition s
+% K(s).HP   - cut-off period in seconds
 %
-% K(s).HP       - low frequencies to be removed (DCT without constant)
-% K(s).LP       - sparse toeplitz low-pass convolution matrix
+% K(s).X0   - low frequencies to be removed (DCT)
 % 
 % Y         - data matrix
 %
 % K         - filter structure
-% Y         - filtered data K.K*Y
+% Y         - filtered data
 %___________________________________________________________________________
 %
-% spm_filter implements band pass filtering in an efficient way by
-% using explicitly the projector matrix form of the High pass
-% component.  spm_filter also configures the filter structure in
-% accord with the specification fields if called with one argument
+% spm_filter implements high-pass filtering in an efficient way by
+% using the residual forming matrix of X0 - low frequency confounds
+%.spm_filter also configures the filter structure in accord with the 
+% specification fields if called with one argument
 %___________________________________________________________________________
 % %W% Karl Friston %E%
+
+
+%===========================================================================
+try
+	for s = 1:length(K)
+		K(s).HP = K(s).HParam 	% compatibility check
+	end
+end
+%===========================================================================
+
 
 % set or apply
 %---------------------------------------------------------------------------
 if nargin == 1 & isstruct(K)
 
-	% set K.HP and/or K.LP
+	% set K.X0
 	%-------------------------------------------------------------------
 	for s = 1:length(K)
 
-		% block size
-		%-----------------------------------------------------------
-		k     = length(K(s).row);
-
-		% create and normalize low-pass filter
-		%-----------------------------------------------------------
-		if isfield(K(s),'LParam')
-
-			sigma   = K(s).LParam/K(s).RT;
-			h       = round(4*sigma);
-			h       = exp(-[-h:h].^2/(2*sigma^2));
-			n       = length(h);
-			d       = [1:n] - (n + 1)/2;
-
-			if      n == 1, h = 1; end
-
-			K(s).KL = spdiags(ones(k,1)*h,d,k,k);
-			K(s).KL = spdiags(1./sum(K(s).KL')',0,k,k)*K(s).KL;
-
-		end
-
 		% make high pass filter
 		%-----------------------------------------------------------
-		if isfield(K(s),'HParam')
-
-			n       = fix(2*(k*K(s).RT)/K(s).HParam + 1);
-			X       = spm_dctmtx(k,n);
-			K(s).KH = sparse(X(:,[2:n]));
-		end
-
+		k       = length(K(s).row);
+		n       = fix(2*(k*K(s).RT)/K(s).HP + 1);
+		K(s).X0 = spm_dctmtx(k,n);
 	end
 
 	% return structure
@@ -78,7 +61,7 @@ else
 
 		% ensure requisite feilds are present
 		%-----------------------------------------------------------
-		if ~isfield(K(1),'KL') & ~isfield(K(1),'HL')
+		if ~isfield(K(1),'X0')
 			K = spm_filter(K);
 		end
 
@@ -88,17 +71,9 @@ else
 			%---------------------------------------------------
 			y = Y(K(s).row,:);
 
-			% apply low pass filter
-			%---------------------------------------------------
-			if isfield(K(s),'KL')
-				y = K(s).KL*y;
-			end
-
 			% apply high pass filter
 			%---------------------------------------------------
-			if isfield(K(s),'KH')
-				y = y - K(s).KH*(K(s).KH'*y);
-			end
+			y = y - K(s).X0*(K(s).X0'*y);
 
 			% reset filtered data in Y
 			%---------------------------------------------------
