1c1b25e123a936f5caea59be8913f8f7029529cd
Karl Friston <k.friston@ucl.ac.uk>
2003-03-26 19:42:27 +0000

    help added

--- spm_mfx.m
+++ spm_mfx.m
@@ -1,6 +1,71 @@
 function [SPM] = spm_mfx(SPM)
 % converts a 1st-level design specification into a MFX specification
 % FORMAT [SPM] = spm_mfx(SPM)
+% SPM  - design and estimation structure after a 1st-level analysis
+%
+% spm_mfx takes the products of a 1st-level analysis of a [balanced]
+% multi-session study and produces the SPM design specification for a
+% full mixed-effects (MFX) analysis.  The 1st-level design (X1) must have
+% the same number of parameters for each session.  These are assumed to
+% represent session-specific realizations of 2nd-level effects.
+%
+% spm_mfx prompts for a 2nd-level design matrix (X2) in the form of an
+% F-contrast.  This is expanded using the Kronecker tensor product to
+% model the effects of each 2nd-level parameter separately.  A new
+% SPM.mat structure is saved in a subdirectory of the 1st-level results
+% directory and can be estimated in the usual way. 2nd-level contrasts
+% can then be used to test specific hypotheses at the 2nd-level in terms
+% of compounds of 1st-level parameters specified by X2 (e.g. their
+% mean).
+%
+% spm_mfx is a full mixed effects analysis in the sense that it allows
+% for unbalanced designs at the 1st-level and different 1st-level error
+% covariances.  Operationally, ReML estimates of the 1st and 2nd-level
+% covariance components are computed by projecting the 2nd-level effects
+% down to the 1st-level and partitioning the covariance of the data in
+% observation space.  The 2nd-level parameter estimates are then computed
+% as linear mixtures of the 1st-level estimates, using the appropriate
+% non-sphericity.  This non-sphericity is a mixture of 1st- and 2nd-level
+% components that renders the ensuing 2nd-level estimates ML.
+%
+% In summary;
+%
+% ReML estimates of V1 are obtained where
+%
+%                y    = X1*B1 + X0*B0 + e1
+%                B1   = X2*B2 + e2;
+%
+% giving;        y    = X1*X2*B2 + X0*B0 + X1*e2 + e1
+%
+% where          V1   = cov(X1*e2 + e1)
+%
+% V1 is now used to give the covariance components of any 1st-level
+% parameter estimators B1h
+%
+%                B1h  = M1*y
+% such that       V2   = cov(B1h) = M1*V1*M1';
+%
+% is the error covariance for the single level model
+%
+%                B1h   = X2*B2 + r2
+%
+% where cov(r2) = cov(B1h) = V2, which can be estimated non-iteratively
+% in the usual way to give the ML estimates of B2.
+%
+% Note that with balanced designs and equal error covariances over
+% sessions, at the 1st level there is no need to compute multiple
+% covariance components because, at the 2nd-level, they are exactly the
+% same (i.e. M1*X1*cov(e2)*X1*M1 has the same form as M1*cov(e1)*M1).
+%
+% The ReML hyperparameters are estimated using the covariance of y over
+% voxels.  This means that the relative amounts of within and
+% between-session variance are assumed to be fixed over voxels but can
+% vary in their overall expression.  The voxels used for this polling are
+% those that show 1st-level responses.
+%
+% See spm_reml.m
+%
+%
 %___________________________________________________________________________
 % %W% Karl Friston %E%
 SCCSid = '%I%';
@@ -17,7 +82,6 @@ if nargin ==0
 end
 swd   = SPM.swd;
 
-
 %-Check this is a repeated measures design
 %-----------------------------------------------------------------------
 n     = length(SPM.Sess);			% number of sessions
@@ -31,22 +95,25 @@ end
 % Build MFX design specification
 %-----------------------------------------------------------------------
 
-% response variable (parameter estimates from 1st-level)
+% response variable (parameter estimates of B1 from the 1st-level)
 %=======================================================================
-nY       = length(SPM.xX.iC);
-nP       = nY/n;
-S.xY.VY  = SPM.Vbeta(SPM.xX.iC);
+iX1      = [SPM.xX.iH SPM.xX.iC];
+iX0      = [SPM.xX.iB SPM.xX.iG];
+nY       = length(iX1);				% number of B1
+nP       = nY/n;				% number of B2
+
+% change relative filenames to full
+%-----------------------------------------------------------------------
+S.xY.VY  = SPM.Vbeta(iX1);
 for i = 1:nY
 	S.xY.VY(i).fname = fullfile(swd,S.xY.VY(i).fname);
 end
 
-% designs
+% design matrices
 %=======================================================================
 
-% 1st-level (X1) and confounds X0 (inlcuding those in filter structure);
+% 1st-level (X1) and confounds X0 (including those in filter structure);
 %-----------------------------------------------------------------------
-iX1   = SPM.xX.iC;
-iX0   = SPM.xX.iB;
 X1    = SPM.xX.X(:,iX1);
 X0    = SPM.xX.X(:,iX0);
 K0    = sparse(0,0);
@@ -56,11 +123,15 @@ try
 	end
 end
 X0    = [X0 K0];
+
+% ensure X1 is orthogonalized w.r.t. X0
+%-----------------------------------------------------------------------
 X1    = X1 - X0*inv(X0'*X0)*(X0'*X1);
 X1    = sparse(X1);
 
 
 % 2nd-level (X2) design (as an F-contrast to ensure estimability)
+% expanded to cover all parameters (kron(X2,eye(nP))
 %-----------------------------------------------------------------------
 sX       = spm_sp('set',eye(n));
 c        = ones(n,1);
@@ -69,12 +140,12 @@ xX.xKXs  = sX;
 for    i = 1:n
 	xX.name{i}  = sprintf('Session %i',i);
 end
-[I,xCon] = spm_conman(xX,xCon,'F',1,'2nd-level contrast','',1)
+[I,xCon] = spm_conman(xX,xCon,'F',1,'2nd-level contrast','',1);
 X2       = xCon(I).c;
 X2       = kron(X2,speye(nP,nP));
 nC       = size(X2,2);
 
-% names
+% names for nC contrasts in X2 and nP parameters
 %-----------------------------------------------------------------------
 name  = {};
 for i = 1:nC
@@ -116,11 +187,12 @@ for i = 1:nP
 	end
 end
 
-% 1st-level non-sphericity
+% 1st-level non-sphericity - ReML estimates, restricted to the Null
+% space of 'fixed' effects X1*X2 and X0
 %-----------------------------------------------------------------------
 V1       = spm_reml(SPM.xVi.Cy,[X1*X2 X0],Q);
 
-% 2nd-level non-sphericity
+% 2nd-level non-sphericity (including original whitening and filtering)
 %-----------------------------------------------------------------------
 W        = SPM.xX.W;
 K        = SPM.xX.K;
@@ -138,12 +210,13 @@ S.xVol   = SPM.xVol;
 S.xM.TH  = -ones(nY,1)/0
 S.xM.I   = 1;
 
-
 %-Change to SPM.swd/mfx and save analysis parameters in SPM.mat file
 %-----------------------------------------------------------------------
+cd(swd)
 mkdir mfx;
 cd mfx
-SPM.swd = pwd;
+SPM      = S;
+SPM.swd  = pwd;
 save SPM SPM
 
 %=======================================================================
