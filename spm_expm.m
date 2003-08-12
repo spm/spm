e740553b7b570f751b24102b415b5cd644525cbb
Karl Friston <k.friston@ucl.ac.uk>
2003-08-12 15:08:17 +0000

    Pade approximation used when Taylor expansion overflows

--- spm_expm.m
+++ spm_expm.m
@@ -12,24 +12,65 @@ function [x] = spm_expm(J,x)
 %___________________________________________________________________________
 % %W% Karl Friston %E%
 
-% assume expm(J) is required
+% expm(J) use Pade approximation
 %---------------------------------------------------------------------------
-if nargin < 2
-	x = speye(length(J));
-end
+if nargin == 1
 
-% compute y = expm(J)*x = (1 + J + J*J/2! + J*J*J/3!  + ...)*x
-%---------------------------------------------------------------------------
-J     = sparse(J);
-x     = sparse(x);
-fx    = J*x;
-j     = 1;
-
-while norm(fx,1) > 1e-16
-
-	% accumulate high-order terms until convergence
+	% ensure norm is <1/2 by scaling by power of 2
 	%-------------------------------------------------------------------
-	j  = j + 1;
-	x  = x + fx;
-	fx = J*fx/j;
+	[f,e] = log2(norm(J,'inf'));
+	s     = max(0,e+1);
+	J     = J/2^s;
+
+	X     = J; 
+	c     = 1/2;
+	E     = speye(size(J)) + c*J;
+	D     = speye(size(J)) - c*J;
+	q     = 6;
+	p     = 1;
+	for k = 2:q
+		c   = c * (q-k+1) / (k*(2*q-k+1));
+ 		X   = J*X;
+ 		cX  = c*X;
+		E   = E + cX;
+		if p
+			D = D + cX;
+		else
+			D = D - cX;
+		end
+		p = ~p;
+	end
+	E = D\E;
+
+	% Undo scaling by repeated squaring
+	%-------------------------------------------------------------------
+	for k = 1:s
+		E = E*E;
+	end
+	x     = E;
+else
+
+	% compute y = expm(J)*x = (1 + J + J*J/2! + J*J*J/3!  + ...)*x
+	%-------------------------------------------------------------------
+	J     = sparse(J);
+	x     = sparse(x);
+	x0    = x;
+	fx    = J*x;
+	j     = 1;
+
+	while norm(fx,1) > 1e-16
+
+		% accumulate high-order terms until convergence
+		%-----------------------------------------------------------
+		j  = j + 1;
+		x  = x + fx;
+		fx = J*fx/j;
+
+		% revert to Pade approximation if numerical overflow
+		%-----------------------------------------------------------
+		if norm(x,1) > 1e16
+			x = spm_expm(J)*x0;
+			return
+		end
+	end
 end
