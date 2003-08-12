20359a2fb2eb75ee81bd2ca7312562a1709160c5
Karl Friston <k.friston@ucl.ac.uk>
2003-08-12 15:19:51 +0000

    Initial revision.

---
+++ spm_cat.m
@@ -0,0 +1,58 @@
+function [x] = spm_cat(x)
+% FORMAT [x] = spm_cat(x);
+% converts an array into a matrix
+% x - cell array
+%
+% Empty array elements are replaced by sparse zero partitions
+% and single 0 entries are expanded to conform to the non-empty
+% non zeror elements.
+%
+% e.g.:
+% > x       = spm_cat({eye(2) []; 0 [1 1; 1 1]})
+% > full(x) =
+%
+%     1     0     0     0
+%     0     1     0     0
+%     0     0     1     1
+%     0     0     1     1
+%____________________________________________________________________________
+% %W% Karl Friston %E%
+
+
+% find dimensions to fill in empty partitions
+%----------------------------------------------------------------------------
+[n m] = size(x);
+for i = 1:n
+for j = 1:m
+	if iscell(x{i,j})
+		x{i,j} = spm_cat(x{i,j});
+	end
+	[u v]  = size(x{i,j});
+	I(i,j) = u;
+	J(i,j) = v;
+end
+end
+I     = max(I,[],2);
+J     = max(J,[],1);
+
+% sparse and empty partitons
+%----------------------------------------------------------------------------
+[n m] = size(x);
+for i = 1:n
+for j = 1:m
+	if isempty(x{i,j})
+		x{i,j} = zeros(I(i),J(j));
+	elseif ~x{i,j}
+		x{i,j} = zeros(I(i),J(j));
+	else
+		x{i,j} = full(x{i,j});
+	end
+end
+end
+
+% concatenate
+%----------------------------------------------------------------------------
+for i = 1:n
+	y{i,1} = cat(2,x{i,:});
+end
+x     = sparse(cat(1,y{:}));
