c7a7a5b73ce12d3947ba7a448ac16d613fd5804d
Karl Friston <k.friston@ucl.ac.uk>
2003-10-04 11:45:10 +0000

    *** empty log message ***

--- spm_reml.m
+++ spm_reml.m
@@ -1,6 +1,6 @@
-function [Ce,h,W,u] = spm_reml(Cy,X,Q,W,h);
+function [Ce,h,W,u,Q] = spm_reml(Cy,X,Q);
 % REML estimation of covariance components from Cov{y}
-% FORMAT [Ce,h,W,u] = spm_reml(Cy,X,Q,W,h);
+% FORMAT [Ce,h,W,u] = spm_reml(Cy,X,Q);
 %
 % Cy  - (m x m) data covariance matrix y*y'  {y = (m x n) data matrix}
 % X   - (m x p) design matrix
@@ -11,12 +11,12 @@ function [Ce,h,W,u] = spm_reml(Cy,X,Q,W,h);
 % W   - (q x q) W*n = precision of hyperparameter estimates
 % u   - {1 x p} estimable components C{i} = u(1,i)*Q{1} + u(2,i)*Q{2} +...
 %___________________________________________________________________________
-% @(#)spm_reml.m	2.25 John Ashburner, Karl Friston 03/10/04
+% %W% John Ashburner, Karl Friston %E%
 
 % Tolerances 
 %---------------------------------------------------------------------------
 TOL = 1e-6;		% for convergence [norm of gradient df/dh]
-TOS = 1e-16;		% for SVD of curvature [ddf/dhdh]
+TOS = 1e-16;	% for SVD of curvature [ddf/dhdh]
 
 % ensure X is not rank deficient
 %---------------------------------------------------------------------------
@@ -28,29 +28,27 @@ X     = sparse(X);
 %---------------------------------------------------------------------------
 m     = length(Q);
 n     = length(Cy);
-try
-	W;
-catch
-	W     = zeros(m,m);
-	for i = 1:m
-		RQ{i}  = Q{i} - X*(X'*Q{i});
-	end
-	for i = 1:m
-	for j = i:m
-		dFdhh  = sum(sum(RQ{i}.*RQ{j}'));
-		W(i,j) = dFdhh;
-		W(j,i) = dFdhh;
-	end
+W     = zeros(m,m);
+for i = 1:m
+    RQ{i}  = Q{i} - X*(X'*Q{i});
+end
+for i = 1:m
+    for j = i:m
+        dFdhh  = sum(sum(RQ{i}.*RQ{j}'));
+        W(i,j) = dFdhh;
+        W(j,i) = dFdhh;
+    end
 end
 
 % eliminate inestimable components
 %---------------------------------------------------------------------------
-u     = spm_svd(W,TOS);
+[u s] = spm_svd(W,TOS);
+u     = u*inv(s);
 for i = 1:size(u,2)
-	C{i}  = sparse(n,n);
-	for j = 1:m
-		C{i} = C{i} + Q{j}*u(j,i);
-	end
+    C{i}  = sparse(n,n);
+    for j = 1:m
+        C{i} = C{i} + Q{j}*u(j,i);
+    end
 end
 Q     = C;
 
@@ -61,83 +59,75 @@ dFdh  = zeros(m,1);
 W     = zeros(m,m); 
 C     = [];
 for i = 1:m
-	C = [C Q{i}(:)];
+    C = [C Q{i}(:)];
 end
-try
-	h;
-catch
-	I     = speye(n,n);
-	h     = inv(C'*C)*(C'*I(:));
-end
+I     = speye(n,n);
+h     = inv(C'*C)*(C'*I(:));
+
 
 % Iterative EM
 %---------------------------------------------------------------------------
-for k = 1:32
-
-	% Q are variance components		
-	%------------------------------------------------------------------
-	Ce    = sparse(n,n);
-	for i = 1:m
-		Ce = Ce + h(i)*Q{i};
-	end
-	iCe   = inv(Ce);
-
-	% E-step: conditional covariance cov(B|y) {Cby}
-	%===================================================================
-        iCeX  = iCe*X;
-        Cby   = inv(X'*iCeX);
-
-	% M-step: ReML estimate of hyperparameters 
-	%===================================================================
-
-	% Gradient dFd/h (first derivatives)
-	%-------------------------------------------------------------------
-	P     = iCe  - iCeX*Cby*iCeX';
-	PCy   = Cy*P'- speye(n,n);
-	for i = 1:m
-
-		% dF/dh = -trace(dF/diCe*iCe*Q{i}*iCe) = 
-		%---------------------------------------------------
-		PQ{i}   = P*Q{i};
-		dFdh(i) = sum(sum(PCy.*PQ{i}))/2;
-
-	end
-
-	% Expected curvature E{ddF/dhh} (second derivatives)
-	%-------------------------------------------------------------------
-	for i = 1:m
-		for j = i:m
-
-			% ddF/dhh = -trace{P*Q{i}*P*Q{j}}
-			%---------------------------------------------------
-			dFdhh  = sum(sum(PQ{i}.*PQ{j}))/2;
-			W(i,j) = dFdhh;
-			W(j,i) = dFdhh;
-		end
-	end
-
-	% Fisher scoring: update dh = -inv(ddF/dhh)*dF/dh
-	%-------------------------------------------------------------------
-	dh    = inv(W)*dFdh;
-	h     = h + dh;
-
-	% Convergence (or break if there is only one hyperparameter)
-	%===================================================================
-	w     = dFdh'*dFdh;
-	if w < TOL | m == 1, break, end
-	if k == 32
-		W          = u*W*u';
-		[Ce,h,W,u] = spm_reml(Cy,X,Q,W,h)
-		return
-	end
-	fprintf('%-30s: %i %30s%e\n','  ReML Iteration',k,'...',full(w));
+for k = 1:64
+    
+    % Q are variance components		
+    %------------------------------------------------------------------
+    Ce    = sparse(n,n);
+    for i = 1:m
+        Ce = Ce + h(i)*Q{i};
+    end
+    iCe   = inv(Ce);
+    
+    % E-step: conditional covariance cov(B|y) {Cby}
+    %===================================================================
+    iCeX  = iCe*X;
+    Cby   = inv(X'*iCeX);
+    
+    % M-step: ReML estimate of hyperparameters 
+    %===================================================================
+    
+    % Gradient dFd/h (first derivatives)
+    %-------------------------------------------------------------------
+    P     = iCe  - iCeX*Cby*iCeX';
+    PCy   = Cy*P'- speye(n,n);
+    for i = 1:m
+        
+        % dF/dh = -trace(dF/diCe*iCe*Q{i}*iCe) = 
+        %---------------------------------------------------
+        PQ{i}   = P*Q{i};
+        dFdh(i) = sum(sum(PCy.*PQ{i}))/2;
+        
+    end
+    
+    % Expected curvature E{ddF/dhh} (second derivatives)
+    %-------------------------------------------------------------------
+    for i = 1:m
+        for j = i:m
+            
+            % ddF/dhh = -trace{P*Q{i}*P*Q{j}}
+            %---------------------------------------------------
+            dFdhh  = sum(sum(PQ{i}.*PQ{j}))/2;
+            W(i,j) = dFdhh;
+            W(j,i) = dFdhh;
+        end
+    end
+    
+    % Fisher scoring: update dh = -inv(ddF/dhh)*dF/dh
+    %-------------------------------------------------------------------
+    dh    = inv(W)*dFdh;
+    h     = h + dh;
+    
+    % Convergence (or break if there is only one hyperparameter)
+    %===================================================================
+    w     = dFdh'*dFdh;
+    if w < TOL | m == 1, break, end
+    fprintf('%-30s: %i %30s%e\n','  ReML Iteration',k,'...',full(w));
 end
 
 % estimate of cov{e}
 %---------------------------------------------------------------------------
 Ce    = sparse(n,n);
 for i = 1:m
-	Ce = Ce + h(i)*Q{i};
+    Ce = Ce + h(i)*Q{i};
 end
 
 % rotate hyperparameter esimates and precision back
