bdbdad66499f6ebdd39cd22f0c117a6cb1ee3624
Will Penny <w.penny@uea.ac.uk>
2003-10-16 12:37:44 +0000

    changed comments

--- spm_dcm_evidence.m
+++ spm_dcm_evidence.m
@@ -9,9 +9,9 @@ function [evidence] = spm_dcm_evidence (DCM)
 % evidence   Contains the following fields:
 %
 %            .region_cost(i)        The cost of prediction errors in region i
-%            .bic_penalty           Bayesian information criterion penalty (in units of NATS)
+%            .bic_penalty           Bayesian information criterion penalty 
 %            .bic_overall           The overall BIC value
-%            .aic_penalty           Akaike's information criterion penalty (in units of NATS)
+%            .aic_penalty           Akaike's information criterion penalty 
 %            .aic_overall           The overall AIC value
 %
 %            All of the above are in units of NATS (not bits)
