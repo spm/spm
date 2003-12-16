989b4436c89c0751ea37b2f4374b3c5f45e758b6
John Ashburner <j.ashburner@ucl.ac.uk>
2003-12-16 16:04:50 +0000

    Changed them to Jesper's versions

--- spm_clusters.m
+++ spm_clusters.m
@@ -1,5 +1,5 @@
-function varargout = spm_clusters(varargin)
-% Returns the cluster index for a point list - a compiled routine
+function A = spm_clusters(L)
+% Returns the cluster index for a point list
 % FORMAT [A] = spm_clusters(L)
 % L     - locations [x y x]' {in voxels}
 %
@@ -11,8 +11,36 @@ function varargout = spm_clusters(varargin)
 % subsets, returning a list of indices in A, such that the ith location
 % belongs to cluster A(i) (using an 18 connectivity scheme).
 %
+% The programming interface of this routine is modeled on the "old"
+% spm_clusters to avoid the need to recode any other parts. The "old"
+% version had the unfourtunate tendency to crash SPM (and Matlab) 
+% whenever invoked with a "too" long list of locations. This new
+% version has the same functionality, but because it is not recursive 
+% it will (hopefully) not crash.
 %_______________________________________________________________________
-% %W% FIL %E%
+% %E% Jesper Andersson %E%
 
-%-This is merely the help file for the compiled routine
-error('spm_clusters.c not compiled - see spm_MAKE.sh')
+%
+% Turn location list to binary 3D volume.
+%
+dim = [max(L(1,:)) max(L(2,:)) max(L(3,:))];
+vol = zeros(dim(1),dim(2),dim(3));
+indx = sub2ind(dim,L(1,:)',L(2,:)',L(3,:)');
+vol(indx) = 1;
+
+%
+% Label each cluster in 3D volume with it's 
+% own little label using an 18 connectivity
+% criterion (without crashing ;-)).
+%
+
+[cci,num] = spm_bwlabel(vol,18);
+
+%
+% Map back to list.
+%
+
+A = cci(indx');
+
+return
+
--- spm_max.m
+++ spm_max.m
@@ -1,5 +1,5 @@
-function varargout = spm_max(varargin)
-% Sizes, maxima and locations of local excursion sets - a compiled routine
+function [N,Z,M,A] = spm_max(X,L)
+% Sizes, maxima and locations of local excursion sets
 % FORMAT [N Z M A] = spm_max(X,L)
 % X     - values of 3-D field
 % L     - locations [x y x]' {in voxels}
@@ -15,9 +15,57 @@ function varargout = spm_max(varargin)
 % Z(i) and its location is given by M(:,i). A(i) identifies the ith
 % maximum with a region. Region A(i) contains N(i) voxels.
 %
-% See also: spm_max.c and spm_clusters.m
+% See also: spm_bwlabel.c and spm_clusters.m
+%
+% Rewrite of "old" spm_max (that used a recursive algorithm) for the
+% connected-component labelling that is part of determining clusters.
+% The programming interface is identical to the old to avoid having
+% to recode any routines calling spm_max or spm_clusters.
 %_______________________________________________________________________
-% %W% Jean-Baptiste Poline %E%
+% %W% Jesper Andersson %E%
 
-%-This is merely the help file for the compiled routine
-error('spm_max.c not compiled - see spm_MAKE.sh')
+%
+% Turn location list to binary 3D volume.
+%
+dim = [max(L(1,:)) max(L(2,:)) max(L(3,:))];
+vol = zeros(dim(1),dim(2),dim(3));
+index = sub2ind(dim,L(1,:)',L(2,:)',L(3,:)');
+vol(index) = 1;
+
+%
+% Label each cluster in 3D volume with it's 
+% own little label using an 18 connectivity
+% criterion (without crashing ;-)).
+%
+
+% cci = connected components image volume.
+
+[cci,num] = spm_bwlabel(vol,18);
+
+%
+% Get size (in no. of voxels) for each
+% connected component.
+%
+
+% ccs = connected component size
+
+ccs = histc(cci(:),[0:max(cci(:))]+0.5);
+ccs = ccs(1:end-1);
+
+%
+% Get indicies into L for voxels that are
+% indeed local maxima (using an 18 neighbour
+% criterion).
+%
+
+vol(index) = X;
+Lindex = spm_get_lm(vol,L); % That was soo not intended.
+
+M = L(:,Lindex);
+Z = X(Lindex);
+mindex = sub2ind(dim,L(1,Lindex)',L(2,Lindex)',L(3,Lindex)');
+A = cci(mindex);
+N = ccs(A);
+
+return
+
