5f9d23374b10376c4eb4d624c22b7091151a4a2e
Karl Friston <k.friston@ucl.ac.uk>
2004-06-23 19:18:26 +0000

    extended functionality

--- spm_cat.m
+++ spm_cat.m
@@ -1,8 +1,9 @@
-function [x] = spm_cat(x)
-% FORMAT [x] = spm_cat(x);
+function [x] = spm_cat(x,d)
 % converts an array into a matrix
+% FORMAT [x] = spm_cat(x,d);
 % x - cell array
-%
+% d - dimension over which to concatenate [default - both]
+%____________________________________________________________________________
 % Empty array elements are replaced by sparse zero partitions
 % and single 0 entries are expanded to conform to the non-empty
 % non zeror elements.
@@ -18,10 +19,44 @@ function [x] = spm_cat(x)
 %____________________________________________________________________________
 % %W% Karl Friston %E%
 
+% check x is not already a matrix
+%----------------------------------------------------------------------------
+if ~iscell(x), return, end
+
+% if concatenation over a specific dimension
+%----------------------------------------------------------------------------
+[n m] = size(x);
+if nargin > 1
+
+    % concatenate over first dimension
+    %------------------------------------------------------------------------
+    if d == 1
+        y = cell(1,m);
+        for i = 1:m
+            y{i} = spm_cat(x(:,i));
+        end
+
+    % concatenate over second
+    %------------------------------------------------------------------------
+    elseif d == 2
+
+        y = cell(n,1);
+        for i = 1:n
+            y{i} = spm_cat(x(i,:));
+        end
+
+    % only vaible for 2-D arrays
+    %------------------------------------------------------------------------
+    else
+        error('uknown option')
+    end
+    x      = y;
+    return
+
+end
 
 % find dimensions to fill in empty partitions
 %----------------------------------------------------------------------------
-[n m] = size(x);
 for i = 1:n
 for j = 1:m
 	if iscell(x{i,j})
