c49ea5ebf03c35c5342fb4ba4dbbf84ad0bd5fdf
Karl Friston <k.friston@ucl.ac.uk>
2004-06-23 19:16:36 +0000

    extended functionalitity for multidimensional derivatives

--- spm_diff.m
+++ spm_diff.m
@@ -1,13 +1,15 @@
 function [J] = spm_diff(varargin)
-% matrix differential
-% FORMAT [dfdx] = spm_diff(f,x,...,P,n)
+% matrix high-order differentials
+% FORMAT [dfdx] = spm_diff(f,x,...,n)
 % 
-% f   - [inline] function f(x,P)
-% x   - argument[s]
-% P   - parameter[s]
-% n   - argument or parameter to differentiate w.r.t.
+% f      - [inline] function f(x{1},...)
+% x      - input argument[s]
+% n      - arguments to differentiate w.r.t.
 %
-% dfdx - df(x,P)/dx{n}
+% dfdx          - df/dx{i}                     ; n =  i
+% dfdx{p}...{q} - df/dx{i}dx{j}(q)...dx{k}(p)  ; n = [i j ... k]
+%
+% - a cunning recursive routine
 %___________________________________________________________________________
 % %W% Karl Friston %E%
 
@@ -17,31 +19,71 @@ function [J] = spm_diff(varargin)
 f     = fcnchk(varargin{1});
 x     = varargin(2:(end - 1));
 n     = varargin{end};
-dx    = 1e-6;
+dx    = 1e-4;
+J     = cell(1,length(x{n(end)}(:)));
 
+% proceed to derivatives.
+%---------------------------------------------------------------------------
 if length(n) == 1
 
-	% dfdx
-	%------------------------------------------------------------------
-	f0    = feval(f,x{:});
-	J     = sparse(length(f0(:)),length(x{n}(:)));
-	if size(J,1)
-		for i = 1:length(x{n}(:))
-			xi       = x;
-			xi{n}(i) = xi{n}(i) + dx;
-			dfdx     = (feval(f,xi{:}) - f0)/dx;
-			J(:,i)   = sparse(dfdx(:));
-		end
-	end
+    % dfdx
+    %-----------------------------------------------------------------------
+    f0    = feval(f,x{:});
+    for i = 1:length(x{n}(:))
+        xi       = x;
+        xi{n}(i) = xi{n}(i) + dx;
+        fi       = feval(f,xi{:});
+        J{i}     = spm_dfdx(fi,f0,dx);
+    end
+
 else
-	% dfdxdx
-	%------------------------------------------------------------------
-	f0    = spm_diff(f,x{:},n(1));
-	J     = cell(1,length(x{n(2)}(:)));
-	for i = 1:length(x{n(2)}(:))
-		xi          = x;
-		xi{n(2)}(i) = xi{n(2)}(i) + dx;
-		dfdx        = (spm_diff(f,xi{:},n(1)) - f0)/dx;
-		J{i}        = sparse(dfdx);
-	end
+
+    % dfdxdxdx....
+    %-----------------------------------------------------------------------
+    f0    = spm_diff(f,x{:},n(1:end - 1));
+    for i = 1:length(x{n(end)}(:))
+        xi            = x;
+        xi{n(end)}(i) = xi{n(end)}(i) + dx;
+        fi            = spm_diff(f,xi{:},n(1:end - 1));
+        J{i}          = spm_dfdx(fi,f0,dx);
+    end
+    return
+
+end
+
+
+try
+    % if there are no arguments to differentiate w.r.t. ...
+    %-----------------------------------------------------------------------
+    if ~length(x{n}(:))
+        J = sparse(length(f0),0);
+    end
+end
+
+try
+    % if there are no arguments to differentiate
+    %-----------------------------------------------------------------------
+    if ~size(J{1},1)
+        J = sparse(0,length(x{n}(:)));
+    end
+end
+
+try
+    % reformat as a matrix
+    %-----------------------------------------------------------------------
+    if size(J{1},2) == 1
+        J = spm_cat(J);
+    end
+end
+
+
+function dfdx = spm_dfdx(f,f0,dx)
+% cell subtraction
+%--------------------------------------------------------------------------
+if iscell(f)
+   for i = 1:length(f)
+       dfdx{i} = spm_dfdx(f{i},f0{i},dx);
+   end
+else
+    dfdx  = (f - f0)/dx;
 end
