5ec6501ad5414eec74932f5aea56878cca70e987
Karl Friston <k.friston@ucl.ac.uk>
2004-08-10 21:04:19 +0000

    Initial revision.

---
+++ spm_logdet.m
@@ -0,0 +1,29 @@
+function [H] = spm_logdet(C)
+% returns the log of the determinant of the positive definite matrix C
+% FORMAT [H] = spm_logdet(C)
+% H = log(det(C))
+%
+% spm_logdet is a computationally efficient operator that can deal with
+% sparse matrices
+%___________________________________________________________________________
+% %W% Karl Friston %E%
+
+% assume diagonal form
+%---------------------------------------------------------------------------
+H     = sum(log(diag(C)));
+
+% invoke det if non-diagonal
+%---------------------------------------------------------------------------
+[i j] = find(C);
+n     = length(C);
+if any(i ~= j)
+      a = exp(H/n);
+      H = H + log(det(C/a));           
+end
+
+% invoke svd is rank deficient
+%---------------------------------------------------------------------------
+if imag(H) | isinf(H)
+    s  = svd(full(C));
+    H  = sum(log(s(s > n*max(s)*eps)));
+end
