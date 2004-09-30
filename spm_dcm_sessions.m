3f204b04d2487298784151aed177e4964d57c428
Will Penny <w.penny@uea.ac.uk>
2004-09-30 17:14:25 +0000

    added help

--- spm_dcm_sessions.m
+++ spm_dcm_sessions.m
@@ -6,6 +6,23 @@ function [] = spm_dcm_sessions ()
 % number of DCM models. This routine can be used, for example,
 % to do Bayesian fixed or random effects analysis on 
 % contrasts of DCM parameters.
+%
+% This function returns p-values for one-sided t-tests. The Bayesian 
+% probabilites are p(effect_size > threshold) where the 
+% threshold is specified by the user. If you wish to test for 
+% effects being smaller than a threshold you can use negative 
+% values when you specify the contrasts. p-values for two-sided
+% tests are twice as large.
+%
+% In Bayesian fixed effects analysis the mean estimates from
+% each DCM are weighted by their relative precision. Bayesian
+% random effects analysis is based on the between-model variance.
+% If the threshold is 0, and p is the random effects p-value 
+% from classical inference then the Bayesian RFX probability value
+% is 1-p. As usual, only the random effects procedures allow 
+% you to make an inference about the population from which the 
+% data (eg. subjects) are drawn.
+%
 % -------------------------------------------------------
 % %W% Will Penny %E%
 
