0140c191817b35310e94790c624037ffec49fefe
Guillaume Flandin <g.flandin@ucl.ac.uk>
2004-11-25 17:28:17 +0000

    Updated description

--- spm_digamma.m
+++ spm_digamma.m
@@ -1,11 +1,17 @@
-function [y] = spm_digamma(x)
-% The digamma function (derivative of the gamma function)
-% FORMAT [y] = spm_digamma(x)
+function y = spm_digamma(x)
+% Digamma function (logarithmic derivative of the gamma function)
+% 
+% FORMAT: y = spm_digamma(x)
+% x - nonnegative, real values
+% y - gamma function evaluated at each value x
 %
-% Implementation via MEX routine
+%                    digamma(x) = d(log(gamma(x)))/dx
 %
-% Solaris: spm_digamma.mexsol
-%
-% Compiled via make function (based on mex spm_digamma.c)
-%
+% Reference:
+%   Mex file derived from a FORTRAN program by D. E. Amos,
+%   ACM Transactions on Mathematical Software, 1983.
+%   Obtained from NETLIB: http://www.netlib.org/toms/610.
+%_______________________________________________________________________
 % %W% Will Penny %E%
+
+error('spm_digamma.c has not been compiled')
