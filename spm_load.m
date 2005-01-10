ae4c86109b1bb333a6bda87802e17954abafe8f7
Karl Friston <k.friston@ucl.ac.uk>
2005-01-10 19:11:23 +0000

    includes a serach for numeric fields in structures

--- spm_load.m
+++ spm_load.m
@@ -4,14 +4,17 @@ function [x] = spm_load(f)
 % f  - file {ascii file containing a regular array of numbers
 % x  - corresponding data matrix
 %_______________________________________________________________________
-% %W% Karl Friston, Andrew Holmes %E%
+% %W% Karl Friston %E%
 
 
 %-Get a filename if none was passed
 %-----------------------------------------------------------------------
+x  = [];
 if nargin == 0
-	[f,p] = uigetfile({'*.mat';'*.txt';'*.dat'});
-    f     = fullfile(p,f);
+    [f,p] = uigetfile({'*.mat';'*.txt';'*.dat'});
+    try
+        f     = fullfile(p,f);
+    end
 end
 
 %-Load the data file into double precision matrix x
@@ -22,8 +25,18 @@ try
 end
 try
     x = load(f,'-mat');
-    while ~isnumeric(x)
-        s = fieldnames(x);
-        x = getfield(x,s{1});
-    end
+    x = getdata(x);
+end
+
+if ~isnumeric(x), x = []; end
+
+function x = getdata(s)
+% get numberic data x from the fields of structure s
+%--------------------------------------------------------------------------
+x = [];
+f = fieldnames(s);
+for i = 1:length(f)
+    x = getfield(s,f{i});
+    if isnumeric(x),return;         end
+    if isstruct(x), x = getdata(x); end
 end
