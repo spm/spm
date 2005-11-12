b626c0cde69a221dfa4c7d168ded2c88a1d667b3
Klaas Enno Stephan <stephan@biomed.ee.ethz.ch>
2005-11-12 16:50:23 +0000

    spm_lx_dcm: corrected a bug in dealing with E0 (this was previously fixed to 0.34 for all areas even though E0 was a free parameter of the state equations; now the MAP estimates are used)
    SVN r293

--- spm_lx_dcm.m
+++ spm_lx_dcm.m
@@ -13,22 +13,28 @@ function [y] = spm_lx_dcm(x,u,P)
 % Copyright (C) 2005 Wellcome Department of Imaging Neuroscience
 
 % Karl Friston
-% $Id: spm_lx_dcm.m 112 2005-05-04 18:20:52Z john $
+% $Id: spm_lx_dcm.m 293 2005-11-12 16:50:23Z klaas $
 
 
 % resting venous volume
 %---------------------------------------------------------------------------
 V0       = 100*0.02;
-E0       = 0.34; 
 
 % coeficients for BOLD signal
 %---------------------------------------------------------------------------
-k1       = 7*E0;
-k2       = 2;
-k3       = 2*E0 - 0.2;
+% get hemodynamic parameters
+m         = max(size(u,1));             % number of inputs
+n         = max(size(x,1)/5);			% number of regions
+[A B C H] = spm_dcm_reshape(P,m,n);
+% estimated area-specific resting oxygen extraction fractions
+E0        = H(:,5);
+% compute coefficients k1,k2,k3 according to Buxton et al. (1998)
+k1        = 7*E0;
+k2        = 2 * ones(n,1);
+k3        = 2*E0 - 0.2;
 
 % BOLD signal
 %---------------------------------------------------------------------------
-x        = reshape(x,length(x)/5,5);
-x(:,3:5) = x(:,3:5) + 1;
-y        = V0*(k1*(1 - x(:,5)) + k2*(1 - x(:,5)./x(:,4)) + k3*(1 - x(:,4)));
+x         = reshape(x,length(x)/5,5);
+x(:,3:5)  = x(:,3:5) + 1;
+y         = V0*(k1.*(1 - x(:,5)) + k2.*(1 - x(:,5)./x(:,4)) + k3.*(1 - x(:,4)));
