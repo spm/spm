307349ce6ad24f637b7e0116ebf3f98c3925a8e0
James Kilner <j.kilner@ucl.ac.uk>
2006-03-17 14:24:11 +0000

    SVN r482

--- spm_eeg_average_TF.m
+++ spm_eeg_average_TF.m
@@ -6,33 +6,35 @@ function D=spm_eeg_average_TF(S)
 % $Id$
 
 try
-	D = S.D;
+    D = S.D;
 catch
     D = spm_select(1, '.*\.mat$', 'Select EEG mat file');
 end
 P = spm_str_manip(D, 'H');
 try
-	D = spm_eeg_ldata(D);
+    D = spm_eeg_ldata(D);
 catch    
-	error(sprintf('Trouble reading file %s', D));
+    error(sprintf('Trouble reading file %s', D));
 end
 try
-	c = S.c;
+    c = S.c;
 catch
-	% if there is no S.c, assume that user wants default average within
-	% trial type
-	c = eye(D.events.Ntypes);
+    % if there is no S.c, assume that user wants default average within
+    % trial type
+    c = eye(D.events.Ntypes);
 end
 
 d=zeros(size(D.data,1),size(D.data,2),size(D.data,3),D.events.Ntypes);
 fh=fopen(fullfile(D.path,D.fnamedat),'r');
 
 for n=1:D.Nevents
-	[m,i]=find(D.events.types==D.events.code(n));
-	data=fread(fh,[1,size(D.data,1)*size(D.data,2)*size(D.data,3)],'short');
-	data=reshape(data,size(D.data,1),size(D.data,2),size(D.data,3),1);
-    data=data.*repmat(D.scale(:,1,1,n),[1,D.Nfrequencies, D.Nsamples]);
-	d(:,:,:,i)=d(:,:,:,i)+data;
+    if D.events.reject(n)== 0
+        [m,i]=find(D.events.types==D.events.code(n));
+        data=fread(fh,[1,size(D.data,1)*size(D.data,2)*size(D.data,3)],'short');
+        data=reshape(data,size(D.data,1),size(D.data,2),size(D.data,3),1);
+        data=data.*repmat(D.scale(:,1,1,n),[1,D.Nfrequencies, D.Nsamples]);
+        d(:,:,:,i)=d(:,:,:,i)+data;
+    end
 end
 
 D.fnamedat = ['m' D.fnamedat];
@@ -41,10 +43,10 @@ fpd = fopen(fullfile(P, D.fnamedat), 'w');
 D.scale = zeros(D.Nchannels, 1, 1, D.events.Ntypes);
 
 for n=1:D.events.Ntypes
-	dat=squeeze(d(:,:,:,n)./length(find(D.events.code==D.events.types(n))));
-	D.scale(:, 1, 1, n) = max(max(squeeze(abs(dat)), [], 3), [], 2)./32767;
-	dat = int16(dat./repmat(D.scale(:, 1, 1, n), [1, D.Nfrequencies, D.Nsamples]));
-	fwrite(fpd, dat, 'int16');
+    dat=squeeze(d(:,:,:,n)./length(find(D.events.code==D.events.types(n) & ~D.events.reject)));
+    D.scale(:, 1, 1, n) = max(max(squeeze(abs(dat)), [], 3), [], 2)./32767;
+    dat = int16(dat./repmat(D.scale(:, 1, 1, n), [1, D.Nfrequencies, D.Nsamples]));
+    fwrite(fpd, dat, 'int16');
 end
 fclose (fh)	;
 
@@ -57,11 +59,11 @@ D.Nevents = size(c, 2);
 % and over-trial type contrasts
 D.events.code = size(1, size(c, 2));
 for i = 1:size(c, 2)
-	if sum(c(:, i)) == 1 & sum(~c(:, i)) == size(c, 1)-1
-		D.events.code(i) = find(c(:, i));
-	else
-		D.events.code(i) = i;
-	end
+    if sum(c(:, i)) == 1 & sum(~c(:, i)) == size(c, 1)-1
+        D.events.code(i) = find(c(:, i));
+    else
+        D.events.code(i) = i;
+    end
 end
 
 D.events.time = [];
@@ -72,8 +74,8 @@ D.events.reject = zeros(1, D.Nevents);
 D.events.blinks = zeros(1, D.Nevents);
 D.fname = ['m' D.fname];
 if str2num(version('-release'))>=14
-	save(fullfile(P, D.fname), '-V6', 'D');
+    save(fullfile(P, D.fname), '-V6', 'D');
 else
-	save(fullfile(P, D.fname), 'D');
+    save(fullfile(P, D.fname), 'D');
 end
 
