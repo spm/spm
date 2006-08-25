23d861cc1e978b9076f04012d07aa3b15fd96b7e
John Ashburner <j.ashburner@ucl.ac.uk>
2006-08-25 18:18:04 +0000

    Improved the accuracy of spm_write_vol.m.  The fix includes: * Put in the correct maximum values for various integer datatypes. * Sorted out the scalefactors and intercepts properly.
    SVN r604

--- spm_write_vol.m
+++ spm_write_vol.m
@@ -8,8 +8,9 @@ function V = spm_write_vol(V,Y)
 % Copyright (C) 2005 Wellcome Department of Imaging Neuroscience
 
 % John Ashburner
-% $Id: spm_write_vol.m 250 2005-10-07 16:08:39Z john $
+% $Id: spm_write_vol.m 604 2006-08-25 18:18:04Z john $
 
+use_offset = false;
 
 if ndims(Y)>3, error('Can only handle a maximum of 3 dimensions.'), end
 
@@ -25,8 +26,7 @@ end
 %-----------------------------------------------------------------------
 dt           = V.dt(1);
 s            = find(dt == [2 4 8 256 512 768]);
-dmnmx        = [0 -2^15 -2^31 -2^7 0 0 ; 2^8-1 2^15-1 2^31-1 2^7-1 2^16 2^32];
-dmnmx(1,:)   = 0; % assume an intercept of zero
+dmnmx        = [0 -2^15 -2^31 -2^7 0 0 ; 2^8-1 2^15-1 2^31-1 2^7-1 2^16-1 2^32-1];
 dmnmx        = dmnmx(:,s);
 
 V.pinfo(1,:) = 1;
@@ -50,9 +50,18 @@ if ~isempty(s),
 			if ~finite(mx), mx = 0; end;
 			if ~finite(mn), mn = 0; end;
 			if mx~=mn,
-				V.pinfo(1,p) = (mx-mn)/(dmnmx(2)-dmnmx(1));
-				V.pinfo(2,p) = ...
-					(dmnmx(2)*mn-dmnmx(1)*mx)/(dmnmx(2)-dmnmx(1));
+				if use_offset,
+					V.pinfo(1,p) = (mx-mn)/(dmnmx(2)-dmnmx(1));
+					V.pinfo(2,p) = ...
+						(dmnmx(2)*mn-dmnmx(1)*mx)/(dmnmx(2)-dmnmx(1));
+				else
+					if dmnmx(1)<0,
+						V.pinfo(1,p) = max(mx/dmnmx(2),mn/dmnmx(1));
+					else
+						V.pinfo(1,p) = mx/dmnmx(2);
+					end;
+					V.pinfo(2,p) = 0;
+				end;
 			else
 				V.pinfo(1,p) = 0;
 				V.pinfo(2,p) = mx;
@@ -64,8 +73,17 @@ if ~isempty(s),
 		if isempty(mx), mx = 0; end;
 		if isempty(mn), mn = 0; end;
 		if mx~=mn,
-			V.pinfo(1,1) = (mx-mn)/(dmnmx(2)-dmnmx(1));
-			V.pinfo(2,1) = (dmnmx(2)*mn-dmnmx(1)*mx)/(dmnmx(2)-dmnmx(1));
+			if use_offset,
+				V.pinfo(1,1) = (mx-mn)/(dmnmx(2)-dmnmx(1));
+				V.pinfo(2,1) = (dmnmx(2)*mn-dmnmx(1)*mx)/(dmnmx(2)-dmnmx(1));
+			else
+				if dmnmx(1)<0,
+					V.pinfo(1) = max(mx/dmnmx(2),mn/dmnmx(1));
+				else
+					V.pinfo(1) = mx/dmnmx(2);
+				end;
+				V.pinfo(2) = 0;
+			end;
 		else
 			V.pinfo(1,1) = 0;
 			V.pinfo(2,1) = mx;
