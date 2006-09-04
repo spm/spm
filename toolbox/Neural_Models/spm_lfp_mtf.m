ed7a611e0c345e1a7099683fc375ca4160e11e08
Karl Friston <k.friston@ucl.ac.uk>
2006-09-04 17:52:52 +0000

    SVN r611

--- toolbox/Neural_Models/spm_lfp_mtf.m
+++ toolbox/Neural_Models/spm_lfp_mtf.m
@@ -12,13 +12,14 @@ function [G,f] = spm_lfp_mtf(P,M)
 
 % compute log-spectral density
 %==========================================================================
-i         = 1;    % i-th input
+i       = 1;    % i-th input
+j       = 1;    % j-th output
 
 % frequnecies of interest
 %--------------------------------------------------------------------------
-N          = 128;
-dt         = 1/N;
-f          = [1:N/2]/(N*dt);
+N       = 128;
+dt      = 1/N;
+f       = [1:N/2]/(N*dt);
 
 
 % augment and bi-linearise
@@ -27,24 +28,24 @@ f          = [1:N/2]/(N*dt);
 
 % compute modulation transfer function using FFT of the kernels
 %--------------------------------------------------------------------------
-N          = 128;
-dt         = 1/N;
-f          = [1:N/2]/(N*dt);
+N       = 128;
+dt      = 1/N;
+f       = [1:N/2]/(N*dt);
 
-[K0,K1]    = spm_kernels(M0,M1,L1,N,dt);
-S          = fft(K1);
-G          = abs(S([1:N/2] + 1,:)).^2;
+[K0,K1] = spm_kernels(M0,M1,L1,N,dt);
+S       = fft(K1(:,:,i));
+G       = abs(S([1:N/2] + 1,j)).^2;
 
 return
 
 % compute modulation transfer function using FFT of the kernels
 %--------------------------------------------------------------------------
-A          = full(M0(2:end,2:end));
-B          = full(M1{i}(2:end,1));
-C          = full(L1(:,2:end));
-[z,p]      = ss2zp(A,B,C,0,i);
-[S f]      = freqz(exp(z*dt),exp(p*dt),f,1/dt);
-G          = abs(S).^2;
+A       = full(M0(2:end,2:end));
+B       = full(M1{i}(2:end,1));
+C       = full(L1(:,2:end));
+[z,p]   = ss2zp(A,B,C,0,i);
+[S f]   = freqz(exp(z*dt),exp(p*dt),f,1/dt);
+G       = abs(S).^2;
 
 
 
