28155f7b56676e890f6ae3b6494212ecdd821203
Volkmar Glauche <volkmar.glauche@uniklinik-freiburg.de>
2006-09-20 06:57:10 +0000

    Error covariance specification should work now for both full and flexible factorial designs
    SVN r631

--- spm_get_vc.m
+++ spm_get_vc.m
@@ -1,115 +1,71 @@
 function SPM=spm_get_vc(SPM)
-% generate error variance components for SPM design
-% FORMAT SPM = spm_get_vc(SPM);
-%
-% in: SPM   - SPM struct
-% required fields:
-%    factor  - factor description
-%    .levels   - # levels for given factor
-%    .variance - 0: all levels have identical error variance
-%                1: error variance different across levels
-%    .dept     - 0: error variances are independent between levels
-%                1: error variances are dependent between levels
-%    xVi.I       - nscan-by-4 index matrix that associates each scan
-%                  with levels for up to 3 user defined factors (column 1
-%                  reserved for implicit replication factor)
-%
-% out: SPM.xVi.Vi   - cell vector of covariance components
-%_______________________________________________________________________
-% 
-% spm_get_vc generates variance components for a given design. For each
-% factor, the user specifies whether its levels have identical variances
-% and are uncorrelated.
-%_______________________________________________________________________
-% Copyright (C) 2006 Freiburg Brain Imaging 
 
-% Volkmar Glauche
-% $Id: spm_get_vc.m 622 2006-09-13 06:04:27Z volkmar $
+Nlevels = max(SPM.xVi.I);
+[nscan nfactor] = size(SPM.xVi.I);
+Vi = {};
 
-nscan = size(SPM.xVi.I,1);
-depVi={};
-VVi={};
-Vi={};
+% first factor in SPM is replications, assume identical variance and independence
+variance = [0 cat(2, SPM.factor.variance) zeros(1,nfactor)];
+dept = [0 cat(2, SPM.factor.dept) zeros(1,nfactor)];
 
-% identical/non-identical variances
-% for each factor, create a single variance component if variances are
-% identical across levels, and level specific variance components if
-% variances are non-identical
-for f=1:numel(SPM.factor)
-    if ~SPM.factor(f).variance
-        varVi{f}{1} = speye(nscan,nscan);
+% first, generate generic index
+Igen = zeros(prod(Nlevels), nfactor);
+Igen(:,1) = kron(ones(1,prod(Nlevels(2:end))),1:Nlevels(1))';
+for cf=2:(nfactor-1)
+    Igen(:,cf) = kron(ones(1,prod(Nlevels((cf+1):end))),kron(1:Nlevels(cf),ones(1,prod(Nlevels(1:(cf-1))))))';
+end;        
+Igen(:,nfactor) = kron(1:Nlevels(nfactor),ones(1,prod(Nlevels(1:(nfactor-1)))))';
+        
+ngen = prod(Nlevels);
+
+% second, generate error variance components
+for f=1:nfactor
+    % identical/non-identical variances
+    % for each factor, create a single variance component if variances are
+    % identical across levels, and level specific variance components if
+    % variances are non-identical
+    nVi = {};
+    if ~variance(f)
+        nVi{1} = speye(Nlevels(f),Nlevels(f));
     else
-        for l=1:max(SPM.factor(f).levels)
-            varVi{f}{l}=double(spdiags(SPM.xVi.I(:,f+1)==l,0,nscan,nscan));
+        for l1=1:Nlevels(f)
+            nVi{l1} = sparse(l1,l1,1,Nlevels(f),Nlevels(f));
         end;
     end;
-end;
-
-% split factor specific variance components if they contain non-identical
-% components of another factor - this may produce duplicate or all-zero
-% variance components
-switch numel(SPM.factor),
-case 1,
-    VVi = varVi{1};
-case 2,
-    for l1=1:numel(varVi{1})
-        for l2=1:numel(varVi{2})
-            VVi{end+1}=varVi{1}{l1}*varVi{2}{l2};
-        end;
-    end;
-case 3,
-    for l1=1:numel(varVi{1})
-        for l2=1:numel(varVi{2})
-            for l3=1:numel(varVi{3})
-                VVi{end+1}=varVi{1}{l1}*varVi{2}{l2}*varVi{3}{l3};
+    if dept(f)
+        for l1 = 1:Nlevels(f)
+            for l2 = 1:(l1-1)
+                nVi{end+1} = sparse([l1 l2],[l2 l1],1,Nlevels(f), ...
+                                         Nlevels(f));
             end;
         end;
     end;
-case 4,
-    for l1=1:numel(varVi{1})
-        for l2=1:numel(varVi{2})
-            for l3=1:numel(varVi{3})
-                for l4=numel(varVi{4})
-                    VVi{end+1}=varVi{1}{l1}*varVi{2}{l2}* ...
-                              varVi{3}{l3}*varVi{4}{l4};
-                end;
+    % combine current factor components with previous ones, thus building
+    % up covariance components block by block
+    if isempty(Vi)
+        Vi = nVi;
+    else
+        oVi = Vi;
+        Vi = {};
+        for nv = 1:numel(nVi)
+            for ov = 1:numel(oVi)
+                Vi{end+1} = kron(nVi{nv}, oVi{ov});
             end;
         end;
     end;
 end;
 
-%independent/dependent variances
-for f=1:numel(SPM.factor)
-    if SPM.factor(f).dept
-        tmpI = SPM.xVi.I;
-        tmpI(:,f+1) = [];
-        % find levels, where non-independent factor varies, but the
-        % others do not. For each combination of the other factors,
-        % create a temporary factor
-        [unused unused tmplvls] = unique(tmpI,'rows');
-        for l1=1:max(SPM.factor(f).levels)
-            ind1 = find(SPM.xVi.I(:,f+1)==l1);
-            for l2=1:(l1-1)
-                ind2 = find(SPM.xVi.I(:,f+1)==l2);
-                tmpdepVi = sparse([ind1; ind2],...
-                                      [ind2; ind1],...
-                                      1,nscan,nscan);
-                for tmpl=1:max(tmplvls)
-                    depVi{end+1}=sparse(nscan,nscan);
-                    rows=find(tmplvls==tmpl);
-                    depVi{end}(rows,rows)=tmpdepVi(rows,rows);
-                end;
-            end;
-        end
-    end;
+% third, sort out rows/columns for real design
+[unused ind] = ismember(SPM.xVi.I,Igen,'rows');
+
+for cVi = 1:numel(Vi)
+    Vi{cVi} = Vi{cVi}(ind,ind);
 end;
 
-Vi=[VVi depVi];
-
-% remove dupl & all-zero variance components
+% last, remove dupl & all-zero variance components
 dupl = zeros(size(Vi));
 for cVi = 1:numel(Vi)
-    dupl(cVi) = dupl(cVi)||full(sum(Vi{cVi}(:)) == 0);
+    dupl(cVi) = dupl(cVi)||full(all(Vi{cVi}(:) == 0));
     if ~dupl(cVi)
         for cVi1 = (cVi+1):numel(Vi)
             dupl(cVi1) = dupl(cVi1)||full(all(Vi{cVi}(:) == Vi{cVi1}(:)));
@@ -117,6 +73,9 @@ for cVi = 1:numel(Vi)
     end;
 end;
 Vi = Vi(~dupl);
+
+% save covariance components. If only one left, use this as error
+% covariance matrix without going through the ReML step
 if numel(Vi) == 1
     SPM.xVi.V = Vi{1};
 else
