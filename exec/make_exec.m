29790e85ddfafaf5a1adfe0d96b8718f9117c958
John Ashburner <j.ashburner@ucl.ac.uk>
2007-05-14 12:30:03 +0000

    Added Kelvin Wong's mexw64 files to the distribution.
    SVN r812

--- exec/make_exec.m
+++ exec/make_exec.m
@@ -8,6 +8,9 @@ function make_exec
 % is included. LD_LIBRARY_PATH or PATH should also contain the directory
 % containing the *.ctf files
 %
+% Software compiled with later MATLAB versions may need 
+% LD_LIBRARY_PATH=$MATLAB/bin/$arch:$MATLAB/sys/os/$arch
+%
 % Note that when compiling, it is important to be careful with your
 % startup.m file.  See the following link for more information:
 % http://www.mathworks.com/support/solutions/data/1-QXFMQ.html?1-QXFMQ
@@ -15,7 +18,7 @@ function make_exec
 % Copyright (C) 2005 Wellcome Department of Imaging Neuroscience
 
 % John Ashburner
-% $Id: make_exec.m 253 2005-10-13 15:31:34Z guillaume $ 
+% $Id: make_exec.m 812 2007-05-14 12:30:03Z john $ 
 
 mcc('-m','-v','-o',['spm_'    computer],'exec_spm.m'   ,'spm_load.m', '-I',spm('Dir'),'-R','-nojvm')
 mcc('-m','-v','-o',['jobman_' computer],'exec_jobman.m','spm_load.m', '-I',spm('Dir'),'-R','-nojvm')
