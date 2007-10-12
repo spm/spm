c3086ae363ce409c202845a7d6ff23ce6490567b
Vladimir Litvak <v.litvak@ucl.ac.uk>
2007-10-12 16:44:04 +0000

    Added two missing functions to MEEGfileio
    SVN r937

---
+++ MEEGfileio/private/deblank2.m
@@ -0,0 +1,15 @@
+function [out] = deblank2(in)
+
+% DEBLANK2 removes all blanks from the beginning and end of  acell or char array
+%
+% out = deblank2(in)
+
+% Copyright (C) 2002, Robert Oostenveld
+%
+% $Log: deblank2.m,v $
+% Revision 1.2  2003/03/17 10:37:28  roberto
+% improved general help comments and added copyrights
+%
+
+
+out = fliplr(deblank(fliplr(deblank(in))));
