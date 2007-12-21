8e773025fdcddcb89dcef1b75fb4c88549a8cdc5
Karl Friston <k.friston@ucl.ac.uk>
2007-12-21 20:34:45 +0000

    Upgrade to spm_Q to return precision components of an AR model More rational construction of covariance components that eschews re-normalization and uses a Toeplitz operator
    SVN r1043

--- spm_Q.m
+++ spm_Q.m
@@ -1,9 +1,10 @@
-function [Q] = spm_Q(A,n)
+function [Q] = spm_Q(a,n,q)
 % returns an (n x n) autocorrelation matrix for an AR(p) process
-% FORMAT [Q] = spm_Q(A,n)
+% FORMAT [Q] = spm_Q(a,n,q)
 %
 % A  - vector pf p AR coeficients
 % n  - size of Q
+% q  - swtich to return precision [default q = 0]
 %__________________________________________________________________________
 % spm_Q uses a Yule-Walker device to compute K where:
 % 
@@ -15,27 +16,43 @@ function [Q] = spm_Q(A,n)
 % cov(y) = <K*z*z'*K> = K*K'
 % 
 % Critically, this is not the correlation because if cov(z) = eye(n) 
-% then trace(cov(y)) ~= n.  This is why the normalization is required
-% 
-% corr(y) = D*K*K'*D';
-% 
-% The reason the diagonals of corr(y)  are not constant is that we 
+% then trace(cov(y)) ~= n.
+% The reason the diagonals of corr(y) are not constant is that we 
 % are modeling finite length AR sequences, which incur boundary effects 
-% at the beginning and end of the sequence.
+% at the beginning and end of the sequence.  These are resolved with a
+% Toeplitz form
 %__________________________________________________________________________
 % Copyright (C) 2005 Wellcome Department of Imaging Neuroscience
 
 % Karl Friston
-% $Id: spm_Q.m 372 2005-12-08 17:12:13Z karl $
+% $Id: spm_Q.m 1043 2007-12-21 20:34:45Z karl $
 
-
-% compute Q
+% default
 %--------------------------------------------------------------------------
-p    = length(A);
-A    = [1 -A(:)'];
-K    = inv(spdiags(ones(n,1)*A,-[0:p],n,n));
-K    = K.*(abs(K) > 1e-4);
-Q    = K*K';
-D    = spdiags(sqrt(1./diag(Q)),0,n,n);
-Q    = D*Q*D;
-Q    = Q.*(abs(Q) > 1e-4);
+try, q; catch, q = 0; end
+
+
+% compute P
+%--------------------------------------------------------------------------
+p    = length(a);
+if q
+
+    % compute P
+    %----------------------------------------------------------------------
+    A    = [-a(1) (1 + a(1)^2) -a(1)];
+    Q    = spdiags(ones(n,1)*A,[-p:p],n,n);
+
+else
+
+    % compute Q
+    %----------------------------------------------------------------------
+    A    = [1 -a(:)'];
+    P    = spdiags(ones(n,1)*A,-[0:p],n,n);
+    K    = inv(P);
+    K    = K.*(abs(K) > 1e-4);
+    Q    = K*K';
+    Q    = toeplitz(Q(:,1));
+
+end
+
+
