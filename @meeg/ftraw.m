c256c4764589e225fa61e7e83a9db8ed5e42f9fb
Vladimir Litvak <v.litvak@ucl.ac.uk>
2008-03-19 15:06:13 +0000

    Method for converting meeg object to Fieldtrip raw struct
    SVN r1231

---
+++ @meeg/ftraw.m
@@ -0,0 +1,54 @@
+function raw = ftraw(this)
+% Method for converting meeg object to Fieldtrip raw struct
+% FORMAT raw = ftraw(this)
+% _______________________________________________________________________
+% Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
+
+% Vladimir Litvak
+% $Id: ftraw.m 1231 2008-03-19 15:06:13Z vladimir $
+
+
+nchans  = nchannels(this);
+ntime   = nsamples(this);
+ntrl =    ntrials(this);
+
+raw = [];
+raw.fsample = fsample(this);
+raw.label   = chanlabels(this)';
+
+wsize=wordsize(this.data.datatype);
+
+for i=1:ntrl
+    offset = (i-1)*nchans*ntime*wsize;
+    trialdim = [nchans ntime];
+    raw.trial{i} = file_array(fullfile(this.path, this.data.fnamedat),trialdim,this.data.datatype,offset,1,0);
+end
+
+raw.time = repmat({time(this)}, 1, ntrl);
+
+function s = wordsize(datatype)
+switch datatype
+    case 'int8'
+        s = 1;
+    case 'int16'
+        s = 2;
+    case 'int32'
+        s = 4;
+    case 'uint8'
+        s = 1;
+    case 'uint16'
+        s = 2;
+    case 'uint32'
+        s = 4;
+    case 'float'
+        s = 4;
+    case 'double'
+        s = 8;
+    case 'float32'
+        s = 4;
+    case 'float64'
+        s = 8;
+    otherwise
+        error('unrecognized datatype');
+        % FIXME, add support for le and be versions
+end
