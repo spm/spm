a0277eb7911c47a99cb8b527979f271b71188adc
Stefan Kiebel <stefan.kiebel@tu-dresden.de>
2008-03-26 11:08:36 +0000

    SVN r1247

--- spm_eeg_epochs.m
+++ spm_eeg_epochs.m
@@ -1,7 +1,7 @@
 function D = spm_eeg_epochs(S)
 % function used for epoching continuous EEG/MEG data
 % FORMAT D = spm_eeg_epochs(S)
-% 
+%
 % S         - optional input struct
 % (optional) fields of S:
 % D         - filename of EEG mat-file with continuous data
@@ -19,7 +19,7 @@ function D = spm_eeg_epochs(S)
 % spm_eeg_epochs extracts single trials from continuous EEG/MEG data. The
 % length of an epoch is determined by the samples before and after stimulus
 % presentation. One can limit the extracted trials to specific trial types.
-% Also, it is possible to re-number trial types, see above. 
+% Also, it is possible to re-number trial types, see above.
 % Note that epoching includes a baseline correction of each single trial,
 % i.e. a subtraction of the average pre-stimulus average from all time
 % points.
@@ -27,7 +27,7 @@ function D = spm_eeg_epochs(S)
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
 % Stefan Kiebel
-% $Id: spm_eeg_epochs.m 1243 2008-03-25 23:02:44Z stefan $
+% $Id: spm_eeg_epochs.m 1247 2008-03-26 11:08:36Z stefan $
 
 [Finter,Fgraph,CmdLine] = spm('FnUIsetup','EEG epoching setup',0);
 
@@ -41,11 +41,11 @@ P = spm_str_manip(D, 'H');
 
 try
     D = spm_eeg_load(D);
-catch    
+catch
     error(sprintf('Trouble reading file %s', D));
 end
-    
-try 
+
+try
     events.start = S.events.start;
 catch
     events.start =...
@@ -59,16 +59,28 @@ catch
         spm_input('end of epoch [ms]', '+1', 'r', '', 1);
 end
 
+% convert to struct
+sD = struct(D);
+ev = cat(2, sD.trials.events(:).value);
+values = unique(ev);
+
 try
     events.types = S.events.types;
 catch
-    disp(D.conditionlabels)
+    disp(sprintf('Nr: Available events (#occurences): '))
+    for i = 1:length(values)
+        Nevents(i) = sum(values(i) == ev);
+        disp(sprintf('%d:   %d (%d)', i, values(i), Nevents(i)))
+
+    end
     events.types = ...
-        spm_input('Event types to epoch', '+1', 's+');
+        spm_input('Event nrs. to epoch)', '+1', 'n', '', inf, length(values));
 end
 
-% returns indices of chosen conditions
-ind = pickconditions(D, events.types);
+
+
+% returns indices of chosen trial types
+selected = find(ismember(ev, values(events.types)));
 
 try
     Inewlist = S.events.Inewlist;
@@ -79,25 +91,28 @@ end
 if Inewlist
     try
         Ec = S.events.Ec;
-    catch    
-        Ec = spm_input('Input event vector', '+1', 'w', [], length(ind));
+    catch
+        Ec = spm_input('Input event vector', '+1', 'w', [], length(values));
     end
 end
 
-spm('Pointer', 'Watch'); drawnow;
-
-
-
-% transform ms to samples
-events.start = indsample(D, events.start);
-events.stop = indsample(D, events.stop);
-
-if events.start <= 1
-    error('Start of pre-stimulus time must be less than %f ms', floor(-D.fsample/1000));
+try
+    events.newlabels = S.events.newlabels;
+catch
+    events.newlabels = ...
+        spm_input('New labels', '+1', 's+', '', length(events.types));
 end
 
-if events.stop <= 1
-    error('End of post-stimulus time must be more than %f ms', ceil(D.fsample/1000));
+events.labels(events.types) = events.newlabels;
+
+spm('Pointer', 'Watch'); drawnow;
+
+% transform ms to samples
+events.startsample = ceil(-events.start*D.fsample/1000);
+events.stopsample = ceil(events.stop*D.fsample/1000);
+
+if events.startsample >= events.stopsample
+    error('Start time must be less than stop time.');
 end
 
 % two passes
@@ -105,56 +120,59 @@ end
 % 1st pass: Count the number of trials to be epoched to know the
 % dimensions of the resulting data array
 ind = [];
-for i = 1:length(events.time)
-    if any(D.events.code(i) == D.events.types)
-        if  D.events.time(i) - events.start < 1 || ...
-                D.events.time(i) + events.stop > D.nsamples
-            % skip this trial
-            warning(sprintf('%s: Event %d not extracted because not enough sample points', D.fname, i));
-        else
-            ind = [ind i];
-        end
+t = round(cat(1, sD.trials.events(:).time)*D.fsample); % sample time
+for i = selected
+    if  t(i) - events.startsample < 1 || t(i) + events.stopsample > D.nsamples
+        % skip this trial
+        warning(sprintf('%s: Event %d not extracted because not enough sample points', D.fname, i));
+    else
+        ind = [ind i];
     end
 end
 
 % 2nd pass: do the epoching
 
+nsamples = events.stopsample + events.startsample+1;
+ntrials = length(ind);
+
 % generate new meeg object with new filenames
-Dnew = newdata(D, ['e' fnamedat(D)], [D.nchannels D.nsamples D.ntrials], dtype(D));
+Dnew = newdata(D, ['e' fnamedat(D)], [D.nchannels nsamples ntrials], D.dtype);
 
-Dnew.nsamples = events.stop + events.start+1;
-Dnew.ntrials = length(ind);
+spm_progress_bar('Init', ntrials, 'Events read'); drawnow;
+if ntrials > 100, Ibar = floor(linspace(1, ntrials, 100));
+else Ibar = [1:ntrials]; end
 
-spm_progress_bar('Init', Dnew.ntrials, 'Events read'); drawnow;
-if Dnew.ntrials > 100, Ibar = floor(linspace(1, Dnew.ntrials, 100));
-else Ibar = [1:Dnew.ntrials]; end
+for i = 1:ntrials
 
-for i = 1:Dnew.ntrials
-        
     k = ind(i);
     if Inewlist
-        D.events.code(i) = Ec(k);
+        ev(i) = Ec(k);
     end
 
-    d = D(:, events.time(i) - events.start :...
-        events.time(i) + events.stop, 1);
-                
-    % baseline subtraction        
-    d = d - repmat(mean(d(:, [1:abs(events.start)+1]),2), 1, D.nsamples);
-    Dnew = putdata(Dnew, 1:Dnew.nchannels, 1:Dnew.nsamples, i, d);
-                                    
+    d = D(:, t(k) - events.startsample : t(k) + events.stopsample, 1);
+
+    % baseline subtraction
+    d = d - repmat(mean(d(:, [1:abs(events.startsample)+1]), 2), 1, nsamples);
+
+    Dnew(1:Dnew.nchannels, 1:nsamples, i) = d;
+
     if ismember(i, Ibar)
         spm_progress_bar('Set', i);
         drawnow;
     end
 end
 
-events.types = unique(events.code(index));
+sDnew = struct(Dnew);
+sDnew.Nsamples = nsamples;
+trials = sDnew.trials;
+sDnew = rmfield(sDnew, 'trials');
 
-events.code = events.code(index);
-events.time = events.time(index);
+for i = 1: ntrials
+    sDnew.trials(i) = struct('onset', t(ind(i))/D.fsample, 'label', num2str(events.labels{find(ev(ind(i)) == values)}));
+end
+Dnew = meeg(sDnew);
 
-if Inewlist & D.Nevents ~= length(Ec)
+if Inewlist && D.ntrials ~= length(Ec)
     warning('Not all events in event list used!')
 end
 
