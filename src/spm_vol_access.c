076d26c39209c553377bed5f2b2516be0d9353fe
Guillaume Flandin <g.flandin@ucl.ac.uk>
2008-04-01 14:50:25 +0000

    Makefile can now also compiles toolboxes.
    SVN r1287

--- src/spm_vol_access.c
+++ src/spm_vol_access.c
@@ -1,5 +1,5 @@
 /*
- * $Id: spm_vol_access.c 938 2007-10-12 19:09:31Z john $
+ * $Id: spm_vol_access.c 1287 2008-04-01 14:50:25Z guillaume $
  * John Ashburner
  */
 
@@ -25,9 +25,10 @@ int get_datasize(int type)
 
 int resample(int m, MAPTYPE *vol, double *out, double *x, double *y, double *z, int hold, double background)
 {
-	extern void resample_uchar(), resample_short(), resample_int(), resample_float(),
-		resample_double(), resample_short_s(), resample_int_s(), resample_float_s(),
-		resample_double_s();
+	extern void resample_uchar(), resample_schar(), resample_short(), resample_ushort(), 
+		resample_int(), resample_uint(), resample_float(), resample_double(), 
+		resample_short_s(), resample_ushort_s(), resample_int_s(), resample_uint_s(), 
+		resample_float_s(), resample_double_s();
 
 	if (vol->dtype == SPM_UNSIGNED_CHAR)
 		 resample_uchar(m,vol->data,out,x,y,z,vol->dim[0],vol->dim[1],vol->dim[2],
@@ -81,9 +82,10 @@ int resample(int m, MAPTYPE *vol, double *out, double *x, double *y, double *z, 
 
 int resample_d(int m, MAPTYPE *vol, double *out, double *gradx, double *grady, double *gradz, double *x, double *y, double *z, int hold, double background)
 {
-	extern void resample_d_uchar(), resample_d_short(), resample_d_int(), resample_d_float(),
-		resample_d_double(), resample_d_short_s(), resample_d_int_s(), resample_d_float_s(),
-		resample_d_double_s();
+	extern void resample_d_uchar(), resample_d_schar(), resample_d_short(), resample_d_ushort(),
+		resample_d_int(), resample_d_uint(), resample_d_float(), resample_d_double(),
+		resample_d_short_s(), resample_d_ushort_s(), resample_d_int_s(), resample_d_uint_s(),
+		resample_d_float_s(), resample_d_double_s();
 
 	if (vol->dtype == SPM_UNSIGNED_CHAR)
 		 resample_d_uchar(m,vol->data,out,gradx,grady,gradz,x,y,z,vol->dim[0],vol->dim[1],vol->dim[2],
@@ -137,6 +139,21 @@ int resample_d(int m, MAPTYPE *vol, double *out, double *gradx, double *grady, d
 
 int slice(double *mat, double *image, int xdim1, int ydim1, MAPTYPE *vol, int hold, double background)
 {
+	extern int slice_uchar(double *, double *, int, int, void **, int, int, int, int, double, double*, double *);
+    extern int slice_schar(double *, double *, int, int, void **, int, int, int, int, double, double*, double *);
+    extern int slice_short(double *, double *, int, int, void **, int, int, int, int, double, double*, double *);
+    extern int slice_ushort(double *, double *, int, int, void **, int, int, int, int, double, double*, double *);
+    extern int slice_int(double *, double *, int, int, void **, int, int, int, int, double, double*, double *);
+    extern int slice_uint(double *, double *, int, int, void **, int, int, int, int, double, double*, double *);
+    extern int slice_float(double *, double *, int, int, void **, int, int, int, int, double, double*, double *);
+    extern int slice_double(double *, double *, int, int, void **, int, int, int, int, double, double*, double *);
+    extern int slice_short_s(double *, double *, int, int, void **, int, int, int, int, double, double*, double *);
+    extern int slice_ushort_s(double *, double *, int, int, void **, int, int, int, int, double, double*, double *);
+    extern int slice_int_s(double *, double *, int, int, void **, int, int, int, int, double, double*, double *);
+    extern int slice_uint_s(double *, double *, int, int, void **, int, int, int, int, double, double*, double *);
+    extern int slice_float_s(double *, double *, int, int, void **, int, int, int, int, double, double*, double *);
+    extern int slice_double_s(double *, double *, int, int, void **, int, int, int, int, double, double*, double *);
+    
 	int sts = 1;
 	if (vol->dtype == SPM_UNSIGNED_CHAR)
 		 sts = slice_uchar(mat, image, xdim1,ydim1, vol->data, vol->dim[0],vol->dim[1],vol->dim[2],
