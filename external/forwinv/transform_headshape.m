c45854cd514688eac506f43e06969ba5d41647ab
SPM <fil.spm@ucl.ac.uk>
2008-04-15 15:39:15 +0000

    automatically pushed some recent changes from the FCDC to the FIL
    SVN r1413

--- external/forwinv/transform_headshape.m
+++ external/forwinv/transform_headshape.m
@@ -11,6 +11,9 @@ function [shape] = transform_headshape(transform, shape)
 % Copyright (C) 2008, Robert Oostenveld
 %
 % $Log: transform_headshape.m,v $
+% Revision 1.2  2008/04/15 15:33:58  roboos
+% fixed small bug (thanks to Vladimir)
+%
 % Revision 1.1  2008/04/11 16:14:03  roboos
 % first implementation, simple helper function for spm integration and symmetry with vol and sens
 %
@@ -19,7 +22,7 @@ if any(transform(4,:) ~= [0 0 0 1])
   error('invalid transformation matrix');
 end
 
-if isfield(shape, 'pnt')
+if isfield(shape, 'pnt') && ~isempty(shape.pnt)
   shape.pnt = apply(transform, shape.pnt);
 end
 
