1d9a4c62795f3b479e23c7ff9fa2bebbadaff6fd
Volkmar Glauche <volkmar.glauche@uniklinik-freiburg.de>
2008-04-16 08:35:08 +0000

    compare targets, not sources in @cfg_dep/del_in_source
    SVN r1431

--- matlabbatch/@cfg_dep/del_in_source.m
+++ matlabbatch/@cfg_dep/del_in_source.m
@@ -10,15 +10,15 @@ function cj = del_in_source(tdeps, cj)
 % Copyright (C) 2007 Freiburg Brain Imaging
 
 % Volkmar Glauche
-% $Id: del_in_source.m 1366 2008-04-11 10:24:17Z volkmar $
+% $Id: del_in_source.m 1431 2008-04-16 08:35:08Z volkmar $
 
-rev = '$Rev: 1366 $';
+rev = '$Rev: 1431 $';
 
 for k = 1:numel(tdeps)
     sitem = subsref(cj, tdeps(k).src_exbranch); % Source item to deal with
     stind = false(1,numel(sitem.sdeps));
     for l = 1:numel(sitem.sdeps)
-        stind(l) = ~isequalsource(tdeps(k), sitem.sdeps(l));
+        stind(l) = ~isequaltarget(tdeps(k), sitem.sdeps(l));
     end;
     sitem.sdeps = sitem.sdeps(stind);
     cj = subsasgn(cj, tdeps(k).src_exbranch,sitem);
