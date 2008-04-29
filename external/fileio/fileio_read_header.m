fca6f114ddda14d5555fc47c26c87440682d023d
SPM <fil.spm@ucl.ac.uk>
2008-04-29 10:33:02 +0000

    automatically pushed some recent changes from the FCDC to the FIL
    SVN r1503

--- external/fileio/fileio_read_header.m
+++ external/fileio/fileio_read_header.m
@@ -3,7 +3,7 @@ function varargout = funname(varargin);
 % this is a SPM wrapper around a FieldTrip function 
 
 % this part is variable
-prefix = 'forwinv_';
+prefix = 'fileio_';
 
 % this part is fixed
 funname   = mfilename;
