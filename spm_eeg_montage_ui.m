c31f575614d58b516d4819d078d81e2e10314612
Jean Daunizeau <jean.daunizeau@gmail.com>
2008-05-13 17:03:20 +0000

    -added editable channel labels (row names) in montage edition GUI
    SVN r1616

--- spm_eeg_montage_ui.m
+++ spm_eeg_montage_ui.m
@@ -11,10 +11,12 @@ pos2 = [40 70 pos(3)-60 pos(4) - 100];
 ha = gca;
 set(ha,'position',pos2);
 
-
 addButtons;
 
-ht = spm_uitable(num2cell(montage.tra),montage.labelorg);
+table = cat(2,montage.labelnew(:),num2cell(montage.tra));
+colnames = cat(2,'channel labels',montage.labelorg(:)');
+
+[ht,hc] = spm_uitable(table,colnames);
 set(ht,'position',pos2,...
     'units','normalized');
 
@@ -23,25 +25,15 @@ ud.ht = ht;
 ud.montage = montage;
 set(0,'userdata',ud);
 
-
 uiwait(fig)
-
 
 ud = get(0,'userdata');
 if isfield(ud,'b4MontageEdition')
     montage = ud.montage;
-    for i=1:size(montage.tra,1)
-        if i <= size(montage.tra,2)
-            montage.labelnew{i} = montage.labelorg{i}
-        else
-            montage.labelnew{i} = num2str(i);
-        end
-    end
     set(0,'userdata',ud.b4MontageEdition);
-else
+else % GUI was closed without 'OK' button
     montage = [];
 end
-
 
 
 
@@ -50,12 +42,16 @@ end
 function [ha] = doAddRow(o1,o2)
 dbstop if error
 ud = get(0,'userdata');
-M = getM(ud.ht);
+[M,newLabels] = getM(ud.ht);
 M = [M;zeros(1,size(M,2))];
+newLabels = cat(1,newLabels(:),num2str(size(M,1)));
 set(ud.ht,'units','pixels');
 pos = get(ud.ht,'Position');
 delete(ud.ht);
-ht = spm_uitable(num2cell(M),ud.montage.labelorg);
+table = cat(2,newLabels,num2cell(M));
+colnames = cat(2,'channel labels',ud.montage.labelorg(:)');
+pause(1) % This is weird, but fixes java troubles.
+ht = spm_uitable(table,colnames);
 set(ht,'position',pos,...
     'units','normalized');
 ud.ht = ht;
@@ -74,7 +70,10 @@ if ~isempty(FileName) || ~isequal(FileName,0)
         set(ud.ht,'units','pixels');
         pos = get(ud.ht,'Position');
         delete(ud.ht);
-        ht = spm_uitable(num2cell(montage.tra),montage.labelorg);
+        table = cat(2,montage.labelnew(:),num2cell(montage.tra));
+        colnames = cat(2,'channel labels',montage.labelorg(:)');
+        pause(1) % This is weird, but fixes java troubles.
+        ht = spm_uitable(table,colnames);
         set(ht,'position',pos,...
             'units','normalized');
         ud.ht = ht;
@@ -85,36 +84,42 @@ end
 function [ha] = doSave(o1,o2)
 dbstop if error
 ud = get(0,'userdata');
-M = getM(ud.ht);
+[M,newLabels] = getM(ud.ht);
 montage.tra = M;
 montage.labelorg = ud.montage.labelorg;
-for i=1:size(montage.tra,1)
-    if i <= size(montage.tra,2)
-        montage.labelnew{i} = montage.labelorg{i};
-    else
-        montage.labelnew{i} = num2str(i);
-    end
-end
+montage.labelnew = newLabels;
 uisave('montage','SPMeeg_montage.mat');
 
 function [] = doOK(o1,o2)
 dbstop if error
 ud = get(0,'userdata');
-M = getM(ud.ht);
+[M,newLabels] = getM(ud.ht);
 montage.tra = M;
 montage.labelorg = ud.montage.labelorg;
+montage.labelnew = newLabels;
 ud.b4MontageEdition = get(0,'userdata');
 ud.montage = montage;
 set(0,'userdata',ud);
 close(gcf)
 
 
-function M = getM(ht)
-M = zeros(get(ht,'NumRows'),get(ht,'NumColumns'));
+function [M,newLabels] = getM(ht)
+nnew = get(ht,'NumRows');
+nold = get(ht,'NumColumns')-1;
+M = zeros(nnew,nold);
 data = get(ht,'data');
-for i =1:get(ht,'NumRows')
-    for j =1:get(ht,'NumColumns')
-        M(i,j) = data(i,j);
+for i =1:nnew
+    if ~isempty(data(i,1))
+        newLabels{i} = data(i,1);
+    else
+        newLabels{i} = [];
+    end
+    for j =1:nold
+        if ~isempty(data(i,j+1))
+            M(i,j) = data(i,j+1);
+        else
+            M(i,j) = 0;
+        end
     end
 end
 
@@ -126,18 +131,14 @@ hAdd = uicontrol('style','pushbutton',...
     'string','Add row','callback',{@doAddRow},...
     'position',[60 20 80 20]);
 set(hAdd,'units','normalized')
-
 hLoad = uicontrol('style','pushbutton',...
     'string','load file','callback',{@doLoad},...
     'position',[180 20 80 20]);
 set(hLoad,'units','normalized')
-
-
 hSave = uicontrol('style','pushbutton',...
     'string','save as','callback',{@doSave},...
     'position',[280 20 80 20]);
 set(hSave,'units','normalized')
-
 hOK = uicontrol('style','pushbutton',...
     'string',' OK ','callback',{@doOK},...
     'position',[400 20 80 20]);
