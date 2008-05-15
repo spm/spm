4e66c1ffff20af4950d321ffd4cbe01f4245e435
Stefan Kiebel <stefan.kiebel@tu-dresden.de>
2008-05-15 17:15:46 +0000

    forgot this
    SVN r1667

---
+++ spm_eeg_Project3DCB.m
@@ -0,0 +1,37 @@
+function [xy,label] = spm_eeg_Project3DCB(D, datatype)
+% Wrapper function to a fieldtrip function to project 3D locations 
+% onto a 2D plane. 
+% FORMAT [xy,label] = spm_eeg_Project3DCB(D, datatype)
+% _______________________________________________________________________
+% Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
+
+% Stefan Kiebel, Vladimir Litvak
+% $Id: spm_eeg_Project3DCB.m 1667 2008-05-15 17:15:46Z stefan $
+
+cfg = [];
+
+switch datatype
+    case 'EEG'
+        cfg.elec = D.sensors('EEG');
+        label = cfg.elec.label;
+    case 'MEG'
+       cfg.grad = D.sensors('MEG');
+       label = cfg.grad.label;
+    otherwise
+        error('Unknown data type');
+end
+
+lay = ft_prepare_layout(cfg);
+[sel1, sel2] = spm_match_str(label, lay.label);
+
+label =lay.label(sel2)';
+xy = [lay.pos(sel2, 2), -lay.pos(sel2, 1)];
+
+nchan = size(xy, 1);
+
+xy =(xy-repmat(min(xy), nchan, 1));
+xy = xy./repmat(max(xy), nchan, 1);
+xy = xy*0.9+0.05;
+xy = xy';
+
+
