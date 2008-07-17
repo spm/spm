a4af9f58fc386345ff373e319c6d18fea36e8c57
SPM <fil.spm@ucl.ac.uk>
2008-07-17 11:16:20 +0000

    automatically pushed some recent changes from the FCDC to the FIL
    SVN r1924

--- external/forwinv/private/match_str.m
+++ external/forwinv/private/match_str.m
@@ -3,7 +3,7 @@ function [sel1, sel2] = match_str(a, b);
 % MATCH_STR looks for matching labels in two listst of strings
 % and returns the indices into both the 1st and 2nd list of the matches.
 % They will be ordered according to the first input argument.
-%  
+%
 % [sel1, sel2] = match_str(strlist1, strlist2)
 %
 % The strings can be stored as a char matrix or as an vertical array of
@@ -12,6 +12,9 @@ function [sel1, sel2] = match_str(a, b);
 % Copyright (C) 2000, Robert Oostenveld
 %
 % $Log: match_str.m,v $
+% Revision 1.7  2008/07/17 10:26:11  roboos
+% cleaned up the code a little bit, no functional change
+%
 % Revision 1.6  2006/11/06 21:11:45  roboos
 % also deal with empty [] input
 %
@@ -45,12 +48,8 @@ elseif ~iscell(b)
   b = cellstr(b);
 end
 
-% ensure that both are column vectors
-a = a(:);
-b = b(:);
-
 % regardless of what optimizations are implemented, the code should remain
-% functionally compatible to the original, which is 
+% functionally compatible to the original, which is
 % for i=1:length(a)
 %   for j=1:length(b)
 %     if strcmp(a(i),b(j))
@@ -60,17 +59,24 @@ b = b(:);
 %   end
 % end
 
+% ensure that both are column vectors
+a = a(:);
+b = b(:);
+Na = numel(a);
+Nb = numel(b);
+
 % replace all unique strings by a unique number and use the fact that
 % numeric comparisons are much faster than string comparisons
 [dum1, dum2, c] = unique([a; b]);
-a = c(1:length(a));
-b = c((length(a)+1):end);
+a = c(1:Na);
+b = c((Na+1):end);
 
 sel1 = [];
 sel2 = [];
 for i=1:length(a)
   % s = find(strcmp(a(i), b));  % for string comparison
   s = find(a(i)==b);            % for numeric comparison
-  sel1 = [sel1; repmat(i, size(s))];
   sel2 = [sel2; s];
+  s(:) = i;
+  sel1 = [sel1; s];
 end
