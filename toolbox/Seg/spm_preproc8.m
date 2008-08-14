cd0b14392acec6ec7de90f93f02c9d60845b83d9
John Ashburner <j.ashburner@ucl.ac.uk>
2008-08-14 13:35:35 +0000

    Improved stability of non-parametric segmentation.
    SVN r2007

--- toolbox/Seg/spm_preproc8.m
+++ toolbox/Seg/spm_preproc8.m
@@ -72,7 +72,7 @@ function results = spm_preproc8(obj)
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
 % John Ashburner
-% $Id: spm_preproc8.m 2005 2008-08-13 18:42:48Z john $
+% $Id: spm_preproc8.m 2007 2008-08-14 13:35:35Z john $
 
 Affine    = obj.Affine;
 tpm       = obj.tpm;
@@ -397,7 +397,7 @@ for iter=1:20,
                 for k1=1:Kb,
                     mom0 = sum(chan(n).hist(:,k1));
                     mom1 = sum(chan(n).hist(:,k1).*x);
-                    chan(n).lam(k1) = sum(chan(n).hist(:,k1).*(x-mom1./mom0).^2+1)/(mom0+1);
+                    chan(n).lam(k1) = sum(chan(n).hist(:,k1).*(x-mom1./mom0).^2+1)/(mom0+1)+1;
                 end
              end
 
@@ -405,7 +405,7 @@ for iter=1:20,
                 oll  = ll;
                 ll   = llr+llrb;
                 for n=1:N,
-                    [chan(n).lik,chan(n).alph] = smohist(chan(n).hist,chan(n).alph,chan(n).lam);
+                    [chan(n).lik,chan(n).alph] = smohist(chan(n).hist,chan(n).lam);
                     chan(n).lik                = chan(n).lik*chan(n).interscal(2);
                     chan(n).hist               = zeros(K,Kb);
                 end
@@ -431,7 +431,7 @@ for iter=1:20,
                     end
                 end
                 for n=1:N,
-                    chan(n).lik   = smohist(chan(n).hist,chan(n).alph,chan(n).lam)*chan(n).interscal(2);
+                    chan(n).lik   = smohist(chan(n).hist,chan(n).lam)*chan(n).interscal(2);
                 end
 
                 fprintf('*\t%g\t%g\t%g\n', ll,llr,llrb);
@@ -528,6 +528,7 @@ for iter=1:20,
                             end
                            %fprintf('%d\t%g\t%g\t| %g\t%g\n', z, min(wt1(:)), max(wt1(:)), min(wt2(:)), max(wt2(:)));
                         end
+                       %wt2   = max(wt2,0); % Regularise
                         b3    = bias(n).B3(z,:)';
                         Beta  = Beta  + kron(b3,spm_krutil(wt1,bias(n).B1,bias(n).B2,0));
                         Alpha = Alpha + kron(b3*b3',spm_krutil(wt2,bias(n).B1,bias(n).B2,1));
@@ -541,7 +542,7 @@ fprintf('%d\t%g\t%g\t%g\n', n, ll, llr,llrb);
                     % Gauss-Newton iteration
                     C          = bias(n).C; % Inverse covariance of priors
                     T          = bias(n).T; % Current estimate
-                    R          = diag(sqrt(Beta.^2*0.1+1)*0.01); % L-M regularisation
+                    R          = diag(sqrt(Beta.^2*1+1)*0.01); % L-M regularisation
                     bias(n).T  = T - reshape((Alpha + C + R)\(C*T(:)+Beta),size(T));
 
                     % Re-generate bias field, and compute terms of the objective function
@@ -834,13 +835,10 @@ end
 %=======================================================================
 
 %=======================================================================
-function [sig0,alph] = smohist(t0,alph,lam)
+function [sig0,alph] = smohist(t0,lam)
 sig0 = zeros(size(t0));
 n  = size(t0,1);
-if nargin<3,
-    if nargin<2,
-        alph = zeros(size(t0));
-    end
+if nargin<2,
     lam = zeros(size(t0,2),1);
     x   = (1:n)';
     for k=1:size(t0,2),
@@ -851,14 +849,19 @@ if nargin<3,
     end
 end
 
+alph = log(t0+1);
 
+% Regularisation
 G0 = spdiags(repmat([-1 2 -1],n,1),[-1 0 1],n,n);
 G0(1,1)     = 1;
 G0(end,end) = 1;
 G0          = G0'*G0;
 
+% Prevent over/underflow
+constr      = log(realmax)-1;
+
 for k=1:size(t0,2),
-    t   = t0(:,k);
+    t   = t0(:,k) + 2*sum(t0(:,k))/realmax;
     G   = G0*lam(k);
     am  = alph(:,k);
     sig = exp(am);
@@ -867,19 +870,29 @@ for k=1:size(t0,2),
     ll  = Inf;
     for it=1:60,
         gr  = sum(t)*sig - t + G*am;
-        W   = spdiags(sig*sum(t),0,n,n);
+        W   = spdiags(sig*sum(t) + abs(gr)*1e-8,0,n,n);
         H   = W + G + L;
-        am  = am - H\gr;
-        am  = am - mean(am);
-        am  = max(am,-700);
-        am  = min(am, 700);
+        da  = H\gr;
+
+        % Try to prevent over/underflow
+        sc  = 1;
+        an  = am - da;
+        while any(abs(an)>constr)
+            sc = sc*0.5;
+            an = am - sc*da;
+        end
+        am  = an - (max(an)+min(an))/2;
+        am  = max(am,-constr);
+        am  = min(am, constr);
+
+        % Softmax function
         sig = exp(am);
         sig = sig/sum(sig);
 
         if ~rem(it,4)
             oll = ll;
             ll  = -sum(log(sig+1e-6).*t) + 0.5*am'*G*am;
-            if oll-ll<n*1e-7, break; end
+            if oll-ll<n*1e-8, break; end
         end
        %if ~rem(it,1), plot(1:n,t/sum(t),'g.',1:n,sig,'k-'); drawnow; end
        %fprintf('%d\t%g\t%g\n', it, gr'*gr, ll);
@@ -887,10 +900,7 @@ for k=1:size(t0,2),
    alph(:,k) = am;
    sig0(:,k) = sig;
 end
-
-figure(6);
-plot(alph)
-drawnow
+%figure(6); plot(alph); drawnow
 %=======================================================================
 
 %=======================================================================
