bd57b2c597c1cf40190508b649cc89455849695c
SPM <fil.spm@ucl.ac.uk>
2008-09-10 09:16:16 +0000

    automatically pushed some recent changes from the FCDC to the FIL
    SVN r2067

--- external/fieldtrip/private/channelselection.m
+++ external/fieldtrip/private/channelselection.m
@@ -12,11 +12,11 @@ function [channel] = channelselection(channel, datachannel)
 % automatically removed from the channel list.
 %
 % E.g.
-%  'gui'     a graphical user interface will pop up to select the channels 
+%  'gui'     a graphical user interface will pop up to select the channels
 %  'all'     is replaced by all channels in the datafile
 %  'MEG'     is replaced by all MEG channels (works for CTF, 4D and Neuromag)
 %  'MEGREF'  is replaced by all MEG reference channels (works for CTF and 4D)
-%  'EEG'     is replaced by all channels in the CTF datafile starting with 'EEG'
+%  'EEG'     is replaced by all recognized EEG channels (this is system dependant)
 %  'EEG1020' is replaced by 'Fp1', 'Fpz', 'Fp2', 'F7', 'F3', ...
 %  'EOG'     is replaced by all recognized EOG channels
 %  'EMG'     is replaced by all channels in the datafile starting with 'EMG'
@@ -51,6 +51,10 @@ function [channel] = channelselection(channel, datachannel)
 % Copyright (C) 2003-2008, Robert Oostenveld
 %
 % $Log: channelselection.m,v $
+% Revision 1.30  2008/09/10 08:39:33  roboos
+% make stronger use of senstype and senslabel helper functions
+% now also supports egi and biosemi systems for EEG (thanks to Vladimir)
+%
 % Revision 1.29  2008/08/13 14:03:52  roboos
 % added hEOG and vEOG
 %
@@ -103,61 +107,77 @@ datachannel = datachannel(:);
 [dum, indx] = sort(indx);
 channel = channel(indx);
 
-% try to distinguish between CTF and 4D-BTi channel labels, required when selection contains 'MEG'
-isctf   = senstype(datachannel, 'ctf');
-isbti   = senstype(datachannel, 'bti');
-isnm122 = senstype(datachannel, 'neuromag122');
-
 % define the known groups with channel labels
 labelall  = datachannel;
-label1020 = {'Fp1', 'Fpz', 'Fp2', 'F7', 'F3', 'Fz', 'F4', 'F8',  'T7', 'C3', 'Cz', 'C4', 'T8',  'P7', 'P3', 'Pz', 'P4', 'P8', 'O1', 'Oz', 'O2'}';
-label1010 = {'Fp1', 'Fpz', 'Fp2', 'AF9', 'AF7', 'AF5', 'AF3', 'AF1', 'AFz', 'AF2', 'AF4', 'AF6', 'AF8', 'AF10', 'F9', 'F7', 'F5', 'F3', 'F1', 'Fz', 'F2', 'F4', 'F6', 'F8', 'F10', 'FT9', 'FT7', 'FC5', 'FC3', 'FC1', 'FCz', 'FC2', 'FC4', 'FC6', 'FT8', 'FT10', 'T9', 'T7', 'C5', 'C3', 'C1', 'Cz', 'C2', 'C4', 'C6', 'T8', 'T10', 'TP9', 'TP7', 'CP5', 'CP3', 'CP1', 'CPz', 'CP2', 'CP4', 'CP6', 'TP8', 'TP10', 'P9', 'P7', 'P5', 'P3', 'P1', 'Pz', 'P2', 'P4', 'P6', 'P8', 'P10', 'PO9', 'PO7', 'PO5', 'PO3', 'PO1', 'POz', 'PO2', 'PO4', 'PO6', 'PO8', 'PO10', 'O1', 'Oz', 'O2', 'I1', 'Iz', 'I2'}';
-label1005 = {'Fp1', 'Fpz', 'Fp2', 'AF9', 'AF7', 'AF5', 'AF3', 'AF1', 'AFz', 'AF2', 'AF4', 'AF6', 'AF8', 'AF10', 'F9', 'F7', 'F5', 'F3', 'F1', 'Fz', 'F2', 'F4', 'F6', 'F8', 'F10', 'FT9', 'FT7', 'FC5', 'FC3', 'FC1', 'FCz', 'FC2', 'FC4', 'FC6', 'FT8', 'FT10', 'T9', 'T7', 'C5', 'C3', 'C1', 'Cz', 'C2', 'C4', 'C6', 'T8', 'T10', 'TP9', 'TP7', 'CP5', 'CP3', 'CP1', 'CPz', 'CP2', 'CP4', 'CP6', 'TP8', 'TP10', 'P9', 'P7', 'P5', 'P3', 'P1', 'Pz', 'P2', 'P4', 'P6', 'P8', 'P10', 'PO9', 'PO7', 'PO5', 'PO3', 'PO1', 'POz', 'PO2', 'PO4', 'PO6', 'PO8', 'PO10', 'O1', 'Oz', 'O2', 'I1', 'Iz', 'I2', 'AFp9h', 'AFp7h', 'AFp5h', 'AFp3h', 'AFp1h', 'AFp2h', 'AFp4h', 'AFp6h', 'AFp8h', 'AFp10h', 'AFF9h', 'AFF7h', 'AFF5h', 'AFF3h', 'AFF1h', 'AFF2h', 'AFF4h', 'AFF6h', 'AFF8h', 'AFF10h', 'FFT9h', 'FFT7h', 'FFC5h', 'FFC3h', 'FFC1h', 'FFC2h', 'FFC4h', 'FFC6h', 'FFT8h', 'FFT10h', 'FTT9h', 'FTT7h', 'FCC5h', 'FCC3h', 'FCC1h', 'FCC2h', 'FCC4h', 'FCC6h', 'FTT8h', 'FTT10h', 'TTP9h', 'TTP7h', 'CCP5h', 'CCP3h', 'CCP1h', 'CCP2h', 'CCP4h', 'CCP6h', 'TTP8h', 'TTP10h', 'TPP9h', 'TPP7h', 'CPP5h', 'CPP3h', 'CPP1h', 'CPP2h', 'CPP4h', 'CPP6h', 'TPP8h', 'TPP10h', 'PPO9h', 'PPO7h', 'PPO5h', 'PPO3h', 'PPO1h', 'PPO2h', 'PPO4h', 'PPO6h', 'PPO8h', 'PPO10h', 'POO9h', 'POO7h', 'POO5h', 'POO3h', 'POO1h', 'POO2h', 'POO4h', 'POO6h', 'POO8h', 'POO10h', 'OI1h', 'OI2h', 'Fp1h', 'Fp2h', 'AF9h', 'AF7h', 'AF5h', 'AF3h', 'AF1h', 'AF2h', 'AF4h', 'AF6h', 'AF8h', 'AF10h', 'F9h', 'F7h', 'F5h', 'F3h', 'F1h', 'F2h', 'F4h', 'F6h', 'F8h', 'F10h', 'FT9h', 'FT7h', 'FC5h', 'FC3h', 'FC1h', 'FC2h', 'FC4h', 'FC6h', 'FT8h', 'FT10h', 'T9h', 'T7h', 'C5h', 'C3h', 'C1h', 'C2h', 'C4h', 'C6h', 'T8h', 'T10h', 'TP9h', 'TP7h', 'CP5h', 'CP3h', 'CP1h', 'CP2h', 'CP4h', 'CP6h', 'TP8h', 'TP10h', 'P9h', 'P7h', 'P5h', 'P3h', 'P1h', 'P2h', 'P4h', 'P6h', 'P8h', 'P10h', 'PO9h', 'PO7h', 'PO5h', 'PO3h', 'PO1h', 'PO2h', 'PO4h', 'PO6h', 'PO8h', 'PO10h', 'O1h', 'O2h', 'I1h', 'I2h', 'AFp9', 'AFp7', 'AFp5', 'AFp3', 'AFp1', 'AFpz', 'AFp2', 'AFp4', 'AFp6', 'AFp8', 'AFp10', 'AFF9', 'AFF7', 'AFF5', 'AFF3', 'AFF1', 'AFFz', 'AFF2', 'AFF4', 'AFF6', 'AFF8', 'AFF10', 'FFT9', 'FFT7', 'FFC5', 'FFC3', 'FFC1', 'FFCz', 'FFC2', 'FFC4', 'FFC6', 'FFT8', 'FFT10', 'FTT9', 'FTT7', 'FCC5', 'FCC3', 'FCC1', 'FCCz', 'FCC2', 'FCC4', 'FCC6', 'FTT8', 'FTT10', 'TTP9', 'TTP7', 'CCP5', 'CCP3', 'CCP1', 'CCPz', 'CCP2', 'CCP4', 'CCP6', 'TTP8', 'TTP10', 'TPP9', 'TPP7', 'CPP5', 'CPP3', 'CPP1', 'CPPz', 'CPP2', 'CPP4', 'CPP6', 'TPP8', 'TPP10', 'PPO9', 'PPO7', 'PPO5', 'PPO3', 'PPO1', 'PPOz', 'PPO2', 'PPO4', 'PPO6', 'PPO8', 'PPO10', 'POO9', 'POO7', 'POO5', 'POO3', 'POO1', 'POOz', 'POO2', 'POO4', 'POO6', 'POO8', 'POO10', 'OI1', 'OIz', 'OI2'}';
+label1020 = senslabel('eeg1020'); % use external helper function
+label1010 = senslabel('eeg1010'); % use external helper function
+label1005 = senslabel('eeg1005'); % use external helper function
 labelchwilla = {'Fz', 'Cz', 'Pz', 'F7', 'F8', 'LAT', 'RAT', 'LT', 'RT', 'LTP', 'RTP', 'OL', 'OR', 'FzA', 'Oz', 'F7A', 'F8A', 'F3A', 'F4A', 'F3', 'F4', 'P3', 'P4', 'T5', 'T6', 'P3P', 'P4P'}';
 labelbham = {'P9', 'PPO9h', 'PO7', 'PPO5h', 'PPO3h', 'PO5h', 'POO9h', 'PO9', 'I1', 'OI1h', 'O1', 'POO1', 'PO3h', 'PPO1h', 'PPO2h', 'POz', 'Oz', 'Iz', 'I2', 'OI2h', 'O2', 'POO2', 'PO4h', 'PPO4h', 'PO6h', 'POO10h', 'PO10', 'PO8', 'PPO6h', 'PPO10h', 'P10', 'P8', 'TPP9h', 'TP7', 'TTP7h', 'CP5', 'TPP7h', 'P7', 'P5', 'CPP5h', 'CCP5h', 'CP3', 'P3', 'CPP3h', 'CCP3h', 'CP1', 'P1', 'Pz', 'CPP1h', 'CPz', 'CPP2h', 'P2', 'CPP4h', 'CP2', 'CCP4h', 'CP4', 'P4', 'P6', 'CPP6h', 'CCP6h', 'CP6', 'TPP8h', 'TP8', 'TPP10h', 'T7', 'FTT7h', 'FT7', 'FC5', 'FCC5h', 'C5', 'C3', 'FCC3h', 'FC3', 'FC1', 'C1', 'CCP1h', 'Cz', 'FCC1h', 'FCz', 'FFC1h', 'Fz', 'FFC2h', 'FC2', 'FCC2h', 'CCP2h', 'C2', 'C4', 'FCC4h', 'FC4', 'FC6', 'FCC6h', 'C6', 'TTP8h', 'T8', 'FTT8h', 'FT8', 'FT9', 'FFT9h', 'F7', 'FFT7h', 'FFC5h', 'F5', 'AFF7h', 'AF7', 'AF5h', 'AFF5h', 'F3', 'FFC3h', 'F1', 'AF3h', 'Fp1', 'Fpz', 'Fp2', 'AFz', 'AF4h', 'F2', 'FFC4h', 'F4', 'AFF6h', 'AF6h', 'AF8', 'AFF8h', 'F6', 'FFC6h', 'FFT8h', 'F8', 'FFT10h', 'FT10'};
 labelref  = {'M1', 'M2', 'LM', 'RM', 'A1', 'A2'}';
 labeleog  = datachannel(strncmp('EOG', datachannel, length('EOG')));               % anything that starts with EOG
 labeleog  = {labeleog{:} 'HEOG', 'VEOG', 'VEOG-L', 'VEOG-R', 'hEOG', 'vEOG'}';     % or any of these
 labelemg  = datachannel(strncmp('EMG', datachannel, length('EMG')));
-labeleeg  = datachannel(strncmp('EEG', datachannel, length('EEG')));
-
-if isctf
-  labelmeg = datachannel(strncmp('M'  , datachannel, length('M'  )));	% all CTF MEG channels start with "M"
-  labelmref = [datachannel(strncmp('B'  , datachannel, 1)); 
-               datachannel(strncmp('G'  , datachannel, 1)); 
-               datachannel(strncmp('P'  , datachannel, 1)); 
-               datachannel(strncmp('Q'  , datachannel, 1)); 
-               datachannel(strncmp('R'  , datachannel, length('G'  )))]; % all CTF reference channels start with B, G, P, Q or R
-elseif isbti
-  labelmeg = datachannel(strncmp('A'  , datachannel, 1));	% all 4D-BTi MEG channels start with "A"
-  labelmref = [datachannel(strncmp('M'  , datachannel, 1)); 
-               datachannel(strncmp('G'  , datachannel, 1))]; % all 4D-BTi reference channels start with M or G
-elseif isnm122
-  labelmeg = datachannel(strncmp('MEG', datachannel, length('MEG')));
-else
-  labelmeg = [];
-end
-
-labelmz   = datachannel(strncmp('MZ' , datachannel, length('MZ' )));	% central MEG channels
-labelml   = datachannel(strncmp('ML' , datachannel, length('ML' )));	% left    MEG channels
-labelmr   = datachannel(strncmp('MR' , datachannel, length('MR' )));	% right   MEG channels
-labelmlc  = datachannel(strncmp('MLC', datachannel, length('MLC')));
-labelmlf  = datachannel(strncmp('MLF', datachannel, length('MLF')));
-labelmlo  = datachannel(strncmp('MLO', datachannel, length('MLO')));
-labelmlp  = datachannel(strncmp('MLP', datachannel, length('MLP')));
-labelmlt  = datachannel(strncmp('MLT', datachannel, length('MLT')));
-labelmrc  = datachannel(strncmp('MRC', datachannel, length('MRC')));
-labelmrf  = datachannel(strncmp('MRF', datachannel, length('MRF')));
-labelmro  = datachannel(strncmp('MRO', datachannel, length('MRO')));
-labelmrp  = datachannel(strncmp('MRP', datachannel, length('MRP')));
-labelmrt  = datachannel(strncmp('MRT', datachannel, length('MRT')));
-labelmzc  = datachannel(strncmp('MZC', datachannel, length('MZC')));
-labelmzf  = datachannel(strncmp('MZF', datachannel, length('MZF')));
-labelmzo  = datachannel(strncmp('MZO', datachannel, length('MZO')));
-labelmzp  = datachannel(strncmp('MZP', datachannel, length('MZP')));
 labellfp  = datachannel(strncmp('lfp', datachannel, length('lfp')));
 labelmua  = datachannel(strncmp('mua', datachannel, length('mua')));
 labelspike  = datachannel(strncmp('spike', datachannel, length('spike')));
+
+% initialize all the system-specific variables to empty
+labelmeg   = [];
+labelmref  = [];
+labeleeg   = [];
+
+switch senstype(datachannel)
+
+  case {'ctf', 'ctf275', 'ctf151', 'ctf275_planar', 'ctf151_planar'}
+    % all CTF MEG channels start with "M"
+    % all CTF reference channels start with B, G, P, Q or R
+    % all CTF EEG channels start with "EEG"
+    labelmeg = datachannel(strncmp('M'  , datachannel, length('M'  )));
+    labelmref = [datachannel(strncmp('B'  , datachannel, 1));
+      datachannel(strncmp('G'  , datachannel, 1));
+      datachannel(strncmp('P'  , datachannel, 1));
+      datachannel(strncmp('Q'  , datachannel, 1));
+      datachannel(strncmp('R'  , datachannel, length('G'  )))];
+    labeleeg  = datachannel(strncmp('EEG', datachannel, length('EEG')));
+
+    % Not sure whether this should be here or outside the switch or
+    % whether these specifications should be supported for systems
+    % other than CTF.
+    labelmz   = datachannel(strncmp('MZ' , datachannel, length('MZ' )));	% central MEG channels
+    labelml   = datachannel(strncmp('ML' , datachannel, length('ML' )));	% left    MEG channels
+    labelmr   = datachannel(strncmp('MR' , datachannel, length('MR' )));	% right   MEG channels
+    labelmlc  = datachannel(strncmp('MLC', datachannel, length('MLC')));
+    labelmlf  = datachannel(strncmp('MLF', datachannel, length('MLF')));
+    labelmlo  = datachannel(strncmp('MLO', datachannel, length('MLO')));
+    labelmlp  = datachannel(strncmp('MLP', datachannel, length('MLP')));
+    labelmlt  = datachannel(strncmp('MLT', datachannel, length('MLT')));
+    labelmrc  = datachannel(strncmp('MRC', datachannel, length('MRC')));
+    labelmrf  = datachannel(strncmp('MRF', datachannel, length('MRF')));
+    labelmro  = datachannel(strncmp('MRO', datachannel, length('MRO')));
+    labelmrp  = datachannel(strncmp('MRP', datachannel, length('MRP')));
+    labelmrt  = datachannel(strncmp('MRT', datachannel, length('MRT')));
+    labelmzc  = datachannel(strncmp('MZC', datachannel, length('MZC')));
+    labelmzf  = datachannel(strncmp('MZF', datachannel, length('MZF')));
+    labelmzo  = datachannel(strncmp('MZO', datachannel, length('MZO')));
+    labelmzp  = datachannel(strncmp('MZP', datachannel, length('MZP')));
+
+  case {'bti248', 'bti148', 'bti248_planar', 'bti148_planar'}
+    % all 4D-BTi MEG channels start with "A"
+    % all 4D-BTi reference channels start with M or G
+    labelmeg = datachannel(strncmp('A'  , datachannel, 1));
+    labelmref = [datachannel(strncmp('M'  , datachannel, 1));
+      datachannel(strncmp('G'  , datachannel, 1))];
+
+  case {'neuromag306', 'neuromag122'}
+    % all neuromag MEG channels start with MEG
+    labelmeg = datachannel(strncmp('MEG', datachannel, length('MEG')));
+
+  case {'biosemi128', 'biosemi256', 'egi64', 'egi128', 'egi256', 'ext1020'}
+    % use an external helper function to define the list with EEG channel names
+    labeleeg = senslabel(senstype(datachannel));
+
+end
 
 % figure out if there are bad channels or channel groups that should be excluded
 findbadchannel = strncmp('-', channel, length('-'));      % bad channels start with '-'
@@ -240,7 +260,7 @@ channel([
   findmua
   findspike
   findgui
-]) = [];
+  ]) = [];
 
 % add the full channel labels to the channel list
 if findall,        channel = [channel; labelall]; end
@@ -286,8 +306,8 @@ chanindx = match_str(channel, datachannel);
 channel  = channel(chanindx);
 
 if findgui
-    indx = select_channel_list(datachannel, match_str(datachannel, channel), 'Select channels');
-    channel = datachannel(indx);
+  indx = select_channel_list(datachannel, match_str(datachannel, channel), 'Select channels');
+  channel = datachannel(indx);
 end
 
 % remove channels that occur more than once, this sorts the channels alphabetically
@@ -296,5 +316,3 @@ end
 % undo the sorting, make the order identical to that of the data channels
 [dataindx, indx] = match_str(datachannel, channel);
 channel = channel(indx);
-
-
