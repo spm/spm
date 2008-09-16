b9123ce259635ef724c3cd4c241cb54a870ba24b
SPM <fil.spm@ucl.ac.uk>
2008-09-16 18:43:32 +0000

    added new fieldtrip chantype function plus spm wrapper
    SVN r2102

---
+++ external/fieldtrip/private/chantype.m
@@ -0,0 +1,80 @@
+function type = chantype(hdr)
+
+% CHANTYPE determines for each channel what type it is, e.g. planar/axial gradiometer or magnetometer
+%
+% Use as
+%   type = chantype(hdr)
+%   type = chantype(sens)
+%   type = chantype(label)
+
+% Copyright (C) 2008, Robert Oostenveld
+%
+% $Log: chantype.m,v $
+% Revision 1.2  2008/09/10 10:06:07  roboos
+% added ctf headloc
+%
+% Revision 1.1  2008/09/10 10:04:22  roboos
+% new function
+%
+
+type = cell(size(hdr.label));
+for i=1:length(type)
+  type{i} = 'unknown';
+end
+
+if senstype(hdr, 'neuromag')
+  % channames-KI is the channel kind, 1=meg, 202=eog, 2=eeg, 3=trigger (I am nut sure, but have inferred this from a single test file)
+  % chaninfo-TY is the Coil type (0=magnetometer, 1=planar gradiometer)
+  if isfield(hdr, 'orig') && isfield(hdr.orig, 'channames')
+    for sel=find(hdr.orig.channames.KI(:)==202)'
+      type{sel} = 'eog';
+    end
+    for sel=find(hdr.orig.channames.KI(:)==2)'
+      type{sel} = 'eeg';
+    end
+    for sel=find(hdr.orig.channames.KI(:)==3)'
+      type{sel} = 'trigger';
+    end
+    % determinge the MEG channel subtype
+    selmeg=find(hdr.orig.channames.KI(:)==1)'
+    for i=1:length(selmeg)
+      if hdr.orig.chaninfo.TY(i)==0
+        type{selmeg(i)} = 'magnetometer';
+      elseif hdr.orig.chaninfo.TY(i)==1
+        type{selmeg(i)} = 'planar';
+      end
+    end
+  end
+
+elseif senstype(hdr, 'ctf')
+  % meg channels are 5, refmag 0, refgrad 1, adcs 18, trigger 11, eeg 9
+  if isfield(hdr, 'orig') && isfield(hdr.orig, 'sensType')
+    origSensType = hdr.orig.sensType;
+  elseif isfield(hdr, 'orig') && isfield(hdr.orig, 'res4')
+    origSensType =  [hdr.orig.res4.senres.sensorTypeIndex];
+  end
+
+  for sel=find(origSensType(:)==5)'
+    type{sel} = 'meg';
+  end
+  for sel=find(origSensType(:)==0)'
+    type{sel} = 'refmag';
+  end
+  for sel=find(origSensType(:)==1)'
+    type{sel} = 'refgrad';
+  end
+  for sel=find(origSensType(:)==18)'
+    type{sel} = 'adc';
+  end
+  for sel=find(origSensType(:)==11)'
+    type{sel} = 'trigger';
+  end
+  for sel=find(origSensType(:)==9)'
+    type{sel} = 'eeg';
+  end
+  for sel=find(origSensType(:)==29)'
+    type{sel} = 'headloc';
+  end
+
+end % senstype
+
