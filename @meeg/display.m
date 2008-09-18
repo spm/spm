201481c80e3e49cdd886ba0a0e694f79622d9bca
Vladimir Litvak <v.litvak@ucl.ac.uk>
2008-09-18 11:14:46 +0000

    Fixes minor problem with display
    SVN r2113

--- @meeg/display.m
+++ @meeg/display.m
@@ -1,20 +1,24 @@
-function display(this)
+function str = display(this)
 % Method for displaying information about an meeg object
 % FORMAT display(this)
 % _______________________________________________________________________
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
 % Vladimir Litvak
-% $Id: display.m 1621 2008-05-13 18:43:32Z vladimir $
+% $Id: display.m 2113 2008-09-18 11:14:46Z vladimir $
 
-disp('SPM M/EEG data object');
-disp(['Type: ' type(this)]);
-disp([num2str(nconditions(this)), ' conditions']);
-disp([num2str(nchannels(this)), ' channels']);
-disp([num2str(nsamples(this)), ' samples/trial']);
-disp([num2str(ntrials(this)), ' trials']);
-disp(['Sampling frequency: ' num2str(fsample(this)) ' Hz']);
-disp(['Loaded from file ' fullfile(this.path, this.fname)]);
-disp(' ');
-disp('Use the syntax D(channels, samples, trials) to access the data');
-disp('Type "methods(''meeg'')" for the list of methods performing other operations with the object');
+str = ['SPM M/EEG data object\n'...
+    'Type: ' type(this) '\n'...
+    num2str(nconditions(this)), ' conditions\n'...
+    num2str(nchannels(this)), ' channels\n'...
+    num2str(nsamples(this)), ' samples/trial\n'...
+    num2str(ntrials(this)), ' trials\n'...
+    'Sampling frequency: ' num2str(fsample(this)) ' Hz\n'...
+    'Loaded from file  %s\n\n'...
+    'Use the syntax D(channels, samples, trials) to access the data\n'...
+    'Type "methods(''meeg'')" for the list of methods performing other operations with the object\n'...
+    ];
+
+str = sprintf(str, fullfile(this.path, this.fname));
+
+disp(str);
