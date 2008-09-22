f4076bd998da915d082eac0fb5253b2ece69a512
SPM <fil.spm@ucl.ac.uk>
2008-09-22 21:16:18 +0000

    automatically pushed some recent changes from the FCDC to the FIL
    SVN r2150

--- external/fieldtrip/private/fieldtripdefs.m
+++ external/fieldtrip/private/fieldtripdefs.m
@@ -2,6 +2,9 @@
 % contains some defaults and path settings
 %
 % $Log: fieldtripdefs.m,v $
+% Revision 1.2  2008/09/22 20:58:57  roboos
+% added modules: fileio, forwinv, preproc
+%
 % Revision 1.1  2008/09/22 20:02:50  roboos
 % initial version, contains fixpath
 %
@@ -10,5 +13,20 @@ try
   % this is to help in converting the directory layout
   % while at the same time maintainiong backward compatibility
   hastoolbox('fixpath', 1, 1);
+end
+
+try
+  % numerous functions depend on this module
+  hastoolbox('fileio', 1, 1);
+end
+
+try
+  % numerous functions depend on this module
+  hastoolbox('forwinv', 1, 1);
+end
+
+try
+  % numerous functions depend on this module
+  hastoolbox('preproc', 1, 1);
 end
 
