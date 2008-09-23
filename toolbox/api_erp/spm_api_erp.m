b8f97f508297459e337516fcb6b87356a50dac84
Guillaume Flandin <g.flandin@ucl.ac.uk>
2008-09-23 10:23:45 +0000

    * A temporary redirection from api_erp to dcm_meeg * Updated spm_Menu.fig accordingly and replaced Defaults button by Batch.
    SVN r2155

---
+++ toolbox/api_erp/spm_api_erp.m
@@ -0,0 +1,8 @@
+function varargout = spm_api_erp(varargin)
+% api_erp moved to dcm_meep.
+% This function is just here for the transition and will disappear in a week.
+
+% $Id: spm_api_erp.m 2155 2008-09-23 10:23:45Z guillaume $
+
+spm('alert!',sprintf(['api_erp has now its own button in\n\nEEG > '...
+'Spatio-temporal modelling > DCM']),'api_erp toolbox moved',spm('CmdLine'),1);
