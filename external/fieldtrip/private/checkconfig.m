c7b2b081ec686404f44569bea58f1716575ddf84
SPM <fil.spm@ucl.ac.uk>
2008-09-30 14:09:13 +0000

    automatically pushed some recent changes from the FCDC to the FIL
    SVN r2252

--- external/fieldtrip/private/checkconfig.m
+++ external/fieldtrip/private/checkconfig.m
@@ -2,8 +2,8 @@ function [cfg] = checkconfig(cfg, varargin)
 
 % CHECKCONFIG checks the input cfg of the main FieldTrip functions. It
 % checks whether the cfg contains all the required options, it gives a
-% warning when renamed or deprecated options are used, and it makes sure 
-% no forbidden options are used. If necessary and possible, this function 
+% warning when renamed or deprecated options are used, and it makes sure
+% no forbidden options are used. If necessary and possible, this function
 % will adjust the cfg to the input requirements.
 %
 % If the input cfg does NOT correspond to the requirements, this function
@@ -19,12 +19,16 @@ function [cfg] = checkconfig(cfg, varargin)
 %   required          = {'opt1', 'opt2', etc.} % list the required options
 %   deprecated        = {'opt1', 'opt2', etc.} % list the deprecated options
 %   forbidden         = {'opt1', 'opt2', etc.} % list the forbidden options
+%   tracking          = 'on', 'off' or 'report'
 %
 % See also CHECKDATA
 
-% Copyright (C) 2007, Robert Oostenveld
+% Copyright (C) 2007-2008, Robert Oostenveld
 %
 % $Log: checkconfig.m,v $
+% Revision 1.5  2008/09/30 13:05:19  roboos
+% adedd first version of configuration tracking for testing
+%
 % Revision 1.4  2008/09/23 12:05:33  sashae
 % some small changes; checkconfig can now handle empty cfgs
 %
@@ -45,7 +49,7 @@ renamedval      = keyval('renamedval',  varargin);
 required        = keyval('required',    varargin);
 deprecated      = keyval('deprecated',  varargin);
 forbidden       = keyval('forbidden',   varargin);
-
+tracking        = keyval('tracking',    varargin); if isempty(tracking), tracking = 'no'; end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % rename old to new options, give warning
@@ -98,4 +102,30 @@ if ~isempty(forbidden)
   if any(ismember(forbidden, fieldsused))
     error(sprintf('The field cfg.%s is forbidden\n', forbidden{[ismember(forbidden, fieldsused)]}));
   end
+end
+
+try
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % switch configuration tracking on/off
+  % FIXME dit is slechts een eerste versie "voor de leuk"
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  switch tracking
+    case 'on'
+      if isa(cfg, 'struct')
+        cfg = configuration(cfg);
+      else
+        warning('cannot convert "%s" to a configuration object for tracking', class(cfg));
+      end
+
+    case 'report'
+      access(cfg);
+
+    case 'off'
+      cfg = struct(cfg);
+
+    otherwise
+      % do nothing
+  end
+catch
+  disp(lasterr);
 end
