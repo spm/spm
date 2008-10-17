0df4bbe613e1b2b356edca1be332c5165a6691b4
Vladimir Litvak <v.litvak@ucl.ac.uk>
2008-10-17 11:50:35 +0000

    Another improvement - added 2D projection of sensors
    SVN r2351

--- toolbox/MEEGtools/spm_eeg_copygrad.m
+++ toolbox/MEEGtools/spm_eeg_copygrad.m
@@ -3,7 +3,7 @@
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
 % Vladimir Litvak
-% $Id: spm_eeg_copygrad.m 2350 2008-10-17 11:45:42Z vladimir $
+% $Id: spm_eeg_copygrad.m 2351 2008-10-17 11:50:35Z vladimir $
 
 spmfile = spm_select(1, '\.mat$', 'Select an SPM8 EEG file');
 
@@ -15,5 +15,18 @@ hdr = fileio_read_header(ctffile);
 D = spm_eeg_load(spmfile);
 D = sensors(D, 'MEG', forwinv_convert_units(hdr.grad, 'mm'));
 D = fiducials(D, forwinv_convert_units(fileio_read_headshape(ctffile), 'mm'));
+
+% Create 2D positions for MEG (when there are no EEG sensors)
+% by projecting the 3D positions to 2D
+if ~isempty(strmatch('MEG', D.chantype, 'exact')) &&...
+    ~isempty(D.sensors('MEG')) && isempty(D.sensors('EEG'))
+    S = [];
+    S.task = 'project3D';
+    S.modality = 'MEG';
+    S.updatehistory = 1;
+    S.D = D;
+    
+    D = spm_eeg_prep(S);
+end
 
 save(D);
