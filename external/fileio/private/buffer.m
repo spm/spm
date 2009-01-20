3515a8c03e730a2f2ab69f44c3b6b03238b20baf
SPM <fil.spm@ucl.ac.uk>
2009-01-20 21:00:44 +0000

    automatically pushed some recent changes from the FCDC to the FIL
    SVN r2633

--- external/fileio/private/buffer.m
+++ external/fileio/private/buffer.m
@@ -46,6 +46,9 @@ function [varargout] = buffer(varargin)
 % Copyright (C) 2008, Robert Oostenveld
 %
 % $Log: buffer.m,v $
+% Revision 1.6  2009/01/20 15:38:37  roboos
+% try to compile the mex file on the fly
+%
 % Revision 1.5  2008/10/24 07:32:05  roboos
 % should be a function, not a script
 %
@@ -62,6 +65,47 @@ function [varargout] = buffer(varargin)
 % new function, only placeholder for the documentation since the actual implementation is in a mex file
 %
 
-error(sprintf('could not locate MEX file for %s', mfilename))
+% remember the original working directory
+pwdir = pwd;
 
+try
+  warning('trying to compile MEX file for %s', mfilename);
+  fname = mfilename('fullpath');
+  [mexdir, mexname] = fileparts(fname);
+  cd(mexdir);
 
+  % the following is specific for this particular mex file
+  mex -I../src -I../../pthreads-win32/include -c buffer_gethdr.c
+  mex -I../src -I../../pthreads-win32/include -c buffer_getdat.c
+  mex -I../src -I../../pthreads-win32/include -c buffer_getevt.c
+  mex -I../src -I../../pthreads-win32/include -c buffer_getprp.c
+  mex -I../src -I../../pthreads-win32/include -c buffer_puthdr.c
+  mex -I../src -I../../pthreads-win32/include -c buffer_putdat.c
+  mex -I../src -I../../pthreads-win32/include -c buffer_putevt.c
+  mex -I../src -I../../pthreads-win32/include -c buffer_putprp.c
+  mex -I../src -I../../pthreads-win32/include -c buffer_flushhdr.c
+  mex -I../src -I../../pthreads-win32/include -c buffer_flushdat.c
+  mex -I../src -I../../pthreads-win32/include -c buffer_flushevt.c
+
+  % this is for the demo acquisition threads
+  mex -I../src -I../../pthreads-win32/include -c ../test/sinewave.c
+  mex -I../src -I../../pthreads-win32/include -c ../test/event.c
+
+  % link all the objects together into a mex file
+  if ispc
+    % this is needed for Borland on windows, but not for other platforms
+    mex -I../src -I../../pthreads-win32/include -c ../src/gettimeofday.c -I../src -I../../pthreads-win32/include
+    % link all the objects together into a mex file
+    mex -I../src -I../../pthreads-win32/include -L../src -L../../pthreads-win32/lib buffer.c event.obj sinewave.obj gettimeofday.obj buffer_gethdr.obj buffer_getdat.obj buffer_getevt.obj buffer_getprp.obj buffer_flushhdr.obj buffer_flushdat.obj buffer_flushevt.obj buffer_puthdr.obj buffer_putdat.obj buffer_putevt.obj buffer_putprp.obj -lbuffer -lpthreadVC2.bcb
+  else
+    % link all the objects together into a mex file
+    mex -I../src -L../src buffer.c event.o sinewave.o buffer_gethdr.o buffer_getdat.o buffer_getevt.o buffer_getprp.o buffer_flushhdr.o buffer_flushdat.o buffer_flushevt.o buffer_puthdr.o buffer_putdat.o buffer_putevt.o buffer_putprp.o -lbuffer
+  end
+
+catch
+  disp(lasterr);
+  error('could not locate MEX file for %s', mfilename);
+end
+
+% return to the original working directory
+cd(pwdir);
