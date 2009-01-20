d17323e0f355d8c29681801bb679e97eee0bd8cd
Maria Joao Rosa <mariajoao@gmail.com>
2009-01-20 16:32:17 +0000

    SVN r2628

--- spm_api_bmc.m
+++ spm_api_bmc.m
@@ -1,74 +1,87 @@
-function spm_api_bmc
-% API to slect and compare DCMs using Bayesian model comparison
-% FORMAT spm_api_bmc
+function out=spm_api_bmc(F,N,alpha,exp_r,xp)
+% API to select and compare DCMs using Bayesian model comparison
+% FORMAT out=spm_api_bmc(F,N,alpha,exp_r,xp)
+%
+%INPUT :
+% F     - Matrix/Vector of log model evidnces
+%
+%
 %__________________________________________________________________________
-% Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
+% 
+%Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
 % Karl Friston
-% $Id: spm_api_bmc.m 2209 2008-09-26 18:58:41Z karl $
+% $Id: spm_api_bmc.m 2628 2009-01-20 16:32:17Z maria $
 
-% get DCM.mat files
-%--------------------------------------------------------------------------
-f = cellstr(spm_select(Inf,'mat','please select DCM files'));
-if length(f) < 2
-    msgbox('please select more than one DCM')
-    return
+if nargin < 3
+    inf_method='FFX';
 else
-    for i=1:length(f)
-        [p{i},f{i}] = fileparts(f{i});
-    end
+    inf_method='RFX';
 end
+nm = length(N);
 
-                 
-% get Free energy approximation to log-evidence
-%--------------------------------------------------------------------------
-F     = [];
-N     = {};
-for i = 1:length(f)
-    
-    name = fullfile(p{i},f{i});
-    DCM  = load(name,'-mat');
-    DCM  = DCM.DCM;
-    try 
-        name = DCM.name;
-    end
-    try
-        [pth, name] = fileparts(name);
-    end
-    try
-        F(end + 1) = DCM.F;
-        N{end + 1} = name;
-    end
+
+switch inf_method
+
+    case ('FFX')
+        % compute conditional probability of DCMs under flat priors.
+        %--------------------------------------------------------------------------
+        F    = F - min(F);
+        i    = F < (max(F) - 32);
+        P    = F;
+        P(i) = max(F) - 32;
+        P    = P - min(P);
+        P    = exp(P);
+        P    = P/sum(P);
+
+        % display results
+        %--------------------------------------------------------------------------
+        Fgraph  = spm_figure('GetWin','Graphics'); figure(Fgraph); clf
+
+        
+        subplot(2,1,1)
+        bar(1:nm,F)
+        set(gca,'XTick',1:nm)
+        set(gca,'XTickLabel',1:nm)
+        ylabel('Log-evidence (relative)','Fontsize',14)
+        xlabel('Models','Fontsize',14)
+        title({'Bayesian Model Selection'},'Fontsize',14)
+        axis square
+        grid on
+        subplot(2,1,2)
+        bar(1:nm,P)
+        set(gca,'XTick',1:nm)
+        set(gca,'XTickLabel',1:nm)
+        ylabel('Posterior Model Probability','Fontsize',14)
+        title('Bayesian Model Selection','Fontsize',14)
+        xlabel('Models','Fontsize',14)
+        axis square
+        grid on
+        out=P;
+        
+    case ('RFX')
+        
+          Fgraph  = spm_figure('GetWin','Graphics'); figure(Fgraph); clf
+
+            subplot(2,1,1)
+            bar(1:length(N),exp_r)
+            set(gca,'XTick',1:length(N))
+            set(gca,'XTickLabel',1:nm)
+            ylabel('Expected Posterior Probability','Fontsize',14)
+            xlabel('Models','Fontsize',14)
+            title('Bayesian Model Selection','Fontsize',14)
+            axis square
+            grid on
+            
+            subplot(2,1,2)
+            bar(1:length(N),xp')
+            set(gca,'XTick',1:length(N))
+            set(gca,'XTickLabel',1:nm)
+            ylabel('Exceedance Probability','Fontsize',14)
+            xlabel('Models','Fontsize',14)
+            title('Bayesian Model Selection','Fontsize',14)
+            axis square
+            grid on
+            out=[];
+        
 end
-
-% compute conditional probability of DCMs under flat priors.
-%--------------------------------------------------------------------------
-F    = F - min(F);
-i    = F < (max(F) - 32);
-P    = F;
-P(i) = max(F) - 32;
-P    = P - min(P);
-P    = exp(P);
-P    = P/sum(P);
-
-   
-% display results
-%--------------------------------------------------------------------------   
-Fgraph  = spm_figure('GetWin','Graphics'); figure(Fgraph); clf
-
-subplot(2,1,1)
-bar(1:length(N),F)
-set(gca,'XTick',1:length(N))
-set(gca,'XTickLabel',N)
-ylabel('log-evidence (relative)')
-title('Bayesian model comparison')
-axis square
-grid on
-
-subplot(2,1,2)
-bar(1:length(N),P)
-set(gca,'XTick',1:length(N))
-set(gca,'XTickLabel',N)
-ylabel('probability')
-axis square
-grid on
