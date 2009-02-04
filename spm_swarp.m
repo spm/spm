0ed2c6f8ebe75f55e634aa4dd01b4b9b48a972e0
John Ashburner <j.ashburner@ucl.ac.uk>
2009-02-04 15:19:07 +0000

    Added an untested function for Vlad.
    SVN r2686

---
+++ spm_swarp.m
@@ -0,0 +1,43 @@
+function that = spm_swarp(this,def,M)
+% Warp surface.
+% FORMAT that = spm_swarp(this,def)
+% this - a gifti object
+% def  - a deformation (nifti object or filename)
+% that - the warped gifti object
+%
+% FORMAT that = spm_swarp(this,def,M)
+% this - a gifti object
+% def  - a deformation field (nx*ny*nz*1*3)
+% M    - mapping from voxels to world, for deformation field
+% that - the warped gifti object
+%
+%__________________________________________________________________________
+% Copyright (C) 2009 Wellcome Trust Centre for Neuroimaging
+
+% John Ashburner
+% $Id: spm_swarp.m 2686 2009-02-04 15:19:07Z john $
+
+if nargin<2, that = this; return; end
+
+if ischar(def) || isa(def,'nifti'),
+    if ischar(def),
+        def = nifti(def);
+    end
+    y   = def(1).dat(:,:,:,:,:);
+    M   = def(1).mat;
+else
+    y = def;
+    if nargin<3, M = eye(4); end
+end
+
+%vrt = this.vertices;
+vrt = subsref(this,substruct('.','vertices'));
+
+vrt = M(1:3,1:4)*[vrt'; ones(1,size(vrt,1))];
+xyz = {double(vrt(:,1)),double(vrt(:,2)),double(vrt(:,3))};
+vrt = [spm_bsplins(y(:,:,:,1,1),xyz{:},[1 1 1 0 0 0]),...
+       spm_bsplins(y(:,:,:,1,2),xyz{:},[1 1 1 0 0 0]),...
+       spm_bsplins(y(:,:,:,1,3),xyz{:},[1 1 1 0 0 0])];
+
+that = subsasgn(this,substruct('.','vertices'),vrt);
+
