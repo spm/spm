13852b3479710412f5cc851b2c7aa9f0c8ec9229
Vladimir Litvak <v.litvak@ucl.ac.uk>
2009-02-11 09:58:07 +0000

    Don't average across condition, but concatenate.
    SVN r2728

--- toolbox/MEEGtools/spm_eeg_spatial_confounds.m
+++ toolbox/MEEGtools/spm_eeg_spatial_confounds.m
@@ -11,15 +11,20 @@
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
 % Vladimir Litvak
-% $Id: spm_eeg_spatial_confounds.m 2347 2008-10-16 12:58:33Z vladimir $
+% $Id: spm_eeg_spatial_confounds.m 2728 2009-02-11 09:58:07Z vladimir $
 
 D = spm_eeg_load;
 
 if spm_input('How to define?','+1','SVD|BESA',[0 1], 0);
     sconf = spm_eeg_read_bsa(spm_select(1, '\.bsa$', 'Select BESA *.bsa file'));
 else
+    cl = D.condlist;
+    svdinput = [];
     timewin = spm_input('PST window (ms)', '+1', 'r', '', 2)/1000;
-    [U S V] = spm_svd(mean(D.selectdata(D.chanlabels(setdiff(D.meegchannels, D.badchannels)), timewin, []), 3));
+    for i = 1:numel(cl)
+        svdinput = [svdinput mean(D.selectdata(D.chanlabels(setdiff(D.meegchannels, D.badchannels)), timewin, cl{i}), 3)];
+    end
+    [U S V] = spm_svd(svdinput);
     n = spm_input('How many components?', '+1', 'n', '1', 1);
     [sel1, sel2] = spm_match_str(D.chanlabels(D.meegchannels), D.chanlabels(setdiff(D.meegchannels, D.badchannels)));
     sconf = [];
@@ -27,7 +32,7 @@ else
     sconf.coeff = nan(length(sconf.label), n);
     sconf.coeff(sel1, :) = U(sel2, 1:n);
     sconf.bad = ones(length(sconf.label), 1);
-    sconf.bad(sel1, :) = 0;    
+    sconf.bad(sel1, :) = 0;
 end
 
 D = sconfounds(D, sconf);
