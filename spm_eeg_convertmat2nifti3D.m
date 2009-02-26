79233a8eb71a2e17b3f742897efc7717b6f6d464
Guillaume Flandin <g.flandin@ucl.ac.uk>
2009-02-26 17:07:41 +0000

    Bug fix when more than one trial per condition. Some revamp.
    SVN r2792

--- spm_eeg_convertmat2nifti3D.m
+++ spm_eeg_convertmat2nifti3D.m
@@ -1,60 +1,66 @@
 function S = spm_eeg_convertmat2nifti3D(S)
-% Convert epoched EEG/ERP data from SPM to NIfTI format by projecting
+% Convert epoched M/EEG data from SPM to NIfTI format by projecting
 % onto the scalp surface
-% FORMAT spm_eeg_convertmat2nifti3D(S)
+% FORMAT S = spm_eeg_convertmat2nifti3D(S)
 %
-% S         - optinal input struct
+% S         - input structure (optional)
 % (optional) fields of S:
-% Fname            - matrix of EEG mat-files
-% n                - size of quadratic output image (size: n x n x ?)
-% pixsize          - Voxel size of output image
-% interpolate_bad  - flag (0/1) whether channels should be used for 
-%                    interpolation if they lie at the border of the setup
-%                    (0: mask out). 
+%   S.Fname            - character array of M/EEG mat-filenames
+%   S.n                - size of square output image (size: n x n x ?)
+%   S.pixsize          - Voxel size of output image [mm]
+%   S.interpolate_bad  - flag (0/1) whether channels should be used for 
+%                        interpolation if they lie at the border of the
+%                        setup [0: mask out]
 % output: 
 % S         - can be used to construct script (as in the history-function)
-%_______________________________________________________________________
+%__________________________________________________________________________
 %
-% spm_eeg_convertmat2nifti3D converts EEG/MEG data from the SPM format to the
-% scalp format. The data will be in 3D format, i.e., peri-stimulus time is the third dimension.
-% The channel data is interpolated to voxel-space using a
-% linear interpolation. Each channel's data will be found in an individual
-% voxel given that n is big enough. The data is written to 3-dim nifti
-% images, i.e. the data of each single trial or evoked response is contained in one
-% image file. The mask out option interpolate_bad=0 will only have an
-% effect if the bad channels are located at the edge of the setup, where it
-% is assumed that there is not enough data for interpolation.
-%_______________________________________________________________________
+% spm_eeg_convertmat2nifti3D converts M/EEG data from the SPM format to the
+% scalp format. The data will be in 3D format, i.e., peri-stimulus time is 
+% the third dimension. The channel data is interpolated to voxel-space 
+% using a linear interpolation. Each channel's data will be found in a 
+% single voxel given that n is big enough. The data is written to 3D NIfTI
+% images, i.e. the data of each single trial or evoked response is contained
+% in one image file. The 'mask out' option interpolate_bad=0 will only have
+% an effect if the bad channels are located at the edge of the setup, where
+% it is assumed that there is not enough data for interpolation.
+%__________________________________________________________________________
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
 % Stefan Kiebel
-% $Id: spm_eeg_convertmat2nifti3D.m 2740 2009-02-12 16:28:45Z vladimir $
+% $Id: spm_eeg_convertmat2nifti3D.m 2792 2009-02-26 17:07:41Z guillaume $
 
-[Finter, Fgraph, CmdLine] = spm('FnUIsetup', 'EEG conversion setup',0);
+SVNrev = '$Rev: 2792 $';
 
+%-Startup
+%--------------------------------------------------------------------------
+spm('FnBanner', mfilename, SVNrev);
+spm('FnUIsetup', 'M/EEG conversion setup', 0);
+
+%-Get parameters
+%--------------------------------------------------------------------------
+try
+    Fname        = S.Fname;
+catch
+    [Fname, sts] = spm_select(Inf, 'mat', 'Select MEEG mat file(s)');
+    if ~sts, S   = []; return; end
+    S.Fname      = Fname;
+end
+Fname            = cellstr(Fname);
 
 try
-    Fname = S.Fname;
+    n            = S.n;
 catch
-    Fname = spm_select(inf, '\.mat$', 'Select EEG mat file(s)');
+    n            = spm_input('Output image dimension', '+1', 'n', '32', 1);
+    S.n          = n;
 end
-
-Nsub = size(Fname, 1);
+if length(n) > 1, error('n must be scalar'); end
 
 try
-    n = S.n;
+    pixsize      = S.pixsize;
 catch
-    n = spm_input('Output image dimension', '+1', 'n', '32', 1);
-end
-
-try
-    pixsize = S.pixsize;
-catch
-    pixsize = spm_input('Pixel dimensions (approx)', '+1', 'n', '3', 1);
-end
-
-if length(n) > 1
-    error('n must be scalar');
+    pixsize      = spm_input('Pixel dimensions (approx)', '+1', 'n', '3', 1);
+    S.pixsize    = pixsize;
 end
 
 try
@@ -62,23 +68,26 @@ try
 catch
     interpolate_bad = spm_input('Interpolate bad channels or mask out?',...
         '+1', 'b', 'Interpolate|Mask out', [1,0]);
+    S.interpolate_bad = interpolate_bad;
 end
 
-spm('Pointer', 'Watch'); drawnow
+%-Load M/EEG data
+%--------------------------------------------------------------------------
+spm('Pointer', 'Watch');
 
-clear D
-for i = 1:Nsub
-    D{i} = spm_eeg_load(deblank(Fname(i,:)));    
+D = cell(1,numel(Fname));
+for i = 1:numel(Fname)
+    D{i} = spm_eeg_load(Fname{i});    
 end
 
+%-For multimodal datasets, set the types of non-chosen modality to 'Other'
+% This is not saved in the dataset
+%--------------------------------------------------------------------------
 modality = spm_eeg_modality_ui(D{1}, 1, 1);
-
-% For multimodal datasets set the types of non-chosen modality
-% to 'Other'. This is not saved in the dataset 
-if isequal(modality, 'MEGPLANAR')
+if strcmp(modality, 'MEGPLANAR')
     error('MEG planar gradiometers are not supported yet.')
 else
-    for i = 1:Nsub
+    for i = 1:numel(Fname)
         otherind = setdiff(1:nchannels(D{i}), strmatch(modality, chantype(D{i})));
         if ~isempty(otherind)
             D{i} = chantype(D{i}, otherind, 'Other');
@@ -86,60 +95,64 @@ else
     end
 end
 
-for k = 1:Nsub
+%-Project M/EEG data on the scalp surface
+%--------------------------------------------------------------------------
+for k = 1:numel(Fname)
 
     [Cel, Cind, x, y] = spm_eeg_locate_channels(D{k}, n, interpolate_bad);
-
-    % nr of (good) channels
-    Nel = length(Cel);
     
-    % generate data directory into which converted data goes
-    [P, F] = fileparts(spm_str_manip(Fname(k, :), 'r'));
-    if isempty(P), P = '.'; end
-    [m, sta] = mkdir(P, spm_str_manip(Fname(k, :), 'tr'));
-    if m ~= 1
-        error(sta);
-        return
-    end
-
-    cd(fullfile(P, spm_str_manip(Fname(k, :), 'tr')));
+    %-Make output directory for each dataset
+    %----------------------------------------------------------------------
+    [P, F] = fileparts(Fname{k});
+    if isempty(P), P = pwd; end
+    [sts, msg] = mkdir(P, F);
+    if ~sts, error(msg); end
+    P  = fullfile(P, F);
     
-    d = spm_cond_units(D{k}(Cind, :,:));
+    %-Loop over conditions
+    %----------------------------------------------------------------------
+    d  = spm_cond_units(D{k}(Cind, :,:));
     cl = D{k}.condlist;
-
     for i = 1 : D{k}.nconditions
-        
-        Itrials = pickconditions(D{k}, cl(i), 1)';
-
+       
+        %-Make output directory for each condition
+        %------------------------------------------------------------------
         dname = sprintf('type_%s', cl{i});
-        [m, sta] = mkdir(dname);
-        cd(dname);
+        [sts, msg] = mkdir(P, dname);
+        if ~sts, error(msg); end
+        Pi = fullfile(P, dname);
         
-        for l = Itrials(:)
-            fname = sprintf('trial%d.img', l);
+        %-Loop over trials
+        %------------------------------------------------------------------
+        Itrials = pickconditions(D{k}, cl(i), 1);
+        for l = Itrials(:)'
+            fname = fullfile(Pi, sprintf('trial%04d.img', l));
                                
-            dat = file_array(fname,[n n D{k}.nsamples 1],'FLOAT32-LE');
-            N = nifti;
+            dat   = file_array(fname,[n n D{k}.nsamples 1],'FLOAT32-LE');
+            N     = nifti;
             N.dat = dat;
-            N.mat = [   pixsize 0 0          -n*pixsize/2;...
-                0 pixsize 0      -n*pixsize/2;...
-                0 0 1000/D{k}.fsample  time(D{k}, 1, 'ms');...
-                0 0 0        1];
+            N.mat = [...
+                pixsize 0       0                  -n*pixsize/2;...
+                0       pixsize 0                  -n*pixsize/2;...
+                0       0       1000/D{k}.fsample  time(D{k}, 1, 'ms');...
+                0       0       0                  1];
             N.mat_intent = 'Aligned';
             create(N);
                         
             for j = 1 : D{k}.nsamples % time bins
-                di = ones(n, n)*NaN;                
-                di(sub2ind([n n], x, y)) = griddata(Cel(:,1), Cel(:,2), double(d(:, j, l)),x,y, 'linear');
+                di = NaN(n,n);                
+                di(sub2ind([n n], x, y)) = griddata(Cel(:,1),Cel(:,2),double(d(:, j, l)),x,y,'linear');
                 N.dat(:,:,j,1) = di;
             end        
             
-            disp(sprintf('File %d, type %d, trial %d', k, i, l))
+            fprintf('Dataset %s, type %s, trial %d\n', F, cl{i}, l);    %-#
             
-        end
-        cd ..
-    end
-    cd ..
-end
+        end % for l = Itrials(:)'
+        
+    end % for i = 1 : D{k}.nconditions
 
-spm('Pointer', 'Arrow');
+end % for k = 1:numel(Fname)
+
+%-Cleanup
+%--------------------------------------------------------------------------
+spm('FigName','M/EEG conversion: done'); spm('Pointer','Arrow');
