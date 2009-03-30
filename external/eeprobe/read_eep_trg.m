73031d8fbcd8ea5c63a2a1eb8bccfd87a5d76f96
Guillaume Flandin <g.flandin@ucl.ac.uk>
2009-03-30 14:11:13 +0000

    Update of the eeprobe library. Some new MEX files, linked with cntopenlib.
    SVN r2995

--- external/eeprobe/read_eep_trg.m
+++ external/eeprobe/read_eep_trg.m
@@ -78,10 +78,10 @@ header = fgetl(fid);
 while ~feof(fid)
   tmp = fscanf(fid, '%f %d %s', 3);
   if ~isempty(tmp)
-    new.time   = tmp(1)*1000;			% in ms
-    new.offset = tmp(2)+1;			% offset 1
-    new.code   = char(tmp(3:end));		% string
-    new.type   = str2double(new.code);		% numeric
+    new.time   = tmp(1)*1000;           % in ms
+    new.offset = tmp(2)+1;              % offset 1
+    new.code   = char(tmp(3:end));      % string
+    new.type   = str2double(new.code);  % numeric
     trg = [trg; new];
   end
 end
