ae8ee6cec01c963646fd46e7438c77904e3e364c
Maria Joao Rosa <mariajoao@gmail.com>
2009-04-21 18:03:57 +0000

    change '.BF' field to '.SF'.
    SVN r3074

--- config/spm_run_bms_dcm_vis.m
+++ config/spm_run_bms_dcm_vis.m
@@ -7,7 +7,7 @@ function out = spm_run_bms_dcm_vis(varargin)
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
 % Chun-Chuan Chen and Maria Joao Rosa
-% $Id: spm_run_bms_dcm_vis.m 2859 2009-03-11 16:52:19Z guillaume $
+% $Id: spm_run_bms_dcm_vis.m 3074 2009-04-21 18:03:57Z maria $
 
 job     = varargin{1};
 
@@ -29,7 +29,7 @@ for loop=1:inf
             if isfield(BMS.DCM,'ffx')
                 N =size(BMS.DCM.ffx.F,2);
                 N=1:N;
-                out=spm_api_bmc(BMS.DCM.ffx.BF,N);
+                out=spm_api_bmc(BMS.DCM.ffx.SF,N);
             else
                 msgbox('This result does not exist! Please perform BMS first!');
                 spm_input('Thank you',1,'d');
@@ -41,7 +41,7 @@ for loop=1:inf
             if isfield(BMS.DCM,'rfx')
                 N        =size(BMS.DCM.rfx.F,2);
                 N        =1:N;
-                out=spm_api_bmc(BMS.DCM.rfx.BF,N,BMS.DCM.rfx.alpha,BMS.DCM.rfx.exp_r,BMS.DCM.rfx.xp);
+                out=spm_api_bmc(BMS.DCM.rfx.SF,N,BMS.DCM.rfx.alpha,BMS.DCM.rfx.exp_r,BMS.DCM.rfx.xp);
             else
                 msgbox('This result does not exist! Please perform BMS first!');
                 spm_input('Thank you',1,'d');
