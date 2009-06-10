1b4e6aeaaf76033b3f2bc2a9fe35b7b45bb17926
Vladimir Litvak <v.litvak@ucl.ac.uk>
2009-06-10 11:06:44 +0000

    Slightly optimized and generalized robust averaging function (not yet used). Removed an unused version of the same thing.
    SVN r3194

--- spm_eeg_robust_average.m
+++ spm_eeg_robust_average.m
@@ -1,103 +1,126 @@
-function [B,Wf] = spm_eeg_robust_average(data)
-% Robust averaging
+function [Y,W] = spm_eeg_robust_average(X, dim, ks, h)
+% Apply robust averaging routine to X sets
+% FORMAT [Y,W] = spm_eeg_robust_averaget(X, dim, ks)
+% X      - data matrix to be averaged
+% dim    - the dimension along which the function will work
+% ks     - offest of the weighting function (default: 3)
+% h      - (optional) matrix of 'leverages' indicating how
+%          important each data point is. This is for computations more
+%          complicated than averaging e.g. regression
+%
+% W      - estimated weights
 %__________________________________________________________________________
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
 % James Kilner
-% $Id: spm_eeg_robust_average.m 2696 2009-02-05 20:29:48Z guillaume $
+% $Id: spm_eeg_robust_average.m 3194 2009-06-10 11:06:44Z vladimir $
 
-data=data';
-%figure(1)
-%plot(mean(data));
-% clear D
-% clear d1
+if nargin < 3 || isempty(ks)
+    ks = 3;
+end
 
- ndata=reshape(data',size(data,1)*size(data,2),1);
+if nargin < 2 || isempty(dim)
+    dim = 1;
+end
+
+%-Remember the original data size and size of the mean
+%--------------------------------------------------------------------------
+origsize       = size(X);
+morigsize      = origsize;
+morigsize(dim) = 1;
+
+%-Convert the data to repetitions x points matrix
+%--------------------------------------------------------------------------
+if dim > 1
+    X  = shiftdim(X, dim-1);  
+end
+
+if length(origsize) > 2
+    X  = reshape(X, size(X, 1), []);
+end
+
+%-Check the leverages and reshape if necessary
+%--------------------------------------------------------------------------
+if nargin > 3
+    if numel(h)>1
+        if morigsize(end) == 1
+            if ~isequal(morigsize(1:(end-1)), size(h))
+                error('The leverages do not match the data.');
+            end
+            
+            if dim > 2
+                h  = shiftdim(h, dim-2);
+            end
+        else
+            if ~isequal(morigsize, size(h))
+                error('The leverages do not match the data.');
+            end 
+            
+            if dim > 1
+                h  = shiftdim(h, dim-1);
+            end
+        end
+        if length(origsize) > 2
+            h  = reshape(h, 1, []);
+        end
+    end
+else
+    h = 1;
+end
+
+%-Rescale the data
+%--------------------------------------------------------------------------
+[X, scalefactor] = spm_cond_units(X);
 
 
-Xs=sparse(repmat(speye(size(data,2)),[size(data,1),1]));
-
-s1=size(data,1);
-s2=size(data,2);
-%clear data;
-Wis=speye(length(ndata));
-h=1./((1-(diag(Xs*(Xs'*Xs)^-1*Xs'))).^0.5);
+%-Actual robust averaging
+%--------------------------------------------------------------------------
 ores=1;
 nres=10;
 n=0;
-while abs(ores-nres)>sqrt(1E-8)
-    abs(ores-nres);
+Y=zeros(1,size(X,1));
+while max(abs(ores-nres))>sqrt(1E-8)
+
     ores=nres;
     n=n+1;
-    B=((Xs'*Wis*Xs)^-1)*Xs'*Wis*ndata;
-    if sum(isnan(B))>0
-        break
+
+    if n==1
+        Y = median(X);
+    else
+        Y = sum(W.*X)./sum(W);
     end
-    if n>500
-        break
+
+    if sum(isnan(Y))>0
+        error('NaNs appeared in the result');
     end
-    res=ndata-Xs*B;
-    mad=median(abs(res-median(res)));
-    res=(res)./mad;
-    res=res.*h; 
-    res=reshape(res,s2,s1);
-    res=repmat((mean(abs(res)).*sign(sum(res))),[s2,1]);
-    res=reshape(res,s1*s2,1);
-    res=abs(res)-2;
+
+    if n > 100
+        warning('Robust averaging could not converge. Maximal number of iterations exceeded.');
+    end
+
+    res = X-repmat(Y, size(X, 1), 1);
+
+    mad = median(abs(res-repmat(median(res), size(res, 1), 1)));
+    res = res./repmat(mad, size(res, 1), 1);
+    res = res.*h;
+    res = abs(res)-ks;
     res(res<0)=0;
-    nres=(sum(res.^2));
-    Wf=(((abs(res)<1) .* (1 - res.^2).^2));
-    clear res;
-    Wis=spdiags(Wf,0,Wis);
+    nres= (sum(res(:).^2));
+    W = (abs(res)<1) .* ((1 - res.^2).^2);
+end
 
+%-Restore the average and weights to the original data dimensions
+%--------------------------------------------------------------------------
+Y = Y./scalefactor;
 
+if length(origsize) > 2   
+    Y  = reshape(Y, circshift(morigsize, [1 -(dim-1)]));
+    W  = reshape(W, circshift(origsize,  [1 -(dim-1)]));
+end
+
+if dim > 1
+    Y  = shiftdim(Y, length(origsize)-dim+1);
+    W  = shiftdim(W, length(origsize)-dim+1);
 end
 
 
-
-% 
-% res=ndata-Xs*B;
-% res=reshape(res,s2,s1);
-% res=repmat(max(abs(res)),[s2,1]);
-% res=reshape(res,s1*s2,1);
-% mad=median(abs(res-median(res)));
-% res=res./mad;
-% res=res.*h;   
-% res=abs(res)-1;
-% res(res<0)=0;
-% nres=(sum(res.^2));
-% n=0;
-% while abs(ores-nres)>sqrt(1E-8)
-%   n=n+1;
-%   figure(10)
-%   plot(n,abs(ores-nres),'.')
-%   hold on
-%   ores=nres;
-%   
-%   
-%   if n>50
-%       break
-%   end
-%   
-%   
-%   B=((Xs'*Wis*Xs)^-1)*Xs'*Wis*ndata;
-%   res=ndata-Xs*B;
-%   mad=median(abs(res-median(res)));
-%   res=(res)./mad;
-%   res=res.*h; 
-%   res=reshape(res,s2,s1);
-%   [df,inds]=(max(abs(res)));
-%   y=1:length(inds);
-%   res=repmat(diag(squeeze(res(inds,y)))',[s2,1]);
-%   res=reshape(res,s1*s2,1);
-% 
-%   res=abs(res)-1;
-%   res(res<0)=0;
-%   nres=(sum(res.^2));
-%   Wf=(((abs(res)<1) .* (1 - res.^2).^2));
-%   clear res;
-%   Wis=spdiags(Wf,0,Wis);
-% 
-% 
-% end
-% 
