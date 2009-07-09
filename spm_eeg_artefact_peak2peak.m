3252d77141e3cefbc9b3a212deea65ce9e1d9517
Vladimir Litvak <v.litvak@ucl.ac.uk>
2009-07-09 10:19:57 +0000

    Checked in the wrong version of the function before.
    SVN r3259

--- spm_eeg_artefact_peak2peak.m
+++ spm_eeg_artefact_peak2peak.m
@@ -1,15 +1,31 @@
 function res = spm_eeg_artefact_peak2peak(S)
 % Plugin for spm_eeg_artefact doing artefact detection based on peak-to-peak amplitude.
+% S                     - input structure
+% fields of S:
+%    S.D                - M/EEG object
+%    S.chanind          - vector of indices of channels that this plugin will look at.
+%                         
+%    Additional parameters can be defined specific for each plugin
+% Output:
+%  res - 
+%   If no input is provided the plugin returns a cfg branch for itself
+%
+%   If input is provided the plugin returns a matrix of size D.nchannels x D.ntrials  
+%   with zeros for clean channel/trials and ones for artefacts.
 %______________________________________________________________________________________
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
 % Vladimir Litvak
-% $Id: spm_eeg_artefact_peak2peak.m 3258 2009-07-08 17:46:54Z vladimir $
+% $Id: spm_eeg_artefact_peak2peak.m 3259 2009-07-09 10:19:57Z vladimir $
 
 
+%-This part if for creating a config branch that plugs into spm_cfg_eeg_artefact
+% Any parameters can be specified and they are then passed to the plugin
+% when it's called.
+%--------------------------------------------------------------------------
 if nargin == 0
     threshold = cfg_entry;
-    threshold.tag = 'thresholdval';
+    threshold.tag = 'threshold';
     threshold.name = 'Threshold';
     threshold.strtype = 'r';
     threshold.num = [1 1];
@@ -25,28 +41,28 @@ if nargin == 0
     return
 end
 
-SVNrev = '$Rev: 3258 $';
+SVNrev = '$Rev: 3259 $';
 
 %-Startup
 %--------------------------------------------------------------------------
-spm('FnBanner', mfilename, SVNrev);
+spm('sFnBanner', mfilename, SVNrev);
 spm('FigName','M/EEG peak to peak artefact detection');
 
 D = spm_eeg_load(S.D);
 
-channels  = S.channels;
+chanind  = S.chanind;
 threshold = S.threshold;
 res = zeros(D.nchannels, D.ntrials);
 
 %-Artefact detection
 %--------------------------------------------------------------------------
 
-spm_progress_bar('Init', D.ntrials, 'Trials thresholded');
+spm_progress_bar('Init', D.ntrials, 'Trials checked');
 if D.ntrials > 100, Ibar = floor(linspace(1, D.ntrials,100));
 else Ibar = [1:D.ntrials]; end
 
 for i = 1:D.ntrials
-    res(channels, i) = (squeeze(max(D(channels, :, i), [], 2) - min(D(channels, :, i), [], 2)))>threshold;
+    res(chanind, i) = (squeeze(max(D(chanind, :, i), [], 2) - min(D(chanind, :, i), [], 2)))>threshold;
     if ismember(i, Ibar), spm_progress_bar('Set', i); end
 end
 
