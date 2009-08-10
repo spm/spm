b54674a09ddd2226fab902a9abbab28f831eb1da
SPM <fil.spm@ucl.ac.uk>
2009-08-10 12:35:18 +0000

    automatically pushed some recent changes from the FCDC to the FIL
    SVN r3316

--- external/fileio/private/read_eeglabheader.m
+++ external/fileio/private/read_eeglabheader.m
@@ -30,6 +30,15 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: read_eeglabheader.m,v $
+% Revision 1.6  2009/08/08 04:07:02  josdie
+% Bug in Joe's brain fixed.  Change to header.nSamplesPre calculation changed back.
+%
+% Revision 1.5  2009/08/08 03:17:26  josdie
+% Fixed bug that was causing hdr.label to have as many labels as there are time points rather than matching the number of channels.
+%
+% Revision 1.4  2009/08/08 03:05:29  josdie
+% Fixed bug in calculation of header.nSamplesPre.
+%
 % Revision 1.3  2009/07/01 16:08:21  vlalit
 % Fixing a bug in converting channel locations to elec struct (reproted by Jakib Scherer)
 %
@@ -73,7 +82,7 @@ try
   header.label       = { EEG.chanlocs.labels }';
 catch
   warning('creating default channel names');
-  for i=1:header.nSamples
+  for i=1:header.nChans
     header.label{i} = sprintf('chan%03d', i);
   end
 end
