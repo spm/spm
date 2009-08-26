ac1e4a976349e2784ed4491af9ca330e07ad2d57
Vladimir Litvak <v.litvak@ucl.ac.uk>
2009-08-26 18:02:58 +0000

    Updating the multimodal chapter (work in progress).
    SVN r3336

---
+++ man/example_scripts/faces_eeg_montage.m
@@ -0,0 +1,17 @@
+D = spm_eeg_load('dspm8_faces_run1.mat');
+
+montage.labelorg = D.chanlabels;
+
+montage.labelnew = [montage.labelorg(1:128), 'HEOG', 'VEOG'];
+
+tra = eye(128);
+
+% VEOG
+tra(129, [131 132]) = [1 -1];
+
+% VEOG
+tra(130, [135 136]) = [1 -1];
+
+montage.tra = tra;
+
+save faces_eeg_montage.mat montage
