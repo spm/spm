448e2f1c8532bd3d47fdfafb393396adf81b3e35
Vladimir Litvak <v.litvak@ucl.ac.uk>
2009-10-07 07:52:29 +0000

    cfg for fusion
    SVN r3447

--- config/spm_cfg.m
+++ config/spm_cfg.m
@@ -4,7 +4,7 @@ function spmjobs = spm_cfg
 %_______________________________________________________________________
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
-% $Id: spm_cfg.m 3430 2009-09-29 16:55:38Z guillaume $
+% $Id: spm_cfg.m 3447 2009-10-07 07:52:29Z vladimir $
 
 %_______________________________________________________________________
 % temporal Temporal
@@ -37,7 +37,7 @@ meeg         = cfg_choice;
 meeg.tag     = 'meeg';
 meeg.name    = 'M/EEG';
 meeg.help    = {'M/EEG functions.'};
-meeg.values  = { spm_cfg_eeg_convert spm_cfg_eeg_montage spm_cfg_eeg_epochs spm_cfg_eeg_filter spm_cfg_eeg_bc spm_cfg_eeg_artefact spm_cfg_eeg_average spm_cfg_eeg_downsample spm_cfg_eeg_merge spm_cfg_eeg_contrast spm_cfg_eeg_grandmean spm_cfg_eeg_tf_rescale spm_cfg_eeg_convert2images};
+meeg.values  = { spm_cfg_eeg_convert spm_cfg_eeg_montage spm_cfg_eeg_epochs spm_cfg_eeg_filter spm_cfg_eeg_bc spm_cfg_eeg_artefact spm_cfg_eeg_average spm_cfg_eeg_downsample spm_cfg_eeg_merge spm_cfg_eeg_fuse spm_cfg_eeg_contrast spm_cfg_eeg_grandmean spm_cfg_eeg_tf_rescale spm_cfg_eeg_convert2images};
 
 % ---------------------------------------------------------------------
 % util Util
---
+++ config/spm_cfg_eeg_fuse.m
@@ -0,0 +1,50 @@
+function S = spm_cfg_eeg_fuse
+% configuration file for fusing M/EEG files
+%_______________________________________________________________________
+% Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
+
+% Vladimir Litvak
+% $Id: spm_cfg_eeg_fuse.m 3447 2009-10-07 07:52:29Z vladimir $
+
+rev = '$Rev: 3447 $';
+D = cfg_files;
+D.tag = 'D';
+D.name = 'File Names';
+D.filter = 'mat';
+D.num = [2 Inf];
+D.help = {'Select the M/EEG mat files.'};
+
+
+S = cfg_exbranch;
+S.tag = 'eeg_fuse';
+S.name = 'M/EEG Fusion';
+S.val = {D};
+S.help = {'Fuse EEG/MEG data.'};
+S.prog = @eeg_fuse;
+S.vout = @vout_eeg_fuse;
+S.modality = {'EEG'};
+
+function out = eeg_fuse(job)
+% construct the S struct
+S.D = strvcat(job.D{:});
+
+out.D = spm_eeg_fuse(S);
+out.Dfname = {out.D.fname};
+
+function dep = vout_eeg_fuse(job)
+% Output is always in field "D", no matter how job is structured
+dep = cfg_dep;
+dep.sname = 'Fused Data';
+% reference field "D" from output
+dep.src_output = substruct('.','D');
+% this can be entered into any evaluated input
+dep.tgt_spec   = cfg_findspec({{'strtype','e'}});
+
+dep(2) = cfg_dep;
+dep(2).sname = 'Fused Datafile';
+% reference field "Dfname" from output
+dep(2).src_output = substruct('.','Dfname');
+% this can be entered into any file selector
+dep(2).tgt_spec   = cfg_findspec({{'filter','mat'}});
+
+
