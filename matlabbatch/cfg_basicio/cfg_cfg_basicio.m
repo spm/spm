1ff406ea7428a0426ab35c4b974bf5ee3792a411
Volkmar Glauche <volkmar.glauche@uniklinik-freiburg.de>
2009-10-27 15:56:02 +0000

    Add BasicIO module that runs any MATLAB function and returns its outputs as dependencies.
    SVN r3513

--- matlabbatch/cfg_basicio/cfg_cfg_basicio.m
+++ matlabbatch/cfg_basicio/cfg_cfg_basicio.m
@@ -4,7 +4,7 @@ function cfg_basicio = cfg_cfg_basicio
 % by MATLABBATCH using ConfGUI. It describes menu structure, validity
 % constraints and links to run time code.
 % Changes to this file will be overwritten if the ConfGUI batch is executed again.
-% Created at 2009-02-02 15:56:28.
+% Created at 2009-10-27 15:47:27.
 % ---------------------------------------------------------------------
 % files Files to move/copy/delete
 % ---------------------------------------------------------------------
@@ -352,10 +352,15 @@ filter.num     = [1  Inf];
 rec         = cfg_menu;
 rec.tag     = 'rec';
 rec.name    = 'Descend into subdirectories';
-rec.help    = {['Files can be selected from the specified directory only ' ...
-                'or from the specified directory and all its subdirectories.']};
-rec.labels  = {'Yes' 'No'};
-rec.values  = {'FPListRec' 'FPList'};
+rec.help    = {'Files can be selected from the specified directory only or from the specified directory and all its subdirectories.'};
+rec.labels = {
+              'Yes'
+              'Yes'
+              }';
+rec.values = {
+              true
+              false
+              }';
 % ---------------------------------------------------------------------
 % file_fplist File Selector (Batch Mode)
 % ---------------------------------------------------------------------
@@ -815,10 +820,201 @@ runjobs.help    = {'Load a set of job files, fill missing inputs and run the fil
 runjobs.prog = @cfg_run_runjobs;
 runjobs.vout = @cfg_vout_runjobs;
 % ---------------------------------------------------------------------
+% evaluated Evaluated Input
+% ---------------------------------------------------------------------
+evaluated         = cfg_entry;
+evaluated.tag     = 'evaluated';
+evaluated.name    = 'Evaluated Input';
+evaluated.strtype = 'e';
+evaluated.num     = [];
+% ---------------------------------------------------------------------
+% string String
+% ---------------------------------------------------------------------
+string         = cfg_entry;
+string.tag     = 'string';
+string.name    = 'String';
+string.strtype = 's';
+string.num     = [];
+% ---------------------------------------------------------------------
+% anyfile Any File
+% ---------------------------------------------------------------------
+anyfile         = cfg_files;
+anyfile.tag     = 'anyfile';
+anyfile.name    = 'Any File';
+anyfile.filter = 'any';
+anyfile.ufilter = '.*';
+anyfile.num     = [0 Inf];
+% ---------------------------------------------------------------------
+% images NIfTI Image(s)
+% ---------------------------------------------------------------------
+images         = cfg_files;
+images.tag     = 'images';
+images.name    = 'NIfTI Image(s)';
+images.filter = 'image';
+images.ufilter = '.*';
+images.num     = [0 Inf];
+% ---------------------------------------------------------------------
+% directory Directory
+% ---------------------------------------------------------------------
+directory         = cfg_files;
+directory.tag     = 'directory';
+directory.name    = 'Directory';
+directory.filter = 'dir';
+directory.ufilter = '.*';
+directory.num     = [0 Inf];
+% ---------------------------------------------------------------------
+% inputs Inputs
+% ---------------------------------------------------------------------
+inputs         = cfg_repeat;
+inputs.tag     = 'inputs';
+inputs.name    = 'Inputs';
+inputs.help    = {'Assemble the inputs to the called function in their correct order.'};
+inputs.values  = {evaluated string anyfile images directory };
+inputs.num     = [0 Inf];
+inputs.forcestruct = true;
+% ---------------------------------------------------------------------
+% s String
+% ---------------------------------------------------------------------
+s         = cfg_const;
+s.tag     = 's';
+s.name    = 'String';
+s.val = {true};
+% ---------------------------------------------------------------------
+% e Evaluated
+% ---------------------------------------------------------------------
+e         = cfg_const;
+e.tag     = 'e';
+e.name    = 'Evaluated';
+e.val = {true};
+% ---------------------------------------------------------------------
+% n Natural number
+% ---------------------------------------------------------------------
+n         = cfg_const;
+n.tag     = 'n';
+n.name    = 'Natural number';
+n.val = {true};
+% ---------------------------------------------------------------------
+% w Whole number
+% ---------------------------------------------------------------------
+w         = cfg_const;
+w.tag     = 'w';
+w.name    = 'Whole number';
+w.val = {true};
+% ---------------------------------------------------------------------
+% i Integer
+% ---------------------------------------------------------------------
+i         = cfg_const;
+i.tag     = 'i';
+i.name    = 'Integer';
+i.val = {true};
+% ---------------------------------------------------------------------
+% r Real number
+% ---------------------------------------------------------------------
+r         = cfg_const;
+r.tag     = 'r';
+r.name    = 'Real number';
+r.val = {true};
+% ---------------------------------------------------------------------
+% strtype Type of output variable
+% ---------------------------------------------------------------------
+strtype         = cfg_choice;
+strtype.tag     = 'strtype';
+strtype.name    = 'Type of output variable';
+strtype.values  = {s e n w i r };
+% ---------------------------------------------------------------------
+% any Any file
+% ---------------------------------------------------------------------
+any         = cfg_const;
+any.tag     = 'any';
+any.name    = 'Any file';
+any.val = {true};
+% ---------------------------------------------------------------------
+% batch Batch file
+% ---------------------------------------------------------------------
+batch         = cfg_const;
+batch.tag     = 'batch';
+batch.name    = 'Batch file';
+batch.val = {true};
+% ---------------------------------------------------------------------
+% dir Directory
+% ---------------------------------------------------------------------
+dir         = cfg_const;
+dir.tag     = 'dir';
+dir.name    = 'Directory';
+dir.val = {true};
+% ---------------------------------------------------------------------
+% image Image(s)
+% ---------------------------------------------------------------------
+image         = cfg_const;
+image.tag     = 'image';
+image.name    = 'Image(s)';
+image.val = {true};
+% ---------------------------------------------------------------------
+% mat MATLAB .mat file
+% ---------------------------------------------------------------------
+mat         = cfg_const;
+mat.tag     = 'mat';
+mat.name    = 'MATLAB .mat file';
+mat.val = {true};
+% ---------------------------------------------------------------------
+% mesh Mesh file
+% ---------------------------------------------------------------------
+mesh         = cfg_const;
+mesh.tag     = 'mesh';
+mesh.name    = 'Mesh file';
+mesh.val = {true};
+% ---------------------------------------------------------------------
+% nifti NIfTI file
+% ---------------------------------------------------------------------
+nifti         = cfg_const;
+nifti.tag     = 'nifti';
+nifti.name    = 'NIfTI file';
+nifti.val = {true};
+% ---------------------------------------------------------------------
+% xml XML File
+% ---------------------------------------------------------------------
+xml         = cfg_const;
+xml.tag     = 'xml';
+xml.name    = 'XML File';
+xml.val = {true};
+% ---------------------------------------------------------------------
+% filter Type of output file
+% ---------------------------------------------------------------------
+filter         = cfg_choice;
+filter.tag     = 'filter';
+filter.name    = 'Type of output file';
+filter.values  = {any batch dir image mat mesh nifti xml };
+% ---------------------------------------------------------------------
+% outputs Outputs
+% ---------------------------------------------------------------------
+outputs         = cfg_repeat;
+outputs.tag     = 'outputs';
+outputs.name    = 'Outputs';
+outputs.values  = {strtype filter };
+outputs.num     = [0 Inf];
+outputs.forcestruct = true;
+% ---------------------------------------------------------------------
+% fun Function to be called
+% ---------------------------------------------------------------------
+fun         = cfg_entry;
+fun.tag     = 'fun';
+fun.name    = 'Function to be called';
+fun.strtype = 'f';
+fun.num     = [1  1];
+% ---------------------------------------------------------------------
+% cfg_call_matlab Call MATLAB function
+% ---------------------------------------------------------------------
+cfg_call_matlab         = cfg_exbranch;
+cfg_call_matlab.tag     = 'cfg_call_matlab';
+cfg_call_matlab.name    = 'Call MATLAB function';
+cfg_call_matlab.val     = {inputs outputs fun };
+cfg_call_matlab.prog = @(job)cfg_call_matlab('run',job);
+cfg_call_matlab.vout = @(job)cfg_call_matlab('vout',job);
+% ---------------------------------------------------------------------
 % cfg_basicio BasicIO
 % ---------------------------------------------------------------------
 cfg_basicio         = cfg_choice;
 cfg_basicio.tag     = 'cfg_basicio';
 cfg_basicio.name    = 'BasicIO';
 cfg_basicio.help    = {'This toolbox contains basic input and output functions. The "Named Input" functions can be used to enter values or file names. These inputs can then be passed on to multiple modules, thereby ensuring all of them use the same input value. Some basic file manipulation is implemented in "Change Directory", "Make Directory", "Move Files". Lists of files can be filtered or splitted into parts using "File Set Filter" and "File Set Split". Output values from other modules can be written out to disk or assigned to MATLAB workspace.'};
-cfg_basicio.values  = {file_move cfg_cd cfg_mkdir cfg_named_dir cfg_named_file file_fplist file_filter cfg_file_split cfg_named_input load_vars cfg_save_vars cfg_assignin runjobs };
+cfg_basicio.values  = {file_move cfg_cd cfg_mkdir cfg_named_dir cfg_named_file file_fplist file_filter cfg_file_split cfg_named_input load_vars cfg_save_vars cfg_assignin runjobs cfg_call_matlab };
