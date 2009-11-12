f70281d8699ea170a286ff998821365ff0b70844
Vladimir Litvak <v.litvak@ucl.ac.uk>
2009-11-12 00:20:08 +0000

    bug fix
    SVN r3559

--- spm_eeg_invert_both.m
+++ spm_eeg_invert_both.m
@@ -120,7 +120,7 @@ function [D] = spm_eeg_invert(D, val)
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
  
 % Karl Friston
-% $Id: spm_eeg_invert_both.m 3558 2009-11-11 20:23:05Z karl $
+% $Id: spm_eeg_invert_both.m 3559 2009-11-12 00:20:08Z vladimir $
  
 % check whether this is a group inversion
 %--------------------------------------------------------------------------
@@ -166,14 +166,14 @@ Nmmax     = Nm;                              % max number of spatial modes
 Nrmax     = Nr;                              % max number of temporal modes
 
  
-fprintf('Checking leadfields:')
+fprintf('Checking leadfields:\n')
 %==========================================================================
 for i = 1:Nl
     for m = 1:Nmod
  
         % Check gain or lead-field matrices
         %------------------------------------------------------------------
-        fprintf(' %s',modalities{m});
+        fprintf('Subject %d: %s\n',i, modalities{m});
         [L D{i}] = spm_eeg_lgainmat(D{i});
         Ic{i,m}  = setdiff(meegchannels(D{i}, modalities{m}), badchannels(D{i}));
         Nd(i)    = size(L,2);                          % number of dipoles
@@ -460,7 +460,7 @@ UL   = spm_cat(UL(:));
 % generate sensor error components (Qe)
 %==========================================================================
 AQe   = {sparse(0)};
-for i = 1:Nmod
+for m = 1:Nmod
     Qe{m} = sparse(0);
 end
  
