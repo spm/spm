ec33844df677fce9aabf0802aa928b4b9cdb3c76
Guillaume Flandin <g.flandin@ucl.ac.uk>
2009-12-07 18:04:24 +0000

    * Change initial approximation in spm_uc_RF (and handle cases when sum(R)<0) * New function to split a 4D volume in as many 3D volumes as 4th dim.
    SVN r3614

--- spm_uc_RF.m
+++ spm_uc_RF.m
@@ -4,10 +4,10 @@ function [u] = spm_uc_RF(a,df,STAT,R,n)
 % a     - critical probability - {alpha}
 % df    - [df{interest} df{residuals}]
 % STAT  - Statistical field
-%       'Z' - Gaussian field
-%       'T' - T - field
-%       'X' - Chi squared field
-%       'F' - F - field
+%         'Z' - Gaussian field
+%         'T' - T field
+%         'X' - Chi-squared field
+%         'F' - F field
 % R     - RESEL Count {defining search volume}
 % n     - number of conjoint SPMs
 %
@@ -16,23 +16,23 @@ function [u] = spm_uc_RF(a,df,STAT,R,n)
 %__________________________________________________________________________
 %
 % spm_uc returns the corrected critical threshold at a specified significance
-% level (a). If n > 1 a conjunction the probability over the n values of the
-% statistic is returned.
+% level (a). If n > 1 a conjunction the probability over the n values of
+% the statistic is returned.
 %__________________________________________________________________________
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
 % Karl Friston
-% $Id: spm_uc_RF.m 1143 2008-02-07 19:33:33Z spm $
+% $Id: spm_uc_RF.m 3614 2009-12-07 18:04:24Z guillaume $
 
 
 % find approximate value
 %--------------------------------------------------------------------------
-u     = spm_u((a/sum(R))^(1/n),df,STAT);
-du    = 1e-6;
+u  = spm_u((a/max(R))^(1/n),df,STAT);
+du = 1e-6;
 
 % approximate estimate using E{m}
 %--------------------------------------------------------------------------
-d     = 1;
+d  = 1;
 while abs(d) > 1e-6
     [P P p] = spm_P_RF(1,0,u,df,STAT,R,n);
     [P P q] = spm_P_RF(1,0,u + du,df,STAT,R,n);
@@ -42,7 +42,7 @@ end
 
 % refined estimate using 1 - exp(-E{m})
 %--------------------------------------------------------------------------
-d     = 1;
+d  = 1;
 while abs(d) > 1e-6
     p       = spm_P_RF(1,0,u,df,STAT,R,n);
     q       = spm_P_RF(1,0,u + du,df,STAT,R,n);
