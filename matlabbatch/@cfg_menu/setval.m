a371790c0b7a36980bcfea73c203a2bea451851a
Volkmar Glauche <volkmar.glauche@uniklinik-freiburg.de>
2009-12-08 15:32:34 +0000

    Fixed bugs in defaults handling as reported by Baerbel Herrnberger.
    SVN r3617

--- matlabbatch/@cfg_menu/setval.m
+++ matlabbatch/@cfg_menu/setval.m
@@ -2,10 +2,10 @@ function item = setval(item, val, dflag)
 
 % function item = setval(item, val, dflag)
 % set item.val{1} to item.values{val}. If val == {}, set item.val to {}
-% If dflag is true, and item.def is not empty, set the default setting for
-% this item instead by calling feval(item.def{:}, val). If val == {}, use
+% If dflag is true, and item.cfg_item.def is not empty, set the default setting for
+% this item instead by calling feval(item.cfg_item.def{:}, val). If val == {}, use
 % the string '<UNDEFINED>' as in a harvested tree. If dflag is true, but
-% no item.def defined, set item.val{1} instead.
+% no item.cfg_item.def defined, set item.val{1} instead.
 %
 % This code is part of a batch job configuration system for MATLAB. See 
 %      help matlabbatch
@@ -14,44 +14,44 @@ function item = setval(item, val, dflag)
 % Copyright (C) 2007 Freiburg Brain Imaging
 
 % Volkmar Glauche
-% $Id: setval.m 1862 2008-06-30 14:12:49Z volkmar $
+% $Id: setval.m 3617 2009-12-08 15:32:34Z volkmar $
 
-rev = '$Rev: 1862 $'; %#ok
+rev = '$Rev: 3617 $'; %#ok
 
 if iscell(val) && isempty(val)
     if dflag
-        if ~isempty(item.def)
+        if ~isempty(item.cfg_item.def)
             try
-                feval(item.def{:}, '<UNDEFINED>');
+                feval(item.cfg_item.def, {'<UNDEFINED>'});
             catch
                 cfg_message('matlabbatch:setval:defaults', ...
                             '%s: unable to set default value.', ...
                             subsasgn_checkstr(item, substruct('.','val')));
-            end;
+            end
         else
             item = subsasgn(item, substruct('.','val'), {});
-        end;
+        end
     else
         item = subsasgn(item, substruct('.','val'), {});
-    end;
+    end
 else
     val = item.values{val};
     if dflag
         [sts val1] = subsasgn_check(item, substruct('.','val'), {val});
         if sts
-            if ~isempty(item.def)
+            if ~isempty(item.cfg_item.def)
                 try
-                    feval(item.def{:}, val1{1});
+                    feval(item.cfg_item.def, val1);
                 catch
                     cfg_message('matlabbatch:setval:defaults', ...
                                 '%s: unable to set default value.', ...
                                 subsasgn_checkstr(item, substruct('.','val')));
-                end;
+                end
             else
-                item = subsasgn(item, substruct('.','val', '{}',{1}), val);
-            end;
-        end;
+                item = subsasgn(item, substruct('.','val'), {val});
+            end
+        end
     else
-        item = subsasgn(item, substruct('.','val', '{}',{1}), val);
-    end;
-end;
+        item = subsasgn(item, substruct('.','val'), {val});
+    end
+end
