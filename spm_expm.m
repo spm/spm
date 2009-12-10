6d1ca6535fe2475adb2d97bd473663ab93003ff9
Guillaume Flandin <g.flandin@ucl.ac.uk>
2009-12-10 17:11:40 +0000

    Temporarily reverting to previous version of spm_expm.m, with full matrices instead of sparse.
    SVN r3631

--- spm_expm.m
+++ spm_expm.m
@@ -9,97 +9,75 @@ function [x] = spm_expm(J,x)
 % comoutationally  expedient  approximation  that can handle sparse
 % matrices when dealing with the special case of expm(J)*x, where x
 % is a vector, in an efficient fashion
-%__________________________________________________________________________
+%___________________________________________________________________________
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
 % Karl Friston
-% $Id: spm_expm.m 3605 2009-12-01 13:29:43Z karl $
+% $Id: spm_expm.m 3631 2009-12-10 17:11:40Z guillaume $
 
 
 % expm(J) use Pade approximation
-%==========================================================================
+%---------------------------------------------------------------------------
+if nargin == 1
 
-% ensure norm is < 1/2 by scaling by power of 2
-%--------------------------------------------------------------------------
-J     = full(J);
-I     = eye(size(J));
-[f,e] = log2(norm(J,'inf'));
-s     = max(0,e+1);
-J     = J/2^s;
-X     = J;
-c     = 1/2;
-E     = I + c*J;
-D     = I - c*J;
-q     = 16;
-p     = 1;
-for k = 2:q
-    c   = c*(q - k + 1)/(k*(2*q - k + 1));
-    X   = J*X;
-    cX  = c*X;
-    E   = E + cX;
-    if p
-        D = D + cX;
-    else
-        D = D - cX;
+    % ensure norm is < 1/2 by scaling by power of 2
+    %-------------------------------------------------------------------
+    J     = full(J);
+    I     = eye(size(J));
+    [f,e] = log2(norm(J,'inf'));
+    s     = max(0,e+1);
+    J     = J/2^s;
+    X     = J; 
+    c     = 1/2;
+    E     = I + c*J;
+    D     = I - c*J;
+    q     = 6;
+    p     = 1;
+    for k = 2:q
+        c   = c*(q - k + 1)/(k*(2*q - k + 1));
+        X   = J*X;
+        cX  = c*X;
+        E   = E + cX;
+        if p
+            D = D + cX;
+        else
+            D = D - cX;
+        end
+        p = ~p;
     end
-    if norm(cX,1) < 1e-16;
-        break
+    E = D\E;
+
+    % Undo scaling by repeated squaring
+    %-------------------------------------------------------------------
+    for k = 1:s
+        E = E*E;
     end
-    p = ~p;
-end
-E = D\E;  % E = inv(D)*E
-
-% Undo scaling by repeated squaring E = E^(2^s)
-%--------------------------------------------------------------------------
-for k = 1:s
-    E = E*E;
-end
-
-% Multiply by x if necessary
-%--------------------------------------------------------------------------
-if nargin > 1
-    x = E*x;
+    x     = E;
+    
 else
-    x = E;
-end
-
-return
-
-
-% notes: Alterntive but slower evaluation of exp(J)*x
-%==========================================================================
-
-if nargin > 1
 
     % compute y = expm(J)*x = (1 + J + J*J/2! + J*J*J/3!  + ...)*x
-    %----------------------------------------------------------------------
-    J     = sparse(J);
-    x     = sparse(x);
+    %-------------------------------------------------------------------
+    J     = full(J);
+    x     = full(x);
     x0    = x;
     fx    = J*x;
     j     = 1;
-    nx    = norm(fx,1);
-    TOL   = nx/256;
 
-    while nx > TOL
+    while norm(fx,1) > 1e-16
 
         % accumulate high-order terms until convergence
-        %------------------------------------------------------------------
+        %-----------------------------------------------------------
         j  = j + 1;
         x  = x + fx;
         fx = J*fx/j;
 
         % revert to Pade approximation if numerical overflow
-        %------------------------------------------------------------------
-        if isinf(nx)
+        %-----------------------------------------------------------
+        if norm(x,1) > 1e16
             fprintf('Reverting to Pade approximation')
-            x  = spm_expm(J)*x0;
+            x = spm_expm(J)*x0;
             return
-        else
-            nx = norm(fx,1);
         end
     end
-    
-else
-    x  = spm_expm(J);
 end
