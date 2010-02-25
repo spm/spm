4532b850a360685e11e6e8778efccedfaec8a1f4
Vladimir Litvak <v.litvak@ucl.ac.uk>
2010-02-25 13:08:21 +0000

    Bug fixes and robustness improvements (thanks to Olivier)
    SVN r3737

--- toolbox/dcm_meeg/spm_L_priors.m
+++ toolbox/dcm_meeg/spm_L_priors.m
@@ -10,7 +10,7 @@ function [pE,pC] = spm_L_priors(dipfit,pE,pC)
 %    dipfit.Lpos     - x,y,z source positions (mm)            (ECD)
 %    dipfit.Nm       - number of modes                        (IMG)
 %    dipfit.Ns       - number of sources
-%    dipfit.Nc       - number of channels           
+%    dipfit.Nc       - number of channels
 %
 % pE - prior expectation
 % pC - prior covariance
@@ -29,10 +29,10 @@ function [pE,pC] = spm_L_priors(dipfit,pE,pC)
 % neuronal dynamics. NeuroImage 20: 1743-1755
 %__________________________________________________________________________
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
- 
+
 % Karl Friston
-% $Id: spm_L_priors.m 3517 2009-10-29 15:11:56Z guillaume $
- 
+% $Id: spm_L_priors.m 3737 2010-02-25 13:08:21Z vladimir $
+
 % defaults
 %--------------------------------------------------------------------------
 try, model    = dipfit.model;    catch, model    = 'LFP'; end
@@ -40,7 +40,7 @@ try, type     = dipfit.type;     catch, type     = 'LFP'; end
 try, location = dipfit.location; catch, location = 0;     end
 try, symmetry = dipfit.symmetry; catch, symmetry = 0;     end
 try, pC;                         catch, pC       = [];    end
- 
+
 
 % number of sources
 %--------------------------------------------------------------------------
@@ -55,22 +55,22 @@ end
 % location priors (4 mm)
 %--------------------------------------------------------------------------
 if location, V = 2^2; else, V = 0; end
- 
+
 % parameters for electromagnetic forward model
 %==========================================================================
 switch type
- 
+    
     case{'ECD'} % mean           and variance
         %------------------------------------------------------------------
         pE.Lpos = dipfit.Lpos;   Lpos = V*eye(3*n);     % positions
         pE.L    = sparse(3,n);   L    =   eye(3*n);     % orientations
- 
+        
     case{'IMG'}
         %------------------------------------------------------------------
         m       = dipfit.Nm;                            % number of modes
         pE.Lpos = sparse(3,0);   Lpos =   eye(0,0);     % positions
         pE.L    = sparse(m,n);   L    =   eye(m*n);     % modes
- 
+        
     case{'LFP'}
         %------------------------------------------------------------------
         pE.Lpos = sparse(3,0);   Lpos =   eye(0,0);     % positions
@@ -78,23 +78,23 @@ switch type
         
     otherwise
         warndlg('Unknown spatial model')
- 
+        
 end
- 
+
 % contributing states (encoded in J)
 %==========================================================================
 switch model
- 
+    
     case{'ERP','SEP'}
         %------------------------------------------------------------------
         pE.J = sparse(1,[1 7 9],[0.2 0.2 0.6],1,9);       % 9 states
         J    = diag(pE.J/64);
- 
+        
     case{'LFP'}
         %------------------------------------------------------------------
         pE.J = sparse(1,[1 7 9],[0.2 0.2 0.6],1,13);      % 13 states
-        J    = diag(pE.J/64); 
- 
+        J    = diag(pE.J/64);
+        
     case{'NMM'}
         %------------------------------------------------------------------
         pE.J = sparse(1,[1,2,3],[0.1 0.1 1],1,9);          % 9 states
@@ -112,34 +112,49 @@ switch model
         
     otherwise
         warndlg('Unknown neural model')
- 
+        
 end
- 
+
 
 % Distance between homolgous sources (16mm)
 %--------------------------------------------------------------------------
-if symmetry, V = 16; else, V = 0; end
+if symmetry, V = 16; else V = 0; end
 
 % symmetry constraints (based on Euclidean distance from mirror image)
 %==========================================================================
 switch type
- 
+    
     case{'ECD'}
- 
-        % correlation
-        %------------------------------------------------------------------
-        Mpos  = [-pE.Lpos(1,:); pE.Lpos(2,:); pE.Lpos(3,:)];
-        for i = 1:n
-            for j = 1:n
-                D(i,j) = sqrt(sum(pE.Lpos(:,i) - Mpos(:,j)).^2);
+        if symmetry
+            
+            % correlation
+            %------------------------------------------------------------------
+            Mpos  = [-pE.Lpos(1,:); pE.Lpos(2,:); pE.Lpos(3,:)];
+            D = Inf*ones(n);
+            for i = 1:n
+                for j = 1:n
+                    if sign(pE.Lpos(1,i)) == sign(Mpos(1, j))
+                        D(i,j) = sqrt(sum(pE.Lpos(:,i) - Mpos(:,j)).^2);
+                    end
+                end
             end
+            D     = (D + D')/2;
+            DD = zeros(n);
+            for i = 1:n
+                [M, I] = min(D(i, :));
+                if M<V
+                    if length(I)>1
+                        warning(sprintf(['Could not uniquely determine the source symmetric to source %d. '...
+                            'Taking first of the candidates (source %d).'], i, I(1)));
+                    end
+                    DD(i, I(1)) = 1;
+                end
+            end
+            
+            L     = L - kron(DD,diag([1 0 0])) + kron(DD,diag([0 1 1]));
         end
-        D     = (D + D')/2;
-        D     = D < V;
-        D     = D - diag(diag(D));
-        L     = L - kron(D,diag([1 0 0])) + kron(D,diag([0 1 1]));
 end
- 
+
 % prior covariance
 %--------------------------------------------------------------------------
 pC  = spm_cat(spm_diag({pC, Lpos, exp(8)*L, J}));
