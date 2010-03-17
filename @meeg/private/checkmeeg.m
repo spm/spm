19c015be5a59726c1db8bd2a2babe834ddee5ad8
Vladimir Litvak <v.litvak@ucl.ac.uk>
2010-03-17 14:24:47 +0000

    Bug fixes for multimodal data handling in DCM (thanks to Rik)
    SVN r3784

--- @meeg/private/checkmeeg.m
+++ @meeg/private/checkmeeg.m
@@ -9,7 +9,7 @@ function [result meegstruct]=checkmeeg(meegstruct, option)
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
 % Vladimir Litvak
-% $Id: checkmeeg.m 3679 2010-01-14 10:49:35Z vladimir $
+% $Id: checkmeeg.m 3784 2010-03-17 14:24:47Z vladimir $
 
 if nargin==1
     option = 'basic';
@@ -406,14 +406,14 @@ megind = strmatch('MEG', chantypes);
 lfpind = strmatch('LFP', chantypes, 'exact');
 
 % Allow DCM on a pure LFP dataset
-if strcmp(option, 'dcm') && isempty([eegind megind])...
+if strcmp(option, 'dcm') && isempty([eegind(:); megind(:)])...
         && ~isempty(lfpind)&& strcmp(meegstruct.transform.ID, 'time')
     result = 1;
     return;
 end
 
 if strcmp(option, 'sensfid') || strcmp(option, '3d') ||...
-        (strcmp(option, 'dcm') && ~isempty([eegind megind]))    
+        (strcmp(option, 'dcm') && ~isempty([eegind(:); megind(:)]))    
     
     if ~strcmp(meegstruct.transform.ID, 'time')
         disp('checkmeeg: incorrect data type for source reconstruction');
