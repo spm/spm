3df22307d484dd0f93f53173cb8ef0ad558c84b2
Volkmar Glauche <volkmar.glauche@uniklinik-freiburg.de>
2010-07-13 10:27:17 +0000

    Display filter in dependency, if available
    SVN r3990

--- matlabbatch/cfg_basicio/cfg_vout_file_fplist.m
+++ matlabbatch/cfg_basicio/cfg_vout_file_fplist.m
@@ -12,12 +12,16 @@ function dep = cfg_vout_file_fplist(job)
 % Copyright (C) 2007 Freiburg Brain Imaging
 
 % Volkmar Glauche
-% $Id: cfg_vout_file_fplist.m 3921 2010-06-11 12:09:57Z volkmar $
+% $Id: cfg_vout_file_fplist.m 3990 2010-07-13 10:27:17Z volkmar $
 
-rev = '$Rev: 3921 $'; %#ok
+rev = '$Rev: 3990 $'; %#ok
 
 dep(1)            = cfg_dep;
-dep(1).sname      = 'Selected Files';
+if ~ischar(job.filter) || strcmp(job.filter,'<UNDEFINED>')
+    dep(1).sname  = 'Selected Files';
+else
+    dep(1).sname  = sprintf('Selected Files (%s)', job.filter);
+end
 dep(1).src_output = substruct('.','files');
 dep(1).tgt_spec   = cfg_findspec({{'class','cfg_files','strtype','e'}}); % This will display for both file and dir inputs. There is no notion of "no directories" in matching.
 dep(2)            = cfg_dep;
