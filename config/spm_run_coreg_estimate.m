a75b0412ca2eedbe8301dacafd1fbb259a4344f1
Guillaume Flandin <g.flandin@ucl.ac.uk>
2011-01-12 18:19:38 +0000

    Bug fix following r4152. Same problem (due to strvcat->char replacement) might occur elsewhere.
    SVN r4157

--- config/spm_run_coreg_estimate.m
+++ config/spm_run_coreg_estimate.m
@@ -6,24 +6,24 @@ function out = spm_run_coreg_estimate(varargin)
 % job    - harvested job data structure (see matlabbatch help)
 % Output:
 % out    - computation results, usually a struct variable.
-%_______________________________________________________________________
+%__________________________________________________________________________
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
-% $Id: spm_run_coreg_estimate.m 4152 2011-01-11 14:13:35Z volkmar $
+% $Id: spm_run_coreg_estimate.m 4157 2011-01-12 18:19:38Z guillaume $
 
 job = varargin{1};
-%disp(job);
-%disp(job.eoptions);
 
-x  = spm_coreg(char(job.ref), char(job.source),job.eoptions);
+x  = spm_coreg(char(job.ref), char(job.source), job.eoptions);
+
 M  = spm_matrix(x);
 PO = [job.source(:); job.other(:)];
+PO = PO(~cellfun('isempty',PO));
 MM = zeros(4,4,numel(PO));
-for j=1:numel(PO),
-    MM(:,:,j) = spm_get_space(PO{j,:});
-end;
-for j=1:numel(PO),
+for j=1:numel(PO)
+    MM(:,:,j) = spm_get_space(PO{j});
+end
+for j=1:numel(PO)
     spm_get_space(PO{j}, M\MM(:,:,j));
-end;
+end
+
 out.cfiles = PO;
-return;
