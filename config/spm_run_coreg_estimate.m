61b20da5082a42c7e10edf83c998f00163ab2483
Volkmar Glauche <volkmar.glauche@uniklinik-freiburg.de>
2011-01-13 13:44:37 +0000

    Use same code snippet as in spm_run_coreg_estwrite.m to deal with empty 'other files'.
    SVN r4161

--- config/spm_run_coreg_estimate.m
+++ config/spm_run_coreg_estimate.m
@@ -9,15 +9,17 @@ function out = spm_run_coreg_estimate(varargin)
 %__________________________________________________________________________
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
-% $Id: spm_run_coreg_estimate.m 4157 2011-01-12 18:19:38Z guillaume $
+% $Id: spm_run_coreg_estimate.m 4161 2011-01-13 13:44:37Z volkmar $
 
 job = varargin{1};
+if isempty(job.other{1})
+    job.other = {};
+end
 
 x  = spm_coreg(char(job.ref), char(job.source), job.eoptions);
 
 M  = spm_matrix(x);
 PO = [job.source(:); job.other(:)];
-PO = PO(~cellfun('isempty',PO));
 MM = zeros(4,4,numel(PO));
 for j=1:numel(PO)
     MM(:,:,j) = spm_get_space(PO{j});
