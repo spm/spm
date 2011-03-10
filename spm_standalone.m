3ee3b8e56dafa3e0e061e69027b11ace7e155096
Guillaume Flandin <g.flandin@ucl.ac.uk>
2011-03-10 19:46:33 +0000

    Added 'script' option.
    SVN r4237

--- spm_standalone.m
+++ spm_standalone.m
@@ -1,33 +1,60 @@
 function spm_standalone(varargin)
 % A function to be compiled, which will run a standalone SPM.
 %
-% See http://www.mathworks.com/products/compiler/
+% See MATLAB Compiler: http://www.mathworks.com/products/compiler/
+% See also config/spm_make_standalone.m
 %__________________________________________________________________________
-% Copyright (C) 2010 Wellcome Trust Centre for Neuroimaging
+% Copyright (C) 2010-2011 Wellcome Trust Centre for Neuroimaging
 
-% John Ashburner
-% $Id: spm_standalone.m 4017 2010-07-26 17:14:31Z guillaume $ 
+% Guillaume Flandin
+% $Id: spm_standalone.m 4237 2011-03-10 19:46:33Z guillaume $ 
 
 [v,r] = spm('Ver');
 fprintf('%s (%s): %s\n',v,r,spm('Dir'));
 
-if nargin && strcmpi(varargin{1},'run')
-    spm('asciiwelcome');
-    %spm('defaults','fmri');
-    spm_jobman('initcfg');
-    if nargin == 1
-        h = spm_jobman;
-        waitfor(h,'Visible','off');
-    else
-        for i=2:nargin
-            try
-                spm_jobman('run',varargin{i});
-            catch
-                fprintf('Execution of batch file ''%s'' failed.\n',varargin{i});
+if ~nargin, action = ''; else action = varargin{1}; end
+
+switch lower(action)
+    
+    case {'batch', 'run'}
+    %----------------------------------------------------------------------
+        spm('asciiwelcome');
+        %spm('defaults','fmri');
+        spm_jobman('initcfg');
+        if nargin == 1
+            h = spm_jobman;
+            waitfor(h,'Visible','off');
+        else
+            %spm_get_defaults('cmdline',true);
+            for i=2:nargin
+                try
+                    spm_jobman('run',varargin{i});
+                catch
+                    fprintf('Execution failed: %s', varargin{i});
+                end
             end
         end
-    end
-    spm('Quit');
-else
-    spm(varargin{:});
+        spm('Quit');
+        
+    case 'script'
+    %----------------------------------------------------------------------
+        spm('asciiwelcome');
+        if nargin > 1
+            inputs = varargin(3:end);
+            fid = fopen(varargin{2});
+            if fid == -1, error('Cannot open %s',varargin{2}); end
+            S = fscanf(fid,'%c');
+            fclose(fid);
+            try
+                eval(S);
+            catch
+                fprintf('Execution failed: %s\n',varargin{2});
+                rethrow(lasterror);
+            end
+        end
+        
+    otherwise
+    %----------------------------------------------------------------------
+        spm(varargin{:});
+        
 end
