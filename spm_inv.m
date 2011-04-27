08bbf2c253a36eb57021d48c68da4876a640a28e
Ged Ridgway <ged.ridgway@gmail.com>
2011-04-27 20:39:03 +0000

    Rewritten spm_inv should be a little more accurate in terms of the consistency of repeated inversions, and can be up to 15% faster for complicated sparse symmetric semidefinite matrices, but at the cost of being a little bit slower for simpler (e.g. diagonal or tri-diagonal) matrices.
    SVN r4317

--- spm_inv.m
+++ spm_inv.m
@@ -9,22 +9,49 @@ function X = spm_inv(A,TOL)
 %
 % This routine simply adds a small diagonal matrix to A and calls inv.m
 %__________________________________________________________________________
-% Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
+% Copyright (C) 2008-2011 Wellcome Trust Centre for Neuroimaging
  
-% Karl Friston
-% $Id: spm_inv.m 4281 2011-03-31 19:49:57Z karl $
+% Karl Friston and Ged Ridgway
+% $Id: spm_inv.m 4317 2011-04-27 20:39:03Z ged $
  
-% check A 
-%--------------------------------------------------------------------------
-[m,n] = size(A);
-if isempty(A), X = sparse(n,m); return, end
- 
-% tolerance
-%--------------------------------------------------------------------------
-if nargin == 1
-    TOL  = max(eps(norm(A,'inf'))*max(m,n),exp(-32)); 
+if nargin < 2
+    TOL  = max(eps(norm(A,'inf'))*length(A), exp(-32));
 end
 
-% inverse
-%--------------------------------------------------------------------------
-X     = inv(sparse(A) + speye(m,n)*TOL);
+[i j] = find(A);
+if isempty(i)
+    % Special cases:  empty or all-zero matrix, return identity/TOL
+    %----------------------------------------------------------------------
+    X = eye(length(A)) / TOL;
+elseif all(i == j)
+    % diagonal matrix
+    %----------------------------------------------------------------------
+    d = diag(A);
+    d = invtol(d, TOL);
+    X = diag(d);
+elseif norm(A - A', 1) < TOL
+    % symmetric, use LDL factorisation (but with L->X to save memory)
+    %----------------------------------------------------------------------
+    [X D P] = ldl(full(A)); % P'*A*P = L*D*L', A = P*L*D*L'*P'
+    d = diag(D);
+    d = invtol(d, TOL);
+    % inv(A) = P*inv(L')*inv(D)*inv(L)*P' = (L\P')'*inv(D)*(L\P')
+    % triangular system should be quick to solve and stay approx triangular
+    X = X\P';
+    X = X'*diag(d)*X;
+else
+    % asymmetric matrix -- use old approach (SVD seems not to work well)
+    %----------------------------------------------------------------------
+    X = inv(A + TOL*eye(length(A)));
+    % [U S X] = svd(full(A));
+    % d = diag(S);
+    % d = invtol(d, TOL);
+    % X = X*diag(d)*U';
+end
+if issparse(A), X = sparse(X); end
+    
+function d = invtol(d, TOL)
+% compute reciprocal of values, clamped to lie between TOL and 1/TOL
+d = max(d, TOL);
+d = 1./d;
+d = max(d, TOL);
