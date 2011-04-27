a91eed50a3c531d5a8f4c1dd83796bd63576839c
Ged Ridgway <ged.ridgway@gmail.com>
2011-04-27 20:58:14 +0000

    Avoid creating full matrices from sparse inputs in my just-checked-in modified spm_inv.
    SVN r4318

--- spm_inv.m
+++ spm_inv.m
@@ -5,14 +5,14 @@ function X = spm_inv(A,TOL)
 % A   - matrix
 % X   - inverse
 %
-% TOL - tolerance: default = max(eps(norm(A,'inf'))*max(m,n),exp(-32))
+% TOL - tolerance: default = max(eps(norm(A,'inf'))*length(A), exp(-32))
 %
 % This routine simply adds a small diagonal matrix to A and calls inv.m
 %__________________________________________________________________________
 % Copyright (C) 2008-2011 Wellcome Trust Centre for Neuroimaging
  
 % Karl Friston and Ged Ridgway
-% $Id: spm_inv.m 4317 2011-04-27 20:39:03Z ged $
+% $Id: spm_inv.m 4318 2011-04-27 20:58:14Z ged $
  
 if nargin < 2
     TOL  = max(eps(norm(A,'inf'))*length(A), exp(-32));
@@ -28,7 +28,12 @@ elseif all(i == j)
     %----------------------------------------------------------------------
     d = diag(A);
     d = invtol(d, TOL);
-    X = diag(d);
+    if issparse(A)
+        n = length(A);
+        X = sparse(1:n, 1:n, d);
+    else
+        X = diag(d);
+    end
 elseif norm(A - A', 1) < TOL
     % symmetric, use LDL factorisation (but with L->X to save memory)
     %----------------------------------------------------------------------
@@ -39,16 +44,17 @@ elseif norm(A - A', 1) < TOL
     % triangular system should be quick to solve and stay approx triangular
     X = X\P';
     X = X'*diag(d)*X;
+    if issparse(A), X = sparse(X); end
 else
     % asymmetric matrix -- use old approach (SVD seems not to work well)
     %----------------------------------------------------------------------
-    X = inv(A + TOL*eye(length(A)));
+    X = inv(A + TOL*speye(length(A))); % returns sparse if A was, else full
     % [U S X] = svd(full(A));
     % d = diag(S);
     % d = invtol(d, TOL);
     % X = X*diag(d)*U';
+    % if issparse(A), X = sparse(X); end
 end
-if issparse(A), X = sparse(X); end
     
 function d = invtol(d, TOL)
 % compute reciprocal of values, clamped to lie between TOL and 1/TOL
