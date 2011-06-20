e0fcc73660beaa51ba5f153af71e507a85166566
Gareth Barnes <g.barnes@ucl.ac.uk>
2011-06-20 14:51:19 +0000

    added wdale to allow extrapolation over time
    SVN r4370

--- spm_eeg_inv_vbecd.m
+++ spm_eeg_inv_vbecd.m
@@ -31,7 +31,7 @@ function P = spm_eeg_inv_vbecd(P)
 % Copyright (C) 2009 Wellcome Trust Centre for Neuroimaging
 
 % Gareth Barnes
-% $Id: spm_eeg_inv_vbecd.m 4281 2011-03-31 19:49:57Z karl $
+% $Id: spm_eeg_inv_vbecd.m 4370 2011-06-20 14:51:19Z gareth $
 
 
 
@@ -57,7 +57,11 @@ y    = P.y;
 sc_y = 1/std(y);
 y    = y*sc_y;
 Y.y  = y;
+
 U.u  = 1;
+if isfield(P,'chanCov'),
+    Y.Q{1}=P.chanCov*sc_y*sc_y;
+end;
 
 outsideflag=1;
 while outsideflag==1, %% don't use sources which end up outside the head
@@ -65,7 +69,7 @@ while outsideflag==1, %% don't use sources which end up outside the head
     % set random moment, scaled by prior variances
     %----------------------------------------------------------------------
     [u,s,v] = svd(S_w0);
-     mu_w   = mu_w0 + u*diag(sqrt(diag(s+eps)))*v'*randn(size(mu_w0));
+    mu_w   = mu_w0 + u*diag(sqrt(diag(s+eps)))*v'*randn(size(mu_w0));
     
     % a random guess for the location, based on variance of the prior
     %----------------------------------------------------------------------
@@ -79,11 +83,11 @@ while outsideflag==1, %% don't use sources which end up outside the head
             outside = outside+ ~ft_inside_vol(pos',P.forward.vol);
         end;
     end;
-
+    
     % get lead fields
     %----------------------------------------------------------------------
     M.pE  = [mu_s;mu_w];            % prior parameter estimate
-  
+    
     
     
     M.pC  = blkdiag(S_s0,S_w0);     % prior covariance estimate
@@ -96,27 +100,47 @@ while outsideflag==1, %% don't use sources which end up outside the head
     startguess=M.pE;
     M.Setup =P;             % pass volume conductor and sensor locations on
     M.sc_y =sc_y;           % pass on scaling factor
-   
-  %% startguess=[-0.3553  -69.8440    1.0484    0.2545    0.3428    1.8526]'
+    
+    %% startguess=[-0.3553  -69.8440    1.0484    0.2545    0.3428    1.8526]'
     
     [starty]=spm_eeg_wrap_dipfit_vbecd(startguess,M,U);
-        [Ep,Cp,Eh,F] = spm_nlsi_GN(M,U,Y);
+    [Ep,Cp,Eh,F] = spm_nlsi_GN(M,U,Y);
     P.Ep = Ep;
     P.Cp = Cp;
     P.Eh = Eh;
     P.F  = F;
-    [P.ypost,outsideflag]=spm_eeg_wrap_dipfit_vbecd(P.Ep,M,U);
+    [P.ypost,outsideflag,leads]=spm_eeg_wrap_dipfit_vbecd(P.Ep,M,U);
     P.ypost = P.ypost./sc_y; %%  scale it back
     
     if outsideflag
         disp('running again, one or more dipoles outside head.');
     end;
-
+    
 end; % while
 P.post_mu_s = Ep(1:length(mu_s));
 P.post_mu_w = Ep(length(mu_s)+1:end);
 P.post_S_s  = Cp(1:length(mu_s),1:length(mu_s));
 P.post_S_w  = Cp(length(mu_s)+1:end,length(mu_s)+1:end);
+
+
+%% return a weight matrix to map channels to dipoles
+fulldipmom=[];
+fulllf=[];
+
+for d=1:Nd,
+    dippos=P.post_mu_s((d-1)*3+1:d*3);
+    dipmom=P.post_mu_w((d-1)*3+1:d*3);
+     
+    fulllf=[fulllf squeeze(leads(d,:,:))']; % *unitmom(d,:)';
+    fulldipmom=[fulldipmom ;dipmom];
+end;
+estdata=fulllf*fulldipmom/1000;
+
+D1=fulldipmom*fulldipmom';
+P.post_wdale=D1*fulllf'*pinv(fulllf*D1*fulllf')*1000; % 1000 to put in nAm
+estdipmom=P.post_wdale*estdata; %% re-estimate moment using the dale operator
+%disp(sprintf('reistimation error %3.2f percent',mean((estdipmom-fulldipmom)./fulldipmom)*100));
+
 
 
 
