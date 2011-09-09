c8f70df0417f025c349de128f6f9104882cfb505
Guillaume Flandin <g.flandin@ucl.ac.uk>
2011-09-09 15:20:38 +0000

    Cleanup.
    SVN r4474

--- config/spm_run_fmri_spec.m
+++ config/spm_run_fmri_spec.m
@@ -10,17 +10,17 @@ function out = spm_run_fmri_spec(job)
 %__________________________________________________________________________
 % Copyright (C) 2005-2011 Wellcome Trust Centre for Neuroimaging
 
-% $Id: spm_run_fmri_spec.m 4472 2011-09-08 17:42:32Z guillaume $
+% $Id: spm_run_fmri_spec.m 4474 2011-09-09 15:20:38Z guillaume $
 
 
-original_dir = pwd;
-my_cd(job.dir);
+%-Check presence of previous analysis
+%==========================================================================
 
-design_only = ~isfield(job,'mask');
+change_dir(job.dir{1});
 
 %-Ask about overwriting files from previous analyses
 %--------------------------------------------------------------------------
-if exist(fullfile(job.dir{1},'SPM.mat'),'file')
+if exist(fullfile(pwd,'SPM.mat'),'file')
     str = { 'Current directory contains existing SPM file:',...
         'Continuing will overwrite existing file!'};
     if spm_input(str,1,'bd','stop|continue',[1,0],1,mfilename);
@@ -30,8 +30,7 @@ if exist(fullfile(job.dir{1},'SPM.mat'),'file')
     end
 end
 
-% If we've gotten to this point we're committed to overwriting files.
-% Delete them so we don't get stuck in spm_spm
+%-Delete old analysis files
 %--------------------------------------------------------------------------
 files = {'^mask\..{3}$','^ResMS\..{3}$','^RPV\..{3}$',...
     '^beta_.{4}\..{3}$','^con_.{4}\..{3}$','^ResI_.{4}\..{3}$',...
@@ -44,21 +43,22 @@ for i=1:length(files)
     end
 end
 
-% Variables
+
+%-Timing parameters & Basis functions
+%==========================================================================
+
+%-Repeat time
 %--------------------------------------------------------------------------
 SPM.xY.RT = job.timing.RT;
-if ~design_only
-    SPM.xY.P = [];
-end
 
-% Basis function variables
+%-Basis function parameters
 %--------------------------------------------------------------------------
 SPM.xBF.UNITS = job.timing.units;
-SPM.xBF.dt    = job.timing.RT/job.timing.fmri_t;
+%SPM.xBF.dt    = job.timing.RT/job.timing.fmri_t;
 SPM.xBF.T     = job.timing.fmri_t;
 SPM.xBF.T0    = job.timing.fmri_t0;
 
-% Basis functions
+%-Basis functions
 %--------------------------------------------------------------------------
 if strcmp(fieldnames(job.bases),'hrf')
     if all(job.bases.hrf.derivs == [0 0])
@@ -68,41 +68,45 @@ if strcmp(fieldnames(job.bases),'hrf')
     elseif all(job.bases.hrf.derivs == [1 1])
         SPM.xBF.name = 'hrf (with time and dispersion derivatives)';
     else
-        error('Unrecognized hrf derivative choices.')
+        error('Unknown HRF derivative choices.');
     end
 else
-    nambase = fieldnames(job.bases);
-    if ischar(nambase)
-        nam=nambase;
-    else
-        nam=nambase{1};
-    end
-    switch nam,
-        case 'fourier',
+    switch char(fieldnames(job.bases))
+        case 'fourier'
             SPM.xBF.name = 'Fourier set';
-        case 'fourier_han',
+        case 'fourier_han'
             SPM.xBF.name = 'Fourier set (Hanning)';
-        case 'gamma',
+        case 'gamma'
             SPM.xBF.name = 'Gamma functions';
-        case 'fir',
+        case 'fir'
             SPM.xBF.name = 'Finite Impulse Response';
         otherwise
-            error('Unrecognized hrf derivative choices.')
+            error('Unknown basis functions.');
     end
     SPM.xBF.length = job.bases.(nam).length;
     SPM.xBF.order  = job.bases.(nam).order;
 end
-SPM.xBF          = spm_get_bf(SPM.xBF);
-if isempty(job.sess),
-    SPM.xBF.Volterra = false;
-else
-    SPM.xBF.Volterra = job.volt;
+%SPM.xBF = spm_get_bf(SPM.xBF);
+
+%-Model interactions (Volterra)
+%--------------------------------------------------------------------------
+SPM.xBF.Volterra = job.volt;
+
+
+%-Data & Design
+%==========================================================================
+
+design_only = ~isfield(job,'mask');
+
+if ~design_only
+    SPM.xY.P = [];
 end
 
-for i = 1:numel(job.sess),
+for i = 1:numel(job.sess)
+    
     sess = job.sess(i);
 
-    % Image filenames
+    %-Image filenames
     %----------------------------------------------------------------------
     if design_only
         SPM.nscan(i) = sess.nscan;
@@ -110,37 +114,45 @@ for i = 1:numel(job.sess),
         SPM.nscan(i) = numel(sess.scans);
         SPM.xY.P     = strvcat(SPM.xY.P,sess.scans{:});
     end
-    U = [];
 
-    % Augment the singly-specified conditions with the multiple
-    % conditions specified in a .mat file provided by the user
+    %-Multiple conditions (structure from a MAT-file)
     %----------------------------------------------------------------------
     if ~isempty(sess.multi{1})
+        
+        %-Load MAT-file
+        %------------------------------------------------------------------
         try
             multicond = load(sess.multi{1});
         catch
             error('Cannot load %s',sess.multi{1});
         end
-        if ~(isfield(multicond,'names')&&isfield(multicond,'onsets')&&...
-                isfield(multicond,'durations')) || ...
-            ~all([numel(multicond.names),numel(multicond.onsets), ...
-                numel(multicond.durations)]==numel(multicond.names))
-            error(['Multiple conditions MAT-file ''%s'' is invalid.\n',...
+        
+        %-Check structure content
+        %------------------------------------------------------------------
+        if ~all(isfield(multicond, {'name','onsets','durations'})) || ...
+           ~iscell(multicond.name) || ...
+           ~iscell(multicond.onsets) || ...
+           ~iscell(multicond.durations) || ...
+           ~isequal(numel(multicond.name), numel(multicond.onsets), ...
+                    numel(multicond.durations))
+            error(['Multiple conditions MAT-file ''%s'' is invalid:\n',...
            'File must contain names, onsets, and durations '...
            'cell arrays of equal length.\n'],sess.multi{1});
         end
     
-        %-contains three cell arrays: names, onsets and durations
-        for j=1:length(multicond.onsets)
+        for j=1:numel(multicond.onsets)
+            
+            %-Mutiple Conditions: names, onsets and durations
+            %--------------------------------------------------------------
             cond.name     = multicond.names{j};
             cond.onset    = multicond.onsets{j};
             cond.duration = multicond.durations{j};
             
-            % Mutiple Conditions Time Modulation
+            %-Mutiple Conditions: Time Modulation
             %--------------------------------------------------------------
-            % initialise the variable.
-            cond.tmod = 0;
-            if isfield(multicond,'tmod');
+            if ~isfield(multicond,'tmod');
+                cond.tmod = 0;
+            else
                 try
                     cond.tmod = multicond.tmod{j};
                 catch
@@ -148,171 +160,166 @@ for i = 1:numel(job.sess),
                 end
             end
 
-            % Mutiple Conditions Parametric Modulation
+            %-Mutiple Conditions: Parametric Modulation
             %--------------------------------------------------------------
-            % initialise the parametric modulation variable.
-            cond.pmod = [];
-            if isfield(multicond,'pmod')
-                % only access existing modulators
+            if ~isfield(multicond,'pmod')
+                cond.pmod = [];
+            else
                 try
-                    % check if there is a parametric modulator. this allows
-                    % pmod structures with fewer entries than conditions.
-                    % then check whether any cells are filled in.
+                    %-Check if a PM is defined for that condition
                     if (j <= numel(multicond.pmod)) && ...
                             ~isempty(multicond.pmod(j).name)
-
-                        % we assume that the number of cells in each
-                        % field of pmod is the same (or should be).
                         for ii = 1:numel(multicond.pmod(j).name)
                             cond.pmod(ii).name  = multicond.pmod(j).name{ii};
                             cond.pmod(ii).param = multicond.pmod(j).param{ii};
                             cond.pmod(ii).poly  = multicond.pmod(j).poly{ii};
                         end
-                    end;
+                    end
                 catch
                     error('Error specifying parametric modulation.');
                 end
             end
+            
+            %-Append to singly-specified conditions
+            %--------------------------------------------------------------
             sess.cond(end+1) = cond;
         end
     end
 
-    % Configure the input structure array
+    %-Conditions
     %----------------------------------------------------------------------
-    for j = 1:length(sess.cond),
+    U = [];
+    
+    for j = 1:numel(sess.cond)
+        
+        %-Name, Onsets, Durations
+        %------------------------------------------------------------------
         cond      = sess.cond(j);
         U(j).name = {cond.name};
         U(j).ons  = cond.onset(:);
         U(j).dur  = cond.duration(:);
         if length(U(j).dur) == 1
-            U(j).dur    = U(j).dur*ones(size(U(j).ons));
-        elseif length(U(j).dur) ~= length(U(j).ons)
-            error('Mismatch between number of onset and number of durations.')
+            U(j).dur = repmat(U(j).dur,size(U(j).ons));
+        elseif numel(U(j).dur) ~= numel(U(j).ons)
+            error('Mismatch between number of onset and number of durations.');
         end
 
+        %-Modulations
+        %------------------------------------------------------------------
         P  = [];
         q1 = 0;
-        if cond.tmod>0,
-            % time effects
+        %-Time Modulation
+        if cond.tmod > 0
             P(1).name = 'time';
-            P(1).P    = U(j).ons*job.timing.RT/60;
+            P(1).P    = U(j).ons * job.timing.RT / 60;
             P(1).h    = cond.tmod;
             q1        = 1;
-        end;
+        end
+        %-Parametric Modulations
         if ~isempty(cond.pmod)
-            for q = 1:numel(cond.pmod),
-                % Parametric effects
+            for q = 1:numel(cond.pmod)
                 q1 = q1 + 1;
                 P(q1).name = cond.pmod(q).name;
                 P(q1).P    = cond.pmod(q).param(:);
                 P(q1).h    = cond.pmod(q).poly;
-            end;
+            end
         end
+        %-None
         if isempty(P)
             P.name = 'none';
             P.h    = 0;
         end
+        
         U(j).P = P;
 
     end
 
     SPM.Sess(i).U = U;
 
-
-    % User specified regressors
+    %-User-specified regressors
     %----------------------------------------------------------------------
     C     = [];
     Cname = cell(1,numel(sess.regress));
-    for q = 1:numel(sess.regress),
+    for q = 1:numel(sess.regress)
         Cname{q} = sess.regress(q).name;
+        if numel(sess.regress(q).val(:)) ~= SPM.nscan(i)
+            error('Length of regressor is not commensurate with data points.');
+        end
         C        = [C, sess.regress(q).val(:)];
     end
-%     % checks from spm_fMRI_design
-%     reg_rows = size(C,1);
-%     if (reg_rows > 0) && ~(reg_rows== k)
-%         str1='Error in spm_fMRI_design.m:';
-%         str2=sprintf('Session %d has %d scans but regressors have %d entries', s,k,reg_rows);
-%         str3='These numbers should match';
-%         warndlg({str1; str2; str3});
-%         return
-%     end
     
-    
-    % Augment the singly-specified regressors with the multiple regressors
-    % specified in the regressors.mat file
+    %-Multiple regressors (from a TXT/MAT-file)
     %----------------------------------------------------------------------
-    if ~strcmp(sess.multi_reg,'')
-        tmp = load(char(sess.multi_reg{:}));
+    if ~isempty(sess.multi_reg{1})
+        tmp = load(sess.multi_reg{1});
         if isstruct(tmp) && isfield(tmp,'R')
             R = tmp.R;
         elseif isnumeric(tmp)
-            % load from e.g. text file
             R = tmp;
         else
             warning('Can''t load user specified regressors in %s', ...
-                char(sess.multi_reg{:}));
+                sess.multi_reg{1});
             R = [];
         end
-
+        
+        if size(R,2) ~= SPM.nscan(i)
+            error('Length of regressor is not commensurate with data points.');
+        end
         C  = [C, R];
-        nr = size(R,2);
-        nq = length(Cname);
-        for inr=1:nr,
-            Cname{inr+nq} = ['R',int2str(inr)];
+        for j=1:size(R,2)
+            Cname{end+1} = sprintf('R%d',j);
         end
     end
+    
     SPM.Sess(i).C.C    = C;
     SPM.Sess(i).C.name = Cname;
 
 end
 
-% Factorial design
+%-Factorial design
 %--------------------------------------------------------------------------
-if isfield(job,'fact')
-    if ~isempty(job.fact)
-        NC=length(SPM.Sess(1).U); % Number of conditions
-        CheckNC=1;
-        for i=1:length(job.fact)
-            SPM.factor(i).name=job.fact(i).name;
-            SPM.factor(i).levels=job.fact(i).levels;
-            CheckNC=CheckNC*SPM.factor(i).levels;
-        end
-        if ~(CheckNC==NC)
-            disp('Error in fmri_spec job: factors do not match conditions');
-            return
-        end
+if ~isempty(job.fact)
+    for i=1:numel(job.fact)
+        SPM.factor(i).name   = job.fact(i).name;
+        SPM.factor(i).levels = job.fact(i).levels;
+    end
+    if prod([SPM.factor.levels]) ~= numel(SPM.Sess(1).U)
+        error('Factors do not match conditions');
     end
 else
-    SPM.factor=[];
+    SPM.factor = [];
 end
 
-% Globals
+%-Globals
 %--------------------------------------------------------------------------
 SPM.xGX.iGXcalc = job.global;
-SPM.xGX.sGXcalc = 'mean voxel value';
-SPM.xGX.sGMsca  = 'session specific';
+%SPM.xGX.sGXcalc = 'mean voxel value';
+%SPM.xGX.sGMsca  = 'session specific';
 
-% Masking threshold
+%-Masking threshold
 %--------------------------------------------------------------------------
 SPM.xM.gMT = job.mthresh;
 
-% High Pass filter
+%-High Pass filter
 %--------------------------------------------------------------------------
 for i = 1:numel(job.sess),
     SPM.xX.K(i).HParam = job.sess(i).hpf;
 end
 
-% Autocorrelation
+%-Autocorrelation
 %--------------------------------------------------------------------------
 SPM.xVi.form = job.cvi;
 
-% Let SPM configure the design
-%--------------------------------------------------------------------------
+
+%-Setting up the GLM
+%==========================================================================
 SPM = spm_fmri_spm_ui(SPM);
 
+%-Explicit mask
+%--------------------------------------------------------------------------
 if ~design_only
-    if ~isempty(job.mask) && ~isempty(job.mask{1})
-        SPM.xM.VM         = spm_vol(job.mask{:});
+    if ~isempty(job.mask{1})
+        SPM.xM.VM         = spm_vol(job.mask{1});
         SPM.xM.xs.Masking = [SPM.xM.xs.Masking, '+explicit mask'];
     end
 end
@@ -329,16 +336,17 @@ end
 fprintf('%30s\n','...SPM.mat saved')                                    %-#
 
 out.spmmat{1} = fullfile(pwd, 'SPM.mat');
-my_cd(original_dir); % Change back dir
-fprintf('Done\n')
-return
+
+change_dir;
+
 
 %==========================================================================
-function my_cd(jobDir)
-if ~isempty(jobDir)
-    try
-        cd(char(jobDir));
-    catch
-        error('Failed to change directory. Aborting run.')
-    end
+function change_dir(wd)
+persistent cwd
+if nargin
+    cwd = pwd;
+    nwd = spm_file(wd,'cpath');
+else
+    nwd = cwd;
 end
+cd(nwd)
--- spm_get_ons.m
+++ spm_get_ons.m
@@ -1,5 +1,5 @@
 function [U] = spm_get_ons(SPM,s)
-% Returns input [designed effects] structures
+% Return input [designed effects] structure
 % FORMAT [U] = spm_get_ons(SPM,s)
 %
 % SPM   - SPM structure (see spm_fMRI_design.m)
@@ -12,7 +12,7 @@ function [U] = spm_get_ons(SPM,s)
 %   U(i).dt     - time bin (seconds)
 %   U(i).ons    - onsets    (in SPM.xBF.UNITS)
 %   U(i).dur    - durations (in SPM.xBF.UNITS)
-%   U(i).P      - parameter struct.
+%   U(i).P      - parameter structure
 %
 %       U(i).P(p).name - parameter name
 %       U(i).P(p).P    - parameter vector
@@ -20,32 +20,27 @@ function [U] = spm_get_ons(SPM,s)
 %       U(i).P(p).i    - sub-indices of u pertaining to P
 %__________________________________________________________________________
 %
-% SLICE TIMING
+% Note on Slice Timing:
 %
 % With longs TRs you may want to shift the regressors so that they are
-% aligned to a particular slice. This is effected by resetting the
-% values of defaults.stats.fmri.t and defaults.stats.fmri.t0 in
-% spm_defaults. 
-% defaults.stats.fmri.t is the number of time-bins per scan used when
-% building regressors. Onsets are defined in temporal units of scans
-% starting at 0.
-% defaults.stats.fmri.t0 is the first time-bin at which the regressors are
-% resampled to coincide with data acquisition. If defaults.stats.fmri.t0
-% is set to 1 then the regressors will be appropriate for the first slice.
+% aligned to a particular slice. This is controlled by two variables:
+% fMRI_T is the number of time-bins per scan used when building regressors.
+% Onsets are defined in temporal units of scans starting at 0.
+% fMRI_T0 is the first time-bin at which the regressors are resampled to 
+% coincide with data acquisition. If fMRI_T0 is set to 1 then the 
+% regressors will be appropriate for the first slice.
 % If you want to temporally realign the regressors so that they match
-% responses in the middle slice then make defaults.stats.fmri.t0 =
-% defaults.stats.fmri.t/2 (assuming there is a negligible gap between
-% volume acquisitions).
-% Default values are defaults.stats.fmri.t=16 and defaults.stats.fmri.t0=1.
-%
+% responses in the middle slice then make fMRI_T0 = fMRI_T/2 (assuming
+% there is a negligible gap between volume acquisitions).
+% Default values are defined in spm_defaults.m
 %__________________________________________________________________________
-% Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
+% Copyright (C) 1999-2011 Wellcome Trust Centre for Neuroimaging
 
 % Karl Friston
-% $Id: spm_get_ons.m 4083 2010-10-08 10:31:55Z guillaume $
+% $Id: spm_get_ons.m 4474 2011-09-09 15:20:38Z guillaume $
 
 
-% time units
+%-Time units
 %--------------------------------------------------------------------------
 k     = SPM.nscan(s);
 T     = SPM.xBF.T;
@@ -56,90 +51,37 @@ catch
     UNITS = 'scans';
 end
 switch UNITS
-
     case 'scans'
-        %------------------------------------------------------------------
         TR = T*dt;
-
     case 'secs'
-        %------------------------------------------------------------------
         TR = 1;
-
     otherwise
-        %------------------------------------------------------------------
         error('Unknown unit "%s".',UNITS);
 end
 
-% get inputs and names
+%-Get inputs and names
 %==========================================================================
-try
-    U   = SPM.Sess(s).U;
-    v   = length(U);
-catch
+U   = SPM.Sess(s).U;
 
-    %-prompt string
-    %----------------------------------------------------------------------
-    str = sprintf('Session %d: trial specification in %s',s,UNITS);
-    spm_input(str,1,'d')
-
-    U   = {};
-    v   = spm_input('number of conditions/trials',2,'w1');
-end
-
-% get trials
+%-Get trials
 %--------------------------------------------------------------------------
-for i = 1:v
+for i = 1:numel(U)
 
-    % get names
-    %----------------------------------------------------------------------
-    try
-        Uname     = U(i).name(1);
-    catch
-        str       = sprintf('name for condition/trial %d ?',i);
-        Uname     = {spm_input(str,3,'s',sprintf('trial %d',i))};
-        U(i).name = Uname;
-    end
-
-    % get main [trial] effects
+    %-Get main [trial] effects
     %======================================================================
 
-    % onsets
+    %-Names
+    Uname = U(i).name(1);
+    
+    %-Onsets
     %----------------------------------------------------------------------
-    try
-        ons = U(i).ons;
-        ons = ons(:);
-    catch
-        ons = [];
-    end
-    if isempty(ons)
-        str      = ['vector of onsets - ' Uname{1}];
-        ons      = spm_input(str,4,'r',' ',[Inf 1]);
-        U(i).ons = ons(:);
-    end
+    ons   = U(i).ons(:);
 
-    % durations
+    %-Durations
     %----------------------------------------------------------------------
-    try
-        dur = U(i).dur;
-        dur = dur(:);
-    catch
-        dur = [];
-    end
-    if isempty(dur)
-        str = 'duration[s] (events = 0)';
-        while 1
-            dur = spm_input(str,5,'r',' ',[Inf 1]);
-            if length(dur) == 1
-                dur    = dur*ones(size(ons));
-            end
-            if length(dur) == length(ons), break, end
-            str = sprintf('enter a scalar or [%d] vector',...
-                length(ons));
-        end
-        U(i).dur = dur;
-    end
+    dur   = U(i).dur(:);
 
-    % peri-stimulus times {seconds}
+    %-Peri-stimulus times {seconds}
     %----------------------------------------------------------------------
     pst   = [0:(k-1)]*T*dt - min(ons)*TR;
     for j = 1:length(ons)
@@ -148,92 +90,36 @@ for i = 1:v
         pst(v) = w(v);
     end
 
-
-    % add parameters x trial interactions
+    %-Add parameters x trial interactions
     %======================================================================
 
-    % get parameter stucture xP
+    %-Get parameter structure xP
     %----------------------------------------------------------------------
-    try
-        xP    = U(i).P;
-        Pname = xP(1).name;
+    xP    = U(i).P;
+    if strcmpi(xP(1).name,'none'), xP.h = 0; end
 
-        switch Pname
-
-            case 'none'
-                %----------------------------------------------------------
-                xP.name  = 'none';
-                xP.h     = 0;
-
-        end
-
-    catch
-
-        Pname = {'none','time','other'};
-        Pname = spm_input('parametric modulation',6,'b',Pname);
-
-        switch Pname
-
-            case 'none'
-                %----------------------------------------------------------
-                xP(1).name  = 'none';
-                xP(1).h     = 0;
-
-            case 'time'   % units: minutes
-                %----------------------------------------------------------
-                xP(1).name  = 'time';
-                xP(1).P     = ons*TR/60;
-                xP(1).h     = spm_input('polynomial order',8,'n1',1);
-
-            case 'other'
-                %----------------------------------------------------------
-                str   = ['# parameters (' Uname{1} ')'];
-                for q = 1:spm_input(str,7,'n1',1);
-
-                    % get names and parametric variates
-                    %------------------------------------------------------
-                    str   = sprintf('parameter %d name',q);
-                    Pname = spm_input(str,7,'s');
-                    P     = spm_input(Pname,7,'r',[],[length(ons),1]);
-
-                    % order of polynomial expansion h
-                    %------------------------------------------------------
-                    h     = spm_input('polynomial order',8,'n1',1);
-
-                    % sub-indices and inputs
-                    %------------------------------------------------------
-                    xP(q).name = Pname;
-                    xP(q).P    = P(:);
-                    xP(q).h    = h;
-
-                end
-        end % switch
-
-    end % try
-
-    % interaction with causes (u) - 1st = main effects
+    %-Interaction with causes (u) - 1st = main effects
     %----------------------------------------------------------------------
     u     = ons.^0;
     for q = 1:length(xP)
         xP(q).i = [1, ([1:xP(q).h] + size(u,2))];
         for   j = 1:xP(q).h
             u   = [u xP(q).P.^j];
-            str = sprintf('%sx%s^%d',Uname{1},xP(q).name,j);
-            Uname{end + 1} = str;
+            Uname{end + 1} = sprintf('%sx%s^%d',Uname{1},xP(q).name,j);
         end
     end
 
-    % orthogonalize inputs
+    %-Orthogonalise inputs
     %----------------------------------------------------------------------
-    u      = spm_orth(u);
+    u     = spm_orth(u);
 
-    % and scale so sum(u*dt) = number of events, if event-related
+    %-And scale so sum(u*dt) = number of events, if event-related
     %----------------------------------------------------------------------
     if ~any(dur)
-        u  = u/dt;
+        u = u/dt;
     end
 
-    % create stimulus functions (32 bin offset)
+    %-Create stimulus functions (32 bin offset)
     %======================================================================
     ton       = round(ons*TR/dt) + 33;               % onsets
     tof       = round(dur*TR/dt) + ton + 1;          % offset
@@ -251,7 +137,7 @@ for i = 1:v
     sf        = cumsum(sf);                         % integrate
     sf        = sf(1:(k*T + 32),:);                 % stimulus
 
-    % place in ouputs structure
+    %-Place in ouputs structure U
     %----------------------------------------------------------------------
     U(i).name = Uname;      % - input names
     U(i).dt   = dt;         % - time bin {seconds}
