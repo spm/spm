96efd6de1065cf49abc50d65b07c048578cfa564
Darren Gitelman <d-gitelman@northwestern.edu>
2011-10-04 16:17:14 +0000

    Changed to set the CurrentFigure rather than calling figure (line 429) so the System Identification window doesn't relentlessly pester the user.
    SVN r4507

--- spm_nlsi_GN.m
+++ spm_nlsi_GN.m
@@ -92,7 +92,7 @@ function [Ep,Cp,Eh,F] = spm_nlsi_GN(M,U,Y)
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
  
 % Karl Friston
-% $Id: spm_nlsi_GN.m 4506 2011-10-04 14:05:38Z ged $
+% $Id: spm_nlsi_GN.m 4507 2011-10-04 16:17:14Z Darren $
  
 % figure (unless disabled)
 %--------------------------------------------------------------------------
@@ -426,7 +426,7 @@ for k = 1:128
         
         % subplot prediction
         %------------------------------------------------------------------
-        figure(Fsi)
+        set(0,'CurrentFigure',Fsi)
         x    = (1:ns)*Y.dt;
         xLab = 'time (seconds)';
         try
