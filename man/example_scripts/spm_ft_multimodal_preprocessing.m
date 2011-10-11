99562e52b348f8a98f33835e3edfad4ca16735a3
Vladimir Litvak <v.litvak@ucl.ac.uk>
2011-10-11 22:39:09 +0000

    ported r4522
    SVN r4523

--- man/example_scripts/spm_ft_multimodal_preprocessing.m
+++ man/example_scripts/spm_ft_multimodal_preprocessing.m
@@ -61,7 +61,7 @@ cfg.resamplefs = 200;
 cfg.detrend    = 'no';
 data = ft_resampledata(cfg, data);
 %% Convert to SPM8 format
-D = spm_eeg_ft2spm(data, ['ft_' spm_file(datasets{1}, 'basename')]);
+D = spm_eeg_ft2spm(data, ['ft_' spm_str_manip(datasets{1}, 'r')]);
 
 %% Posp-processing of converted data
 
@@ -89,3 +89,25 @@ D = spm_eeg_prep(S);
 
 %% Save the dataset
 save(D);
+%%
+%% Create a head model
+matlabbatch{1}.spm.meeg.source.headmodel.D = {fullfile(D.path, D.fname)};
+
+matlabbatch{1}.spm.meeg.source.headmodel.val = 1;
+matlabbatch{1}.spm.meeg.source.headmodel.comment = '';
+matlabbatch{1}.spm.meeg.source.headmodel.meshing.meshes.template = 1;
+matlabbatch{1}.spm.meeg.source.headmodel.meshing.meshres = 3;
+
+matlabbatch{1}.spm.meeg.source.headmodel.coregistration.coregspecify.fiducial(1).fidname = 'nas';
+matlabbatch{1}.spm.meeg.source.headmodel.coregistration.coregspecify.fiducial(1).specification.select = 'nas';
+matlabbatch{1}.spm.meeg.source.headmodel.coregistration.coregspecify.fiducial(2).fidname = 'lpa';
+matlabbatch{1}.spm.meeg.source.headmodel.coregistration.coregspecify.fiducial(2).specification.select = 'FIL_CTF_L';
+matlabbatch{1}.spm.meeg.source.headmodel.coregistration.coregspecify.fiducial(3).fidname = 'rpa';
+matlabbatch{1}.spm.meeg.source.headmodel.coregistration.coregspecify.fiducial(3).specification.select = 'FIL_CTF_R';
+matlabbatch{1}.spm.meeg.source.headmodel.coregistration.coregspecify.useheadshape = 0;
+
+
+matlabbatch{1}.spm.meeg.source.headmodel.forward.eeg = 'EEG BEM';
+matlabbatch{1}.spm.meeg.source.headmodel.forward.meg = 'Single Shell';
+
+spm_jobman('run', matlabbatch);
