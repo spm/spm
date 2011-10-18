45d7f5afcc0113eb00c9f157470e64ff66156549
Guillaume Flandin <g.flandin@ucl.ac.uk>
2011-10-18 14:58:20 +0000

    Cosmetics changes.
    SVN r4529

--- toolbox/dcm_meeg/spm_dcm_erp_bma.m
+++ toolbox/dcm_meeg/spm_dcm_erp_bma.m
@@ -15,32 +15,35 @@ function [r,xp] = spm_dcm_erp_bma (BMS_file,stats,params)
 %
 %
 % r             posterior samples
-% xp            exceedance probability. 
+% xp            exceedance probability
 %               This is the posterior probability that the connection is
 %               larger than params.xt. Alternatively, if you are looking at
 %               a contrast of connections, its the posterior probability
 %               that the contrast is greater than zero.
 %
-% The parameters returned by Bayesian Model Averaging (BMA) are the 'latent' variables
-% A and B which are Gaussian (and consequently can be positive or negative).  
+% The parameters returned by Bayesian Model Averaging (BMA) are the 'latent'
+% variables A and B which are Gaussian (and consequently can be positive or
+% negative).  
 %
 % The corresponding connection strengths (rA) or gains in connection
 % strength (rB) are an exponential function of these latent variables. 
 % These are the values we are interested in and want to make an inference
 % about.
 %
-% This routine computes the posterior distribution over cA or gB by
+% This routine computes the posterior distribution over rA or rB by
 % generating samples from the latent variables, and exponentiating each
 % sample. 
 %
-% The probability that the rA or RB values are greater than some threshold xt
-% (such as unity) is then just the proportion of posterior samples that
+% The probability that the rA or RB values are greater than some threshold
+% xt (such as unity) is then just the proportion of posterior samples that
 % are greater than xt.
 %
-% If a contrast matrix (C) is not specifed this function looks at a single connection
-% or gain. To look at relative sizes connection/gain values enter a .C
-% matrix. eg. to test, in a 3 region DCM, is connection from 3 to 2 bigger than 2 to 3 ? 
-% set C=[0 0 0; 0 0 1; 0 -1 0]
+% If a contrast matrix (C) is not specifed this function looks at a single
+% connection or gain. To look at relative sizes of connection/gain values 
+% enter a C matrix. eg. to test, in a 3 region DCM, is connection from 3
+% to 2 bigger than 2 to 3 ? set C=[0 0 0; 0 0 1; 0 -1 0].
+%
+%--------------------------------------------------------------------------
 % 
 % Example usage: 
 %
@@ -65,105 +68,102 @@ function [r,xp] = spm_dcm_erp_bma (BMS_file,stats,params)
 % Copyright (C) 2011 Wellcome Trust Centre for Neuroimaging
 
 % Will Penny
-% $Id: spm_dcm_erp_bma.m 4528 2011-10-18 14:37:26Z will $
+% $Id: spm_dcm_erp_bma.m 4529 2011-10-18 14:58:20Z guillaume $
 
+
+%-Get parameters
+%--------------------------------------------------------------------------
 try
     load (BMS_file);
 catch
-    BMS_file = spm_select(1,'mat','Select BMS.mat file',[],[],'BMS.mat');
+    [BMS_file,sts] = spm_select(1,'^BMS\.mat$','Select BMS.mat file');
+    if ~sts, return; end
     load (BMS_file);
 end
 
-try
-    params.C;
-    con=1;
-catch
-    con=0;
+if isfield(params,'C')
+    con = 1;
+else
+    con = 0;
 end
     
 switch lower(stats)
-    case 'ffx',
-        bma=BMS.DCM.ffx.bma;
-    case 'rfx',
-        bma=BMS.DCM.rfx.bma;
+    case 'ffx'
+        bma = BMS.DCM.ffx.bma;
+    case 'rfx'
+        bma = BMS.DCM.rfx.bma;
     otherwise
-        disp('The stats field shout be set to ''ffx'' or ''rfx''');
-        return
+        error('The stats field shout be set to ''ffx'' or ''rfx''');
 end
 
-% Get mean and SD of effects
+%-Get mean and SD of effects
+%--------------------------------------------------------------------------
 switch params.type
-    case 'A',
-        hier_type={'forward','backward','lateral'};
-        hind=strcmp('forward',hier_type);
-        h=find(hind==1);
-        M=bma.mEp.A{h};
-        S=bma.sEp.A{h};
+    case 'A'
+        hier_type = {'forward','backward','lateral'};
+        h = find(ismember(hier_type,params.hier));
+        M = bma.mEp.A{h};
+        S = bma.sEp.A{h};
         if ~con
             str=sprintf('%s connection (rA) from region %d to %d:',hier_type{h},params.from,params.to);
         end
-    case 'B',
-        M=bma.mEp.B{params.ip};
-        S=bma.sEp.B{params.ip};
+    case 'B'
+        M = bma.mEp.B{params.ip};
+        S = bma.sEp.B{params.ip};
         if ~con
             str=sprintf('Effect (rB) of modulatory input %d on connection from region %d to %d:',params.ip,params.from,params.to);
         end
     otherwise
-        disp('conn_type must be ''A'' or ''B''');
-        return
+        error('conn_type must be ''A'' or ''B''');
 end
         
 
-% Generate N samples in latent space, then exponentiate to 
-% get estimate of effects
+%-Generate N samples in latent space, then exponentiate to get estimate
+% of effects
+%--------------------------------------------------------------------------
 N=10000;
 
 switch con
-    case 1,
+    case 1
         % Look at contrast of connections
-        nr=size(M,1);
-        m=M(:);
-        s=S(:);
-        c=params.C(:);
+        nr = size(M,1);
+        m  = M(:);
+        s  = S(:);
+        c  = params.C(:);
         
         % Full posterior covariances are not stored 
         % only univariate SD's so use these
-        x=(s*ones(1,N)).*randn(nr*nr,N)+m*ones(1,N);
-        r=c'*exp(x);
-        rm=mean(r);
-        xp=length(find(r>0))/length(r);
+        x  = (s*ones(1,N)).*randn(nr*nr,N)+m*ones(1,N);
+        r  = c'*exp(x);
+        rm = mean(r);
+        xp = length(find(r>0))/length(r);
         switch params.type
-            case 'A',
+            case 'A'
                 str=sprintf('Contrast of %s connections (rA)',hier_type{h});
-            case 'B',
+            case 'B'
                 str=sprintf('Contrast of effects (rB) of modulatory input %d ',params.ip);
         end
-    case 0,
+    case 0
         % Just look at a single connection
-        m=M(params.to,params.from);
-        s=S(params.to,params.from);
-        x=s*randn(N,1)+m;
-        r=exp(x);
-        rm=mean(r);
-        xp=length(find(r>params.xt))/length(r);
+        m  = M(params.to,params.from);
+        s  = S(params.to,params.from);
+        x  = s*randn(N,1)+m;
+        r  = exp(x);
+        rm = mean(r);
+        xp = length(find(r>params.xt))/length(r);
 end
 
+%-Display
+%--------------------------------------------------------------------------
 figure
 hist(r,20);
 set(gca,'FontSize',18);
 set(gca,'YTickLabel',[]);
-switch params.type
-    case 'A',
-        ylabel('p(rA|Y)');
-        xlabel('rA');
-    case 'B',
-        ylabel('p(rB|Y)');
-        xlabel('rB');
-end
+ylabel(sprintf('p(r%s|Y)',params.type));
+xlabel(sprintf('r%s',params.type));
 
-disp(str);
-disp(sprintf('Posterior mean = %1.2f',rm));
-disp(sprintf('Exceedance probability = %1.6f', xp));
-
-
-
+%-Report
+%--------------------------------------------------------------------------
+fprintf('%s\n',str);
+fprintf('Posterior mean = %1.2f\n',rm);
+fprintf('Exceedance probability = %1.6f\n', xp);
