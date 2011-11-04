9589f2008aeb957996ff6613823dec71a97404d4
Guillaume Flandin <g.flandin@ucl.ac.uk>
2011-11-04 18:33:58 +0000

    Porting r4548 from branches/spm8: Edited help so as to explain that bma.a,bma.b,bma.c and bma.d contain samples from the `Group Posterior Density' and to explain what that is.
    SVN r4551

--- spm_dcm_bma.m
+++ spm_dcm_bma.m
@@ -10,20 +10,58 @@ function bma = spm_dcm_bma(post,post_indx,subj,Nsamp,oddsr)
 % oddsr     posterior odds ratio for defining Occam's window (default=0, ie
 %           all models used in average)
 %
-% Ep        [Np x Nsamp] posterior density matrix. Parameter vector is of
-%           dimension Np and there are Nsamp samples
-% Nocc      Number of models in Occam's window
+% bma       Returned data structure contains
+%
+%           .nsamp  Number of samples
+%           .oddsr  odds ratio
+%           .Nocc   number of models in Occam's window
+%           .Mocc   index of models in Occam's window
+%           .indx   subject specific indices of models in Occam's window
+%
+%           For `Subject Parameter Averaging (SPA)':
+%
+%           .mEp    posterior mean 
+%           .sEp    posterior SD%           
+%           .mEps   subject specific posterior mean 
+%           .sEps   subject specific posterior SD
+%
+%           use the above values in t-tests, ANOVAs to look for significant
+%           effects in the group
+%
+%           For `Group Parameter Averaging (GPA)':
+%
+%           The following structures contain samples of the DCM A,B,C and D
+%           matrices from the group posterior density. See pages 6 and 7 of [1]
+%
+%           .a [dima x Nsamp] 
+%           .b [dima x Nsamp] 
+%           .c [dima x Nsamp] 
+%           .d [dima x Nsamp] 
+%                       
+%           Use these to make inferences using the group posterior density approach. 
+%           Essentially, for each parameter, GPA gets a sample which is the average 
+%           over subjects. The collection of samples then constitutes a distribution of
+%           the group mean from which inferences can be made directly. This is to
+%           be contrasted with SPA where, for each subject, we average over
+%           samples to get a mean for that subject. Group level inferences
+%           are then made using classical inference. SPA is the standard
+%           approach.
+%
 %
 %           For RFX BMA, different subject can have different models in
 %           Occam's window (and different numbers of models in Occam's
 %           window)
 %
 % This routine implements Bayesian averaging over models and subjects
+%
+% See [1] W Penny, K Stephan, J. Daunizeau, M. Rosa, K. Friston, T. Schofield 
+% and A Leff. Comparing Families of Dynamic Causal Models. 
+% PLoS Computational Biology, Mar 2010, 6(3), e1000709.
 %__________________________________________________________________________
 % Copyright (C) 2009 Wellcome Trust Centre for Neuroimaging
 
 % Will Penny
-% $Id: spm_dcm_bma.m 4438 2011-08-25 11:56:07Z guillaume $
+% $Id: spm_dcm_bma.m 4551 2011-11-04 18:33:58Z guillaume $
 
 if nargin < 4 || isempty(Nsamp)
     Nsamp = 1e3;
@@ -59,7 +97,7 @@ else
 end
 
 if rfx
-
+    
     for i = 1:Ni,
 
         mp          = max(post(i,:));
