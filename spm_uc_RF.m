49061210e95dbf39b5b9ee6c7bf874056993bc07
Guillaume Flandin <g.flandin@ucl.ac.uk>
2012-02-01 19:01:09 +0000

    Return Inf in some pathological situation.
    SVN r4634

--- spm_uc_RF.m
+++ spm_uc_RF.m
@@ -1,5 +1,5 @@
 function [u] = spm_uc_RF(a,df,STAT,R,n)
-% corrected critical height threshold at a specified significance level
+% Corrected critical height threshold at a specified significance level
 % FORMAT [u] = spm_uc_RF(a,df,STAT,R,n)
 % a     - critical probability - {alpha}
 % df    - [df{interest} df{residuals}]
@@ -16,38 +16,38 @@ function [u] = spm_uc_RF(a,df,STAT,R,n)
 %__________________________________________________________________________
 %
 % spm_uc returns the corrected critical threshold at a specified significance
-% level (a). If n > 1 a conjunction the probability over the n values of
-% the statistic is returned.
+% level (a). If n > 1 a conjunction probability over the n values of the
+% statistic is returned.
 %__________________________________________________________________________
-% Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
+% Copyright (C) 1999-2012 Wellcome Trust Centre for Neuroimaging
 
 % Karl Friston
-% $Id: spm_uc_RF.m 4633 2012-02-01 18:44:02Z guillaume $
+% $Id: spm_uc_RF.m 4634 2012-02-01 19:01:09Z guillaume $
 
 
-% find approximate value
+%-Find approximate value
 %--------------------------------------------------------------------------
 u  = spm_u((a/max(R))^(1/n),df,STAT);
 du = 1e-6;
 
-% approximate estimate using E{m}
+%-Approximate estimate using E{m}
 %--------------------------------------------------------------------------
 d  = 1;
 while abs(d) > 1e-6
-    [P P p] = spm_P_RF(1,0,u,df,STAT,R,n);
-    [P P q] = spm_P_RF(1,0,u + du,df,STAT,R,n);
-    d       = (a - p)/((q - p)/du);
-    u       = u + d;
-    %if u < 0, u=+Inf; return; end
+    [P, P, p] = spm_P_RF(1,0,u,df,STAT,R,n);
+    [P, P, q] = spm_P_RF(1,0,u + du,df,STAT,R,n);
+    d         = (a - p)/((q - p)/du);
+    u         = u + d;
+    if isinf(u), u=+Inf; return; end
 end
 
-% refined estimate using 1 - exp(-E{m})
+%-Refined estimate using 1 - exp(-E{m})
 %--------------------------------------------------------------------------
 d  = 1;
 while abs(d) > 1e-6
-    p       = spm_P_RF(1,0,u,df,STAT,R,n);
-    q       = spm_P_RF(1,0,u + du,df,STAT,R,n);
-    d       = (a - p)/((q - p)/du);
-    u       = u + d;
+    p         = spm_P_RF(1,0,u,df,STAT,R,n);
+    q         = spm_P_RF(1,0,u + du,df,STAT,R,n);
+    d         = (a - p)/((q - p)/du);
+    u         = u + d;
     if isinf(u), u=+Inf; return; end
 end
