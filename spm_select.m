22bd7f36cea1534ca7b79132ad24b5ac46e9ba48
Guillaume Flandin <g.flandin@ucl.ac.uk>
2012-09-24 11:44:51 +0000

    Bug fix for direct call to spm_select without input arguments.
    SVN r4952

--- spm_select.m
+++ spm_select.m
@@ -80,7 +80,7 @@ function varargout = spm_select(varargin)
 % Copyright (C) 2005-2012 Wellcome Trust Centre for Neuroimaging
 
 % John Ashburner
-% $Id: spm_select.m 4951 2012-09-24 10:22:16Z volkmar $
+% $Id: spm_select.m 4952 2012-09-24 11:44:51Z guillaume $
 
 
 persistent isInitSelect;
@@ -125,7 +125,7 @@ else
             ismember(lower(varargin{1}),{'filter','cpath'}) && ischar(varargin{2})
         varargin{2} = cellstr(varargin{2});
         needchar = true;
-    elseif ischar(varargin{1}) && ...
+    elseif nargin > 0 && ischar(varargin{1}) && ...
             ismember(lower(varargin{1}),{'extlist','extfplist','extfplistrec'})
         varargin{1} = varargin{1}(4:end);
         if nargin > 3
