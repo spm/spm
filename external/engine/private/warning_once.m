d0e995b3ff2bc57abccba01a4ae7443e8a2a0002
Guillaume Flandin <g.flandin@ucl.ac.uk>
2012-11-01 14:36:06 +0000

    Adding 'engine' module from FieldTrip into spm/external.
    SVN r5029

---
+++ external/engine/private/warning_once.m
@@ -0,0 +1,118 @@
+function [ws warned] = warning_once(varargin)
+%
+% Use as one of the following
+%   warning_once(string)
+%   warning_once(string, timeout)
+%   warning_once(id, string, timeout)
+% where timeout should be inf if you don't want to see the warning ever
+% again. The default timeout value is 60 seconds.
+%
+% It can be used instead of the MATLAB built-in function WARNING, thus as
+%   s = warning_once(...)
+% or as
+%   warning_once(s)
+% where s is a structure with fields 'identifier' and 'state', storing the
+% state information. In other words, warning_once accepts as an input the
+% same structure it returns as an output. This returns or restores the
+% states of warnings to their previous values.
+%
+% It can also be used as
+%    [s w] = warning_once(...)
+% where w is a boolean that indicates whether a warning as been thrown or not.
+%
+% Please note that you can NOT use it like this
+%   warning_once('the value is %d', 10)
+% instead you should do
+%   warning_once(sprintf('the value is %d', 10))
+
+% Copyright (C) 2012, Robert Oostenveld
+%
+% This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id: warning_once.m 5029 2012-11-01 14:36:06Z guillaume $
+
+persistent stopwatch previous
+
+if nargin < 1
+  error('You need to specify at least a warning message');
+end
+
+warned = false;
+if isstruct(varargin{1})
+  warning(varargin{1});
+  return;
+end
+
+if nargin==3
+  msgid = varargin{1};
+  msgstr = varargin{2};
+  timeout = varargin{3};
+elseif nargin==2
+  msgstr= ''; % this becomes irrelevant
+  msgid = varargin{1}; % this becomes the real msgstr
+  timeout = varargin{2};
+elseif nargin==1
+  msgstr= ''; % this becomes irrelevant
+  msgid = varargin{1}; % this becomes the real msgstr
+  timeout = 60; % default timeout in seconds
+end
+
+if isempty(timeout)
+  error('Timeout ill-specified');
+end
+
+if isempty(stopwatch)
+  stopwatch = tic;
+end
+if isempty(previous)
+  previous = struct;
+end
+
+now = toc(stopwatch); % measure time since first function call
+fname = fixname([msgid '_' msgstr]); % make a nice string that is allowed as structure fieldname, copy the subfunction from  ft_hastoolbox
+fname = decomma(fname);
+
+if length(fname) > 63 % MATLAB max name
+  fname = fname(1:63);
+end
+
+if isfield(previous, fname) && now>previous.(fname).timeout
+  % it has timed out, give the warning again
+  ws = warning(msgid, msgstr);
+  previous.(fname).timeout = now+timeout;
+  previous.(fname).ws = ws;
+  warned = true;
+elseif ~isfield(previous, fname)
+  % the warning has not been issued before
+  ws = warning(msgid, msgstr);
+  previous.(fname).timeout = now+timeout;
+  previous.(fname).ws = ws;
+  warned = true;
+else
+  % the warning has been issued before, but has not timed out yet
+  ws = previous.(fname).ws;
+end
+
+end % function
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% helper functions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function name = decomma(name)
+name(name==',')=[];
+end % function
