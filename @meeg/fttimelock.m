54f378d6930bbb04577a152e2a1cbbe56af2199c
Vladimir Litvak <v.litvak@ucl.ac.uk>
2012-11-16 12:00:53 +0000

    Improvements to fttimelock. Now outputs different structure variants.
    SVN r5062

--- @meeg/fttimelock.m
+++ @meeg/fttimelock.m
@@ -1,11 +1,15 @@
 function timelock = fttimelock(this, chanind, timeind, trialind, freqind)
 % Method for converting meeg object to Fieldtrip timelock/freq struct
 % FORMAT timelock = fttimelock(this, chanind, timeind, trialind, freqind)
+%
+% The method support both time and TF data and outputs different variants
+% of timelock or freq FT struct depending on the dataset type and requested
+% data dimensions.
 % _______________________________________________________________________
-% Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
+% Copyright (C) 2008-2012 Wellcome Trust Centre for Neuroimaging
 
 % Vladimir Litvak
-% $Id: fttimelock.m 5025 2012-10-31 14:44:13Z vladimir $
+% $Id: fttimelock.m 5062 2012-11-16 12:00:53Z vladimir $
 
 if ~islinked(this)
     error('There is no data linked to the object');
@@ -32,28 +36,58 @@ timelock             = [];
 timelock.label       = chanlabels(this, chanind)';
 
 if isequal(transformtype(this), 'time')
-    timelock.dimord  = 'rpt_chan_time';
-    timelock.trial   =  permute(this.data(chanind, timeind, trialind), [3 1 2]);
-    timelock.dimord  = 'rpt_chan_time';
+   if isequal(type(this), 'continuous')
+            error('For continuous data use ftraw method');
+   end
+   
+   if isequal(type(this), 'single') || length(trialind)>1
+       timelock.dimord  = 'rpt_chan_time';
+       timelock.trial   =  permute(this.data(chanind, timeind, trialind), [3 1 2]);
+   else
+       timelock.dimord  = 'chan_time';
+       timelock.avg     =  spm_squeeze(this.data(chanind, timeind, trialind), 3);
+   end
+   
+   timelock.time       = time(this, timeind);
+   
 elseif strncmpi(transformtype(this),'TF',2)
-    timelock.dimord  = 'rpt_chan_freq_time';
-    timelock.powspctrm = permute(this.data(chanind, freqind, timeind, trialind), [4 1 2 3]);
-    timelock.freq    =  frequencies(this, freqind);
+    if length(timeind)>1
+        if isequal(type(this), 'single') || length(trialind)>1
+            timelock.dimord    = 'rpt_chan_freq_time';
+            timelock.powspctrm = permute(this.data(chanind, freqind, timeind, trialind), [4 1 2 3]);
+        else
+            timelock.dimord     = 'chan_freq_time';
+            timelock.powspctrm  =  spm_squeeze(this.data(chanind, freqind, timeind, trialind), 3);
+        end
+        
+        timelock.time       = time(this, timeind);
+    else
+        if isequal(type(this), 'single') || length(trialind)>1
+            timelock.dimord    = 'rpt_chan_freq';
+            timelock.powspctrm = spm_squeeze(permute(this.data(chanind, freqind, timeind, trialind), [4 1 2 3]), 4);
+        else
+            timelock.dimord     = 'chan_freq';
+            timelock.powspctrm  =  spm_squeeze(this.data(chanind, freqind, timeind, trialind), [3 4]);
+        end
+    end
+    
+    timelock.freq      = frequencies(this, freqind);
 else
-  error('Unknown transform type.');
-end
+    error('Unknown transform type.');
+end   
 
-timelock.time       = time(this, timeind);
-
-clist      =  condlist(this);
-
-condlabels = conditions(this, trialind);
-
-timelock.trialinfo = 0*trialind;
-for k = 1:numel(clist)
-  fprintf('mapping condition label "%s" to condition code %d\n', clist{k}, k);
-  sel = strcmp(clist{k}, condlabels);
-  timelock.trialinfo(sel) = k;
+if length(trialind)>1
+    
+    clist      =  condlist(this);
+    condlabels = conditions(this, trialind);
+    timelock.trialinfo = 0*trialind;
+    
+    for k = 1:numel(clist)
+        fprintf('mapping condition label "%s" to condition code %d\n', clist{k}, k);
+        sel = strcmp(clist{k}, condlabels);
+        timelock.trialinfo(sel) = k;
+    end
+    
 end
 
 if ~isempty(sensors(this, 'MEG'))
