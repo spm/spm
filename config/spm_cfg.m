cb31069286ebb759ee08efa6303e48794a7122d6
Gareth Barnes <g.barnes@ucl.ac.uk>
2013-02-04 14:10:30 +0000

    added simulation option
    SVN r5231

--- config/spm_cfg.m
+++ config/spm_cfg.m
@@ -3,7 +3,7 @@ function spmjobs = spm_cfg
 %__________________________________________________________________________
 % Copyright (C) 2008-2011 Wellcome Trust Centre for Neuroimaging
 
-% $Id: spm_cfg.m 5212 2013-01-26 13:16:36Z vladimir $
+% $Id: spm_cfg.m 5231 2013-02-04 14:10:30Z gareth $
 
 %--------------------------------------------------------------------------
 % Temporal
@@ -77,7 +77,7 @@ source        = cfg_choice;
 source.tag    = 'source';
 source.name   = 'M/EEG Source reconstruction';
 source.help   = {'M/EEG source reconstruction.'};
-source.values = { spm_cfg_eeg_inv_headmodel, spm_cfg_eeg_inv_headmodelhelmet, spm_cfg_eeg_inv_invert, spm_cfg_eeg_inv_invertiter, spm_cfg_eeg_inv_results, spm_cfg_eeg_inv_extract }; 
+source.values = { spm_cfg_eeg_inv_headmodel, spm_cfg_eeg_inv_headmodelhelmet, spm_cfg_eeg_inv_invert, spm_cfg_eeg_inv_invertiter,,spm_cfg_eeg_inv_simulate, spm_cfg_eeg_inv_results, spm_cfg_eeg_inv_extract }; 
 
 %--------------------------------------------------------------------------
 % M/EEG other
