913c8d3c838e2ecf3e043cde05681bf79a5cd874
Gareth Barnes <g.barnes@ucl.ac.uk>
2013-02-05 10:16:17 +0000

    SVN r5239

---
+++ spm_eeg_invertiter.m
@@ -0,0 +1,76 @@
+function [Dtest,modelF,allF]=spm_eeg_invertiter(Dtest,Np,Npatchiter,Nm,Nt)
+
+
+val=1;
+disp('running iterative inversion');
+if nargin<4,
+    disp('estimating spatial modes from lead fields');
+    Nm=[];
+end;
+
+if nargin<5,
+    disp('estimating temporal modes from data');
+    Nt=[];
+end;
+
+
+Nvert=size(Dtest{1}.inv{val}.mesh.tess_mni.vert,1);
+twindow=Dtest{1}.inv{val}.inverse.woi./1000; %% in sec
+fband=[Dtest{1}.inv{val}.inverse.hpf Dtest{1}.inv{1}.inverse.lpf];
+allF=zeros(Npatchiter,1);
+disp('Reseting random number seed !');
+rng(0); 
+for patchiter=1:Npatchiter, %% change patches
+    
+    randind=randperm(Nvert);
+    Ip=randind(1:Np);
+    
+    
+    
+    
+    Dtest{1}	= spm_eeg_invert_demo_grb(Dtest{1},Ip,fband,Nm,Nt,[],twindow);			% Source reconstruction
+    Dtest{1}.inv{val}.inverse.Ip=Ip;
+    if isempty(Nm),
+        allNt=max([size(Dtest{1}.inv{val}.inverse.T,2),1]);
+        allNm=size(Dtest{1}.inv{val}.inverse.M,2);
+        Nm=allNm;
+        Nt=allNt;
+    end;
+    
+    modelF(patchiter).inverse=Dtest{1}.inv{val}.inverse;
+    allF(patchiter)=[Dtest{1}.inv{val}.inverse.F];
+    allJ{patchiter}=Dtest{1}.inv{val}.inverse.J;
+    manyinverse{patchiter}=modelF(patchiter).inverse;
+    disp('iteration:');
+    patchiter
+    allF
+    
+    
+end; % for patchiter
+
+[bestF,bestind]=max(allF);
+disp('model evidences relative to maximum:')
+
+sort(allF-bestF)
+
+Dtest{1}.inv{val}.inverse=modelF(bestind).inverse; %% return best model for now
+if Dtest{1}.inv{val}.inverse.BMA,
+    disp('Running BMA to get current estimate');
+    Jbma=spm_bma_msp(manyinverse,allF); %% onlt the mean is calculated using BMA (but could extend to covariance)
+    Dtest{1}.inv{val}.inverse.T=1; %% Jbma is the sum of all modes
+    Dtest{1}.inv{val}.inverse.J={Jbma};
+else
+    disp('Using best patch set to current estimate');
+end; % if BMA
+
+Dtest{1}.inv{val}.inverse.allF=allF;
+spm_eeg_invert_display(Dtest{1});
+%Dtest{1}.inv{val}.inverse.J=Jbma;
+
+
+
+  
+
+
+
+
