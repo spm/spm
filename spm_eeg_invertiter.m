b2b75e064260ee1c67cd3706add8faee87c6ce21
Gareth Barnes <g.barnes@ucl.ac.uk>
2013-03-06 17:15:06 +0000

    SVN r5306

--- spm_eeg_invertiter.m
+++ spm_eeg_invertiter.m
@@ -10,7 +10,7 @@ function [Dtest,modelF,allF]=spm_eeg_invertiter(Dtest,Npatchiter,funcname)
 % Copyright (C) 2010 Wellcome Trust Centre for Neuroimaging
 %
 % Gareth Barnes
-% $Id: spm_eeg_invertiter.m 5297 2013-03-04 11:50:39Z gareth $
+% $Id: spm_eeg_invertiter.m 5306 2013-03-06 17:15:06Z gareth $
 
 if nargin<2,
     Npatchiter=[];
@@ -43,35 +43,31 @@ fprintf('Checking leadfields')
 [L Dtest{1}] = spm_eeg_lgainmat(Dtest{1});	% Generate/load lead field- this stops it being done at each iteration
   rand('state',0);
 
+
+%% make random patch centers, except on the first iteration when we keep to a fixed stepsize
 for f=1:Npatchiter,
     tmp=randperm(Nvert);
-    allIp(f,:)=tmp(1:Np);
+    allIp(f).Ip=tmp(1:Np);
 end;
+allIp(1).Ip=[]; %% fix the step size of the first patch set
 
-%%parfor patchiter=1:Npatchiter, %% change patches- could be parallelized
-for patchiter=1:Npatchiter, 
-  
+
+
+for patchiter=1:Npatchiter,
     Din=Dtest{1};
     Dout=Din;
-    Ip=allIp(patchiter,:)
-    
-
+    Ip=allIp(patchiter).Ip;
     
     switch funcname,
         case 'Classic',
-            
             Din.inv{val}.inverse.Ip=Ip;
-            
             Dout	= spm_eeg_invert_classic(Din);
-        case 'Current'
+        case 'Current',
             warning('Patch centres are currently fixed for this algorithm (iteration will have no effect!)');
-               
-              Dout	= spm_eeg_invert(Din); %
-              Dout.inv{val}.inverse.Ip=Ip;
-     end;
-     modelF(patchiter).inverse=Dout.inv{val}.inverse;
-     
-    
+            Dout	= spm_eeg_invert(Din); %
+            Dout.inv{val}.inverse.Ip=Ip;
+    end;
+    modelF(patchiter).inverse=Dout.inv{val}.inverse;
 end; % for patchiter
 
 
@@ -87,7 +83,7 @@ sort(allF-bestF)
 
 Dtest{1}.inv{val}.inverse.allF=allF;
 Dtest{1}.inv{val}.inverse=modelF(bestind).inverse; %% return best model for now
-if (Dtest{1}.inv{val}.inverse.BMAflag==1)
+if (Dtest{1}.inv{val}.inverse.BMAflag==1)&&(Npatchiter>1)
     disp('Running BMA to get current estimate');
     [Jbma,qCbma]=spm_eeg_invert_bma(manyinverse,allF); %% onlt the mean is calculated using BMA (but could extend to covariance)
     %Dtest{1}.inv{val}.inverse.T=1; %% Jbma is the sum of all modes
