f80e2f8ed686b85515f71c9187752db963eec014
Gareth Barnes <g.barnes@ucl.ac.uk>
2013-03-06 15:57:07 +0000

    updated simulation parameters
    SVN r5302

--- config/spm_cfg_eeg_inv_simulate.m
+++ config/spm_cfg_eeg_inv_simulate.m
@@ -5,7 +5,7 @@ function simulate = spm_cfg_eeg_inv_simulate
 % Copyright (C) 2010 Wellcome Trust Centre for Neuroimaging
 
 % Vladimir Litvak
-% $Id: spm_cfg_eeg_inv_simulate.m 5230 2013-02-04 14:03:21Z gareth $
+% $Id: spm_cfg_eeg_inv_simulate.m 5302 2013-03-06 15:57:07Z gareth $
 
 D = cfg_files;
 D.tag = 'D';
@@ -52,19 +52,6 @@ whatconditions.name = 'What conditions to include?';
 whatconditions.values = {all, conditions};
 whatconditions.val = {all};
 
-% standard = cfg_const;
-% standard.tag = 'standard';
-% standard.name = 'Standard';
-% standard.help = {'Use default settings for the simulation'};
-% standard.val  = {1};
-% %
-% invtype = cfg_menu;
-% invtype.tag = 'invtype';
-% invtype.name = 'Inversion type';
-% invtype.help = {'Select the desired inversion type'};
-% invtype.labels = {'GS', 'ARD', 'MSP (GS+ARD)' 'COH', 'IID'};
-% invtype.values = {'GS', 'ARD', 'MSP', 'LOR', 'IID'};
-% invtype.val = {'GS'};
 
 woi = cfg_entry;
 woi.tag = 'woi';
@@ -92,61 +79,22 @@ dipmom.num = [Inf 1];
 dipmom.val = {[20;20]};
 dipmom.help = {'Enter dipole moments for sources (nAm)'};
 
-%
-% nsources = cfg_entry;
-% nsources.tag = 'nsources';
-% nsources.name = 'Number of sources';
-% nsources.strtype = 'i';
-% nsources.num = [1 1];
-% nsources.val = {[2]};
-% nsources.help = {'Number of simulated patches'};
-%
-% patchind = cfg_entry;
-% patchind.tag = 'patchind';
-% patchind.name = 'Number of iterations';
-% patchind.strtype = 'i';
-% patchind.num = [1 2];
-% patchind.val = {[100 150]};
-% patchind.help = {'Number of times the inversion will be run using a random set of patches each time'};
-%
-% nsmodes = cfg_entry;
-% nsmodes.tag = 'nsmodes';
-% nsmodes.name = 'Number of spatial modes';
-% nsmodes.strtype = 'i';
-% nsmodes.num = [1 1];
-% nsmodes.val = {[100]};
-% nsmodes.help = {'Number of spatial modes'};
-%
-% ntmodes = cfg_entry;
-% ntmodes.tag = 'ntmodes';
-% ntmodes.name = 'Number of temporal modes';
-% ntmodes.strtype = 'i';
-% ntmodes.num = [1 1];
-% ntmodes.val = {[4]};
-% ntmodes.help = {'Number of temporal modes'};
 
-%
-% priorsmask  = cfg_files;
-% priorsmask.tag = 'priorsmask';
-% priorsmask.name = 'Priors file';
-% priorsmask.filter = '(.*\.gii$)|(.*\.mat$)|(.*\.nii(,\d+)?$)|(.*\.img(,\d+)?$)';
-% priorsmask.num = [0 1];
-% priorsmask.help = {'Select a mask or a mat file with priors.'};
-% priorsmask.val = {{''}};
-%
-% space = cfg_menu;
-% space.tag = 'space';
-% space.name = 'Prior image space';
-% space.help = {'Space of the mask image.'};
-% space.labels = {'MNI', 'Native'};
-% space.values = {1, 0};
-% space.val = {1};
-%
-% priors = cfg_branch;
-% priors.tag = 'priors';
-% priors.name = 'Source priors';
-% priors.help = {'Restrict solutions to pre-specified VOIs'};
-% priors.val  = {priorsmask, space};
+setSNR = cfg_entry;
+setSNR.tag = 'setSNR';
+setSNR.name = 'Sensor level SNR (dBs)';
+setSNR.strtype = 'r';
+setSNR.num = [1 1];
+setSNR.val = {[0]};
+setSNR.help = {'Enter sensor level SNR=20*log10(rms source/ rms noise)'};
+
+
+isSNR = cfg_choice;
+isSNR.tag = 'isSNR';
+isSNR.name = 'Set SNR or set moments';
+isSNR.help = {'Choose whether to a fixed SNR or specify source moments in nAm'};
+isSNR.values = {setSNR, dipmom};
+isSNR.val = {setSNR};
 
 locs  = cfg_entry;
 locs.tag = 'locs';
@@ -155,53 +103,11 @@ locs.strtype = 'r';
 locs.num = [Inf 3];
 locs.help = {'Input mni source locations (mm) as n x 3 matrix, or patch indices only in 1st column'};
 locs.val = {[ 53.7203  -25.7363    9.3949;  -52.8484  -25.7363    9.3949]};
-%
-% radius = cfg_entry;
-% radius.tag = 'radius';
-% radius.name = 'Radius of VOI (mm)';
-% radius.strtype = 'r';
-% radius.num = [1 1];
-% radius.val = {32};
-%
-% restrict = cfg_branch;
-% restrict.tag = 'restrict';
-% restrict.name = 'Restrict solutions';
-% restrict.help = {'Restrict solutions to pre-specified VOIs'};
-% restrict.val  = {locs, radius};
-%
-% custom = cfg_branch;
-% custom.tag = 'custom';
-% custom.name = 'Custom';
-% custom.help = {'Define custom settings for the inversion'};
-% custom.val  = {invtype, woi, foi, hanning,nsources,patchind,nsmodes,ntmodes, priors, restrict};
-
-% isstandard = cfg_choice;
-% isstandard.tag = 'isstandard';
-% isstandard.name = 'Inversion parameters';
-% isstandard.help = {'Choose whether to use standard or custom inversion parameters.'};
-% isstandard.values = {standard, custom};
-% isstandard.val = {standard};
-%
-% modality = cfg_menu;
-% modality.tag = 'modality';
-% modality.name = 'Select modalities';
-% modality.help = {'Select modalities for the inversion (only relevant for multimodal datasets).'};
-% modality.labels = {'All', 'EEG', 'MEG', 'MEGPLANAR', 'EEG+MEG', 'MEG+MEGPLANAR', 'EEG+MEGPLANAR'};
-% modality.values = {
-%     {'All'}
-%     {'EEG'}
-%     {'MEG'}
-%     {'MEGPLANAR'}
-%     {'EEG', 'MEG'}
-%     {'MEG', 'MEGPLANAR'}
-%     {'EEG', 'MEGPLANAR'}
-%     }';
-% modality.val = {{'All'}};
 
 simulate = cfg_exbranch;
 simulate.tag = 'simulate';
 simulate.name = 'simulation of sources on cortex';
-simulate.val = {D, val, prefix, condlabel, whatconditions,locs,foi,woi,dipmom};
+simulate.val = {D, val, prefix, whatconditions,locs,foi,woi,isSNR};
 simulate.help = {'Run simulation'};
 simulate.prog = @run_simulation;
 simulate.vout = @vout_simulation;
@@ -212,27 +118,25 @@ function  out = run_simulation(job)
 
 D = spm_eeg_load(job.D{1});
 
-sim = [];
+
+trialind=[];
 if isfield(job.whatconditions, 'condlabel')
-    sim.trials = job.whatconditions.condlabel;
+    trialind =D.indtrial( job.whatconditions.condlabel);
+    if isempty(trialind),
+        error('No such condition found');
+    end; 
 end
 if numel(job.D)>1,
-    error('Simulattion routine only meant to replace data for single subjects');
+    error('Simulation routine only meant to replace data for single subjects');
 end;
 
 
 
-
-sim.woi  = [max(job.woi(1), D.time(1)) min(job.woi(2), D.time(end))];
-
 Nsources=size(job.foi,1);
+
 if size(job.locs,1)~=Nsources,
     error('Number of locations must equal number of frequencies specified');
 end;
-
-
-
-
 
 
 
@@ -242,7 +146,6 @@ if ~strcmp(mod, 'MEG')
 end;
 
 D = {};
-
 
 
 for i = 1:numel(job.D) %% only set up for one subject at the moment but leaving this for the future
@@ -266,10 +169,19 @@ end; % for i
 
 mnimesh=[]; %% using mesh defined in forward model at the moment
 SmthInit=[]; %% leave patch size as default for now
-keep_leads=[]; %% recompute lead fields each time
 
-SIdipmom=job.dipmom*1e-9; %% put dipole moment into Am rather than nAm
-[D,meshsourceind,signal]=spm_eeg_simulate(D,job.prefix, job.locs,job.foi,job.woi,SIdipmom,mnimesh,keep_leads,SmthInit);
+
+if isfield(job.isSNR,'dipmom'),
+    SIdipmom=job.isSNR.dipmom*1e-9; %% put dipole moment into Am rather than nAm
+    SNRdB=[];
+    whitenoise=[]; %% need to add this later
+else
+    SNRdB=job.isSNR.setSNR;
+    SIdipmom=[];
+    whitenoise=[];
+end;
+
+[D,meshsourceind,signal]=spm_eeg_simulate(D,job.prefix, job.locs,job.foi,job.woi,SIdipmom,whitenoise,SNRdB,trialind,mnimesh,SmthInit);
 
 if ~iscell(D)
     D = {D};
