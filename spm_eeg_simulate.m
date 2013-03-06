cc1b593dcb7e86140c348d10f8af640d68c1480a
Gareth Barnes <g.barnes@ucl.ac.uk>
2013-03-06 15:57:35 +0000

    updated and corrected
    SVN r5303

--- spm_eeg_simulate.m
+++ spm_eeg_simulate.m
@@ -1,4 +1,4 @@
-function [D,meshsourceind,signal]=spm_eeg_simulate(D,prefix,patchmni,dipfreq,woi,dipmoment,mnimesh,keep_leads,SmthInit);
+function [D,meshsourceind,signal]=spm_eeg_simulate(D,prefix,patchmni,dipfreq,woi,dipmoment,whitenoise,SNRdB,trialind,mnimesh,SmthInit);
 %% Simulate a number of MSP patches at specified locations on existing mesh
 % Synthetic MEG data generator for SPM8
 % This is a demo version related with the Technical Note:
@@ -13,12 +13,11 @@ function [D,meshsourceind,signal]=spm_eeg_simulate(D,prefix,patchmni,dipfreq,woi
 %% patchmni : patch centres in mni space or patch indices
 %% dipfreq : frequency of simulated sources (Hz)
 %% mnimesh : a new mesh with vertices in mni space
-%% keep_leads: a flag if set to 0 then lead fields are re-calculated
 %% dipmoment : dipole moment in Am
 %% woi : time window of source activity
 %% SmthInit - the smoothing step that creates the patch- larger numbers larger patches default 0.6. Note current density should be constant (i.e. larger patch on tangential surface will not give larger signal)
-% 
-% $Id: spm_eeg_simulate.m 5229 2013-02-04 14:02:29Z gareth $
+%
+% $Id: spm_eeg_simulate.m 5303 2013-03-06 15:57:35Z gareth $
 
 %% LOAD IN ORGINAL DATA
 useind=1; % D to use
@@ -27,7 +26,7 @@ if nargin<2,
 end;
 
 if nargin<3,
-    patchmni=[]; 
+    patchmni=[];
 end;
 
 
@@ -43,14 +42,24 @@ if nargin<6,
 end;
 
 if nargin<7,
+    whitenoise=[];
+end;
+
+if nargin<8,
+    SNRdB=[];
+end;
+
+if nargin<9,
+    trialind=[];
+end;
+
+if nargin<10,
     mnimesh=[];
 end;
 
-if nargin<8, %% use previously computed lead fields
-    keep_leads=[];
-end;
 
-if nargin<9
+
+if nargin<11
     SmthInit=[]; %% number of iterations used to smooth patch out (more iterations, larger patch)
 end;
 
@@ -66,38 +75,49 @@ if isempty(woi),
     woi=[D{useind}.time(1) D{useind}.time(end)];
 end;
 
-if isempty(keep_leads),
-    keep_leads=0;
-end;
+
 val=D{useind}.val;
 
-if ~keep_leads,
-    D{useind}.inv{val}.gainmat=''; %% force re-calculation of leads
-end;
+
 
 if isempty(patchmni),
-         patchmni=[-45.4989  -30.6967    4.9213;...
-   46.7322  -31.2311    4.0085];
+    patchmni=[-45.4989  -30.6967    4.9213;...
+        46.7322  -31.2311    4.0085];
     
 end;
 
-if isempty(dipmoment),
-    dipmoment= ones(Ndips,1)*20*1e-9;		% dipole moment in Am
+
+if ~xor(isempty(dipmoment),isempty(SNRdB))
+    error('Must specify either dipole moment or sensor level SNR');
+end;
+
+if ~isempty(SNRdB),
+    dipmoment=ones(size(patchmni,1),1).*20e-9; %% set to 20nAm
 end;
 
 
+
+
 [a1 b1 c1]=fileparts(D{useind}.fname);
+newfilename=[prefix b1];
+try
+    Dnew=spm_eeg_load(newfilename);
+    disp('Overwriting data in existing file');
+catch
+    Dnew=D{useind}.clone([prefix b1]);
+end;
 
-Dnew=D{useind}.clone([prefix b1]);
-
+if isempty(trialind)
+    trialind=1:Dnew.ntrials;
+end;
 disp('Simulating data on MEG channels only'); % for now
 chanind=strmatch('MEG',Dnew.chantype);
 
 if ~isempty(mnimesh),
-    D{useind}.inv{1}.mesh.tess_mni.vert=mnimesh.vert;
-    D{useind}.inv{1}.mesh.tess_mni.face=mnimesh.face;
-    D{useind}.inv{1}.forward.mesh.vert=spm_eeg_inv_transform_points(D.inv{D.val}.datareg.fromMNI,mnimesh.vert);
-    D{useind}.inv{1}.forward.mesh.face=mnimesh.face;
+    Dnew.inv{val}.mesh.tess_mni.vert=mnimesh.vert;
+    Dnew.inv{val}.mesh.tess_mni.face=mnimesh.face;
+    Dnew.inv{val}.forward.mesh.vert=spm_eeg_inv_transform_points(Dnew.inv{val}.datareg.fromMNI,mnimesh.vert);
+    Dnew.inv{val}.forward.mesh.face=mnimesh.face;
     
 end; % if
 
@@ -114,6 +134,7 @@ end;
 
 meshsourceind=[];
 
+disp('Using closest mesh vertices to the specified coordinates')
 for d=1:Ndips,
     vdist= Dnew.inv{val}.mesh.tess_mni.vert-repmat(patchmni(d,:),size(Dnew.inv{val}.mesh.tess_mni.vert,1),1);
     dist=sqrt(dot(vdist',vdist'));
@@ -127,30 +148,29 @@ end;
 
 %% some default noise levels
 
-sensor_noise_TrtHz=10e-15; %% Sensor noise in Tesla per root Hz; default 10 fT/rtHz
-sensor_bw_Hz=80; %% recording bandwith in Hz
-whitenoise=sqrt(sensor_bw_Hz)*sensor_noise_TrtHz;
-
-
-
-
- switch(Dnew.inv{val}.forward.vol.unit),
-         case 'mm'
-             dipmoment=dipmoment.*1000; %% Ampere mm
-         case 'cm'
-             dipmoment=dipmoment.*100; % Ampere cm
-         otherwise
-         error('unknown volume unit')
- end;
-
-switch Dnew.sensors('MEG').chanunit{1}
-    case 'T'
-        whitenoise=whitenoise; %% rms tesla 
-    case 'fT'
-        whitenoise=whitenoise*1e15; %% rms femto tesla
-     otherwise
-        error('unknown sensor unit')
+if isempty(whitenoise)&&isempty(SNRdB),
+    sensor_noise_TrtHz=10e-15; %% Sensor noise in Tesla per root Hz; default 10 fT/rtHz
+    sensor_bw_Hz=80; %% recording bandwith in Hz
+    whitenoise=sqrt(sensor_bw_Hz)*sensor_noise_TrtHz;
+    disp('setting default 10ftrtHz white noise in 80Hz BW');
+else
+    whitenoise=1;
 end;
+
+
+
+
+switch(Dnew.inv{val}.forward.vol.unit), %% correct for non-SI lead field scaling
+    case 'mm'
+        
+        Lscale=1000*1000;
+    case 'cm'
+        
+        Lscale=100*100;
+    otherwise
+        error('unknown volume unit')
+end;
+
 
 %% WAVEFORM FOR EACH SOURCE
 
@@ -164,20 +184,21 @@ f1ind = intersect(find(Dnew.time>startf1),find(Dnew.time<=endf1));
 % Create the waveform for each source
 signal = zeros(Ndip,length(Dnew.time));
 for j=1:Ndip				% For each source
-	for i=1:Ntrials			% and trial
-		f1 = dipfreq(j);	% Frequency depends on stim condition
-		amp1 = dipmoment(j);	% also the amplitude
-		phase1 = pi/2;
-		signal(j,f1ind) = signal(j,f1ind)...
-			+ amp1*sin((Dnew.time(f1ind)...
-			- Dnew.time(min(f1ind)))*f1*2*pi + phase1);
-	end
+    for i=1:Ntrials			% and trial
+        f1 = dipfreq(j);	% Frequency depends on stim condition
+        amp1 = dipmoment(j);	% also the amplitude
+        phase1 = pi/2;
+        signal(j,f1ind) = signal(j,f1ind)...
+            + amp1*sin((Dnew.time(f1ind)...
+            - Dnew.time(min(f1ind)))*f1*2*pi + phase1);
+    end
 end
 
 %% CREATE A NEW FORWARD PROBLEM
 
 fprintf('Computing Gain Matrix: ')
 spm_input('Creating gain matrix',1,'d');	% Shows gain matrix computation
+
 [L Dnew] = spm_eeg_lgainmat(Dnew);				% Gain matrix
 
 Nd    = size(L,2);							% number of dipoles
@@ -207,26 +228,55 @@ fprintf(' - done\n')
 
 % Add waveform of all smoothed sources to their equivalent dipoles
 % QGs add up to 0.9854
-for j=1:Ndip 
-	for i=1:size(Dnew,2)
-		X(:,i) = X(:,i) + signal(j,i)*QG(:,meshsourceind(j));
-	end
+for j=1:Ndip
+    for i=1:size(Dnew,2)
+        X(:,i) = X(:,i) + signal(j,i)*QG(:,meshsourceind(j)); %% this will be in Am
+    end
 end
 
 
-% Copy same data to all trials 
-tmp=L*X; 
-tmp=tmp./repmat(Dnew.inv{val}.forward.scale,1,size(tmp,2)); %% account for scaling of lead fields
+% Copy same data to all trials
+tmp=L*X;
 
-%meanrmssignal=mean(allchanstd);
+tmp=Lscale.*tmp./Dnew.inv{val}.forward.scale;
+
+switch Dnew.sensors('MEG').chanunit{1}
+    case 'T'
+        whitenoise=whitenoise; %% rms tesla
+        tmp=tmp;
+    case 'fT'
+        whitenoise=whitenoise*1e15; %% rms femto tesla
+        tmp=tmp*1e15;
+    otherwise
+        error('unknown sensor unit')
+end;
+
+
+allchanstd=std(tmp');
+meanrmssignal=mean(allchanstd);
+
+
+if ~isempty(SNRdB),
+    whitenoise = meanrmssignal.*(10^(SNRdB/20));
+    disp(sprintf('Setting white noise to give sensor level SNR of %dB',SNRdB));
+end;
+
+
 for i=1:Ntrials
-	Dnew(:,:,i) = tmp;
+    if find(i,trialind), %% only add signal to specific trials
+        Dnew(:,:,i) = tmp;
+    else
+        Dnew=zeros(size(tmp));
+    end;
     Dnew(:,:,i)=Dnew(:,:,i)+randn(size(Dnew(:,:,i))).*whitenoise; %% add white noise in fT
 end
 
 
 
+
 %% Plot and save
+[dum,plotind]=sort(allchanstd);
+
 
 Nj		= size(vert,1);
 M		= mean(X(:,f1ind)'.^2,1);
@@ -243,9 +293,15 @@ drawnow
 
 figure
 hold on
-aux = tmp(1,:);
-plot(Dnew.time,Dnew(1,:,1),Dnew.time,aux,'r');
-title('Measured activity over first sensor');
+aux = tmp(plotind(end),:);
+subplot(2,1,1);
+plot(Dnew.time,Dnew(plotind(end),:,1),Dnew.time,aux,'r');
+title('Measured activity over max sensor');
+legend('Noisy','Noiseless');
+subplot(2,1,2);
+aux = tmp(plotind(floor(length(plotind)/2)),:);
+plot(Dnew.time,Dnew(plotind(floor(length(plotind)/2)),:,1),Dnew.time,aux,'r');
+title('Measured activity over median sensor');
 legend('Noisy','Noiseless');
 
 Dnew.save;
