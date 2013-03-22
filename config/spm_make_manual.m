854aae24ec7e1309f5e0c1da9f747692e32c0dc3
Guillaume Flandin <g.flandin@ucl.ac.uk>
2013-03-22 12:35:40 +0000

    Rename function that generates TeX files for the manual.
    SVN r5346

--- config/spm_make_manual.m
+++ config/spm_make_manual.m
@@ -1,10 +1,11 @@
-function spm_latex_cfg(c)
+function spm_make_manual(c)
 % Convert a job configuration tree into a series of LaTeX documents
 %__________________________________________________________________________
-% Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
+% Copyright (C) 2005-2013 Wellcome Trust Centre for Neuroimaging
 
 % John Ashburner
-% $Id: spm_latex_cfg.m 5219 2013-01-29 17:07:07Z spm $
+% $Id: spm_make_manual.m 5346 2013-03-22 12:35:40Z guillaume $
+
 
 if ~nargin, c = spm_cfg; end
 if nargin && ischar(c), clean_latex_compile; return; end
@@ -33,55 +34,55 @@ fprintf(fp,'\\maketitle\n');
 fprintf(fp,'\\dominitoc\\tableofcontents\n\n');
 fprintf(fp,'\\newpage\n\\section*{The SPM User Interface}\n');
 write_help(c,fp);
-for i=1:numel(c.values),
+for i=1:numel(c.values)
    % this is always false, and each cfg_item has a tag
-   %if isfield(c.values{i},'tag'),
+   %if isfield(c.values{i},'tag')
        part(c.values{i},fp);
-   %end;
-end;
+   %end
+end
 %fprintf(fp,'\\parskip=0mm\n\\bibliography{methods_macros,methods_group,external}\n\\end{document}\n\n');
 fprintf(fp,'\\parskip=0mm\n');
 bibcstr = get_bib(fullfile(spm('dir'),'man','biblio'));
 
 tbxlist = dir(fullfile(spm('dir'),'toolbox'));
 for k = 1:numel(tbxlist)
-    if tbxlist(k).isdir,
+    if tbxlist(k).isdir
         bibcstr=[bibcstr(:); get_bib(fullfile(spm('dir'),'toolbox', ...
                  tbxlist(k).name))];
-    end;
-end;
+    end
+end
 bibcstr = strcat(bibcstr,',');
 bibstr  = strcat(bibcstr{:});
 fprintf(fp,'\\bibliography{%s}\n',bibstr(1:end-1));
 fprintf(fp,'\\end{document}\n\n');
 fclose(fp);
-return;
+
 
 %==========================================================================
 function part(c,fp)
 % this is always false, and each cfg_item has a tag
-%if isstruct(c) && isfield(c,'tag'),
+%if isstruct(c) && isfield(c,'tag')
     fprintf(fp,'\\part{%s}\n',texify(c.name));
     % write_help(c,fp);
-    if isa(c,'cfg_repeat')||isa(c,'cfg_choice')||isa(c,'cfg_menu'),
-        for i=1:numel(c.values),
-            %if isfield(c.values{i},'tag'),
+    if isa(c,'cfg_repeat')||isa(c,'cfg_choice')||isa(c,'cfg_menu')
+        for i=1:numel(c.values)
+            %if isfield(c.values{i},'tag')
                 fprintf(fp,'\\include{%s}\n',c.values{i}.tag);
                 chapter(c.values{i});
-            %end;
-        end;
-    end;
-    %if isfield(c,'val'),
-        for i=1:numel(c.val),
-            if isfield(c.val{i},'tag'),
-                if chapter(c.val{i}),
+            %end
+        end
+    end
+    %if isfield(c,'val')
+        for i=1:numel(c.val)
+            if isfield(c.val{i},'tag')
+                if chapter(c.val{i})
                     fprintf(fp,'\\include{%s}\n',c.val{i}.tag);
-                end;
-            end;
-        end;
-    %end;
-%end;
-return;
+                end
+            end
+        end
+    %end
+%end
+
 
 %==========================================================================
 function sts = chapter(c)
@@ -90,65 +91,65 @@ if strcmp(bn,'preproc') && ~isempty(strfind(c.name,'EEG'))
     bn = ['MEEG_' bn]; % fix for name clash with other 'preproc'
 end
 fp = fopen([bn '.tex'],'w');
-if fp==-1, sts = false; return; end;
+if fp==-1, sts = false; return; end
 
 fprintf(fp,'\\chapter{%s  \\label{Chap:%s}}\n\\minitoc\n\n\\vskip 1.5cm\n\n',texify(c.name),c.tag);
 write_help(c,fp);
 
-switch class(c),
-case {'cfg_branch','cfg_exbranch'},
-    for i=1:numel(c.val),
-        section(c.val{i},fp);
-    end;
-case {'cfg_repeat','cfg_choice'},
-    for i=1:numel(c.values),
-        section(c.values{i},fp);
-    end;
-end;
+switch class(c)
+    case {'cfg_branch','cfg_exbranch'}
+        for i=1:numel(c.val)
+            section(c.val{i},fp);
+        end
+    case {'cfg_repeat','cfg_choice'}
+        for i=1:numel(c.values)
+            section(c.values{i},fp);
+        end
+end
 fclose(fp);
 sts = true;
-return;
+
 
 %==========================================================================
 function section(c,fp,lev)
-if nargin<3, lev = 1; end;
+if nargin<3, lev = 1; end
 sec = {'section','subsection','subsubsection','paragraph','subparagraph','textbf','textsc','textsl','textit'};
-if lev<=length(sec),
+if lev<=length(sec)
     fprintf(fp,'\n\\%s{%s}\n',sec{lev},texify(c.name));
     write_help(c,fp);
-    switch class(c),
-    case {'cfg_branch','cfg_exbranch'},
-        for i=1:numel(c.val),
-            section(c.val{i},fp,lev+1);
-        end;
-    case {'cfg_repeat','cfg_choice'},
-        for i=1:numel(c.values),
-            section(c.values{i},fp,lev+1);
-        end;
-    end;
+    switch class(c)
+        case {'cfg_branch','cfg_exbranch'}
+            for i=1:numel(c.val)
+                section(c.val{i},fp,lev+1);
+            end
+        case {'cfg_repeat','cfg_choice'}
+            for i=1:numel(c.values)
+                section(c.values{i},fp,lev+1);
+            end
+    end
 else
     warning(['Too many nested levels... ' c.name]);
-end;
-return;
+end
+
 
 %==========================================================================
 function write_help(hlp,fp)
-if isa(hlp, 'cfg_item'),
-    if ~isempty(hlp.help),
+if isa(hlp, 'cfg_item')
+    if ~isempty(hlp.help)
        hlp = hlp.help;
     else
         return;
-    end;
-end;
-if iscell(hlp),
-    for i=1:numel(hlp),
+    end
+end
+if iscell(hlp)
+    for i=1:numel(hlp)
         write_help(hlp{i},fp);
-    end;
+    end
     return;
-end;
+end
 str = texify(hlp);
 fprintf(fp,'%s\n\n',str);
-return;
+
 
 %==========================================================================
 function str = texify(str0)
@@ -156,23 +157,23 @@ st1  = strfind(str0,'/*');
 en1  = strfind(str0,'*/');
 st = [];
 en = [];
-for i=1:numel(st1),
+for i=1:numel(st1)
     en1  = en1(en1>st1(i));
-    if ~isempty(en1),
+    if ~isempty(en1)
         st  = [st st1(i)];
         en  = [en en1(1)];
         en1 = en1(2:end);
-    end;
-end;
+    end
+end
 
 str = [];
 pen = 1;
-for i=1:numel(st),
+for i=1:numel(st)
     str = [str clean_latex(str0(pen:st(i)-1)) str0(st(i)+2:en(i)-1)];
     pen = en(i)+2;
-end;
+end
 str = [str clean_latex(str0(pen:numel(str0)))];
-return;
+
 
 %==========================================================================
 function str = clean_latex(str)
@@ -185,7 +186,7 @@ str  = strrep(str,'#','\#');
 str  = strrep(str,'|','$|$');
 str  = strrep(str,'>','$>$');
 str  = strrep(str,'<','$<$');
-return;
+
 
 %==========================================================================
 function bibcstr = get_bib(bibdir)
@@ -195,6 +196,7 @@ for k = 1:numel(biblist)
     n          = spm_file(biblist(k).name,'basename');
     bibcstr{k} = fullfile(bibdir,n);
 end
+
 
 %==========================================================================
 function clean_latex_compile
