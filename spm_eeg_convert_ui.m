66706de9b6c346cf67963c6206d096cf6af43a34
Guillaume Flandin <g.flandin@ucl.ac.uk>
2013-04-18 17:34:49 +0000

    Bug fix: 'dataset' variable was undefined.
    SVN r5428

--- spm_eeg_convert_ui.m
+++ spm_eeg_convert_ui.m
@@ -1,32 +1,31 @@
 function spm_eeg_convert_ui
 % User interface for M/EEG data conversion facility
 % FORMAT spm_eeg_convert_ui
-%__________________________________________________________________________
 % 
-% See spm_eeg_convert for a description of input structure S.
+% See spm_eeg_convert for details.
 %__________________________________________________________________________
-% Copyright (C) 2008-2012 Wellcome Trust Centre for Neuroimaging
+% Copyright (C) 2008-2013 Wellcome Trust Centre for Neuroimaging
 
 % Vladimir Litvak
-% $Id: spm_eeg_convert_ui.m 5164 2012-12-28 16:40:06Z vladimir $
+% $Id: spm_eeg_convert_ui.m 5428 2013-04-18 17:34:49Z guillaume $
 
-SVNrev = '$Rev: 5164 $';
+SVNrev = '$Rev: 5428 $';
 
 %-Startup
 %--------------------------------------------------------------------------
 spm('FnBanner', mfilename, SVNrev);
 spm('FnUIsetup','M/EEG data conversion',0);
 
+[fname, sts] = spm_select(1, '.*', 'Select M/EEG data file');
+if ~sts, return; end
 
 if spm_input('Define settings?','+1','yes|just read',[1 0], 0);
-    spm_jobman('interactive','','spm.meeg.convert', dataset);
+    matlabbatch{1}.spm.meeg.convert.dataset = {fname};
+    spm_jobman('interactive', matlabbatch);
 else
-    [fname, sts] = spm_select(1, '.*', 'Select M/EEG data file');
-    if sts,
-        spm_eeg_convert(fname);
-    end
+    spm_eeg_convert(fname);
 end
 
 %-Cleanup
 %--------------------------------------------------------------------------
-spm('FigName',''); 
+spm('FigName','');
