6be8afc9635d505a238fb9bfa33dccf0da252fba
Vladimir Litvak <v.litvak@ucl.ac.uk>
2013-05-03 15:20:14 +0000

    Some changes following Debbie's comments
    SVN r5464

--- @meeg/private/warning_flexible.m
+++ @meeg/private/warning_flexible.m
@@ -5,8 +5,8 @@ function warning_flexible(varargin)
 % Copyright (C) 2012 Wellcome Trust Centre for Neuroimaging
 
 % Vladimir Litvak
-% $Id: warning_flexible.m 5057 2012-11-15 13:03:35Z vladimir $
+% $Id: warning_flexible.m 5464 2013-05-03 15:20:14Z vladimir $
 
-warning off backtrace
+%warning off backtrace
 warning(varargin{:});
-warning on backtrace
+%warning on backtrace
