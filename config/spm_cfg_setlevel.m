60996e1901ac7979794329007991a13199ccf232
Gareth Barnes <g.barnes@ucl.ac.uk>
2013-06-12 12:09:35 +0000

    set level test
    SVN r5547

---
+++ config/spm_cfg_setlevel.m
@@ -0,0 +1,35 @@
+function setlevel = spm_cfg_setlevel
+% SPM Configuration file for Set level tests based on Barnes et al. NIMG
+% 2012
+%_______________________________________________________________________
+% Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
+
+% $Id: spm_cfg_setlevel.m 5547 2013-06-12 12:09:35Z gareth $
+
+% ---------------------------------------------------------------------
+% spmmat Select SPM.mat
+% ---------------------------------------------------------------------
+spmmat         = cfg_files;
+spmmat.tag     = 'spmmat';
+spmmat.name    = 'Select SPM.mat';
+spmmat.help    = {
+                  'Select the SPM.mat file that contains the design specification. '
+                  'The directory containing this file is known as the input directory.'
+}';
+spmmat.filter  = 'mat';
+spmmat.ufilter = '^SPM\.mat$';
+spmmat.num     = [1 1];
+
+% ---------------------------------------------------------------------
+% Set level test 
+% ---------------------------------------------------------------------
+setlevel          = cfg_exbranch;
+setlevel.tag      = 'setlevel';
+setlevel.name     = 'Set Level test';
+setlevel.val      = {spmmat};
+setlevel.help     = {'A set level test how likely the statistical image is a random field'};
+setlevel.prog     = @spm_run_setlevel;
+%setlevel.vout     = @vout_stats;
+setlevel.modality = {'FMRI' 'PET' 'EEG'};
+
+
