818d4a978317a8a14a3e88f43678fc5a115c4c5f
Guillaume Flandin <g.flandin@ucl.ac.uk>
2013-07-22 11:10:19 +0000

    Add spectral toolbox to MATLAB path.
    SVN r5590

--- spm_dcm_fmri_csd_data.m
+++ spm_dcm_fmri_csd_data.m
@@ -1,5 +1,5 @@
 function DCM = spm_dcm_fmri_csd_data(DCM)
-% gets cross-spectral density data-features using a VAR model
+% Get cross-spectral density data-features using a VAR model
 % FORMAT DCM = spm_dcm_fmri_csd_data(DCM)
 % DCM    -  DCM structure or fMRI
 %
@@ -10,20 +10,23 @@ function DCM = spm_dcm_fmri_csd_data(DCM)
 %    DCM.Y.csd     - cross spectral density over sources
 %    DCM.Y.Hz      - Frequency bins
 %__________________________________________________________________________
-% Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
+% Copyright (C) 2013 Wellcome Trust Centre for Neuroimaging
  
 % Karl Friston
-% $Id: spm_dcm_fmri_csd_data.m 5588 2013-07-21 20:59:39Z karl $
- 
+% $Id: spm_dcm_fmri_csd_data.m 5590 2013-07-22 11:10:19Z guillaume $
 
+
+if ~isdeployed
+    addpath(fullfile(spm('Dir'),'toolbox', 'spectral'));
+end
 
 % Time[s] of data
-%----------------------------------------------------------------------
+%--------------------------------------------------------------------------
 Nb        = size(DCM.Y.y,1);              % number of bins
 Nu        = size(DCM.U.u,1);              % number of bins
 DCM.Y.pst = (1:Nb)*DCM.Y.dt;              % PST
 
-% get frequency range
+% Get frequency range
 %--------------------------------------------------------------------------
 try
     Hz1   = DCM.options.Fdcm(1);          % lower frequency
@@ -37,12 +40,12 @@ end
 %--------------------------------------------------------------------------
 DCM.Y.Hz  = linspace(Hz1,Hz2,64);          % Frequencies
 
+
 % Cross spectral density - respones
 %==========================================================================
 mar       = spm_mar(DCM.Y.y,16);
 mar       = spm_mar_spectra(mar,DCM.Y.Hz,1/DCM.Y.dt);
 DCM.Y.csd = mar.P;
-
 
 % Decimate U.u from micro-time
 % -------------------------------------------------------------------------
@@ -56,5 +59,3 @@ u         = Dy*(Du'*DCM.U.u);
 mar       = spm_mar(full(u),16);
 mar       = spm_mar_spectra(mar,DCM.Y.Hz,1/DCM.Y.dt);
 DCM.U.csd = mar.P;
-            
-
