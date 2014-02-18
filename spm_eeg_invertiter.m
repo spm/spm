98d2e7007531f2aada04fa328a71d79979df9352
Gareth Barnes <g.barnes@ucl.ac.uk>
2014-02-18 11:12:45 +0000

    trapped bug for 1 iteration
    SVN r5884

--- spm_eeg_invertiter.m
+++ spm_eeg_invertiter.m
@@ -14,7 +14,7 @@ function [Dtest,modelF,allF]=spm_eeg_invertiter(Dtest,Npatchiter,funcname,patchi
 % Copyright (C) 2010 Wellcome Trust Centre for Neuroimaging
 %
 % Gareth Barnes
-% $Id: spm_eeg_invertiter.m 5882 2014-02-18 10:30:05Z gareth $
+% $Id: spm_eeg_invertiter.m 5884 2014-02-18 11:12:45Z gareth $
 
 if nargin<2,
     Npatchiter=[];
@@ -86,11 +86,11 @@ for patchiter=1:Npatchiter,
             warning('Patch centres are currently fixed for this algorithm (iteration will have no effect!)');
             Dout    = spm_eeg_invert(Din); %
             Dout.inv{val}.inverse.Ip=Ip;
-        otherwise 
+        otherwise
             error('unknown function');
     end;
     if (Dtest{1}.inv{val}.inverse.mergeflag==1), %% will need all posteriors to merge them (could consider getting rid of worst ones here to save memory
-            modelF(patchiter).inverse=Dout.inv{val}.inverse;
+        modelF(patchiter).inverse=Dout.inv{val}.inverse;
     else %% just keep track of best inversion
         if Dout.inv{val}.inverse.F>bestF,
             modelF(1).inverse=Dout.inv{val}.inverse;
@@ -111,18 +111,22 @@ disp('model evidences relative to maximum:')
 sort(allF-bestF)
 
 
+%% for 1 iteration or for best solution
+Dtest{1}.inv{val}.inverse=modelF(1).inverse; %% return best model (only 1 model saved in this option)
+Dtest{1}.inv{val}.inverse.allF=allF;
+
 if Npatchiter>1, %% keep iterations if more than 1
- 
+    
     if (Dtest{1}.inv{val}.inverse.mergeflag==1)
         Dtest{1}.inv{val}.inverse=modelF(bestind).inverse; % start with best model so far
         Qpriors=sparse(zeros(Npatchiter,size(modelF(1).inverse.qC,1)));
-        for patchiter=1:Npatchiter,    
+        for patchiter=1:Npatchiter,
             Qpriors(patchiter,:)=modelF(patchiter).inverse.qC;
         end;
         disp('Merging posterior distributions..');
         ugainfiles=Dout.inv{val}.gainmat;
         surfind=ones(Npatchiter,1);
-        %% now mix the posteriors 
+        %% now mix the posteriors
         [Dtest{1}] = spm_eeg_invert_classic_mix(Dtest{1},val,Qpriors,surfind,ugainfiles);
         
         
@@ -134,16 +138,12 @@ if Npatchiter>1, %% keep iterations if more than 1
         disp(sprintf('Improvement (when +ve) over best iteration %3.2f',mixF-bestF));
         
     else % NO merge - just take the best
+        
         disp('Using best patch set to current estimate');
-        
-        Dtest{1}.inv{val}.inverse=modelF(1).inverse; %% return best model (only 1 model saved in this option)
         Dtest{1}.inv{val}.comment{1}=sprintf('Best F of %d solutions',Npatchiter);
-        
-        
-
     end; % if BMA
     
-    Dtest{1}.inv{val}.inverse.allF=allF;
+    
 end;
 
 
