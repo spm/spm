8f05ed5e4bcb613afaf0cfd3945620e29b37c98c
Guillaume Flandin <g.flandin@ucl.ac.uk>
2014-04-01 17:41:20 +0000

    New feature: review PPI file.
    SVN r5937

--- spm_peb_ppi.m
+++ spm_peb_ppi.m
@@ -25,17 +25,17 @@ function PPI = spm_peb_ppi(varargin)
 % showGraphics - empty or 1 = yes, 0 = no.
 %
 %
-% PPI.ppi    - (PSY*xn  or xn1*xn2) convolved with the HRF
-% PPI.Y      - Original BOLD eigenvariate. Use as covariate of no interest
-% PPI.P      - PSY convolved with HRF for psychophysiologic interactions,
-%              or in the case of physiophysologic interactions contains
-%              the eigenvariate of the second region. 
-% PPI.name   - Name of PPI
-% PPI.xY     - Original VOI information
-% PPI.xn     - Deconvolved neural signal(s)
-% PPI.U.u    - Psychological variable or input function (PPIs only)
-% PPI.U.w    - Contrast weights for psychological variable (PPIs only)
-% PPI.U.name - Names of psychological conditions (PPIs only)
+% PPI.ppi      - (PSY*xn  or xn1*xn2) convolved with the HRF
+% PPI.Y        - Original BOLD eigenvariate. Use as covariate of no interest
+% PPI.P        - PSY convolved with HRF for psychophysiologic interactions,
+%                or in the case of physiophysologic interactions contains
+%                the eigenvariate of the second region. 
+% PPI.name     - Name of PPI
+% PPI.xY       - Original VOI information
+% PPI.xn       - Deconvolved neural signal(s)
+% PPI.psy.u    - Psychological variable or input function (PPIs only)
+% PPI.psy.w    - Contrast weights for psychological variable (PPIs only)
+% PPI.psy.name - Names of psychological conditions (PPIs only)
 %__________________________________________________________________________
 %
 % This routine is effectively a hemodynamic deconvolution using full priors
@@ -47,7 +47,8 @@ function PPI = spm_peb_ppi(varargin)
 % Copyright (C) 2002-2014 Wellcome Trust Centre for Neuroimaging
 
 % Darren Gitelman
-% $Id: spm_peb_ppi.m 5931 2014-03-27 19:45:53Z guillaume $
+% $Id: spm_peb_ppi.m 5937 2014-04-01 17:41:20Z guillaume $
+
 
 % SETTING UP A PPI THAT ACCOUNTS FOR THE HRF
 % =========================================================================
@@ -108,7 +109,7 @@ function PPI = spm_peb_ppi(varargin)
 % effects are not proper as inferences on the interaction will include a
 % mixture of both main and interaction effects. 
 %
-% Once the model has been setup and run, a contrast of [1 0 0 ] over the
+% Once the model has been setup and run, a contrast of [1 0 0] over the
 % PPI.ppi, PPI.Y and PPI.P columns respectively, will show regions with a
 % positive relationship to the interaction term, discounting any main
 % effects. Negative regressions can be examined with [-1 0 0]. A PPI random
@@ -116,6 +117,20 @@ function PPI = spm_peb_ppi(varargin)
 % t-contrast for each subject and forwarding them to a second level
 % analysis.
 
+
+% Shortcut for PPI display
+%--------------------------------------------------------------------------
+if nargin && ischar(varargin{1}) && strcmpi(varargin{1},'display')
+    if nargin > 1
+        PPI = varargin{2};
+    else
+        [PPI, sts] = spm_select(1,'^PPI.*\.mat$');
+        if ~sts, return; end
+    end
+    if ischar(PPI), load(PPI); end
+    display_ppi(PPI);
+    return;
+end
 
 % Set up the graphical interface
 %--------------------------------------------------------------------------
@@ -128,11 +143,7 @@ spm_clf(Finter); set(Finter,'name','PPI Setup');
 %--------------------------------------------------------------------------
 if nargin && isstruct(varargin{1})
     SPM = varargin{1};
-    try
-        swd = SPM.pwd;
-    catch
-        swd = '';
-    end
+    try, swd = SPM.pwd; catch, swd = pwd; end
 else
     try
         P            = varargin{1};
@@ -143,17 +154,10 @@ else
     swd = spm_file(P,'fpath');
     load(fullfile(swd,'SPM.mat'));
 end
-if isempty(swd) || strcmp(swd,'.'), swd = pwd; end
-SPM.swd = swd;
+SPM.swd = spm_file(swd,'cpath');
 cwd     = pwd;
 cd(SPM.swd)
 
-% Setup variables
-%--------------------------------------------------------------------------
-RT      = SPM.xY.RT;
-dt      = SPM.xBF.dt;
-NT      = round(RT/dt);
-fMRI_T0 = SPM.xBF.T0;
 
 % Ask whether to perform physiophysiologic or psychophysiologic interactions
 %--------------------------------------------------------------------------
@@ -199,7 +203,7 @@ switch lower(ppiflag)
         try
             VOI = varargin{3};
             if size(VOI,1) ~= 2
-                error('Must include 2 VOI filenames for physiophygiologic interactions')
+                error('Must include 2 VOI filenames for physiophysiologic interactions')
             end
             for i = 1:2
                 p     = load(deblank(VOI(i,:)),'xY');
@@ -255,10 +259,10 @@ switch lower(ppiflag)
                     str             = 'Contrast weight';
                     tmpw            = spm_input(str,4,'e',[],1);
                     % if tmpw==0 then don't include the column in the
-                    % design. This takes care of the possibility that
-                    % the user would select to include the column but
-                    % then give it a 0 weight.
-                    %------------------------------------------------
+                    % design. This takes care of the possibility that the
+                    % user would select to include the column but then give
+                    % it a 0 weight.
+                    %------------------------------------------------------
                     if tmpw ~= 0
                         U.w             = [U.w tmpw];
                         U.u             = [U.u Sess.U(i).u(33:end,j)];
@@ -283,7 +287,6 @@ try
 catch
     PPI.name = spm_input('Name of PPI',3,'s','PPI');
 end
-ppiFilename  = spm_file(PPI.name,'basename');
 
 
 % Check if Graphical output should be shown
@@ -293,22 +296,20 @@ try
 catch
     showGraphics = 1;
 end
-if showGraphics
-    Fgraph       = spm_figure('GetWin','PPI');
-    spm_clf(Fgraph);
-    FS           = spm('FontSizes');
-end
 
 
-% Setup more variables
+% Setup variables
 %--------------------------------------------------------------------------
-N = length(xY(1).u);
-k = 1:NT:N*NT;                             % microtime to scan time indices
+RT      = SPM.xY.RT;
+dt      = SPM.xBF.dt;
+NT      = round(RT/dt);
+fMRI_T0 = SPM.xBF.T0;
+N       = length(xY(1).u);
+k       = 1:NT:N*NT;                       % microtime to scan time indices
 
 
 % Create basis functions and hrf in scan time and microtime
 %--------------------------------------------------------------------------
-spm('Pointer','watch')
 hrf = spm_hrf(dt);
 
 
@@ -329,7 +330,7 @@ X0 = xY(1).X0;
 M  = size(X0,2);
 
 
-% Get response variable,
+% Get response variable
 %--------------------------------------------------------------------------
 for i = 1:size(xY,2)
     Y(:,i) = xY(i).u;
@@ -379,41 +380,9 @@ switch ppiflag
     % Use a microtime to scan time index to convert to scan time: e.g.,
     % k = 1:NT:N*NT; where NT = number of bins per TR = TR/dt or SPM.xBF.T
     % and N = number of scans in the session. Finally account for slice
-    % timing effects by shifting the index accordingly. See approximately
-    % lines 420 and 421 below for an example.)
+    % timing effects by shifting the index accordingly.
     %----------------------------------------------------------------------
     PPI.xn = xn;
-
-    % Plot results
-    %----------------------------------------------------------------------
-    if showGraphics
-        figure(Fgraph);
-        t = RT*[1:N];
-        T = dt*[1:(N*NT)];
-
-        ax = subplot(2,1,1);
-        plot(t,Yc,T,PPI.xn)
-        title('hemodynamic and neuronal responses')
-        xlabel('time (secs)')
-        axis tight square
-        grid on
-        legend('BOLD','neuronal')
-
-        str = sprintf('Simple Deconvolution: %s\n',ppiFilename);
-        str = [str sprintf('VOI file: %s',xY.name)];
-        hAx = axes('Position',[0 0 1 1],...
-                'DefaultTextFontSize',FS(8),...
-                'DefaultTextInterpreter','Tex',...
-                'DefaultTextVerticalAlignment','Baseline',...
-                'Parent',Fgraph,...
-                'Units','points',...
-                'Visible','off');
-        AxPos = get(hAx,'Position'); 
-        set(hAx,'XLim',[0,AxPos(3)]); set(hAx,'YLim',[0,AxPos(4)])
-        dy    = FS(9);
-        h     = text(dy,floor(AxPos(4))-2*dy,str);
-        set(h,'Parent',hAx);
-    end
 
     case  {'physiophysiologic interaction','phipi'}
     %======================================================================
@@ -435,54 +404,6 @@ switch ppiflag
     PPI.xn  = [xn1 xn2];
     PPI.ppi = spm_detrend(ppi);
 
-    % Plot results
-    %----------------------------------------------------------------------
-    if showGraphics
-        figure(Fgraph);
-        t = RT*[1:N];
-        T = dt*[1:(N*NT)];
-
-        ax = subplot(2,1,1);
-        plot(t,PPI.ppi)
-        title('PPI')
-        xlabel('time (secs)')
-        axis tight square
-        grid on
-
-        subplot(2,2,3)
-        plot(t,Yc(:,1),T,PPI.xn(:,1))
-        title('hemodynamic and neuronal responses (1st)')
-        xlabel('time (secs)')
-        axis tight square
-        grid on
-        legend('BOLD','neuronal')
-
-        subplot(2,2,4)
-        plot(t,Yc(:,2),T,PPI.xn(:,2))
-        title('hemodynamic and neuronal responses (2nd)')
-        xlabel('time (secs)')
-        axis tight square
-        grid on
-        legend('BOLD','neuronal')
-        
-        str = sprintf('Physiophysiologic Interaction: %s\n',ppiFilename);
-        str = [str, sprintf('VOI File 1: %s\n',xY(1).name)];
-        str = [str, sprintf('VOI File 2: %s',xY(2).name)];
-        hAx = axes('Position',[0 0 1 1],...
-                'DefaultTextFontSize',FS(8),...
-                'DefaultTextInterpreter','Tex',...
-                'DefaultTextVerticalAlignment','Baseline',...
-                'Parent',Fgraph,...
-                'Units','points',...
-                'Visible','off');
-        AxPos = get(hAx,'Position'); 
-        set(hAx,'XLim',[0,AxPos(3)]); set(hAx,'YLim',[0,AxPos(4)])
-        dy    = FS(9);
-        h     = text(dy,floor(AxPos(4))-2*dy,str);
-        set(h,'Parent',hAx);
-        
-    end
-
     case  {'psychophysiologic interaction','ppi'}
     %======================================================================
 
@@ -490,8 +411,8 @@ switch ppiflag
     % use basis set in microtime
     %----------------------------------------------------------------------
     % get parameter estimates and neural signal; beta (C) is in scan time
-    % This clever trick allows us to compute the betas in scan time which is
-    % much quicker than with the large microtime vectors. Then the betas
+    % This clever trick allows us to compute the betas in scan time which
+    % is much quicker than with the large microtime vectors. Then the betas
     % are applied to a microtime basis set generating the correct neural
     % activity to convolve with the psychological variable in microtime
     %----------------------------------------------------------------------
@@ -505,9 +426,8 @@ switch ppiflag
     for i = 1:size(U.u,2)
         PSY = PSY + full(U.u(:,i)*U.w(:,i));
     end
-    % PSY = spm_detrend(PSY);  <- removed centering of psych variable
-    % prior to multiplication with xn. Based on discussion with Karl
-    % and Donald McLaren. 
+    % PSY = spm_detrend(PSY);  % removed centering of psych variable prior
+                               % to multiplication with xn in r3271.
 
     % Multiply psychological variable by neural signal
     %----------------------------------------------------------------------
@@ -530,76 +450,164 @@ switch ppiflag
     PPI.P   = PSYHRF;
     PPI.xn  = xn;
     PPI.ppi = spm_detrend(ppi);
-
-    % Plot results
-    %----------------------------------------------------------------------
-    if showGraphics
-        figure(Fgraph);
-        t = RT*[1:N];
-        T = dt*[1:(N*NT)];
-
-        str = sprintf('Psychophyiologic Interaction: %s\n',ppiFilename);
-        str = [str, sprintf('VOI File: %s\n',xY(1).name)];
-        str = [str, sprintf('Factors: ')];
-        for i = 1:numel(U.name)
-            str = [str, sprintf('%s [%0.0f]',U.name{i},U.w(i))];
-            if i < numel(U.name)
-                str = [str, sprintf('; ')];
-            end
-        end
-
-        ax = subplot(2,1,1);
-        plot(t,Yc(:,1),T,PPI.xn(:,1))
-        title('hemodynamic and neuronal responses')
-        xlabel('time (secs)')
-        axis tight square
-        grid on
-        legend('BOLD','neuronal')
-
-        subplot(2,2,3)
-        plot(T,PSY,'LineStyle','--','Color',[0 .65 0]);
-        hold on
-        plot(t,PPI.P,'LineStyle','-','LineWidth',1,'Color','b');
-        hold off
-        title('[convolved] psych. variable')
-        xlabel('time (secs)')
-        axis tight square
-        grid on
-
-        subplot(2,2,4)
-        plot(t,PPI.ppi)
-        title('PPI')
-        xlabel('time (secs)')
-        axis tight square
-        grid on
-        
-        hAx = axes('Position',[0 0 1 1],...
-                'DefaultTextFontSize',FS(8),...
-                'DefaultTextInterpreter','None',...
-                'DefaultTextVerticalAlignment','Baseline',...
-                'Parent',Fgraph,...
-                'Units','points',...
-                'Visible','off');
-        AxPos = get(hAx,'Position'); 
-        set(hAx,'XLim',[0,AxPos(3)]); set(hAx,'YLim',[0,AxPos(4)])
-        dy    = FS(9);
-        h     = text(dy,floor(AxPos(4))-2*dy,str);
-        set(h,'Parent',hAx);
-    end
     
 end % (switch)
 
-% Setup other output variables and Save
+% Setup other output variables
 %--------------------------------------------------------------------------
 PPI.xY = xY;
+PPI.RT = RT;
 PPI.dt = dt;
+
+% Display
+%--------------------------------------------------------------------------
+if showGraphics
+    display_ppi(PPI);
+end
+
+% Save
+%--------------------------------------------------------------------------
 str    = ['PPI_' PPI.name '.mat'];
 
 save(fullfile(SPM.swd,str),'PPI', spm_get_defaults('mat.format'))
 
-fprintf('   PPI saved as %s\n',fullfile(SPM.swd,str));
+if desktop('-inuse')
+    dispf = @(f) ...
+        sprintf('<a href="matlab:spm_peb_ppi(''display'',''%s'');">%s</a>',f,f);
+else
+    dispf = @(f) f;
+end
+
+fprintf('   PPI saved as %s\n',dispf(fullfile(SPM.swd,str)));
 
 % Clean up
 %--------------------------------------------------------------------------
-spm('Pointer','arrow'); set(Finter,'name',header);
+set(Finter,'name',header);
 cd(cwd);
+
+
+%==========================================================================
+% function display_ppi(PPI)
+%==========================================================================
+function display_ppi(PPI)
+
+Fgraph = spm_figure('GetWin','PPI');
+FS     = spm('FontSizes');
+spm_clf(Fgraph);
+
+try
+    RT = PPI.RT;
+catch
+    RT = spm_get_defaults('stats.fmri.t')*PPI.dt; % backward compatibility
+end
+N  = length(PPI.xY(1).u);
+NT = round(RT/PPI.dt);
+t  = PPI.RT*[1:N];
+T  = PPI.dt*[1:(N*NT)];
+
+%-Simple deconvolution
+%--------------------------------------------------------------------------
+if numel(PPI.xY) == 1 && ~isfield(PPI,'psy')
+    
+    ax = subplot(1,1,1);
+    plot(t,PPI.Y,T,PPI.xn)
+    title('hemodynamic and neuronal responses')
+    xlabel(['time (secs)'])
+    axis tight square
+    grid on
+    legend('BOLD','neuronal')
+    
+    str = sprintf('Simple Deconvolution: %s\n',PPI.name);
+    str = [str sprintf('VOI file: %s',PPI.xY.name)];
+    
+%-Physiophysiologic interaction
+%--------------------------------------------------------------------------
+elseif numel(PPI.xY) == 2
+    
+    ax = subplot(2,1,1);
+    plot(t,PPI.ppi)
+    title('PPI')
+    xlabel(['time (secs)'])
+    axis tight square
+    grid on
+    
+    subplot(2,2,3)
+    plot(t,PPI.Y(:,1),T,PPI.xn(:,1))
+    title('hemodynamic and neuronal responses (1st)')
+    xlabel(['time (secs)'])
+    axis tight square
+    grid on
+    legend('BOLD','neuronal')
+    
+    subplot(2,2,4)
+    plot(t,PPI.P,T,PPI.xn(:,2))
+    title('hemodynamic and neuronal responses (2nd)')
+    xlabel(['time (secs)'])
+    axis tight square
+    grid on
+    legend('BOLD','neuronal')
+    
+    str = sprintf('Physiophysiologic Interaction: %s\n',PPI.name);
+    str = [str, sprintf('VOI File 1: %s\n',PPI.xY(1).name)];
+    str = [str, sprintf('VOI File 2: %s',PPI.xY(2).name)];
+    
+%-Psychophysiologic interaction
+%--------------------------------------------------------------------------
+elseif isfield(PPI,'psy')
+    
+    PSY = zeros(N*NT,1);
+    for i = 1:size(PPI.psy.u,2)
+        PSY = PSY + full(PPI.psy.u(:,i)*PPI.psy.w(:,i));
+    end
+    
+    ax = subplot(2,1,1);
+    plot(t,PPI.Y,T,PPI.xn(:,1))
+    title('hemodynamic and neuronal responses')
+    xlabel(['time (secs)'])
+    axis tight square
+    grid on
+    legend('BOLD','neuronal')
+    
+    subplot(2,2,3)
+    plot(T,PSY,'LineStyle','--','Color',[0 .65 0]);
+    hold on
+    plot(t,PPI.P,'LineStyle','-','LineWidth',1,'Color','b');
+    hold off
+    title('[convolved] psych. variable')
+    xlabel(['time (secs)'])
+    axis tight square
+    grid on
+    
+    subplot(2,2,4)
+    plot(t,PPI.ppi)
+    title('PPI')
+    xlabel(['time (secs)'])
+    axis tight square
+    grid on
+    
+    str = sprintf('Psychophysiologic Interaction: %s\n',PPI.name);
+    str = [str, sprintf('VOI File: %s\n',PPI.xY(1).name)];
+    str = [str, sprintf('Factors: ')];
+    for i = 1:numel(PPI.psy.name)
+        str = [str, sprintf('%s [%0.0f]',PPI.psy.name{i},PPI.psy.w(i))];
+        if i < numel(PPI.psy.name)
+            str = [str, sprintf('; ')];
+        end
+    end
+    
+else
+    error('Unknown PPI type.');
+end
+
+hAx = axes('Position',[0 0.90 1 0.1],...
+    'DefaultTextFontSize',FS(8),...
+    'DefaultTextInterpreter','None',...
+    'DefaultTextVerticalAlignment','Baseline',...
+    'Parent',Fgraph,...
+    'Units','points',...
+    'Visible','off');
+AxPos = get(hAx,'Position');
+set(hAx,'XLim',[0,AxPos(3)]); set(hAx,'YLim',AxPos(2)+[0,AxPos(4)])
+dy    = FS(9);
+h     = text(dy,floor(AxPos(2)+AxPos(4))-2*dy,str);
+set(h,'Parent',hAx);
