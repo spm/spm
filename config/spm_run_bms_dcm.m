6f4b84952b39e5c1546441d81112790ef5480008
Guillaume Flandin <g.flandin@ucl.ac.uk>
2014-04-08 17:36:45 +0000

    Bug fix: do not verify data identity if data have been specified through a model space file.
    SVN r5943

--- config/spm_run_bms_dcm.m
+++ config/spm_run_bms_dcm.m
@@ -1,34 +1,41 @@
-function out = spm_run_bms_dcm (varargin)
-% API to compare DCMs on the basis of their log-evidences. Four methods
-% are available to identify the best among alternative models:
+function out = spm_run_bms_dcm(job)
+% Compare DCMs on the basis of their log-evidences
+% Four methods are available to identify the best among alternative models:
 %
 %  (1) single subject BMS using Bayes factors
-%     (see Penny et al, NeuroImage, 2004)
+%      (see Penny et al, NeuroImage, 2004)
 %  (2) fixed effects group BMS using group Bayes factors
-%     (see Stephan et al, NeuroImage, 2007)
+%      (see Stephan et al, NeuroImage, 2007)
 %  (3) random effects group BMS using exceedance probabilities
-%     (see Stephan et al, NeuroImage, 2009)
+%      (see Stephan et al, NeuroImage, 2009)
 %  (4) comparing model families
-%     (see Penny et al, PLOS-CB, submitted)
+%      (see Penny et al, PLOS-CB, 2010)
 %
 % Note: All functions use the negative free energy (F) as an approximation
 % to the log model evidence.
-% _________________________________________________________________________
-% Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
+%__________________________________________________________________________
+% Copyright (C) 2009-2014 Wellcome Trust Centre for Neuroimaging
 
 % CC Chen & Maria Joao Rosa
-% $Id: spm_run_bms_dcm.m 5735 2013-11-07 18:51:28Z mohamed $
+% $Id: spm_run_bms_dcm.m 5943 2014-04-08 17:36:45Z guillaume $
 
-% input
-% -------------------------------------------------------------------------
-job     = varargin{1};
 
+SVNid = '$Rev: 5943 $';
+
+%-Say hello
+%--------------------------------------------------------------------------
+spm('FnBanner',mfilename,SVNid);
+
+%-Input parameters
+%--------------------------------------------------------------------------
 fname   = 'BMS.mat';                     % Output filename
 fname   = fullfile(job.dir{1},fname);    % Output filename (inc. directory)
 ld_f    = ~isempty(job.load_f{1});       % Log-evidence file
 ld_msp  = ~isempty(job.model_sp{1});     % Model space file
 bma_do  = isfield(job.bma,'bma_yes');    % Compute BMA
 data_se = ~isempty(job.sess_dcm);        % DCM files
+
+out.files{1} = '';
 
 % check whether a BMS.mat file exists in the selected directory
 if exist(fname,'file')
@@ -40,12 +47,11 @@ if exist(fname,'file')
             spm('time'));
         Fgraph  = spm_figure('GetWin','Graphics');
         spm_clf(Fgraph);
-        out = [];
         return
     end
 end
 
-% method
+%-Method
 % -------------------------------------------------------------------------
 if strcmp(job.method,'FFX');
     method = 'FFX';                      % Fixed-effects
@@ -53,16 +59,17 @@ else
     method = 'RFX';                      % Random-effects
 end
 
-% check DCM.mat files and BMA
-% -------------------------------------------------------------------------
+%-Check DCM.mat files and BMA
+%--------------------------------------------------------------------------
 do_bma_famwin = 0;
 do_bma_all    = 0;
 
 if bma_do
     if ld_msp
-        disp('Loading model space')
+        fprintf('%-40s: ','Loading model space')                        %-#
         load(job.model_sp{1});
         load(subj(1).sess(1).model(1).fname)
+        fprintf('%30s\n','...done')                                     %-#
     else
         if data_se
             load(job.sess_dcm{1}(1).mod_dcm{1})
@@ -86,8 +93,9 @@ if bma_do
     end
 else
     if ld_msp
-        disp('Loading model space')
+        fprintf('%-40s: ','Loading model space')                        %-#
         load(job.model_sp{1});
+        fprintf('%30s\n','...done')                                     %-#
         if ~exist('subj','var')
             spm('alert','Incorrect model space file! File must contain ''subj'' structure.','Error');
             return
@@ -95,13 +103,13 @@ else
     end
 end
 
-% initialise
-% -------------------------------------------------------------------------
+%-Initialise
+%--------------------------------------------------------------------------
 F      = [];                % Free energy
 N      = {};                % Models
 
-% prepare the data
-% -------------------------------------------------------------------------
+%-Prepare the data
+%--------------------------------------------------------------------------
 if  ld_f
     data      = job.load_f{1};
     load(data);
@@ -195,6 +203,7 @@ else
                     % Data ID verification. At least for now we'll
                     % re-compute the IDs rather than use the ones stored
                     % with the DCM.
+                    if ~exist('DCM','var'), job.verify_id = 0; end
                     if job.verify_id
                         
                         disp(sprintf('Identifying data: model %d', j));
@@ -230,7 +239,6 @@ else
             if job.verify_id
                 failind = find(max(abs(diff(ID)),[],1) > eps);
                 if ~isempty(failind)
-                    out.files{1} = [];
                     str_error = ['The models for subject ' num2str(k) ...
                         ' session(s) ' num2str(failind) ' were not fitted to the same data.'];
                     spm('alert',str_error,'Error');
@@ -238,7 +246,6 @@ else
                 end
             end
         else
-            out.files{1} = [];
             spm('alert','The number of sessions/models should be the same for all subjects!','Error');
             return         
         end
@@ -246,15 +253,14 @@ else
     end
 end
 
-% bayesian model selection
-% -------------------------------------------------------------------------
-% -------------------------------------------------------------------------
+%-Bayesian Model Selection
+%==========================================================================
 
 % free energy
 sumF = sum(F,1);
 
-% family or model level
-% -------------------------------------------------------------------------
+%-Family or model level
+%--------------------------------------------------------------------------
 if isfield(job.family_level,'family_file')
     
     if ~isempty(job.family_level.family_file{1})
@@ -270,7 +276,6 @@ if isfield(job.family_level,'family_file')
         
         if nfam ~= npart || npart == 1 || maxpart > npart
             spm('alert','Invalid family file!','Error');
-            out.files{1} = [];
             return
         end
     else
@@ -303,7 +308,6 @@ else
         
         if nfam ~= npart || npart == 1 || maxpart > npart || nmodfam > nm
             spm('alert','Invalid family!','Error');
-            out.files{1} = [];
             return
         end
         
@@ -313,21 +317,22 @@ else
     
 end
 
-% single subject BMS or 1st level( fixed effects) group BMS
-% -------------------------------------------------------------------------
-if strcmp(method,'FFX');
+%-Single subject BMS or 1st level (fixed effects) group BMS
+%--------------------------------------------------------------------------
+if strcmp(method,'FFX')
     
-    disp('Computing FFX model/family posteriors ...');
+    fprintf('%-40s: ','Computing FFX model/family posteriors')          %-#
     if ~do_family
         family         = [];
         model.post     = spm_api_bmc(sumF,N,[],[]);
     else
         Ffam           = F(:,sort(m_indx));
-        [family,model] = spm_compare_families (Ffam,family);
+        [family,model] = spm_compare_families(Ffam,family);
         P              = spm_api_bmc(sumF,N,[],[],family);
     end
+    fprintf('%30s\n','...done')                                         %-#
     
-    if bma_do,
+    if bma_do
         if do_family
             if do_bma_famwin
                 [fam_max,fam_max_i]  = max(family.post);
@@ -352,11 +357,11 @@ if strcmp(method,'FFX');
             indx = 1:nm;
         end
         
-        disp('FFX Bayesian model averaging ...');
-        
-        % bayesian model averaging
-        % -----------------------------------------------------------------
+        %-Bayesian Model Averaging
+        %------------------------------------------------------------------
+        fprintf('%-40s: ','FFX Bayesian model averaging')               %-#
         bma = spm_dcm_bma(post,indx,subj,nsamp,odds_ratio);
+        fprintf('%30s\n','...done')                                     %-#
         
     else
         
@@ -379,16 +384,11 @@ if strcmp(method,'FFX');
     BMS.DCM.ffx.family  = family;
     BMS.DCM.ffx.bma     = bma;
     
-    disp('Saving BMS.mat file...')
-    save(fname,'BMS', spm_get_defaults('mat.format'));
-    
-    out.files{1} = fname;
-    
-    % 2nd-level (random effects) BMS
-    % ---------------------------------------------------------------------
+% 2nd-level (random effects) BMS
+%--------------------------------------------------------------------------
 else
     
-    disp('Computing RFX model/family posteriors...');
+    fprintf('%-40s: ','Computing RFX model/family posteriors')          %-#
     if ~do_family
         [exp_r,xp,r_samp,g_post] = spm_BMS_gibbs(F);
         model.g_post             = g_post;
@@ -398,20 +398,20 @@ else
         model.xp    = xp;
         family      = [];
     else
-        
         Ffam           = F(:,sort(m_indx));
         [family,model] = spm_compare_families(Ffam,family);
         
     end
+    fprintf('%30s\n','...done')                                         %-#
     
-    % display the result
+    %-Display result
     if do_family
         P = spm_api_bmc(sumF,N,model.exp_r,model.xp,family);
     else
         P = spm_api_bmc(sumF,N,model.exp_r,model.xp);
     end
     
-    if bma_do,
+    if bma_do
         if do_family
             if do_bma_famwin
                 [fam_max,fam_max_i]  = max(family.exp_r);
@@ -436,10 +436,11 @@ else
             indx = 1:nm;
         end
         
-        disp('RFX Bayesian model averaging ...');
-        % bayesian model averaging
-        % ------------------------------------------------------------------
+        %-Bayesian Model Averaging
+        %------------------------------------------------------------------
+        fprintf('%-40s: ','RFX Bayesian model averaging')               %-#
         bma = spm_dcm_bma(post,indx,subj,nsamp,odds_ratio);
+        fprintf('%30s\n','...done')                                     %-#
         
     else
         
@@ -462,27 +463,28 @@ else
     BMS.DCM.rfx.family  = family;
     BMS.DCM.rfx.bma     = bma;
     
-    disp('Saving BMS.mat file...')
-    save(fname,'BMS', spm_get_defaults('mat.format'));
-
-    out.files{1}= fname;
-    
 end
 
-% Save model_space
-% -------------------------------------------------------------------------
-if ~ld_msp && data_se && ~ld_f
-    disp('Saving model space...')
-    save(fname_msp,'subj', spm_get_defaults('mat.format'));
-end
-
-% Data verification
-% -------------------------------------------------------------------------
+%-Data verification
+%--------------------------------------------------------------------------
 if job.verify_id
-    disp('Data identity has been verified');
+    fprintf('%-40s: %30s\n','Data identity','...verified')              %-#
 else
-    disp('Data identity has not been verified');
+    fprintf('%-40s: %30s\n','Data identity','...not verified')          %-#
 end
 
-end
+%-Save BMS.mat
+%--------------------------------------------------------------------------
+fprintf('%-40s: %30s','Saving BMS.mat','...writing')                    %-#
+save(fname,'BMS', spm_get_defaults('mat.format'));
+fprintf('%s%30s\n',repmat(sprintf('\b'),1,30),'...done')                %-#
 
+out.files{1} = fname;
+
+%-Save model_space
+%--------------------------------------------------------------------------
+if ~ld_msp && data_se && ~ld_f
+    fprintf('%-40s: %30s','Saving model space','...writing')            %-#
+    save(fname_msp,'subj', spm_get_defaults('mat.format'));
+    fprintf('%s%30s\n',repmat(sprintf('\b'),1,30),'...done')            %-#
+end
