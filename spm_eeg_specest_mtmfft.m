2c5765420ec2ab248b8194f5f06376623b276520
Vladimir Litvak <v.litvak@ucl.ac.uk>
2014-05-27 16:38:54 +0000

    Added new spectral plugin
    SVN r6020

--- spm_eeg_specest_mtmfft.m
+++ spm_eeg_specest_mtmfft.m
@@ -25,7 +25,7 @@ function res = spm_eeg_specest_mtmconvol(S, data, time)
 %______________________________________________________________________________________
 % Copyright (C) 2011-2013 Wellcome Trust Centre for Neuroimaging
 
-% $Id: spm_eeg_specest_mtmconvol.m 6020 2014-05-27 16:38:54Z vladimir $
+% $Id: spm_eeg_specest_mtmfft.m 6020 2014-05-27 16:38:54Z vladimir $
 
 
 %-This part if for creating a config branch that plugs into spm_cfg_eeg_tf
@@ -33,22 +33,6 @@ function res = spm_eeg_specest_mtmconvol(S, data, time)
 % when it's called.
 %--------------------------------------------------------------------------
 if nargin == 0
-    timeres = cfg_entry;
-    timeres.tag = 'timeres';
-    timeres.name = 'Time resolution';
-    timeres.strtype = 'r';
-    timeres.num = [1 1];
-    timeres.val = {400};
-    timeres.help = {'Length of the sliding time window (in ms)'};
-    
-    timestep = cfg_entry;
-    timestep.tag = 'timestep';
-    timestep.name = 'Time step';
-    timestep.strtype = 'r';
-    timestep.num = [1 1];
-    timestep.val = {50};
-    timestep.help = {'Step to slide the time window by (in ms)'};
-    
     freqres = cfg_entry;
     freqres.tag = 'freqres';
     freqres.name = 'Frequency resolution';
@@ -67,12 +51,12 @@ if nargin == 0
     taper.values = {'hanning', 'rectwin', 'dpss', 'sine'};
     taper.val = {'sine'};
     
-    mtmconvol = cfg_branch;
-    mtmconvol.tag = 'mtmconvol';
-    mtmconvol.name = 'Multi-taper';
-    mtmconvol.val = {taper, timeres, timestep, freqres};
+    mtmfft = cfg_branch;
+    mtmfft.tag = 'mtmfft';
+    mtmfft.name = 'Spectrum';
+    mtmfft.val = {taper, freqres};
     
-    res = mtmconvol;
+    res = mtmfft;
     
     return
 elseif nargin < 3
@@ -82,19 +66,9 @@ end
 %-Defaults
 %--------------------------------------------------------------------------
 if ~isfield(S, 'taper')
-    S.taper = 'dpss';
+    S.taper = 'sine';
 end
 
-if ~isfield(S, 'timeres')
-    S.timeres = 400;
-end
-
-timeres = 1e-3*S.timeres;
-
-if ~isfield(S, 'timestep')
-    S.timestep = 50;
-end
-timestep = 1e-3*S.timestep;
 
 dt = time(end) - time(1) + diff(time(1:2));
 
@@ -104,7 +78,7 @@ if ~isfield(S, 'frequencies') || isempty(S.frequencies)
 end
 
 if ~isfield(S, 'freqres')
-    S.freqres = max(1/timeres, floor(timeres)/timeres);
+    S.freqres = max(1/dt, floor(dt)/dt);
 end
 
 if length(S.freqres) == 1
@@ -120,38 +94,27 @@ end
 fsample = 1./diff(time(1:2));
 
 df = unique(diff(S.frequencies));
-if length(df) == 1  
+if length(df) == 1
     pad = ceil(dt*df)/df;
 else
     pad = [];
 end
 
-% Correct the time step to the closest multiple of the sampling interval to
-% keep the time axis uniform
-timestep = round(fsample*timestep)/fsample;
-
-timeoi=(time(1)+(timeres/2)):timestep:(time(end)-(timeres/2)-1/fsample); % Time axis
-
-[spectrum,ntaper,freqoi,timeoi] = ft_specest_mtmconvol(data, time, 'taper', S.taper, 'timeoi', timeoi, 'freqoi', S.frequencies,...
-    'timwin', repmat(timeres, 1, length(S.frequencies)), 'tapsmofrq', freqres, 'pad', pad, 'verbose', 0);
-
+[spectrum,ntaper,freqoi] = ft_specest_mtmfft(data, time, 'taper', S.taper,'freqoi', S.frequencies,...
+    'tapsmofrq', freqres, 'pad', pad, 'padtype', 'mirror', 'verbose', 0);
 
 % This in principle should not happen
-if length(unique(diff(timeoi)))>1 && max(abs(diff(unique(diff(timeoi)))))>1e-6
-    error('Non-uniform output time axis')
-end
-
 if length(unique(diff(freqoi)))>1 && max(abs(diff(unique(diff(freqoi)))))>1e-3
     error('Non-uniform output frequency axis')
 end
 
 res = [];
-res.freq = freqoi;
-res.time = timeoi;
+res.freq = linspace(freqoi(1), freqoi(end), length(freqoi));
+res.time = [1 1]*mean(time);
 
-if ndims(spectrum) == 4 && size(spectrum, 1)>1
+if ndims(spectrum) == 3 && size(spectrum, 1)>1
     res.pow = spm_squeeze(nanmean(spectrum.*conj(spectrum), 1), 1);
-elseif ndims(spectrum) == 4
+elseif ndims(spectrum) == 3
     res.fourier = spm_squeeze(spectrum, 1);
 else
     res.fourier = spectrum;
@@ -162,9 +125,9 @@ function y = nansum(x, dim)
 
 x(isnan(x)) = 0;
 if nargin==1
-  y = sum(x);
+    y = sum(x);
 else
-  y = sum(x,dim);
+    y = sum(x,dim);
 end
 end
 
