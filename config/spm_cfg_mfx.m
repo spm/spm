d4ac9e2f1f1c48f4207a6f55cf60e5b487a8f847
Guillaume Flandin <g.flandin@ucl.ac.uk>
2014-05-30 16:29:42 +0000

    Enhancement: add option to specify contrast used to define 2nd level design matrix in MFX.
    SVN r6028

--- config/spm_cfg_mfx.m
+++ config/spm_cfg_mfx.m
@@ -1,14 +1,14 @@
 function mfx = spm_cfg_mfx
 % SPM Configuration file for MFX
-%______________________________________________________________________
-% Copyright (C) 2010 Wellcome Trust Centre for Neuroimaging
+%__________________________________________________________________________
+% Copyright (C) 2010-2014 Wellcome Trust Centre for Neuroimaging
 
 % Guillaume Flandin
-% $Id: spm_cfg_mfx.m 5859 2014-01-30 15:12:14Z guillaume $
+% $Id: spm_cfg_mfx.m 6028 2014-05-30 16:29:42Z guillaume $
 
-% ---------------------------------------------------------------------
+%--------------------------------------------------------------------------
 % dir Directory
-% ---------------------------------------------------------------------
+%--------------------------------------------------------------------------
 dir         = cfg_files;
 dir.tag     = 'dir';
 dir.name    = 'Directory';
@@ -17,9 +17,9 @@ dir.filter = 'dir';
 dir.ufilter = '.*';
 dir.num     = [1 1];
 
-% ---------------------------------------------------------------------
+%--------------------------------------------------------------------------
 % spmmat Select SPM.mat
-% ---------------------------------------------------------------------
+%--------------------------------------------------------------------------
 spmmat         = cfg_files;
 spmmat.tag     = 'spmmat';
 spmmat.name    = 'Select SPM.mat files';
@@ -32,9 +32,9 @@ spmmat.filter  = 'mat';
 spmmat.ufilter = '^SPM\.mat$';
 spmmat.num     = [1 Inf];
 
-% ---------------------------------------------------------------------
+%--------------------------------------------------------------------------
 % ffx Create first-level design
-% ---------------------------------------------------------------------
+%--------------------------------------------------------------------------
 ffx       = cfg_exbranch;
 ffx.tag   = 'ffx';
 ffx.name  = 'FFX Specification';
@@ -43,9 +43,9 @@ ffx.help  = {'Create FFX multi-session first-level design'};
 ffx.prog  = @spm_local_ffx;
 ffx.vout  = @vout_ffx;
 
-% ---------------------------------------------------------------------
+%--------------------------------------------------------------------------
 % spmmat Select SPM.mat
-% ---------------------------------------------------------------------
+%--------------------------------------------------------------------------
 spmmat         = cfg_files;
 spmmat.tag     = 'spmmat';
 spmmat.name    = 'Select SPM.mat';
@@ -56,39 +56,47 @@ spmmat.ufilter = '^SPM\.mat$';
 spmmat.num     = [1 1];
 
 %--------------------------------------------------------------------------
-% con F-contrast weights matrix
+% con Contrast
 %--------------------------------------------------------------------------
 con         = cfg_entry;
 con.tag     = 'contrast';
-con.name    = 'F-contrast';
+con.name    = 'Contrast';
 con.help    = {
-    'F-contrast used to define 2nd level design matrix.'
+    'Contrast used to define 2nd level design matrix.'
     'E.g. ones(n,1) contrast where n is the number of sessions/subjects.'
+    ['The specification of a contrast that is not ones(n,1) allows, ' ...
+    'for example, specified sessions/subjects to be ignored.']
+    ['If left empty (default), a contrast ones(n,1) will be specified ' ...
+    'automatically at run time.']
     };
 con.strtype = 'r';
 con.num     = [Inf Inf];
+con.val     = {[]};
 
-% ---------------------------------------------------------------------
+%--------------------------------------------------------------------------
 % spec MFX Specification
-% ---------------------------------------------------------------------
+%--------------------------------------------------------------------------
 spec       = cfg_exbranch;
 spec.tag   = 'spec';
 spec.name  = 'MFX Specification';
-spec.val   = {spmmat}; % con
+spec.val   = {spmmat con};
 spec.help  = {'MFX Specification'};
 spec.prog  = @spm_local_mfx;
 spec.vout  = @vout_mfx;
 
-% ---------------------------------------------------------------------
+%--------------------------------------------------------------------------
 % mfx Mixed-effects (MFX) analysis
-% ---------------------------------------------------------------------
+%--------------------------------------------------------------------------
 mfx         = cfg_choice;
 mfx.tag     = 'mfx';
 mfx.name    = 'Mixed-effects (MFX) analysis';
 mfx.help    = {'Mixed-effects (MFX) analysis'};
 mfx.values  = {ffx spec};
 
-% =====================================================================
+
+%==========================================================================
+% function out = spm_local_ffx(job)
+%==========================================================================
 function out = spm_local_ffx(job)
 spmmat = job.spmmat;
 SPMS = cell(size(spmmat));
@@ -192,22 +200,31 @@ spm_jobman('run',matlabbatch);
 
 out.spmmat{1} = fullfile(job.dir{1},'SPM.mat');
 
-% =====================================================================
+%==========================================================================
+% function dep = vout_ffx(job)
+%==========================================================================
 function dep = vout_ffx(job)
 dep = cfg_dep;
 dep.sname      = 'SPM.mat File';
 dep.src_output = substruct('.','spmmat');
 dep.tgt_spec   = cfg_findspec({{'filter','mat','strtype','e'}});
 
-% =====================================================================
+%==========================================================================
+% function out = spm_local_mfx(job)
+%==========================================================================
 function out = spm_local_mfx(job)
 load(job.spmmat{1},'SPM');
-n = length(SPM.Sess);
-c = ones(n,1);
+c = job.contrast;
+if isempty(c)
+    n = length(SPM.Sess);
+    c = ones(n,1);
+end
 spm_mfx(SPM,c);
 out.spmmat{1} = fullfile(fileparts(job.spmmat{1}),'mfx','SPM.mat');
 
-% =====================================================================
+%==========================================================================
+% function dep = vout_mfx(job)
+%==========================================================================
 function dep = vout_mfx(job)
 dep = cfg_dep;
 dep.sname      = 'SPM.mat File';
