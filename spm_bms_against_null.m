616333c64b07de88149ba8559f80a15bd6e0af68
Will Penny <w.penny@uea.ac.uk>
2014-06-04 15:22:42 +0000

    Added functionality for Bayesian ANOVA's and t-tests with JZS/unit-information priors
    SVN r6038

--- spm_bms_against_null.m
+++ spm_bms_against_null.m
@@ -1,17 +1,16 @@
-function spm_bms_test_null(logbf_file)
-% Plot PPM showing evidence for null
-% FORMAT spm_bms_test_null(logbf_file)
+function spm_bms_against_null(logbf_file)
+% Plot PPM showing evidence against null
+% FORMAT spm_bms_against_null(logbf_file)
 %
 % logbf_file  -  Log Bayes Factor file providing evidence against null
 %
 % Call this function when SPM is already running
 % or set SPM to appropriate modality eg. spm('defaults','FMRI');
 %__________________________________________________________________________
-% Copyright (C) 2013 Wellcome Trust Centre for Neuroimaging
+% Copyright (C) 2014 Wellcome Trust Centre for Neuroimaging
 
 % Will Penny
-% $Id: spm_bms_test_null.m 6038 2014-06-04 15:22:42Z will $
-
+% $Id: spm_bms_against_null.m 6038 2014-06-04 15:22:42Z will $
 
 if nargin < 1 || isempty(logbf_file)
     logbf_file = spm_select(1,'image',...
@@ -20,16 +19,9 @@ end
 
 p = spm_file(logbf_file,'fpath');
 
-% Create negative logBF image
-j1.input      = {logbf_file};
-j1.output     = {spm_file(logbf_file,'basename','logBF_for_null')};
-j1.expression = '-i1';
-j1.flags      = {0,0,0,spm_type('float32')};
-spm_imcalc(char(j1.input), char(j1.output), j1.expression, j1.flags);
-
 % Create BMS mat file and PPM file
 j2.dir         = {spm_file(logbf_file,'fpath')};
-j2.sess_map{1}.mod_map = j1.output;
+j2.sess_map{1}.mod_map = {logbf_file};
 j2.mod_name    = {};
 j2.method_maps = 'FFX';
 j2.out_file    = 0;
