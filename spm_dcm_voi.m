82736a7284c42565e9319f85f7c87eb82705aa5d
Mohamed Seghier <mseghier@gmail.com>
2014-09-26 10:46:19 +0000

    minor changes (must select the exact number of regions).
    SVN r6202

--- spm_dcm_voi.m
+++ spm_dcm_voi.m
@@ -6,7 +6,7 @@ function DCM = spm_dcm_voi(DCM,VOIs)
 % VOIs  - cell array of new VOI filenames 
 %         eg. {'VOI_V1','VOI_V5','VOI_SPC'}
 %
-% The RT is assumed to be the same as before.
+% The TR, TE and delays are assumed to be the same as before.
 %
 % This function can be used, for example, to replace subject X's data by 
 % subject Y's. The model can then be re-estimated without having to go 
@@ -15,7 +15,7 @@ function DCM = spm_dcm_voi(DCM,VOIs)
 % Copyright (C) 2002-2014 Wellcome Trust Centre for Neuroimaging
 
 % Will Penny
-% $Id: spm_dcm_voi.m 6006 2014-05-21 18:09:05Z guillaume $
+% $Id: spm_dcm_voi.m 6202 2014-09-26 10:46:19Z mohamed $
 
 
 %-Get input arguments
@@ -28,34 +28,33 @@ if ~isstruct(DCM)
     DCMfile = DCM;
     load(DCM);
 end
-
+n = DCM.n ;
 if nargin < 2
-    [VOIs, sts] = spm_select(Inf,'^VOI.*\.mat$','select VOIs');
+    [VOIs, sts] = spm_select(n,'^VOI.*\.mat$','select VOIs');
     if ~sts, return; end
 end
 VOIs = cellstr(VOIs);
 
 %-Check we have matching number of regions
 %--------------------------------------------------------------------------
-if DCM.n ~= numel(VOIs)
+if n ~= numel(VOIs)
     error('DCM contains %d regions while %d VOI files were given.',...
-        DCM.n, numel(VOIs));
+        n, numel(VOIs));
 end
 
 %-Replace relevant fields in DCM with xY
 %--------------------------------------------------------------------------
 DCM               = rmfield(DCM,'xY');
 DCM.Y.y           = [];
-for i=1:DCM.n
-    load(VOIs{i});
-    
-    DCM.v         = size(xY.u,1);
+for i=1:n
+    load(VOIs{i});    
     DCM.Y.y(:,i)  = xY.u;
     DCM.Y.name{i} = xY.name;
-    DCM.Y.X0      = xY.X0;
-    DCM.Y.Q       = spm_Ce(ones(1,DCM.n)*DCM.v);
     DCM.xY(i)     = xY;
 end
+DCM.v         = size(xY.u,1);
+DCM.Y.X0      = xY.X0;
+DCM.Y.Q       = spm_Ce(ones(1,n)*DCM.v);
 
 %-Save (overwrite) new DCM file
 %--------------------------------------------------------------------------
