6053ebe4700784b7ab7442e3befc1b123db6fc12
SPM <fil.spm@ucl.ac.uk>
2014-10-01 11:00:05 +0000

    update of external/fieldtrip to r9869
    SVN r6222

--- external/fieldtrip/fileio/ft_chantype.m
+++ external/fieldtrip/fileio/ft_chantype.m
@@ -47,7 +47,7 @@ function type = ft_chantype(input, desired)
 %    You should have received a copy of the GNU General Public License
 %    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
 %
-% $Id: ft_chantype.m 9833 2014-09-24 13:37:14Z vlalit $
+% $Id: ft_chantype.m 9869 2014-10-01 10:34:03Z vlalit $
 
 % this is to avoid a recursion loop
 persistent recursion
@@ -330,6 +330,12 @@ elseif ft_senstype(input, 'bti')
     type(configtype==4) = {'aux'};
     type(configtype==5) = {'trigger'};
     
+    % refine the distinction between refmag and refgrad to make the types
+    % in grad and header consistent
+    sel = myregexp('^M[CLR][xyz][aA]*$', label);
+    type(sel) = {'refmag'};
+    sel = myregexp('^G[xyz][xyz]A$', label);
+    type(sel) = {'refgrad'};    
   else
     % determine the type on the basis of the channel labels
     % all 4D-BTi MEG channels start with "A" followed by a number
