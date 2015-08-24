2c4d6dbca6d55c3a160921f68c2e268ff06f4e15
Peter Zeidman <peter.zeidman@ucl.ac.uk>
2015-08-24 10:57:35 +0000

    Updated documentation in spm_fmri_concatenate.m
    SVN r6533

--- spm_fmri_concatenate.m
+++ spm_fmri_concatenate.m
@@ -1,7 +1,8 @@
 function spm_fmri_concatenate(P, scans)
-% Adjust an SPM.mat which has concatenated sessions to improve accuracy
+% Adjust an SPM.mat which has concatenated sessions.
 % FORMAT spm_post_concatenate(P, scans)
-% The high pass filter will be re-specified as if sessions are separate.
+% Session regressors are added and the high-pass filter and non-sphericity
+% estimates adjusted as if sessions are separate.
 %
 % P     - filename of the SPM.mat file to adjust
 % scans - [1 x n] vector with the original number of scans in each session
@@ -14,10 +15,6 @@ function spm_fmri_concatenate(P, scans)
 %
 % Tips:
 %
-% - The session regressors should be binary vectors. E.g. a regressor named
-%   'sess1' would include 1s for every scan in session 1 and zeros
-%   elsewhere.
-%
 % - The BOLD-response may overhang from one session to the next. To reduce
 %   this, acquire additional volumes at the end of each session and / or
 %   add regressors to model the trials at the session borders.
@@ -25,7 +22,7 @@ function spm_fmri_concatenate(P, scans)
 % Copyright (C) 2015 Wellcome Trust Centre for Neuroimaging
 
 % Guillaume Flandin & Peter Zeidman
-% $Id: spm_fmri_concatenate.m 6495 2015-07-07 14:56:18Z peter $
+% $Id: spm_fmri_concatenate.m 6533 2015-08-24 10:57:35Z peter $
 
 
 %-Input parameters
