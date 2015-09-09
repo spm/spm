2b6522064eaa71dd8deede9d2263b22a7518bc99
Karl Friston <k.friston@ucl.ac.uk>
2015-09-09 11:46:56 +0000

    Upgrade of hyper-prior optimisation for PEB - BMC
    SVN r6541

--- spm_dcm_peb_rnd_search.m
+++ spm_dcm_peb_rnd_search.m
@@ -1,6 +1,6 @@
-function [HP] = spm_dcm_peb_rnd_search(DCM,M,field)
+function [BMC,M] = spm_dcm_peb_rnd_search(DCM,M,field)
 % Re-randomisation testing for empirical Bayes and DCM
-% FORMAT [HP] = spm_dcm_peb_rnd_search(DCM,M,field)
+% FORMAT [BMC,M] = spm_dcm_peb_rnd_search(DCM,M,field)
 %
 % DCM   - {N x 1} structure DCM array of (M) DCMs from (N) subjects
 % -------------------------------------------------------------------
@@ -19,31 +19,40 @@ function [HP] = spm_dcm_peb_rnd_search(DCM,M,field)
 % field  - parameter fields in DCM{i}.Ep to optimise [default: {'A','B'}]
 %          'All' will invoke all fields
 % 
-% HP     - maximum entropy hyperprior (precision 1/M.hC)
+% BMC    - Bayesian model comparison structure 
+% -------------------------------------------------------------
+%     BMC.F    - free energy over joint model space
+%     BMC.P    - posterior probability over models
+%     BMC.Px   - posterior probability over 1st level models
+%     BMC.Pw   - posterior probability over 2nd level models
+%     BMC.M    - second level model
+%     BMC.K    - model space
 %__________________________________________________________________________
 %
 % This routine calls spm_dcm_peb_rnd to assess the distribution of log Bayes
 % factors for different hyperpriors on between subject precision. It is
 % assumed that the best hyperpriors maximise the entropy of the null
-% distribution of ensuing p-values
+% distribution of ensuing p-values. This type of prior is then used to
+% perform Bayesian model comparison. The optimised priors are in the second
+% level model (M.hE, M.hC) in the output arguments.
 %
 % See also: spm_dcm_peb_rnd.m and spm_dcm_loo.m
 %__________________________________________________________________________
 % Copyright (C) 2015 Wellcome Trust Centre for Neuroimaging
  
 % Karl Friston
-% $Id: spm_dcm_peb_rnd_search.m 6474 2015-06-06 10:41:55Z karl $
+% $Id: spm_dcm_peb_rnd_search.m 6541 2015-09-09 11:46:56Z karl $
 
 
 % Set up
 %==========================================================================
-hP    = 4:4:32;
-M.hE  = 0;
-M.N   = 128;
+hE    = linspace(-4,4,8);
+M.hC  = 1/32;
+M.N   = 64;
 bins  = 1/40:1/20:1;
-for i = 1:numel(hP)
+for i = 1:numel(hE)
     rng('default');
-    M.hC     = 1/hP(i);
+    M.hE     = hE(i);
     [p,P,f]  = spm_dcm_peb_rnd(DCM,M,field);
     E(i)     = p;
     F(i)     = 1 - f;
@@ -58,21 +67,25 @@ S     = -sum(H.*log(H + 1e-6));
 
 % repeat with maximum entropy hyperprior
 %--------------------------------------------------------------------------
-HP    = hP(i);
-M.hC  = 1/hP(i);
+M.hE  = hE(i);
+M.N   = 128;
 spm_dcm_peb_rnd(DCM,M,field);
 
 subplot(3,2,3)
-imagesc(hP,bins,H),    hold on
-plot(hP,H(end,:),'w'), hold on
-plot([hP(1) hP(end)],[1 1]/20,':w'), hold off
+imagesc(hE,bins,H),    hold on
+plot(hE,H(end,:),'w'), hold on
+plot([hE(1) hE(end)],[1 1]/20,':w'), hold off
 xlabel('hyperprior'), ylabel('p-value')
 title('Null distribution of p-values','FontSize',16)
 
 subplot(3,2,5)
-plot(hP,E,  'b',[hP(1) hP(end)],[1 1]/20,     ':b'), hold on
-plot(hP,F,'-.b',[hP(1) hP(end)],[1 1]*log(20),':r'), hold on
-plot(hP,S,  'r',[hP(i) hP(i)  ],[0 s],        ':r'), hold off
+plot(hE,E,  'b',[hE(1) hE(end)],[1 1]/20,     ':b'), hold on
+plot(hE,F,'-.b',[hE(1) hE(end)],[1 1]*log(20),':r'), hold on
+plot(hE,S,  'r',[hE(i) hE(i)  ],[0 s],        ':r'), hold off
 xlabel('hyperprior'), ylabel('p-value and entropy')
-title('p-value and (null) entropy','FontSize',16)
+title('p-values and entropy','FontSize',16)
+
+% Bayesian model comparison (at first and second levels)
+%--------------------------------------------------------------------------
+BMC   = spm_dcm_bmc_peb(DCM,M,field);
 
