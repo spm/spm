d7369a9a5deab783d4b897047c7021f615925cd2
Guillaume Flandin <g.flandin@ucl.ac.uk>
2015-12-03 18:58:05 +0000

    New feature: rewrote spm_load to handle delimiter-separated values files (csv,tsv).
    SVN r6624

--- spm_load.m
+++ spm_load.m
@@ -1,46 +1,119 @@
-function [x] = spm_load(f)
-% function to load ascii file data as matrix
-% FORMAT [x] = spm_load(f)
-% f  - file {ascii file containing a regular array of numbers
-% x  - corresponding data matrix
-%_______________________________________________________________________
-% Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
+function x = spm_load(f)
+% Load text and numeric data from file
+% FORMAT x = spm_load(f)
+% f  - filename {txt,mat,csv,tsv,json}
+% x  - corresponding data array or structure
+%__________________________________________________________________________
+% Copyright (C) 1995-2015 Wellcome Trust Centre for Neuroimaging
 
-% Karl Friston
-% $Id: spm_load.m 1143 2008-02-07 19:33:33Z spm $
-
+% Guillaume Flandin
+% $Id: spm_load.m 6624 2015-12-03 18:58:05Z guillaume $
 
 
 %-Get a filename if none was passed
-%-----------------------------------------------------------------------
-x  = [];
-if nargin == 0
-    [f,p] = uigetfile({'*.mat';'*.txt';'*.dat'});
-    try
-        f     = fullfile(p,f);
-    end
-end
-
-%-Load the data file into double precision matrix x
-%-----------------------------------------------------------------------
-try
-    x = load(f,'-ascii');
-    return
-end
-try
-    x = load(f,'-mat');
-    x = getdata(x);
-end
-
-if ~isnumeric(x), x = []; end
-
-function x = getdata(s)
-% get numberic data x from the fields of structure s
 %--------------------------------------------------------------------------
-x = [];
-f = fieldnames(s);
-for i = 1:length(f)
-    x = s.(f{i});
-    if isnumeric(x),return;         end
-    if isstruct(x), x = getdata(x); end
+if ~nargin
+    [f,sts] = spm_select(1,{...
+        'mat',...                        % *.txt, *.mat
+        '^.*\.csv$','^.*\.csv.gz$',...   % *.csv, *.csv.gz
+        '^.*\.tsv$','^.*\.tsv.gz$',...   % *.tsv, *.tsv.gz
+        '^.*\.json$','^.*\.json.gz$',... % *.json, *.json.gz
+        });
+    if ~sts, x = []; return; end
+end
+
+if ~exist(f,'file')
+    error('Unable to read file ''%s''',f);
+end
+
+%-Load the data file
+%--------------------------------------------------------------------------
+switch spm_file(f,'ext')
+    case 'txt'
+        x = load(f,'-ascii');
+    case 'mat'
+        x  = load(f,'-mat');
+        fn = fieldnames(x);
+        if numel(fn) == 1 && isnumeric(x.(fn{1}))
+            x = x.(fn{1});
+        end
+    case 'csv'
+        try
+            x = csvread(f);
+        catch
+            x = dsvread(f,',');
+        end
+    case 'tsv'
+        try
+            x = dlmread(f,'\t');
+        catch
+            x = dsvread(f,'\t');
+        end
+    case 'json'
+        x = spm_jsonread(f);
+        % check if x only contains numeric and, if so, return array
+    case 'gz'
+        fz  = gunzip(f,tempname);
+        sts = true;
+        try
+            x   = spm_load(fz{1});
+        catch
+            sts = false;
+        end
+        delete(fz{1});
+        rmdir(spm_file(fz{1},'path'));
+        if ~sts, error('Cannot load ''%s''.',f); end
+    otherwise
+        try
+            x = load(f);
+        catch
+            error('Unknown file format.');
+        end
+end
+
+
+%==========================================================================
+function x = dsvread(f,delim)
+% Read delimiter-separated values file containing a header line
+% 'n/a' fields are replaced with NaN
+
+if nargin < 2, delim = '\t'; end
+delim = sprintf(delim);
+eol = sprintf('\n');
+
+S   = fileread(f);
+if isempty(S), x = []; return; end
+if S(end) ~= eol, S = [S eol]; end
+S   = regexprep(S,{'\r\n','(\n)\1+'},{'\n','$1'});
+h   = find(S == eol,1);
+hdr = S(1:h-1);
+var = regexp(hdr,delim,'split');
+try
+    var = genvarname(var);
+catch
+    var = matlab.lang.makeValidName(var);
+    var = matlab.lang.makeUniqueStrings(var);
+end
+N   = numel(var);
+S   = S(h+1:end);
+
+d = textscan(S,'%s','Delimiter',delim);
+if rem(numel(d{1}),N), error('Varying number of delimiters per line.'); end
+d = reshape(d{1},N,[])';
+for i=1:numel(var)
+    sts = true;
+    dd = zeros(size(d,1),1);
+    for j=1:size(d,1)
+        if strcmp(d{j,i},'n/a')
+            dd(j) = NaN;
+        else
+            dd(j) = str2double(d{j,i});
+            if isnan(dd(j)), sts = false; break; end
+        end
+    end
+    if sts
+        x.(var{i}) = dd;
+    else
+        x.(var{i}) = d(:,i);
+    end
 end
