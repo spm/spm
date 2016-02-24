0fbed1610c49c22a7ed390346802ab19b8d07116
Vladimir Litvak <v.litvak@ucl.ac.uk>
2016-02-24 15:47:23 +0000

    Fix to also read fiducials from sfp files
    SVN r6733

--- external/fieldtrip/fileio/ft_read_headshape.m
+++ external/fieldtrip/fileio/ft_read_headshape.m
@@ -880,6 +880,11 @@ switch fileformat
 	case 'besa_sfp'
 		[lab, pos] = read_besa_sfp(filename, 0);
 		shape.pos = pos;
+        hs = strmatch('headshape', lab);
+        lab(hs) = [];
+        pos(hs, :) = [];
+        shape.fid.label = lab;
+        shape.fid.pos = pos;
 		
   case 'asa_elc'
     elec = ft_read_sens(filename);
