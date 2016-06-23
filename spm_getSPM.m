c0dff1f78c8fa1f42070ff97f10afa7d83680f84
Guillaume Flandin <g.flandin@ucl.ac.uk>
2016-06-23 12:12:47 +0000

    Enhancement: don't block in command line mode if model has not been estimated.
    SVN r6820

--- spm_getSPM.m
+++ spm_getSPM.m
@@ -179,10 +179,10 @@ function [SPM,xSPM] = spm_getSPM(varargin)
 % see spm_results_ui.m for further details of the SPM results section.
 % see also spm_contrasts.m
 %__________________________________________________________________________
-% Copyright (C) 1999-2014 Wellcome Trust Centre for Neuroimaging
+% Copyright (C) 1999-2016 Wellcome Trust Centre for Neuroimaging
 
 % Andrew Holmes, Karl Friston & Jean-Baptiste Poline
-% $Id: spm_getSPM.m 6619 2015-12-01 19:20:22Z guillaume $
+% $Id: spm_getSPM.m 6820 2016-06-23 12:12:47Z guillaume $
 
 
 %-GUI setup
@@ -225,17 +225,10 @@ cd(SPM.swd);
 try
     SPM.xVol.S;
 catch
-    
-    %-Check the model has been estimated
-    %----------------------------------------------------------------------
-    str = { 'This model has not been estimated.';...
-            'Would you like to estimate it now?'};
-    if spm_input(str,1,'bd','yes|no',[1,0],1)
-        SPM = spm_spm(SPM);
-    else
-        SPM = []; xSPM = [];
-        return
-    end
+    spm('alert*',{'This model has not been estimated.','',...
+        fullfile(swd,'SPM.mat')}, mfilename, [], ~spm('CmdLine'));
+    SPM = []; xSPM = [];
+    return
 end
 
 xX   = SPM.xX;                      %-Design definition structure
