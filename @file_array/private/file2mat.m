fab95f1750b68d3910efc8825aa6ae99fbe9dd81
Guillaume Flandin <g.flandin@ucl.ac.uk>
2017-01-16 12:38:29 +0000

    Bug fix: fix overflow when using linear indexing (part 1).
    dat        = file_array;
    dat.fname  = 'largefile.nii';
    dat.dim    = [228 228 96 576];
    dat.offset = ceil(348/8)*8;
    
    N = nifti;
    N.dat = dat;
    create(N,true);
    
    for i=1:dat.dim(4)
        dat(:,:,:,i) = i/2;
    end
    
    V = spm_vol(dat.fname);
    for i=1:dat.dim(4), a(i) = spm_data_read(V(i),prod(dat.dim(1:3))); end
    for i=1:dat.dim(4), b(i) = spm_data_read(V(i),'xyz',dat.dim(1:3)'); end
    figure,plot(a);hold on;plot(b,'r')
    
    SVN r6988

--- @file_array/private/file2mat.m
+++ @file_array/private/file2mat.m
@@ -2,7 +2,7 @@ function val = file2mat(a,varargin)
 % Function for reading from file_array objects.
 % FORMAT val = file2mat(a,ind1,ind2,ind3,...)
 % a      - file_array object
-% indx   - indices for dimension x (int32)
+% indx   - indices for dimension x (int64)
 % val    - the read values
 %
 % This function is normally called by file_array/subsref
@@ -10,7 +10,7 @@ function val = file2mat(a,varargin)
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
 % John Ashburner
-% $Id: file2mat.m 1530 2008-04-30 19:28:22Z guillaume $
+% $Id: file2mat.m 6988 2017-01-16 12:38:29Z guillaume $
 
 %-This is merely the help file for the compiled routine
 error('file2mat.c not compiled - see Makefile');
