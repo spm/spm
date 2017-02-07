facf292ca030c624c47dee51ef60fd65a7346fe6
Vladimir Litvak <v.litvak@ucl.ac.uk>
2017-02-07 18:15:30 +0000

    Trying to fix problems with plotting sensors
    SVN r7010

--- spm_eeg_inv_checkforward.m
+++ spm_eeg_inv_checkforward.m
@@ -5,7 +5,7 @@ function spm_eeg_inv_checkforward(varargin)
 % Copyright (C) 2008-2014 Wellcome Trust Centre for Neuroimaging
 
 % Vladimir Litvak
-% $Id: spm_eeg_inv_checkforward.m 6965 2016-12-08 13:47:06Z vladimir $
+% $Id: spm_eeg_inv_checkforward.m 7010 2017-02-07 18:15:30Z vladimir $
 
 
 %-SPM data structure
@@ -69,12 +69,12 @@ hold on
 
 ft_plot_vol(vol, 'edgecolor', [0 0 0], 'facealpha', 0);
 
-if ft_senstype(sens, 'eeg')
-    ft_plot_sens(sens, 'style', '*g', 'coil', true);
-else
-    try
-    ft_plot_sens(sens, 'style', '*g');
-    end
+hold on
+
+try
+    ft_plot_sens(sens, 'style', '*', 'edgecolor', 'g', 'coil', ft_senstype(sens, 'eeg'));
+catch
+    ft_plot_sens(sens, 'edgecolor', 'g', 'coilshape', 'point', 'coil', true);
 end
 
 rotate3d on;
