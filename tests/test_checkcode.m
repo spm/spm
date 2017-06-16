d0bd0716d3d5029dc63316ff9175eaec52c58619
Guillaume Flandin <g.flandin@ucl.ac.uk>
2017-06-16 09:01:09 +0000

    Compatibility: movie2avi/VideoWriter and wavwrite/audiowrite.
    SVN r7111

--- tests/test_checkcode.m
+++ tests/test_checkcode.m
@@ -3,24 +3,40 @@ function tests = test_checkcode
 %__________________________________________________________________________
 % Copyright (C) 2017 Wellcome Trust Centre for Neuroimaging
 
-% $Id: test_checkcode.m 7107 2017-06-14 09:31:40Z guillaume $
+% $Id: test_checkcode.m 7111 2017-06-16 09:01:09Z guillaume $
 
 tests = functiontests(localfunctions);
 
+
 function test_checkcode_all(testCase)
+testCase.assumeTrue(strcmp(spm_check_version,'matlab'));
 
 f = cellstr(spm_select('FPlistRec',spm('Dir'),'\.m$')); % discard external?
 s = checkcode(f,'-id');
-check = false(size(s));
+checked = false(size(s));
 for i=1:numel(s)
     for j=1:numel(s{i})
-        if isequal(s{i}(j).id,'SYNER'); % Syntax error
-            check(i) = true;
+        if isequal(s{i}(j).id,'SYNER') % Syntax error
+            checked(i) = true;
         end
     end
 end
-testCase.verifyFalse(any(check));
-if any(check)
+testCase.verifyFalse(any(checked));
+if any(checked)
     fprintf('Problematic files are:\n');
-    disp(f(check));
+    disp(f(checked));
+end
+
+
+function t_e_s_t_analyzeCodeCompatibility(testCase) % disabled for now
+testCase.assumeFalse(~strcmp(spm_check_version,'matlab') | ...
+    spm_check_version('matlab','9.3') < 0);
+    
+import matlab.unittest.constraints.*
+r = analyzeCodeCompatibility(spm('Dir'),'IncludeSubfolders',true); % discard external?
+idx = find(r.Recommendations.Severity == 'Error');
+testCase.verifyThat(numel(idx), IsEqualTo(0));
+for i=1:numel(idx)
+    fprintf('%s:\n',r.Recommendations.File(idx(i)));
+    fprintf('  %s\n',r.Recommendations.Description(idx(i)));
 end
