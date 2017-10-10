1040e7127cdb32ca40b03b486f28250ca90b2e2a
Guillaume Flandin <g.flandin@ucl.ac.uk>
2017-10-10 10:12:38 +0000

    Doc: minor adjustments.
    SVN r7184

--- toolbox/MEEGtools/spm_opm_convert.m
+++ toolbox/MEEGtools/spm_opm_convert.m
@@ -1,27 +1,16 @@
-function Dout= spm_opm_convert(array,fnamedat,fs,scale)
-% Converts array into SPM MEEG object
+function Dout = spm_opm_convert(array,fnamedat,fs,scale)
+% Convert array into SPM MEEG object
 % FORMAT Dout = spm_opm_convert(array,fnamedat,fs,scale)
 %
-% array        - A numeric Array of 2,3,4 dimensions(channels,time,trials)
-% fnamedat     - String specifing output path of object(include extension .dat)
-% fs           - Specify sampling frequency
-% scale        - Scale factor to convert to fT (defaults to 1)
-% _________________________________________________________________________
-%     Copyright (C) <2017>  <Tim Tierney>
-%     This program is free software; you can redistribute it and/or modify
-%     it under the terms of the GNU General Public License s published by
-%     the Free Software Foundation; either version 2 of the License, or
-%     (at your option) any later version.
-% 
-%     This program is distributed in the hope that it will be useful,
-%     but WITHOUT ANY WARRANTY; without even the implied warranty of
-%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%     GNU General Public License for more details.
-%
-%     You should have received a copy of the GNU General Public License
-%     along with this program; if not, write to the Free Software
-%     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+% array       - numeric Array of 2,3,4 dimensions(channels,time,trials)
+% fnamedat    - string specifing output path of object(include extension .dat)
+% fs          - sampling frequency
+% scale       - scale factor to convert to fT [default: 1]
 %__________________________________________________________________________
+% Copyright (C) 2017 Tim Tierney
+
+% Tim Tierney
+% $Id: spm_opm_convert.m 7184 2017-10-10 10:12:38Z guillaume $
 
 % determine output filename
 [a, b] = fileparts(fnamedat);
@@ -36,12 +25,12 @@ array = array.*scale;
 
 % find number of dimensions and decide what to do based on result
 dim = size(array);
-L = length(dim);
+L   = length(dim);
 
-if L>4
+if L > 4
     % throw error for having unsupported number of dimensions
-    ME = MException('array should not have more than 4 dimensions');
-elseif L ==4
+    error('Array should not have more than 4 dimensions.');
+elseif L == 4
     % create MEG object with appropriate size
     Dout = meeg(dim(1),dim(2),dim(3),dim(4));
     
@@ -53,25 +42,24 @@ elseif L ==4
     
     % fill in data with supplied array
     Dout(1:dim(1),1:dim(2),1:dim(3),1:dim(4)) = array;
-elseif L ==3
+elseif L == 3
     % same with less dimensions
     Dout = meeg(dim(1),dim(2),dim(3));
     Dout= blank(Dout,fnamedat);
     Dout = Dout.fsample(fs);
     Dout(1:dim(1),1:dim(2),1:dim(3)) = array;
     
-elseif L ==2 
+elseif L == 2 
     %same with less dimensions
     Dout = meeg(dim(1),dim(2),1);
     Dout= blank(Dout,fnamedat);
     Dout = Dout.fsample(fs);
     Dout(1:dim(1),1:dim(2),1) = array;
 else
-    % throw exception if  I really don't know what to do
-    ME = MException('array must have between 2 and  4 dimensions');
+    % throw exception if I really don't know what to do
+    error('Array must have between 2 and  4 dimensions.');
 end
 
 % set the filename and save
 Dout = fname(Dout,outMat);
-Dout.save
-end
+Dout.save;
--- toolbox/MEEGtools/spm_opm_denoise.m
+++ toolbox/MEEGtools/spm_opm_denoise.m
@@ -1,39 +1,26 @@
-function Dout= spm_opm_denoise(D,refD,derivative,gs,update,prefix)
-% Denoises OPM data with specified regressors, optionally with derivatives
+function Dout = spm_opm_denoise(D,refD,derivative,gs,update,prefix)
+% Denoise OPM data with specified regressors, optionally with derivatives
 % and global signal
-% FORMAT Dout - spm_opm_denoise(D,refD,derivative,gs,update,prefix)
+% FORMAT Dout = spm_opm_denoise(D,refD,derivative,gs,update,prefix)
 %
-% D            - MEEG object
-% refD         - array or MEEG object containing regresssors for denoising.
-% derivative   - boolean indicating whether to use derivatives or not.
-% gs           - boolean indicating whether to use Global Signal or not.
-% update       - boolean indicating whether to create MEEG object.
-% prefix       - string to prefix file path with if update is TRUE.
-% _________________________________________________________________________
-% Copyright (C) <2017>  <Tim Tierney>
-%     This program is free software; you can redistribute it and/or modify
-%     it under the terms of the GNU General Public License as published by
-%     the Free Software Foundation; either version 2 of the License, or
-%     (at your option) any later version.
-%
-%     This program is distributed in the hope that it will be useful,
-%     but WITHOUT ANY WARRANTY; without even the implied warranty of
-%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%     GNU General Public License for more details.
-%
-%     You should have received a copy of the GNU General Public License
-%     along with this program; if not, write to the Free Software
-%     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+% D           - MEEG object
+% refD        - array or MEEG object containing regresssors for denoising
+% derivative  - boolean indicating whether to use derivatives or not
+% gs          - boolean indicating whether to use Global Signal or not
+% update      - boolean indicating whether to create MEEG object
+% prefix      - string to prefix file path with if update is TRUE
 %__________________________________________________________________________
+% Copyright (C) 2017 Tim Tierney
 
+% Tim Tierney
+% $Id: spm_opm_denoise.m 7184 2017-10-10 10:12:38Z guillaume $
 
 
 % check to make sure dimensions match between reference and MEEG object
 ndim = size(D);
 ldim = size(refD);
 if (~all(ndim(2:3)==ldim(2:3)))
-    ME = MException('Dimensions of D and refD should be equal');
-    throw(ME)
+    error('Dimensions of D and refD should be equal.');
 end
 
 
@@ -47,21 +34,21 @@ Ntrials = size(sensors,3);
 winSize = size(sensors,2);
 
 % loop (inefficient due to continued memory reallocation but ...)
-for i =1:Ntrials
+for i=1:Ntrials
      % add a mean column to the reference regressors
      intercept = ones(winSize,1);
      reference = ref(:,:,i)';
-     reference=[reference intercept];
+     reference = [reference intercept];
     
      % optionally add derivatives
-     if(derivative)
+     if (derivative)
         drefer=diff(reference);
         drefer=[drefer(1,:); drefer];
         reference=[drefer reference];
      end
      
     % optionally add global signal 
-    if(gs)
+    if (gs)
         trial = D(:,:,i)';
         gsrefer =  mean(trial,2);
         reference=[gsrefer reference];
@@ -79,15 +66,13 @@ else
 end
 
 
-if(update)
+if (update)
     % name, clone and fill with residual data 
-    inFile = fnamedat(D);
-    [a b]=fileparts(inFile);
+    inFile  = fnamedat(D);
+    [a,b]   = fileparts(inFile);
     outfile = fullfile(a,[prefix b '.dat']);
-    Dout = clone(D,outfile,outsize);
+    Dout    = clone(D,outfile,outsize);
     Dout(:,:,:) = res;
 else
     Dout = res;
-end
-
 end
