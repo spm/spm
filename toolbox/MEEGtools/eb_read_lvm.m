946b0c51fbe182f241ff16d37ac829a6c248a8d9
Gareth Barnes <g.barnes@ucl.ac.uk>
2017-11-24 13:47:09 +0000

    update opm code
    SVN r7231

--- toolbox/MEEGtools/eb_read_lvm.m
+++ toolbox/MEEGtools/eb_read_lvm.m
@@ -1,9 +1,15 @@
-function [time, B] = eb_read_lvm(filename)
+function [time, B] = eb_read_lvm(filename,timeind)
 if num2str(filename(end-3:end)) == '.lvm'
 else
     filename = [filename,'.lvm'];
 end
+if nargin<2,
+    timeind=[];
+end;
+if isempty(timeind),
+    timeind=1; %% default 
+end;
 data = dlmread(filename, '\t',23,0);
 % data = dlmread(filename, '\t',7,0);
-time = data(:,1);
+time = data(:,timeind);
 B = data(:,2:end);
--- toolbox/MEEGtools/spm_opm_denoise.m
+++ toolbox/MEEGtools/spm_opm_denoise.m
@@ -1,62 +1,82 @@
-function Dout = spm_opm_denoise(D,refD,derivative,gs,update,prefix)
-% Denoise OPM data with specified regressors, optionally with derivatives
-% and global signal
-% FORMAT Dout = spm_opm_denoise(D,refD,derivative,gs,update,prefix)
+function Dout= spm_opm_denoise(D,refD,derivative,gs,update,prefix)
+% Denoises OPM data with specified regressors, optionally with derivatives
+% and global signal.
+% FORMAT Dout - spm_opm_denoise(D,refD,derivative,gs,update,prefix)
 %
-% D           - MEEG object
-% refD        - array or MEEG object containing regresssors for denoising
-% derivative  - boolean indicating whether to use derivatives or not
-% gs          - boolean indicating whether to use Global Signal or not
-% update      - boolean indicating whether to create MEEG object
-% prefix      - string to prefix file path with if update is TRUE
+% D            - MEEG object
+% refD         - array or MEEG object containing regresssors for denoising.
+% derivative   - boolean indicating whether to use derivatives or not.
+% gs           - boolean indicating whether to use Global Signal or not.
+% update       - boolean indicating whether to create MEEG object.
+% prefix       - string to prefix file path with if update is TRUE.
+%% modfied by GRB to only denoise MEG channels
+% _________________________________________________________________________
+% Copyright (C) <2017>  <Tim Tierney>
+%     This program is free software; you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation; either version 2 of the License, or
+%     (at your option) any later version.
+%
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+%
+%     You should have received a copy of the GNU General Public License
+%     along with this program; if not, write to the Free Software
+%     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 %__________________________________________________________________________
-% Copyright (C) 2017 Tim Tierney
-
-% Tim Tierney
-% $Id: spm_opm_denoise.m 7184 2017-10-10 10:12:38Z guillaume $
 
 
+% $Id: spm_opm_denoise.m 7231 2017-11-24 13:47:09Z gareth $
 % check to make sure dimensions match between reference and MEEG object
 ndim = size(D);
 ldim = size(refD);
 if (~all(ndim(2:3)==ldim(2:3)))
-    error('Dimensions of D and refD should be equal.');
+    ME = MException('Dimensions of D and refD should be equal');
+    throw(ME)
 end
 
 
 % ref, sensors and residual objects 
 ref = refD;
 sensors = D;
-res = zeros(size(sensors));
+
 
 % need to loop over Ntrials of size winSize
 Ntrials = size(sensors,3);
 winSize = size(sensors,2);
+%% only want to denoise MEG channels (not trigs or references)
+megind=D.indchantype('MEG');
+megres = zeros(size(sensors(megind,:,:)));
 
 % loop (inefficient due to continued memory reallocation but ...)
-for i=1:Ntrials
+for i =1:Ntrials
      % add a mean column to the reference regressors
      intercept = ones(winSize,1);
      reference = ref(:,:,i)';
-     reference = [reference intercept];
+     reference=[reference intercept];
     
      % optionally add derivatives
-     if (derivative)
+     if(derivative)
         drefer=diff(reference);
         drefer=[drefer(1,:); drefer];
         reference=[drefer reference];
      end
      
     % optionally add global signal 
-    if (gs)
+    if(gs)
         trial = D(:,:,i)';
         gsrefer =  mean(trial,2);
         reference=[gsrefer reference];
     end
     % reference is column major so transpose sensors
-    beta = pinv(reference)*sensors(:,:,i)';
-    res(:,:,i) = (sensors(:,:,i)'- reference*beta)';
+    beta = pinv(reference)*sensors(megind,:,i)';
+    megres(:,:,i) = (sensors(megind,:,i)'- reference*beta)';
 end
+
+res=sensors(:,:,:); %% new file will only have MEG sensors changed
+res(megind,:,:)=megres;
 
 % make sure output has the singleton dimension if matrix supplied
 if ((length(size(res)))==2)
@@ -66,13 +86,15 @@ else
 end
 
 
-if (update)
+if(update)
     % name, clone and fill with residual data 
-    inFile  = fnamedat(D);
-    [a,b]   = fileparts(inFile);
+    inFile = fnamedat(D);
+    [a b]=fileparts(inFile);
     outfile = fullfile(a,[prefix b '.dat']);
-    Dout    = clone(D,outfile,outsize);
+    Dout = clone(D,outfile,outsize);
     Dout(:,:,:) = res;
 else
     Dout = res;
+end
+
 end
