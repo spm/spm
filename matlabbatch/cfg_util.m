aacdfd85861c4be13fa071f2d993fad5e72adc9b
Volkmar Glauche <volkmar.glauche@uniklinik-freiburg.de>
2018-06-15 12:44:45 +0000

    Allow a mix of job files and job variables to be initialised.
    SVN r7343

--- matlabbatch/cfg_util.m
+++ matlabbatch/cfg_util.m
@@ -423,9 +423,9 @@ function varargout = cfg_util(cmd, varargin)
 % Copyright (C) 2007 Freiburg Brain Imaging
 
 % Volkmar Glauche
-% $Id: cfg_util.m 7341 2018-06-15 12:44:43Z volkmar $
+% $Id: cfg_util.m 7343 2018-06-15 12:44:45Z volkmar $
 
-rev = '$Rev: 7341 $';
+rev = '$Rev: 7343 $';
 
 %% Initialisation of cfg variables
 % load persistent configuration data, initialise if necessary
@@ -762,10 +762,15 @@ switch lower(cmd),
         else
             if ischar(varargin{1}) || iscellstr(varargin{1})
                 [job, jobdedup] = cfg_load_jobs(varargin{1});
-            elseif iscell(varargin{1}) && iscell(varargin{1}{1})
+            elseif iscell(varargin{1})
                 % try to initialise cell array of jobs
-                job = varargin{1};
-                jobdedup = NaN; % Postpone duplicate detection
+                job = cell(size(varargin{1}));
+                jobdedup = NaN*ones(size(varargin{1})); % Postpone duplicate detection
+                jfiles = cellfun(@ischar, varargin{1});
+                [job(jfiles), jobdedup(jfiles)] = cfg_load_jobs(varargin{1}(jfiles));
+                for cj = find(~jfiles)
+                    job{cj} = varargin{1}(cj);
+                end
             else
                 % try to initialise single job
                 job{1} = varargin{1};
@@ -777,13 +782,17 @@ switch lower(cmd),
                 isjob(k) = iscell(job{k}) && all(cellfun('isclass', job{k}, 'struct'));
             end
             job = job(isjob);
+            jobdedup = jobdedup(isjob);
             if isempty(job)
                 cfg_message('matlabbatch:initialise:invalid','No valid job.');
             else
                 if any(isnan(jobdedup))
                     % build up list of unique jobs
-                    jobdedup = NaN*ones(size(job));
-                    cu = 0;
+                    if any(~isnan(jobdedup))
+                        cu = max(jobdedup(~isnan(jobdedup)));
+                    else
+                        cu = 0;
+                    end
                     for k = 1:numel(job)
                         if isnan(jobdedup(k))
                             % found new candidate
