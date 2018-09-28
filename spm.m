eb9119a9aea4dc76fa9dbab29157aeff1b04b948
Guillaume Flandin <g.flandin@ucl.ac.uk>
2018-09-28 14:35:27 +0000

    Refactoring: own function for SPM's menu.
    SVN r7432

--- spm.m
+++ spm.m
@@ -53,7 +53,7 @@ function varargout=spm(varargin)
 % Copyright (C) 1991,1994-2018 Wellcome Trust Centre for Neuroimaging
 
 % Andrew Holmes
-% $Id: spm.m 7372 2018-07-09 16:50:44Z guillaume $
+% $Id: spm.m 7432 2018-09-28 14:35:27Z guillaume $
 
 
 %=======================================================================
@@ -373,7 +373,7 @@ case 'chmod'                      %-Change SPM modality PET<->fMRI<->EEG
 %-Sort out arguments
 %-----------------------------------------------------------------------
 if nargin<2, Modality = ''; else Modality = varargin{2}; end
-[Modality,ModNum] = spm('CheckModality',Modality);
+Modality = spm('CheckModality',Modality);
 
 %-Sort out global defaults
 %-----------------------------------------------------------------------
@@ -381,25 +381,8 @@ spm('defaults',Modality);
 
 %-Sort out visibility of appropriate controls on Menu window
 %-----------------------------------------------------------------------
-Fmenu = spm_figure('FindWin','Menu');
-if ~isempty(Fmenu)
-    if strcmpi(Modality,'PET')
-        set(findobj(Fmenu, '-regexp', 'Tag', 'FMRI'), 'Visible', 'off');
-        set(findobj(Fmenu, '-regexp', 'Tag', 'EEG'),  'Visible', 'off');
-        set(findobj(Fmenu, '-regexp', 'Tag', 'PET'),  'Visible', 'on' );
-    elseif strcmpi(Modality,'FMRI')
-        set(findobj(Fmenu, '-regexp', 'Tag', 'EEG'),  'Visible', 'off');
-        set(findobj(Fmenu, '-regexp', 'Tag', 'PET'),  'Visible', 'off');
-        set(findobj(Fmenu, '-regexp', 'Tag', 'FMRI'), 'Visible', 'on' );
-    else
-        set(findobj(Fmenu, '-regexp', 'Tag', 'PET'),  'Visible', 'off');
-        set(findobj(Fmenu, '-regexp', 'Tag', 'FMRI'), 'Visible', 'off');
-        set(findobj(Fmenu, '-regexp', 'Tag', 'EEG'),  'Visible', 'on' );
-    end
-    set(findobj(Fmenu,'Tag','Modality'),'Value',ModNum,'UserData',ModNum);
-else
-    warning('SPM Menu window not found');
-end
+spm_Menu('Switch',Modality);
+
 
 %=======================================================================
 case 'defaults'                  %-Set SPM defaults (as global variable)
@@ -502,72 +485,7 @@ case 'createmenuwin'                            %-Create SPM menu window
 %=======================================================================
 % Fmenu = spm('CreateMenuWin',Vis)
 %-----------------------------------------------------------------------
-if nargin<2, Vis='on'; else Vis=varargin{2}; end
-
-%-Close any existing 'Menu' 'Tag'ged windows
-%-----------------------------------------------------------------------
-delete(spm_figure('FindWin','Menu'));
-if ~isempty(which('openfig'))
-    Fmenu = openfig(fullfile(spm('Dir'),'spm_Menu.fig'),'new','invisible');
-else
-    fprintf('\nFunction openfig.m is missing. Download it from:\n');
-    fprintf(' https://savannah.gnu.org/bugs/download.php?file_id=33499\n');
-    Fmenu = [];
-end
-set(Fmenu,'name',[spm('Version') ': Menu']);
-S0 = spm('WinSize','0',1);
-SM = spm('WinSize','M');
-set(Fmenu,'Units','pixels', 'Position',[S0(1) S0(2) 0 0] + SM);
-
-%-Set SPM colour
-%-----------------------------------------------------------------------
-set(findobj(Fmenu,'Tag', 'frame'),'backgroundColor',spm('colour'));
-set(Fmenu,'Color',[1 1 1]*.8);
-if ismac
-    set(findobj(Fmenu,'UserData','LABEL'),'Visible','off','Tag','');
-end
-
-%-Set Utils
-%-----------------------------------------------------------------------
-set(findobj(Fmenu,'Tag', 'Utils'), 'String',{'Utils...',...
-    'CD',...
-    'PWD',...
-    'Run M-file',...
-    'Load MAT-file',...
-    'Save MAT-file',...
-    'Delete files',...
-    'Show SPM',...
-    'Show MATLAB'});
-set(findobj(Fmenu,'Tag', 'Utils'), 'UserData',{...
-    ['spm(''FnBanner'',''CD'');' ...
-     'cd(spm_select(1,''dir'',''Select new working directory''));' ...
-     'spm(''alert"'',{''New working directory:'',[''    '',pwd]},''CD'',1);'],...
-    ['spm(''FnBanner'',''PWD'');' ...
-     'spm(''alert"'',{''Present working directory:'',[''    '',pwd]},''PWD'',1);'],...
-    ['spm(''FnBanner'',''Run M-file'');' ...
-     'spm(''Run'');' ...
-     'fprintf(''%-40s: %30s\n'',''Completed'',spm(''time''));'],...
-    ['spm(''FnBanner'',''Load MAT-file'');' ...
-     'load(spm_select(1,''mat'',''Select MAT-file''));'],...
-    ['spm(''FnBanner'',''Save MAT-file'');' ...
-     'save(spm_input(''Output filename'',1,''s''), spm_get_defaults(''mat.format''));'],...
-    ['spm(''FnBanner'',''Delete files'');' ...
-     'spm(''Delete'');'],...
-    ['spm(''FnBanner'',''Show SPM'');' ...
-     'spm(''Show'');'],...
-    ['spm(''FnBanner'',''Show Command Window'');' ...
-     'commandwindow;']});
-
-%-Set Toolboxes
-%-----------------------------------------------------------------------
-xTB       = spm('tbs');
-if ~isempty(xTB)
-    set(findobj(Fmenu,'Tag', 'Toolbox'),'String',{'Toolbox:' xTB.name });
-    set(findobj(Fmenu,'Tag', 'Toolbox'),'UserData',xTB);
-else
-    set(findobj(Fmenu,'Tag', 'Toolbox'),'Visible','off')
-end
-set(Fmenu,'Visible',Vis);
+Fmenu = spm_Menu('Create',varargin{2:end});
 varargout = {Fmenu};
 
 
