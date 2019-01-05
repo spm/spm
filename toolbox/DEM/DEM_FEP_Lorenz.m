15979d4f2e71314a4c0ba069d5d107dc23f872ca
Karl Friston <k.friston@ucl.ac.uk>
2019-01-05 21:13:16 +0000

    Improvements to stochastic chaos demonstration routines
    SVN r7511

--- toolbox/DEM/DEM_FEP_Lorenz.m
+++ toolbox/DEM/DEM_FEP_Lorenz.m
@@ -26,7 +26,7 @@ function DEM_FEP_Lorenz
 % Copyright (C) 2005 Wellcome Trust Centre for Neuroimaging
 
 % Karl Friston
-% $Id: DEM_FEP_Lorenz.m 7502 2018-12-02 12:28:03Z karl $
+% $Id: DEM_FEP_Lorenz.m 7511 2019-01-05 21:13:16Z karl $
 
 % generative model
 %==========================================================================                       % switch for demo
@@ -38,16 +38,27 @@ G.f   = @(x,v,P,G) v(:) + [-P(1) P(1) 0; P(3) -1 -x(1); x(2) 0 P(2)]*x/64;
 
 % set up
 %--------------------------------------------------------------------------
-dt    = 1/64;                       % time step
-T     = 512;                        % length of trajectory
-N     = 2048;                       % number of paths
-U.u   = zeros(T,3);                 % inputs
-P     = [10; -8/3; 28];             % Rayleigh parameter
+ax    = [-20 20 -20 60];              % axis range
+dt    = 1/64;                         % time step
+T     = 512;                          % length of trajectory
+N     = 2^13;                         % number of paths 
+P     = [10; -8/3; 28];               % Rayleigh parameter (28 or 8)
+g     = exp(0);                      % random fluctuations
+
+% initial states
+%--------------------------------------------------------------------------
+U.u   = randn(T,3)*g;
+G.x   = [1; 1; 24];
+x     = spm_int_L(P,G,U);
+i     = T/4:T;
+x0    = mean(x(i,:))';
+s0    = std(x(i,3));
 for k = 1:N
     
     % integrate timeseries with random initial hidden state
     %----------------------------------------------------------------------
-    G.x      = [1; 1; 25 + randn*4];
+    U.u      = randn(T,3)*g;    % random fluctuations
+    G.x      = x0 + [0;0;randn*s0];
     x(:,:,k) = spm_int_L(P,G,U);
     
 end
@@ -59,31 +70,68 @@ c     = cov(squeeze(x(T,:,:))');
 
 % convergence to nonequilibrium state
 %--------------------------------------------------------------------------
-D     = [];
-tt    = fix(linspace(1,T,64));
+tt    = 1:(T - 1);
 for t = 1:numel(tt)
+    
+    % divergence from nonequilibrium steady-state
+    %----------------------------------------------------------------------
     mt   = mean(squeeze(x(tt(t),:,:))');
     ct   = cov(squeeze(x(tt(t),:,:))');
     D(t) = spm_kl_normal(mt,ct,m,c);
+    
+    
+    % information length
+    %----------------------------------------------------------------------
+    mdt   = mean(squeeze(x(tt(t) + 1,:,:))');
+    cdt   = cov(squeeze(x(tt(t) + 1,:,:))');
+    dL(t) = spm_kl_normal(mdt,cdt,mt,ct);
+    
 end
+
+% information length
+%--------------------------------------------------------------------------
+dL = dL.*(dL > exp(-8));
+L  = cumsum(sqrt(dL));
+L  = L - L(end);
 
 % plot ensemble densities
 %--------------------------------------------------------------------------
 td    = fix(linspace(1,T,4));
+b1    = linspace(ax(1),ax(2),128);
+b2    = linspace(ax(3),ax(4),128);
+d1    = b1(2) - b1(1);
+d2    = b2(2) - b2(1);
 for t = 1:4
-    xt = squeeze(x(td(t),:,:))';
-    subplot(3,4,t),plot(xt(:,1),xt(:,3),'.b','MarkerSize',1)
-    axis square, axis([-20 20 0 60])
+    
+    % sample density
+    %----------------------------------------------------------------------
+    xt    = squeeze(x(td(t),:,:))';
+    p     = zeros(numel(b1),numel(b2)) + 1;
+    for i = 1:N
+        i1  = round((xt(i,1) - ax(1))/d1);
+        i2  = round((xt(i,3) - ax(3))/d2);
+        try
+            p(i1,i2) = p(i1,i2) + 1;
+        end
+    end
+    p     = p/sum(p(:));
+    
+    % plot
+    %----------------------------------------------------------------------
+    subplot(3,4,t),imagesc(b1,b2,1- spm_conv(p',2,2))
+    axis square, axis xy
     title('Predictive density')
     xlabel('active state'),ylabel('external state'),drawnow
-    
 end
 
 % and Kulback-Leibler divergence
 %--------------------------------------------------------------------------
 subplot(3,1,2)
-plot(tt*dt,D)
-title('Kulback-Leibler divergence','Fontsize',16)
-xlabel('time (seconds)'),ylabel('divergence (nats)')
+plot(tt*dt,D,tt*dt,L,':b',tt*dt,D*0,'-.b')
+title('Kullback-Leibler divergence','Fontsize',16)
+xlabel('time (seconds)'),ylabel('divergence (nats)'), box off
+set(gca,'YLim',[-1.2,1.2]*min(D(1),128))
+
+legend('KL divergence','Information length'),legend BOXOFF
 
 return
