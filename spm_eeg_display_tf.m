5f0c1c3411ecba9e641de2803ee9ef90afa581d6
Guillaume Flandin <g.flandin@ucl.ac.uk>
2019-10-07 09:07:14 +0000

    New feature: Display for TF images
    SVN r7673

--- spm_eeg_display_tf.m
+++ spm_eeg_display_tf.m
@@ -1,24 +1,24 @@
 function spm_eeg_display_tf(files)
-% Display of TF images saved as NIfTI
-% Up to 6 images are supported 
-% FORMAT spm_eeg_display_tf
+% Display TF images saved as NIfTI
 % FORMAT spm_eeg_display_tf(files)
-%        files - list of images to display 
-%                (as char or cell aray of strings)
+% files  -  list of images to display (as char or cell aray of strings)
+%           Up to 6 images are supported 
 %__________________________________________________________________________
 % Copyright (C) 2019 Wellcome Centre for Human Neuroimaging
 
 % Vladimir Litvak
-% $Id: 
+% $Id: spm_eeg_display_tf.m 7673 2019-10-07 09:07:14Z guillaume $
 
-if nargin == 0
-    files = cellstr(spm_select([1 6], 'image', 'Select TF image files'));%
-elseif isa(files, 'char')
-    files = cellstr(files);
+
+if ~nargin
+    [files, sts] = spm_select([1 6], 'image', 'Select TF image files');
+    if ~sts, return; end
 end
-    
-%%
-Fgraph  = spm_figure('GetWin','Graphics'); spm_figure('Clear',Fgraph);
+files = cellstr(files);
+
+
+Fgraph = spm_figure('GetWin','Graphics');
+spm_figure('Clear',Fgraph);
 
 switch numel(files)
     case 1
@@ -32,19 +32,19 @@ switch numel(files)
     case {5, 6}
         dim = [3 2];
 end
-        
+
 for f = 1:numel(files)
     subplot(dim(1), dim(2), f);
-    n = nifti(files{f});
+    N = nifti(files{f});
     
-    if ~ismatrix(n.dat) || ~all(size(n.dat)>1)
-        error('2D image expected');
+    if numel(N.dat.dim) ~= 2 || ~all(size(N.dat) > 1)
+        error('2D image expected.');
     end
     
-    f = (1:size(n.dat, 1))*n.mat(1,1)+n.mat(1, 4);
-    t = (1:size(n.dat, 2))*n.mat(2,2)+n.mat(2, 4);
+    fr = (1:size(N.dat, 1)) * N.mat(1,1) + N.mat(1, 4);
+    t  = (1:size(N.dat, 2)) * N.mat(2,2) + N.mat(2, 4);
     
-    imagesc(t, f, n.dat(:,:));
+    imagesc(t, fr, N.dat(:,:));
     axis xy;
     xlabel('time (ms)');
     ylabel('frequency (Hz)');
