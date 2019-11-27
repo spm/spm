8b5e0a0c1cc603a847078f14eef63b6448cb0587
Peter Zeidman <peter.zeidman@ucl.ac.uk>
2019-11-27 17:18:56 +0000

    Minor update to NVC (neurovascular coupling): removed unused code from spm_gen_par.m
    SVN r7722

--- toolbox/NVC/spm_gen_par.m
+++ toolbox/NVC/spm_gen_par.m
@@ -17,35 +17,10 @@ function Q = spm_gen_par(P,M,U)
 % Copyright (C) 2019 Wellcome Trust Centre for Neuroimaging
 
 % Amirhossein Jafarian
-% $Id: spm_gen_par.m 7713 2019-11-25 16:00:34Z spm $
+% $Id: spm_gen_par.m 7722 2019-11-27 17:18:56Z peter $
 
 %--------------------------------------------------------------------------
 if nargin < 3, U.X = sparse(1,0); end
-
-% check input u = f(t,P,M) and switch off full delay operator
-%--------------------------------------------------------------------------
-try M.fu; catch, M.fu  = 'spm_erp_u'; end
-try M.ns; catch, M.ns  = 128;         end
-try M.N;  catch, M.N   = 0;           end
-try U.dt; catch, U.dt  = 0.004;       end
-
-% peristimulus time
-%--------------------------------------------------------------------------
-if nargout > 1
-    pst = (1:M.ns)*U.dt - M.ons/1000;
-end
-
-% within-trial (exogenous) inputs
-%--------------------------------------------------------------------------
-if ~isfield(U,'u')    
-    % peri-stimulus time inputs
-    U.u = feval(M.fu,(1:M.ns)*U.dt,P,M);    
-end
-
-if isfield(M,'u')    
-    % remove M.u to preclude endogenous input
-    M = rmfield(M,'u');    
-end
 
 % between-trial (experimental) inputs
 %--------------------------------------------------------------------------
@@ -59,11 +34,7 @@ if ~size(X,1)
     X = sparse(1,0);
 end
 
-% trials
-%--------------------------------------------------------------------------
-y      = cell(size(X,1),1);
-c = P.xc ;
-
 % condition-specific parameters
 %--------------------------------------------------------------------------
-Q   = spm_gen_Q(P,X(c,:));
+c = P.xc;
+Q = spm_gen_Q(P,X(c,:));
