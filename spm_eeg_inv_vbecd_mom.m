daec8d0f920c3de16504160999d7d441ed7ca12a
SPM <fil.spm@ucl.ac.uk>
2020-01-02 15:34:03 +0000

    Doc: SVN properties
    SVN r7764

--- spm_eeg_inv_vbecd_mom.m
+++ spm_eeg_inv_vbecd_mom.m
@@ -1,6 +1,5 @@
 function P = spm_eeg_inv_vbecd_mom(P)
 % Model inversion routine for ECDs using variational Bayesian approach
-%
 % FORMAT P = spm_eeg_inv_vbecd_mom(P)
 %
 % Input:
@@ -25,25 +24,24 @@ function P = spm_eeg_inv_vbecd_mom(P)
 % Reference:
 % Kiebel et al., Variational Bayesian inversion of the equivalent current
 % dipole model in EEG/MEG., NeuroImage, 39:728-741, 2008
-% (Although this algorithm uses a function for general Bayesian inversion of
-% a non-linear model - see spm_nlsi_gn)
+% (Although this algorithm uses a function for general Bayesian inversion
+% of a non-linear model - see spm_nlsi_gn)
 %__________________________________________________________________________
-% Copyright (C) 2009 Wellcome Trust Centre for Neuroimaging
+% Copyright (C) 2020 Wellcome Trust Centre for Neuroimaging
 
 % Gareth Barnes
-% $Id: spm_eeg_inv_vbecd.m 7628 2019-06-27 11:48:39Z gareth $
-
+% $Id: spm_eeg_inv_vbecd_mom.m 7764 2020-01-02 15:34:03Z spm $
 
 
 % unpack model, priors, data
 %--------------------------------------------------------------------------
 
 priormom = P.priors.mom;
-Nd    = length(priormom); %% number sources= number of moments to estimate
-priormomvar  = P.priors.momvar; % diagonal on covariance matrix for moment for each source
+Nd    = length(priormom);   % number sources= number of moments to estimate
+priormomvar = P.priors.momvar; % diagonal on covariance matrix for moment for each source
 
-dippos_ctf = P.dippos_ctf; %% dipole positions, fixed
-dipor_ctf=P.dipor_ctf; %% dipole orientations, fixed.
+dippos_ctf = P.dippos_ctf;  % dipole positions, fixed
+dipor_ctf  = P.dipor_ctf;   % dipole orientations, fixed.
 
 
 y    = P.y;
@@ -52,36 +50,32 @@ Y.y  = y;
 U.u  = 1;
 
 % set random moment vector, scaled by prior variances
-%----------------------------------------------------------------------
-startmoments   = priormom + randn(Nd,1).*sqrt(priormomvar);
+%--------------------------------------------------------------------------
+startmoments = priormom + randn(Nd,1) .* sqrt(priormomvar);
 
 % get lead fields
-%----------------------------------------------------------------------
-M.pE  = priormom;            % prior parameter estimate
-M.pC  = diag(priormomvar);     % prior covariance estimate
+%--------------------------------------------------------------------------
+M.pE = priormom;           % prior parameter estimate
+M.pC = diag(priormomvar);  % prior covariance estimate
 
-M.hE=P.priors.hE;
-M.hC=P.priors.hC;
+M.hE = P.priors.hE;
+M.hC = P.priors.hC;
 
-M.IS  = 'spm_eeg_wrap_momfit_vbecd';
+M.IS = 'spm_eeg_wrap_momfit_vbecd';
 
-M.Setup =P;             % pass volume conductor and sensor locations on
+M.Setup = P;         % pass volume conductor and sensor locations on
 
-M.pos=dippos_ctf; %% source positions
-M.ori=dipor_ctf; %% source orientations
+M.pos = dippos_ctf;  % source positions
+M.ori = dipor_ctf;   % source orientations
 
 
-[starty]=spm_eeg_wrap_momfit_vbecd(startmoments,M,U);
+[starty] = spm_eeg_wrap_momfit_vbecd(startmoments,M,U);
 [Ep,Cp,Eh,F] = spm_nlsi_GN(M,U,Y);
 P.Ep = Ep;
 P.Cp = Cp;
 P.Eh = Eh;
 P.F  = F;
-[P.ypost,outsideflag,leads]=spm_eeg_wrap_momfit_vbecd(P.Ep,M,U);
+[P.ypost,outsideflag,leads] = spm_eeg_wrap_momfit_vbecd(P.Ep,M,U);
 
 P.post_mom = Ep;
 P.post_momvar = Cp;
-
-
-
-
