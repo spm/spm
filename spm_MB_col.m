823d45c616c99b4b2a55d13776a3d66a9a6b16e2
SPM <fil.spm@ucl.ac.uk>
2020-01-07 11:37:19 +0000

    Enhancement: Backward compatibility with rng()
    SVN r7768

--- spm_MB_col.m
+++ spm_MB_col.m
@@ -1,18 +1,20 @@
 function [col,bol,msz] = spm_MB_col(n)
 % FORMAT [col,bol,msz] = spm_MB_col(n)
-% returns colours and market size for number of partitions
+% Return colours and marker size for number of partitions
 % n  - number of partitions
 %__________________________________________________________________________
-% Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
+% Copyright (C) 2019-2020 Wellcome Trust Centre for Neuroimaging
 
 % Karl Friston
-% $Id: spm_MB_col.m 7655 2019-08-25 20:10:20Z karl $
+% $Id: spm_MB_col.m 7768 2020-01-07 11:37:19Z spm $
 
-% Marker color and size
+% Marker colour and size
 %--------------------------------------------------------------------------
-rng(1);
+s = rand('twister');
+rand('twister',1);
 msz   = fix(16 + 64/n);
 for k = 1:n
     bol{k} = spm_softmax(log(rand(3,1))*2);
     col{k} = bol{k}*(1 - 1/2) + ones(3,1)/2;
 end
+rand('twister',s);
