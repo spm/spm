d6fd183835d605637fc24aa448cf2e9ca3197b47
Guillaume Flandin <g.flandin@ucl.ac.uk>
2020-03-13 10:55:45 +0000

    Enhancements: reformating, licence, backward and octave compatibility, 'Label' option.
    SVN r7801

--- spm_circularGraph.m
+++ spm_circularGraph.m
@@ -1,59 +1,102 @@
 function spm_circularGraph(A,varargin)
-% spm_circularGraph: Plots a circular graph to illustrate connections
+% Plot a circular graph to illustrate connections
+% FORMAT spm_circularGraph(A,'PropertyName',propertyvalue,...)
+% X      - symmetric (NxN) matrix of numeric or logical values
 %
-%% Syntax
-% spm_circularGraph(X)
-% spm_circularGraph(X,'PropertyName',propertyvalue,...)
-% h = spm_circularGraph(...)
+% Optional properties:
+%   'Colormap'     - (Nx3) matrix of [r g b] triples
+%   'Label'        - cell array of N strings
 %
-%% Description
 % A 'circular graph' is a visualization of a network of nodes and their
 % connections. The nodes are laid out along a circle, and the connections
 % are drawn within the circle.
-% Required input arguments.
-% X  (N,N) : A symmetric matrix of numeric or logical values.
-%
-% Optional properties.
-% Colormap : A N by 3 matrix of [r g b] triples, where N is the
-%            length(adjacenyMatrix).
-%%
-% Copyright 2016 The MathWorks, Inc.
 
-% Constructor
+% $Id: spm_circularGraph.m 7801 2020-03-13 10:55:45Z guillaume $
+% https://github.com/paul-kassebaum-mathworks/circularGraph
+
+% Copyright (c) 2016, The MathWorks, Inc.
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
+% met:
+% 
+% * Redistributions of source code must retain the above copyright
+%   notice, this list of conditions and the following disclaimer.
+% * Redistributions in binary form must reproduce the above copyright
+%   notice, this list of conditions and the following disclaimer in
+%   the documentation and/or other materials provided with the distribution
+% * Neither the name of the The MathWorks, Inc. nor the names
+%   of its contributors may be used to endorse or promote products derived
+%   from this software without specific prior written permission.
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+% IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+% THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+% PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+% CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+% EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+% PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+% PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+% LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+% Input parameters
 %--------------------------------------------------------------------------
-p = inputParser;
+if ~(isnumeric(A) || islogical(A))
+    error('Input A must be a symmetric matric of numeric or logical values.');
+end
 
-defaultColorMap = parula(length(A));
-addRequired(p,'A',@(x)(isnumeric(x) || islogical(x)));
-addParameter(p,'ColorMap',defaultColorMap,@(colormap)length(colormap) == length(A));
-
-parse(p,A,varargin{:});
-ColorMap = p.Results.ColorMap;
+if strcmpi(spm_check_version,'matlab')
+    ColorMap = parula(length(A));
+else
+    ColorMap = viridis(length(A));
+end
+Label = arrayfun(@(x) num2str(x), 1:length(A), 'UniformOutput',false);
+for i=1:2:numel(varargin)
+    switch lower(varargin{i})
+        case 'colormap'
+            ColorMap = varargin{i+1};
+        case 'label'
+            Label = varargin{i+1};
+        otherwise
+            error('Unknown option.');
+    end
+end
+if length(ColorMap) ~= length(A) || length(Label) ~= length(A)
+    error('Optional input length does not match input data.');
+end
+ax = gca;
 
 % Draw the nodes
 %--------------------------------------------------------------------------
 t     = linspace(-pi,pi,length(A) + 1).'; % theta for each node
 for i = 1:length(A)
-    line(cos(t(i)),sin(t(i)),1,...
-        'Marker','.','MarkerSize',64,...
-        'Color',ColorMap(i,:))
-        text(cos(t(i)),sin(t(i)),1,num2str(i),'HorizontalAlignment','center')
+    line(ax, cos(t(i)), sin(t(i)), 1,...
+        'Marker', '.',...
+        'MarkerSize', 64,...
+        'Color', ColorMap(i,:));
+    text(ax, cos(t(i)), sin(t(i)), 1,...
+        Label{i},...
+        'HorizontalAlignment', 'center');
 end
 
-% Find non-zero values of s and their indices
+% Find non-zero values of A and their indices
 %--------------------------------------------------------------------------
 A             = abs(A);
 A             = A - diag(diag(A));
 [row,col,v]   = find(A);
 
-% Calculate line widths based on values of s (stored in v)
+% Calculate line widths based on values of A (stored in v)
 %--------------------------------------------------------------------------
 minLineWidth  = 0;
 lineWidthCoef = 4;
 lineWidth     = v./max(v);
 lineWidth     = lineWidthCoef*lineWidth + minLineWidth;
 
-% Draw connections on the Poincare hyperbolic disk.
+% Draw connections on the Poincare hyperbolic disk
 %==========================================================================
 % Equation of the circles on the disk:
 % x^2 + y^2
@@ -80,7 +123,6 @@ for i = 1:length(v)
             j = row(i);
         end
         
-        
         % draw lines
         %------------------------------------------------------------------
         if abs(row(i) - col(i)) - length(A)/2 == 0
@@ -89,7 +131,7 @@ for i = 1:length(v)
             %--------------------------------------------------------------
             u = [cos(t(row(i)));sin(t(row(i)))];
             v = [cos(t(col(i)));sin(t(col(i)))];
-            line(...
+            line(ax,...
                 [u(1);v(1)],...
                 [u(2);v(2)],...
                 'LineWidth', lineWidth(i),...
@@ -109,14 +151,13 @@ for i = 1:length(v)
             % ensure the arc is within the unit disk
             %--------------------------------------------------------------
             if u(1) >= 0 && v(1) >= 0
-                
                 theta = [linspace(max(thetaLim),pi,50),...
                     linspace(-pi,min(thetaLim),50)].';
             else
                 theta = linspace(thetaLim(1),thetaLim(2)).';
             end
             
-            line(...
+            line(ax,...
                 r*cos(theta)+x0,...
                 r*sin(theta)+y0,...
                 'LineWidth', lineWidth(i),...
