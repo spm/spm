3bb94968f82557563a063114cdef6669b43fe3fc
Guillaume Flandin <g.flandin@ucl.ac.uk>
2020-07-06 17:26:06 +0000

    Doc: rename variables in function mesh smoothing.
    SVN r7890

--- spm_mesh_smooth.m
+++ spm_mesh_smooth.m
@@ -1,31 +1,30 @@
 function T = spm_mesh_smooth(M, T, S)
 % Perform Gaussian smoothing on data lying on a surface mesh
-% FORMAT GL = spm_mesh_smooth(M)
-% M        - a patch structure or a handle to a patch
-% GL       - graph Laplacian
+% FORMAT K = spm_mesh_smooth(M)
+% M        - patch structure
+% K        - smoothing kernel (based on graph Laplacian)
 %
 % FORMAT T = spm_mesh_smooth(M, T, S)
-% FORMAT T = spm_mesh_smooth(GL, T, S)
+% FORMAT T = spm_mesh_smooth(K, T, S)
 % T        - [vx1] data vector
 % S        - smoothing parameter (number of iterations)
 %__________________________________________________________________________
-% Copyright (C) 2010 Wellcome Trust Centre for Neuroimaging
+% Copyright (C) 2010-2020 Wellcome Centre for Human Neuroimaging
 
 % Karl Friston, Guillaume Flandin
-% $Id: spm_mesh_smooth.m 4079 2010-10-07 11:41:54Z guillaume $
+% $Id: spm_mesh_smooth.m 7890 2020-07-06 17:26:06Z guillaume $
+
 
 if isstruct(M) || numel(M) == 1
-    A  = spm_mesh_distmtx(M,0);
-    N  = size(A,1);
-    GL = speye(N,N) + (A - spdiags(sum(A,2),0,N,N))/16;
+    A = spm_mesh_distmtx(M,0);
+    N = size(A,1);
+    K = speye(N,N) + (A - spdiags(sum(A,2),0,N,N)) / 16;
 else
-    GL = M;
+    K = M;
 end
 
-if nargin == 1, T = GL; return; end
+if nargin == 1, T = K; return; end
 
 for i=1:S
-    T = GL * T;
+    T = K * T;
 end
-
-return;
---
+++ tests/test_spm_mesh_smooth.m
@@ -0,0 +1,31 @@
+function tests = test_spm_mesh_smooth
+% Unit Tests for spm_mesh_smooth
+%__________________________________________________________________________
+% Copyright (C) 2020 Wellcome Centre for Human Neuroimaging
+
+% $Id: test_spm_mesh_smooth.m 7890 2020-07-06 17:26:06Z guillaume $
+
+tests = functiontests(localfunctions);
+
+
+function test_spm_mesh_smooth_1(testCase)
+
+M = spm_mesh_polyhedron('tetrahedron');
+K = spm_mesh_smooth(M);
+exp = [size(M.vertices,1) size(M.vertices,1)];
+act = size(K);
+testCase.verifyEqual(act, exp);
+
+exp = K;
+act = spm_mesh_smooth(K);
+testCase.verifyEqual(act, exp);
+
+
+function test_spm_mesh_smooth_2(testCase)
+
+M = spm_mesh_sphere(4);
+K = spm_mesh_smooth(M);
+T = rand(size(M.vertices,1),1);
+T = spm_mesh_smooth(K,T,10);
+testCase.verifyTrue(min(T)>=0);
+testCase.verifyTrue(max(T)<=1);
