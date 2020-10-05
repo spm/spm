abdbaf6c1d0d9089c75e6f48dbfe942f09d3951e
Guillaume Flandin <g.flandin@ucl.ac.uk>
2020-10-05 16:09:34 +0000

    Doc: Surface for geodesic distances must be a manifold triangular mesh. src/spm_mesh_geodesic.cpp otherwise segfaults:   M = spm_mesh_polyhedron('tetrahedron');   spm_mesh_geodesic(M,1);   % egde 1-3 now shared by 3 triangles => non-manifold   M.faces(end+1,:) = [1 3 5];   M.vertices(end+1,:) = 0;   spm_mesh_geodesic(M,1);
    SVN r7973

--- spm_mesh_geodesic.m
+++ spm_mesh_geodesic.m
@@ -1,6 +1,6 @@
 function [D,L,P] = spm_mesh_geodesic(M,i,d,s)
 % Compute geodesic distances on a triangle mesh - a compiled routine
-% FORMAT [D,L,P] = spm_mesh_geodesic(M,i)
+% FORMAT [D,L,P] = spm_mesh_geodesic(M,i,d,s)
 % M      - a patch structure with n vertices
 % i      - index of source vertices
 % d      - maximal distance from the sources [default: Inf]
@@ -9,19 +9,22 @@ function [D,L,P] = spm_mesh_geodesic(M,i,d,s)
 % D      - a [nx1] vector of geodesic distances from i
 % L      - a [nx1] vector of index of the nearest source (Voronoi)
 % P      - a [nx1] cell vector of [px3] coordinates of geodesic lines
+%
+% The input surface is assumed to be a manifold triangular mesh. In
+% particular, this means that no edge is shared by more than two triangles.
 %__________________________________________________________________________
 %
 % Based on C++ library: https://code.google.com/archive/p/geodesic/
 % Copyright (C) 2008 Danil Kirsanov, MIT License
 % [1] J.S.B. Mitchell, D.M. Mount, and C.H. Papadimitriou, The discrete
-%     geodesic problem, SIAM Journal on Computing,16(4) (1987), 647–66.
+%     geodesic problem, SIAM Journal on Computing,16(4) (1987), 647-666.
 % [2] J. O'Rourke, Computational Geometry Column 35, SIGACT News, 30(2)
-%     Issue #111 (1999
+%     Issue #111 (1999).
 %__________________________________________________________________________
 % Copyright (C) 2010-2020 Wellcome Centre for Human Neuroimaging
 
 % Guillaume Flandin
-% $Id: spm_mesh_geodesic.m 7889 2020-07-06 11:21:54Z guillaume $
+% $Id: spm_mesh_geodesic.m 7973 2020-10-05 16:09:34Z guillaume $
 
 % % A previous version of this function was computing approximate
 % % geodesic distances using Dijkstra algorithm
