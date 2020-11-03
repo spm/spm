555827f85bac7b50c8ed104987a21ad0384a6138
Guillaume Flandin <g.flandin@ucl.ac.uk>
2020-11-03 15:02:01 +0000

    Enhancements: use cell arrays of nifti object for speed efficiency. Add option for parallel execution, default being serial.
    SVN r7999

--- toolbox/Shoot/spm_shoot_template.m
+++ toolbox/Shoot/spm_shoot_template.m
@@ -1,6 +1,6 @@
 function out = spm_shoot_template(job)
 % Iteratively compute a template with mean shape and intensities
-% format spm_shoot_template(job)
+% FORMAT out = spm_shoot_template(job)
 % Fields of job:
 %     job.images{1} first set of images (eg rc1*.nii)
 %     job.images{2} second set of images (eg rc2*.nii)
@@ -8,15 +8,15 @@ function out = spm_shoot_template(job)
 %
 % Other settings are defined in spm_shoot_defaults.m
 %
-% The outputs are flow fields (v*.nii), deformation fields (y*.nii)
-% and a series of Template images.
-%_______________________________________________________________________
-% Copyright (C) Wellcome Trust Centre for Neuroimaging (2009)
+% The outputs are flow fields (v_*.nii), deformation fields (y_*.nii),
+% Jacobian determinants (j_*.nii) and a series of Template images.
+%__________________________________________________________________________
+% Copyright (C) 2009-2020 Wellcome Centre for Human Neuroimaging
 
 % John Ashburner
-% $Id: spm_shoot_template.m 7773 2020-01-22 10:42:47Z john $
+% $Id: spm_shoot_template.m 7999 2020-11-03 15:02:01Z guillaume $
 
-%_______________________________________________________________________
+%__________________________________________________________________________
 d       = spm_shoot_defaults;
 tname   = d.tname;   % Base file name for templates
 issym   = d.issym;   % Use a symmetric template
@@ -31,20 +31,21 @@ eul_its = d.eul_its; % Start with fewer steps
 scale   = d.scale;   % Fraction of Gauss-Newton update step to use
 
 bs_args = d.bs_args; % B-spline settings for interpolation
-%_______________________________________________________________________
+%__________________________________________________________________________
+
+par_opt = 0;         % parfor option [0 for serial]
 
 spm_diffeo('boundary',0);
 
 % Sort out handles to images
 n1 = numel(job.images);
 n2 = numel(job.images{1});
-NF = struct('NI',[],'vn',[1 1]);
-NF(n1,n2) = struct('NI',[],'vn',[1 1]);
+NF = struct('NI',repmat([],n1,n2),'vn',[1 1]);
 
 % Pick out individual volumes within NIfTI files
 for i=1:n1
     if numel(job.images{i}) ~= n2
-        error('Incompatible number of images');
+        error('Incompatible number of images.');
     end
     for j=1:n2
         [pth,nam,ext,num] = spm_fileparts(job.images{i}{j});
@@ -59,57 +60,44 @@ dm = [size(NF(1,1).NI.dat) 1];
 dm = dm(1:3);
 M  = NF(1,1).NI.mat;
 
-NU     = nifti;
-NU(n2) = nifti;
-NY     = nifti;
-NY(n2) = nifti;
-NJ     = nifti;
-NJ(n2) = nifti;
+NU = repmat({nifti},1,n2);
+NY = repmat({nifti},1,n2);
+NJ = repmat({nifti},1,n2);
 
 t  = zeros([dm n1+1],'single');
 
-for i=1:n2
-    % Generate files for flow fields, deformations and Jacobian determinants.
-    [pth,nam,ext]   = fileparts(NF(1,i).NI.dat.fname);
-
-    NU(i) = nifti;
-    NY(i) = nifti;
-    NJ(i) = nifti;
+parfor (i=1:n2, par_opt) % Loop over subjects
+    % Generate files for flow fields, deformations and Jacobian determinants
+    [pth,nam,ext] = fileparts(NF(1,i).NI.dat.fname);
 
     if ~isempty(tname)
-        NU(i).dat = file_array(fullfile(pth,['v_' nam '_' tname '.nii']),...
-                               [dm 1 3], 'float32-le', 352, 1, 0);
-        NY(i).dat = file_array(fullfile(pth,['y_' nam '_' tname '.nii']),...
-                               [dm 1 3], 'float32-le', 352, 1, 0);
-        NJ(i).dat = file_array(fullfile(pth,['j_' nam '_' tname '.nii']),...
-                               [dm 1 1], 'float32-le', 352, 1, 0);
-    else
-        NU(i).dat = file_array(fullfile(pth,['v_' nam '.nii']),...
-                               [dm 1 3], 'float32-le', 352, 1, 0);
-        NY(i).dat = file_array(fullfile(pth,['y_' nam '.nii']),...
-                               [dm 1 3], 'float32-le', 352, 1, 0);
-        NJ(i).dat = file_array(fullfile(pth,['j_' nam '.nii']),...
-                               [dm 1 1], 'float32-le', 352, 1, 0);
+        nam = [nam '_' tname];
     end
+    NU{i}.dat = file_array(fullfile(pth,['v_' nam '.nii']),...
+                           [dm 1 3], 'float32-le', 352, 1, 0);
+    NY{i}.dat = file_array(fullfile(pth,['y_' nam '.nii']),...
+                           [dm 1 3], 'float32-le', 352, 1, 0);
+    NJ{i}.dat = file_array(fullfile(pth,['j_' nam '.nii']),...
+                           [dm 1 1], 'float32-le', 352, 1, 0);
 
-    NU(i).descrip = sprintf('Velocity (%.4g %.4g %.4g %.4g %.4g)', rparam(1), rparam(2), rparam(3), rparam(4), rparam(5));
-    NU(i).mat     = M;
-    NU(i).mat0    = NF(1,i).NI.mat0;
+    NU{i}.descrip = sprintf('Velocity (%.4g %.4g %.4g %.4g %.4g)', rparam(1), rparam(2), rparam(3), rparam(4), rparam(5));
+    NU{i}.mat     = M;
+    NU{i}.mat0    = NF(1,i).NI.mat0;
 
-    NY(i).descrip = 'Deformation (templ. to. ind.)';
-    NY(i).mat     = M;
-    NY(i).mat0    = M;
+    NY{i}.descrip = 'Deformation (templ. to. ind.)';
+    NY{i}.mat     = M;
+    NY{i}.mat0    = M;
 
-    NJ(i).descrip = 'Jacobian det (templ. to. ind.)';
-    NJ(i).mat     = M;
-    NJ(i).mat0    = M;
+    NJ{i}.descrip = 'Jacobian det (templ. to. ind.)';
+    NJ{i}.mat     = M;
+    NJ{i}.mat0    = M;
 
-    create(NU(i)); NU(i).dat(:,:,:,:,:) = 0;
-    create(NY(i)); NY(i).dat(:,:,:,:,:) = reshape(affind(spm_diffeo('Exp',zeros([dm,3],'single'),[0 1]),NU(i).mat0),[dm,1,3]);
-    create(NJ(i)); NJ(i).dat(:,:,:)     = 1;
+    create(NU{i}); NU{i}.dat(:,:,:,:,:) = 0;
+    create(NY{i}); NY{i}.dat(:,:,:,:,:) = reshape(affind(spm_diffeo('Exp',zeros([dm,3],'single'),[0 1]),NU{i}.mat0),[dm,1,3]);
+    create(NJ{i}); NJ{i}.dat(:,:,:)     = 1;
 end
 
-for i=1:n2, % Loop over subjects. Can replace FOR with PARFOR.
+parfor (i=1:n2, par_opt) % Loop over subjects
 
     % Add to sufficient statistics for generating initial template
     tmp = zeros([dm n1+1],'single');
@@ -185,14 +173,13 @@ for it=1:nits
     % a less accurate approximation in the integration.
     prm      = [vx, rparam*sched(it+1)*prod(vx)]; % FIX THIS
     int_args = [eul_its(it), cyc_its];
-    drawnow
 
     t   = zeros([dm n1+1],'single');
     su  = zeros([dm 3]);
 
     % Update velocities
     spm_progress_bar('Init',n2,sprintf('Update velocities (%d)',it),'Subjects done');
-    for i=1:n2 % Loop over subjects. Can replace FOR with PARFOR.
+    parfor (i=1:n2, par_opt) % Loop over subjects
 
         if ok(i)
             fprintf('%3d %5d | ',it,i);
@@ -201,17 +188,15 @@ for it=1:nits
             f = loadimage(NF(:,i));
 
             % Load this subject's flow field and deformation
-            u  = squeeze(single(NU(i).dat(:,:,:,:,:)));
-            y  = affind(squeeze(single(NY(i).dat(:,:,:,:,:))),inv(NU(i).mat0));
-            dt = squeeze(single(NJ(i).dat(:,:,:)));
-            drawnow
+            u  = squeeze(single(NU{i}.dat(:,:,:,:,:)));
+            y  = affind(squeeze(single(NY{i}.dat(:,:,:,:,:))),inv(NU{i}.mat0));
+            dt = squeeze(single(NJ{i}.dat(:,:,:)));
 
             % Gauss-Newton iteration to re-estimate deformations for this subject
             u = spm_shoot_update(g,f,u,y,dt,prm,bs_args,scale);
             %clear f y
 
-            drawnow
-            NU(i).dat(:,:,:,:,:) = reshape(u,[dm 1 3]);
+            NU{i}.dat(:,:,:,:,:) = reshape(u,[dm 1 3]);
             su = su + u;
             %clear u
             spm_progress_bar('Set',i);
@@ -232,27 +217,26 @@ for it=1:nits
  
     % Update template sufficient statistics
     spm_progress_bar('Init',n2,sprintf('Update deformations and template (%d)',it),'Subjects done');
-    for i=1:n2 % Loop over subjects. Can replace FOR with PARFOR.
+    parfor (i=1:n2, par_opt) % Loop over subjects
 
         if ok(i)
             % Load velocity, mean adjust and re-save
-            u  = squeeze(single(NU(i).dat(:,:,:,:,:)));
+            u  = squeeze(single(NU{i}.dat(:,:,:,:,:)));
             if isempty(sparam) || smits==0
                 u  = u - su; % Subtract mean (unless template is smoothed)
             end
-            NU(i).dat(:,:,:,:,:) = reshape(u,[dm 1 3]);
+            NU{i}.dat(:,:,:,:,:) = reshape(u,[dm 1 3]);
 
             [y,dt] = defdet(u,prm,int_args, K);
 
             if any(~isfinite(dt(:)) | dt(:)>100 | dt(:)<1/100)
                 ok(i) = false;
-                fprintf('Problem with %s (dets: %g .. %g)\n', NU(i).dat.fname, min(dt(:)), max(dt(:)));
+                fprintf('Problem with %s (dets: %g .. %g)\n', NU{i}.dat.fname, min(dt(:)), max(dt(:)));
                 %clear dt
             end
 
-            NY(i).dat(:,:,:,:,:) = reshape(affind(y,NU(i).mat0),[dm 1 3]);
-            NJ(i).dat(:,:,:)     = dt;
-            drawnow;
+            NY{i}.dat(:,:,:,:,:) = reshape(affind(y,NU{i}.mat0),[dm 1 3]);
+            NJ{i}.dat(:,:,:)     = dt;
 
             % Load image data for this subject
             f = loadimage(NF(:,i));
@@ -307,13 +291,13 @@ for it=1:nits
 
     % Compute template's B-spline coefficients
     for j=1:n1+1, g{j} = spm_bsplinc(log(g{j}), bs_args); end
-    drawnow
+    
 end
 
 if any(~ok)
     fprintf('Problems with:\n');
     for i=find(~ok)'
-        fprintf('\t%s\n', NU(i).dat.fname);
+        fprintf('\t%s\n', NU{i}.dat.fname);
     end
 end
 
@@ -329,22 +313,22 @@ out.vel = cell(n2,1);
 out.def = cell(n2,1);
 out.jac = cell(n2,1);
 for i=1:n2
-    out.vel{i} = NU(i).dat.fname;
-    out.def{i} = NY(i).dat.fname;
-    out.jac{i} = NJ(i).dat.fname;
+    out.vel{i} = NU{i}.dat.fname;
+    out.def{i} = NY{i}.dat.fname;
+    out.jac{i} = NJ{i}.dat.fname;
 end
-%=======================================================================
+%==========================================================================
 
-%=======================================================================
+%==========================================================================
 function y1 = affind(y0,M)
 % Affine transform of deformation
 y1 = zeros(size(y0),'single');
 for d=1:3
     y1(:,:,:,d) = y0(:,:,:,1)*M(d,1) + y0(:,:,:,2)*M(d,2) + y0(:,:,:,3)*M(d,3) + M(d,4);
 end
-%=======================================================================
+%==========================================================================
 
-%=======================================================================
+%==========================================================================
 function f = loadimage(NF)
 n1      = size(NF,1);
 f       = cell(n1+1,1);
@@ -357,17 +341,15 @@ for j=1:n1
     msk     = ~isfinite(f{j});
     f{j}(msk) = 0;
     f{n1+1} = f{n1+1} - f{j};
-    drawnow
 end
 f{n1+1}(msk) = 0.00001;
-%=======================================================================
+%==========================================================================
 
-%=======================================================================
+%==========================================================================
 function [y,dt] = defdet(u,prm,int_args, K)
 % Generate deformation
 [y,J] = spm_shoot3d(u,prm,int_args, K);
 dt    = spm_diffeo('det',J);
-%=======================================================================
+%==========================================================================
 
-%=======================================================================
-
+%==========================================================================
