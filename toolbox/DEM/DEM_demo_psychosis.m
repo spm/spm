b32af76c166d2e5cb0b751f95fc13dbc64fcf8e9
Karl Friston <k.friston@ucl.ac.uk>
2021-06-01 14:21:12 +0000

    Upgrade to psychosis demo for modelling fluctuations in psychopathology with discrete data points
    SVN r8109

--- toolbox/DEM/DEM_demo_psychosis.m
+++ toolbox/DEM/DEM_demo_psychosis.m
@@ -39,6 +39,16 @@ function DEM_demo_psychosis
 %==========================================================================
 rng('default')
 
+% create some synthetic time courses to model
+%--------------------------------------------------------------------------
+R  = zeros(148,2);
+R( 1:18,1)  = 1;
+R(42:50,1)  = 1;
+R(82:92,1)  = 1;
+R( 1:20,2)  = 1;
+R(80:end,2) = 1;
+Y           = R;
+
 % dynamics and parameters of a Lorentz system (with Jacobian)
 %==========================================================================
 % dxdt = f(x,v)             Underlying dynamics with exogenous inputs        
@@ -51,7 +61,7 @@ rng('default')
 % multiple thresholds to generate discrete scales of the sort found in
 % clinical instruments.
 %--------------------------------------------------------------------------
-M.h  = @(x,P)spm_phi([x(:,1) - P.u(1), x(:,2) - P.u(2)]*128);
+M.h  = @(x,P)spm_phi([x(:,1) - P.u(1), x(:,2) - P.u(2)]*8);
  
 % level 2: the level that generates latent causes v = g(x) from
 % (pathophysiological) states (x) that are subject to interventions (v).
@@ -63,7 +73,7 @@ M.h  = @(x,P)spm_phi([x(:,1) - P.u(1), x(:,2) - P.u(2)]*128);
 % manifestation (P.B)
 %--------------------------------------------------------------------------
 M.f  = @(x,v,P,M)[-P.A(1) P.A(1) 0; ((1 - v*P.A(3))*P.A(2) - x(3)) -1 0; x(2) 0 -8/3]*x/P.t;
-M.d  = @(x,P) x(:,[1 3])*P.B/16;
+M.d  = @(x,P) x*P.B/16;
 
 % level 3: the level that perturbed the physiological dynamics by
 % intervening on the parameters governing flow. These interventions or
@@ -72,27 +82,30 @@ M.d  = @(x,P) x(:,[1 3])*P.B/16;
 % changes that are parameterised with the coefficients of a discrete cosine
 % function of time (P.v).
 %--------------------------------------------------------------------------
-M.v  = @(T,P) spm_dctmtx(T,numel(P.v))*P.v(:);
+M.v  = @(T,P) spm_dctmtx(T,numel(P.C))*P.C(:);
 
 % Having established the structure of the generative model we can now
 % specify the prior expectations over the requisite parameters; including
 % the overall rate at which the dynamics are expressed (P.t). Here, P.u
 % parameterises the initial states at the beginning of the timeseries.
 %--------------------------------------------------------------------------
-pE.A = [10 16 1];
-pE.B = [2 0;0 1];
-pE.C = 8;
-pE.x = [2; 4; 32];
-pE.u = [1,1,1.5];
-pE.v = [0 0];
-pE.t = 64;
+pE.A = [10 16 1];                      % parameters of pathophysiology
+pE.B = [2 0;0 1;0 0];                  % mapping to psychopathology
+pE.C = [0 0 0 0 0 0 0 0];              % parameters of perturbations
+pE.x = [16; 4; 32];                    % initial physiological states
+pE.u = [1,1];                          % threshold for (two) outcomes
+pE.t = 128;                            % time constant of dynamics
 
 % The priors are completed by specifying the prior covariance, that
 % determines which parameters are fixed and which are unknown and need to
 % be estimated
 %--------------------------------------------------------------------------
-pC   = diag([1 1 1  0 0 0 0  1/64  0 0 0  0 0 0 1/32 1/32 1]);
-
+pC.A = spm_zeros(pE.A) + 1;
+pC.B = spm_zeros(pE.B) + 1;
+pC.C = spm_zeros(pE.C) + 1/32;
+pC.x = spm_zeros(pE.x) + 4;
+pC.u = spm_zeros(pE.u) + 0;
+pC.t = 1;
 
 % model inversion with Variational Laplace
 %==========================================================================
@@ -121,7 +134,7 @@ pC   = diag([1 1 1  0 0 0 0  1/64  0 0 0  0 0 0 1/32 1/32 1]);
 
 % length of trajectory (e.g., symptom score timeseries)
 %--------------------------------------------------------------------------
-T      = 128;                      % number of assessments
+T      = size(Y,1);                % number of assessments
 
 % model specification
 %==========================================================================
@@ -129,21 +142,53 @@ M.Nmax = 32;                       % maximum number of iterations
 M.G    = @spm_psychosis_gen;       % generative function
 M.FS   = @(Y)spm_conv(Y,16,0);     % feature selection  (link function)
 M.pE   = pE;                       % prior expectations (parameters)
-M.pC   = pC;                       % prior covariances  (parameters)
-M.hE   = 4;                        % prior expectation  (log-precision)
-M.hC   = 1/512;                    % prior covariances  (log-precision)
+M.pC   = diag(spm_vec(pC));        % prior covariances  (parameters)
+M.hE   = 2;                        % prior expectation  (log-precision)
+M.hC   = 1/64;                     % prior covariances  (log-precision)
 U      = zeros(T,1);               % Prior expectation of fluctuations
 
+
+% multi-start inversion with initial parameters sampled from prior here, we
+% will try to fit the empirical data above and use the best priors to
+% generate synthetic data (with different Rayleigh parameters) for further
+% analysis
+%==========================================================================
+F     = -Inf;
+for i = 1:16
+    
+    % initialise parameters
+    %----------------------------------------------------------------------
+    dP           = spm_sqrtm(M.pC)*randn(spm_length(M.pE))/8;
+    M.P          = spm_unvec(spm_vec(M.pE) + dP,M.pE);
+    [ep,cp,eh,f] = spm_nlsi_GN(M,U,Y);
+    
+    % update priors if model evidence has increased
+    %----------------------------------------------------------------------
+    if f > F
+        F    = f;
+        M.pE = ep;
+    end
+end
+
 % generate synthetic symptom scores with a particular Rayleigh parameter
-%--------------------------------------------------------------------------
-P      = pE;                       % start with prior expectations 
-P.A(2) = 21;                       % specify a Rayleigh parameter
-P.v    = randn(size(pE.v))/4;      % specify the coefficients of inputs
+%==========================================================================
+P      = M.pE;                     % start with prior expectations 
 Y      = spm_psychosis_gen(P,M,U); % generate a synthetic timeseries
 
+% fix most prior covariances for numerical convenience
+%--------------------------------------------------------------------------
+pC.A = spm_zeros(pE.A) + 0;
+pC.B = spm_zeros(pE.B) + 0;
+pC.C = spm_zeros(pE.C) + 0;
+pC.x = spm_zeros(pE.x) + 0;
+pC.u = spm_zeros(pE.u) + 0;
+pC.t = 0;
+
+pC.A(2) = 1;
 
 % multi-start inversion with Variational Laplace
 %--------------------------------------------------------------------------
+M.pC  = diag(spm_vec(pC));         % prior covariances  (parameters)
 pA    = 8:4:24;                    % initial values of Rayleigh parameter
 F     = -Inf;
 for i = 1:numel(pA)
@@ -184,75 +229,57 @@ spm_DCM_plot(DCM)
 
 % set up the simulated group study
 %--------------------------------------------------------------------------
-M.nograph = 1;                         % suppress inversion graphics
+M.nograph = 1;                           % suppress inversion graphics
 
-n     = 16;                            % number of subjects
-pP    = zeros(n,1);                    % values of Rayleigh parameter
-qP    = zeros(n,1);                    % posterior estimates
-pY    = [];                            % true symptom scores
-qY    = [];                            % predicted scores
-pA    = 8:2:24;                        % multi-start Rayleigh  parameters
+n     = 16;                              % number of subjects
+pP    = zeros(n,1);                      % values of Rayleigh parameter
+qP    = zeros(n,1);                      % posterior estimates
+pY    = [];                              % true symptom scores
+qY    = [];                              % predicted scores
 for j = 1:n
     
     % generate symptom scores with this subject's Rayleigh parameter
     %----------------------------------------------------------------------
-    P      = pE;                       % start with prior expectations
-    P.A(2) = rand*16 + 8;              % specify a random subject effect
-    P.v    = randn(size(pE.v))/4;      % and some random fluctuations
-    Y      = spm_psychosis_gen(P,M,U); % generate timeseries
+    P      = M.pE;                       % start with prior expectations
+    P.A(2) = 16 + rand*2;                % specify a random subject effect
+    Y      = spm_psychosis_gen(P,M,U);   % generate timeseries
     
     % model inversion with Variational Laplace
     %----------------------------------------------------------------------
-    F     = -Inf;
-    for i = 1:numel(pA)
-        
-        % initialise parameters (i.e., prior expectations) and invert
-        %------------------------------------------------------------------
-        M.pE.A(2)    = pA(i);
-        [ep,cp,eh,f] = spm_nlsi_GN(M,U,Y);
-        
-        % retain posteriors if model evidence has increased
-        %------------------------------------------------------------------
-        if f > F
-            F  = f;
-            Ep = ep;
-        end
-    end
+    Ep     = spm_nlsi_GN(M,U,Y);
 
     % generate predicted symptom scores using posterior expectations
-    %------------------------------------------------------------------
+    %----------------------------------------------------------------------
     y     = spm_psychosis_gen(Ep,M,U);
     pY    = [pY Y];
     qY    = [qY y];
     pP(j) =  P.A(2);
     qP(j) = Ep.A(2);
     
+    % illustrate true and inferred symptom scores and parameters
+    %----------------------------------------------------------------------
+    spm_figure('GetWin','Figure 2'); clf
+    
+    subplot(2,2,1)
+    imagesc(pY')
+    xlabel('time points')
+    ylabel('subjects and symptom scores')
+    title('Synthetic symptom scores','FontSize',16), axis square
+    
+    subplot(2,2,2)
+    imagesc(qY')
+    xlabel('time points')
+    ylabel('subjects and symptom scores')
+    title('Predicted symptom scores','FontSize',16), axis square
+    
+    subplot(2,1,2)
+    plot(pP,qP,'.','MarkerSize',32), hold on, plot(pP,pP,':'), hold off
+    xlabel('parameter used to generate data')
+    ylabel('posterior parameter estimate')
+    title('True and estimated parameters','FontSize',16), axis square
+    drawnow
+
 end
-
-
-% illustrate true and inferred symptom scores and parameters
-%--------------------------------------------------------------------------
-spm_figure('GetWin','Figure 2'); clf
-
-subplot(2,2,1)
-imagesc(pY')
-xlabel('time points')
-ylabel('subjects and symptom scores')
-title('Synthetic symptom scores','FontSize',16), axis square
-
-subplot(2,2,2)
-imagesc(qY')
-xlabel('time points')
-ylabel('subjects and symptom scores')
-title('Predicted symptom scores','FontSize',16), axis square
-
-subplot(2,1,2)
-plot(pP,qP,'.','MarkerSize',32), hold on, plot(pP,pP,':'), hold off
-xlabel('parameter used to generate data')
-ylabel('posterior parameter estimate')
-title('True and estimated parameters','FontSize',16), axis square
-
-
 
 function [y,s,x] = spm_psychosis_gen(P,M,U)
 % FORMAT [y,s,x] = spm_psychosis_gen(P,M,U)
