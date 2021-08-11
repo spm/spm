d20eea6284188ff82580f9de2dea1f5397505a38
John Ashburner <j.ashburner@ucl.ac.uk>
2021-08-11 10:40:24 +0000

    * Added the labelling code as part of the mb toolbox.
    SVN r8133

---
+++ toolbox/mb/fil_subvol.m
@@ -0,0 +1,31 @@
+function [d,M] = fil_subvol(Nii,bb)
+% Dimensions and voxel-to world mapping of a subvolume
+% FORMAT VO = subvol(Nii,bb)
+% V      - SPM NIfTI object
+% bb     - bounding box (2 x 3)
+%__________________________________________________________________________
+% Copyright (C) 2021 Wellcome Centre for Human Neuroimaging
+
+% John Ashburner
+% $Id$
+
+if ~isa(Nii,'nifti')
+    Nii = nifti(Nii);
+end
+bb0 =  [1 1 1; size(Nii.dat,1) size(Nii.dat,2) size(Nii.dat,3)];
+if nargin<2
+    bb = bb0;
+else
+    if ~all(size(bb)==[2 3])
+        error('Incorrectly sized bb.');
+    end
+    msk     = ~isfinite(bb);
+    bb(msk) = bb0(msk);
+    bb      = sort(bb);
+    bb(1,:) = round(max(bb(1,:),bb0(1,:)));
+    bb(2,:) = round(min(bb(2,:),bb0(2,:)));
+end
+d = diff(bb)+1;
+M = Nii.mat*spm_matrix((bb(1,:)-1));
+%==========================================================================
+
