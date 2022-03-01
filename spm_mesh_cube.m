266d51002e33fa3667399b27b33eb89d438ed168
Guillaume Flandin <g.flandin@ucl.ac.uk>
2022-03-01 14:24:08 +0000

    New feature: unit cube as triangle mesh
    SVN r8229

---
+++ spm_mesh_cube.m
@@ -0,0 +1,37 @@
+function M = spm_mesh_cube
+% Triangle mesh of a unit cube
+% M        - patch structure
+%__________________________________________________________________________
+%
+% Return a triangle mesh of a unit cube (sides of 1 unit long).
+% See https://www.wikipedia.org/wiki/Unit_cube
+%__________________________________________________________________________
+% Copyright (C) 2022 Wellcome Centre for Human Neuroimaging
+
+% Guillaume Flandin
+% $Id: spm_mesh_cube.m 8229 2022-03-01 14:24:08Z guillaume $
+
+
+M.vertices = [...
+    1 0 1
+    1 0 0
+    1 1 0
+    1 1 1
+    0 0 1
+    0 0 0
+    0 1 0
+    0 1 1];
+
+M.faces = [...
+    5 1 4
+    5 4 8
+    1 2 3
+    1 3 4
+    2 6 7
+    2 7 3
+    6 5 8
+    6 8 7
+    8 4 3
+    8 3 7
+    1 6 2
+    1 5 6];
