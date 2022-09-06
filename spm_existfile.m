10cdf95e4bdf37fc92fc4b3aeee0b787566d817f
Guillaume Flandin <g.flandin@ucl.ac.uk>
2022-09-06 15:06:41 +0000

    Doc: mention that spm_existfile does not perform tilde expansion
    SVN r8309

--- spm_existfile.m
+++ spm_existfile.m
@@ -7,8 +7,9 @@ function s = spm_existfile(filename)
 %
 % This compiled routine is equivalent to:
 % >> s = exist(filename,'file') == 2;
-% and was written for speed purposes. The differences in behaviour is that
-% spm_existfile does not look in MATLAB's search path.
+% and was written for speed purposes. The differences in behaviour are that
+% spm_existfile does not look in MATLAB's search path and does not perform
+% tilde '~' expansion.
 %__________________________________________________________________________
 
 % Guillaume Flandin
