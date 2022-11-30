4626f6d02d6214f106dd057f6fecc931a9e433ee
Guillaume Flandin <g.flandin@ucl.ac.uk>
2022-11-30 11:57:16 +0000

    Bug fix: fix type mismatch in mexFunction
    SVN r8362

--- src/shoot_bsplines.h
+++ src/shoot_bsplines.h
@@ -3,5 +3,5 @@
  * Copyright (C) 2011-2022 Wellcome Centre for Human Neuroimaging
  */
 
-void bsplinc_mexFunction(mwSize nlhs, mxArray *plhs[], mwSize nrhs, const mxArray *prhs[]);
-void bsplins_mexFunction(mwSize nlhs, mxArray *plhs[], mwSize nrhs, const mxArray *prhs[]);
+void bsplinc_mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
+void bsplins_mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
