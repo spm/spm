ef2eddc5dd39449e4227ba6878c6c6e7b434d57c
Guillaume Flandin <g.flandin@ucl.ac.uk>
2023-03-08 20:42:12 +0000

    Enhancement: file download for factorisation-based image labelling
    SVN r8428

--- toolbox/mb/spm_label.m
+++ toolbox/mb/spm_label.m
@@ -1,10 +1,10 @@
 function out = spm_label(cfg)
-%
+% Factorisation-based Image Labelling
 % FORMAT out = spm_label(cfg)
 %__________________________________________________________________________
 
 % John Ashburner
-% Copyright (C) 2021-2022 Wellcome Centre for Human Neuroimaging
+% Copyright (C) 2021-2023 Wellcome Centre for Human Neuroimaging
 
 
 if nargin==0
@@ -20,34 +20,9 @@ end
 
 % Needs some files uploaded from:
 % https://figshare.com/projects/Factorisation-based_Image_Labelling/128189
+%--------------------------------------------------------------------------
+[mufile,filfile] = fil_install;
 
-datadir = fullfile(spm('dir'),'toolbox','mb','data'); % Suggested location for data files
-if ~exist(datadir,'dir')
-    try
-        fprintf('Attempting to make %s directory.\n', datadir);
-        mkdir(datadir);
-    catch
-        fprintf('Failed to make directory %s\n', datadir);
-        datadir = spm_select(1,'dir','Select directory for labelling data');
-        %error(['Failed to make a directory for the template and labelling files (' datadir ').']);
-    end
-end
-
-%datadir  = '.'; % Edit to indicate location of various files
-mufname  = 'mu_X.nii';
-filfname = 'fil30-nuNaN-v1-d4-K24-r2-sd1.5.mat';
-%filfname = 'fil15-nuNaN-v1-d4-K24-r3-sd2.mat';
-mufile   = fullfile(datadir,mufname);  % Head Tissue Template
-filfile  = fullfile(datadir,filfname); % Trained FIL Model (15 training + 15 test subjects)
-if ~exist(mufile,'file') || ~exist(filfile,'file')
-    try
-        fprintf('Attempting to download %s and %s files.\n', mufile, filfile);
-        websave(mufile, 'https://figshare.com/ndownloader/files/31699187');
-        websave(filfile,'https://figshare.com/ndownloader/files/31784579');
-    catch
-        error('Failed to download the template and labelling data.');
-    end
-end
 % Could use Normal-Wishart priors for intensities. The following might help for
 % labelling T1w scans.
 %   nw_priors = fullfile(datadir,'prior_X_2.mat'); % MRI Intensity Priors for T1w
@@ -55,7 +30,8 @@ end
 
 nw_priors = ''; % Any data
 
-% Settings for MB alignment and tissue classification
+%-Settings for MB alignment and tissue classification
+%--------------------------------------------------------------------------
 clear chan
 chan.inu.inu_reg = 10000;               % Intensity non-uniformity (INU) regularisation
 chan.inu.inu_co  = 40;                  % Cut-off for INU
@@ -88,14 +64,18 @@ cfg.save       = false;                 % Don't save to disk
 cfg.nworker    = 0;                     % No parallelisation
 
 % Set search path to find additional functions
-addpath(fullfile(spm('dir'),'toolbox','Shoot'));
-addpath(fullfile(spm('dir'),'toolbox','Longitudinal'));
+if ~isdeployed
+    addpath(fullfile(spm('dir'),'toolbox','Shoot'));
+    addpath(fullfile(spm('dir'),'toolbox','Longitudinal'));
+end
 fil = load(filfile);
 
+%-Label image(s)
+%--------------------------------------------------------------------------
 out.labels = cell(size(P,1),1);
 for n=1:size(P,1)
     cfg.gmm.chan.images = {deblank(P(n,:))};
-    [odir,onam,ext] = fileparts(cfg.gmm.chan.images{1}); % Output directory and filename
+    [odir,onam] = fileparts(cfg.gmm.chan.images{1}); % Output directory and filename
     [dat,sett]  = spm_mb_init(cfg);                  % Set up data structure
     dat(1).v    = zeros([dat(1).dm 3],'single');     % Modify structure to work in memory
     dat(1).psi  = zeros([dat(1).dm 3],'single');     % Modify structure to work in memory
