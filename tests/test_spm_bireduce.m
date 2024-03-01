0f83da8407a8330236065def7815c17acb48b8d0
Peter Zeidman <peter.zeidman@ucl.ac.uk>
2024-03-01 11:25:55 +0000

    Minor fix to unit test test_spm_bireduce

--- tests/test_spm_bireduce.m
+++ tests/test_spm_bireduce.m
@@ -9,22 +9,21 @@ tests = functiontests(localfunctions);
 % -------------------------------------------------------------------------
 function test_bireduce(testCase)
 
-% Consider the system:
+% Creates a bilinear approximation for the system:
 %
 %              dx/dt  = 1./(1 + exp(-x*P)) + u
 %                y    = x + e
 %
 
 % Specify a model structure
-M.f  = inline('1./(1 + exp(-P*x)) + [u; 0]','x','u','P','M');
-M.g  = inline('x','x','u','P','M');
+M.f  = @(x,u,P,M) 1./(1 + exp(-P*x)) + [u; 0];
+M.g  = @(x,u,P,M) x;
 M.pE = [-1 .3;.5 -1];           % Prior expectation of parameters
 M.pC = speye(4,4);              % Prior covariance for parameters
-M.x  = zeros(2,1);               % initial state x(0)
+M.x  = zeros(2,1);              % initial state x(0)
 M.m  = 1;                       % number of inputs
 M.n  = 2;                       % number of states
 M.l  = 2;                       % number of outputs
 
-%y = spm_int(M.pE,M,zeros(50,1));
-[M0,M1,L1,L2] = spm_bireduce(M,M.pE);
-
+% Verify spm_bireduce runs without warnings
+testCase.verifyWarningFree(@() spm_bireduce(M,M.pE));
