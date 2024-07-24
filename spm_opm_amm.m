814db47159c2ec8b56c561499dff416d7dc5099e
Tim Tierney <fiachmri@gmail.com>
2024-07-24 17:23:25 +0100

    Enhancement: spheroid fit not dependent on units

--- spm_opm_amm.m
+++ spm_opm_amm.m
@@ -73,18 +73,17 @@ error('Y is not longest axis.... fix please')
 end
 
 inside = (v(:,1)-o(1)).^2/r(1)^2+(v(:,2)-o(2)).^2/r(2)^2+(v(:,3)-o(3)).^2/r(3)^2;
+c = sum(inside>1);
+stepsize = max(r*.005);
 
-c = sum(inside<1);
-while c>0
-  rt = r-1;
-  inside = (v(:,1)-o(1)).^2/r(1)^2+(v(:,2)-o(2)).^2/r(2)^2+(v(:,3)-o(3)).^2/r(3)^2;
-
-  cc = sum(inside<1);
-  if(cc<=c)
-    r = r-1;  
+while c~=size(v,1)
+  rt = r-stepsize;
+  inside = (v(:,1)-o(1)).^2/rt(1)^2+(v(:,2)-o(2)).^2/rt(2)^2+(v(:,3)-o(3)).^2/rt(3)^2;
+  cc = sum(inside>1);
+  if(cc>=c)
+    r = r-stepsize;  
     c = cc;
   end 
-  
 end
 
 if S.plotSpheroid
