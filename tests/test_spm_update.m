eb76d412568339d1b50a222bdc20d30e754de8be
Peter Zeidman <peter.zeidman@ucl.ac.uk>
2025-02-10 10:55:16 +0000

    Test: fix to test_spm_update

--- tests/test_spm_update.m
+++ tests/test_spm_update.m
@@ -9,6 +9,21 @@ tests = functiontests(localfunctions);
 
 
 function test_version_check(testCase)
-% Checks whether the server can be contacted
-sts = spm_update();
-testCase.assertFalse(all(isnan(sts)) | all(isinf(sts)) | isempty(sts));
+% Checks whether the server can be contacted. Makes max 3 attempts.
+
+attempts = 3;
+failed = false(attempts,1);
+for i = 1:attempts
+    sts = spm_update();
+    failed(i) = all(isnan(sts)) | all(isinf(sts)) | isempty(sts);
+    
+    if failed(i)
+        % Failure - pause a second
+        pause(1);
+    else
+        % Success - stop here
+        break
+    end
+end
+
+testCase.assertFalse(all(failed));
