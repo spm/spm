49e95f2d570528e2d8cccc95b621896a2456a3d5
AlexanderNA <n.alexander@ucl.ac.uk>
2025-05-01 12:47:50 +0100

    Enhancement: refinement of channel select and inspection

--- toolbox/MEEGtools/spm_opm_psd.m
+++ toolbox/MEEGtools/spm_opm_psd.m
@@ -11,13 +11,17 @@ function [po,freq,indices] = spm_opm_psd(S)
 %   S.units         - units of measurement                  - Default: 'fT'
 %   S.constant      - constant line to draw as reference    - Default: 15
 %   S.wind          - function handle for window            - Default: @hanning
-%	  S.selectbad		  - enable selection of badchannels			  - Default: 0
-
+%	S.selectbad		- highlights and enables selection of 
+%                     bad channels			                - Default: 0
+%
 %
 % Output:
 %   psd             - power spectral density
 %   f               - frequencies psd is sampled at
-%	  indices				      - selected channel index 
+%	indices			- selected channel index
+%						To get labels use:
+%							plotted_lab = chanlabels(S.D,S.channels);
+%							sel_lab = plotted_lab(sel);
 %__________________________________________________________________________
 
 % Tim Tierney
@@ -36,16 +40,13 @@ if ~isfield(S, 'plot'),          S.plot = 0; end
 if ~isfield(S, 'D'),             error('D is required'); end
 if ~isfield(S, 'trials'),        S.trials=0; end
 if ~isfield(S, 'wind'),          S.wind=@hanning; end
-if ~isfield(S, 'interact'),      S.interact =1; end
-if ~isfield(S, 'selectbad'),		 S.selectbad = 0; end
-
-indices = [];
+if ~isfield(S, 'selectbad'),     S.selectbad = 0; end
 
 %-channel Selection
 %--------------------------------------------------------------------------
-
+indices = [];
 labs = S.channels;
-regex = {};
+regex = cell(1,length(labs));
 for i = 1:length(labs)
     if isa(labs,'cell')
         regex{i} = ['regexp_(',labs{i},')'];
@@ -81,7 +82,7 @@ catch
   wind = ones(N,1);
 end
 
-Nf= ceil((N+1)/2);
+% Nf= ceil((N+1)/2);
 coFac= max(wind)/mean(wind);
 wind = repmat(wind,1,length(chans));
 
@@ -133,25 +134,24 @@ end
 %--------------------------------------------------------------------------
 
 po = psdx;
+% Identify outliers
 med = repmat(median(po,2),1,size(po,2));
 ma = repmat(median(abs(po-med),2)*1.48,1,size(po,2));
 Z = (po-med)./ma;
 bin = Z>abs(spm_invNcdf(.025/(size(po,2)*100),0,1));
-badlabs = labs(sum(bin)>0)';
 
 if(S.plot)
-  f= figure();
-		h = plot(freq,po,'LineWidth',2);
-		hold on
+    figure();
+
+	h = plot(freq,po,'LineWidth',2);
+	hold on
+	for i = 1:numel(h)
+		tag = [labs{i}, ', Index: ' num2str(indchannel(S.D,labs{i}))];
+		set(h(i), 'UserData', false, 'tag',tag);
+	end
+
+    set(gca,'yscale','log')
     
-		for i = 1:numel(h)
-			set(h(i), 'UserData', true);
-      tag = [labs{i}, ', Index: ' num2str(indchannel(S.D,labs{i}))];
-      set(h(i), 'tag', tag);      
-      set(h(i), 'Color', [h(i).Color(1:3), 1]);
-    end
-    
-    set(gca,'yscale','log')  
     xp2 =0:round(freq(end));
     yp2=ones(1,round(freq(end))+1)*S.constant;
     p2 =plot(xp2,yp2,'--k');
@@ -161,6 +161,7 @@ if(S.plot)
     xlabel('Frequency (Hz)')
     labY = ['$$PSD (' S.units ' \sqrt[-1]{Hz}$$)'];
     ylabel(labY,'interpreter','latex')
+    
     grid on
     ax = gca; % current axes
     ax.FontSize = 13;
@@ -168,70 +169,125 @@ if(S.plot)
     fig= gcf;
     fig.Color=[1,1,1];
     xlim([0,100]);
-     
-    
-    datacursormode on
-    dcm = datacursormode(gcf);
-    set(dcm,'UpdateFcn',@getLabel);
-    if(S.selectbad)
-      sel = false(1,length(labs));
-      % add badchannels to plot
-      for i = 1:size(po,2)
-        tag = [labs{i}, ', Index: ' num2str(indchannel(S.D,labs{i}))];
-        z=plot(freq(bin(:,i)), po(bin(:,i),i),'k*','tag',tag);
-        h = [h;z];
-      end
-      % Set up selection mechanism
-      set(fig, 'WindowButtonMotionFcn', @startSelection);
-      % Wait for user to interact and close the figure
-      waitfor(fig);
-    end
-    
+
+	% Highlight bad channel/frequency combinations
+	if(S.selectbad)
+		g = gobjects(1, numel(labs));
+		hold on
+		for i = 1:numel(labs)
+    		x = freq(bin(:,i));
+    		y = po(bin(:,i),i);
+		
+    		if ~isempty(x) && ~isempty(y)
+        		tag = [labs{i}, ', Index: ' num2str(indchannel(S.D, labs{i}))];
+        		g(i) = plot(x, y, 'k*', 'Tag', tag);
+        		set(g(i), 'UserData', false);
+    		end
+		end
+		sel = false(1,length(labs));
+		numChannels = size(po, 2);
+		anno = [];
+		set(fig, 'WindowButtonDownFcn', @startSelection);
+		
+		% Wait for user to interact and close the figure
+		waitfor(fig);
+	else
+		datacursormode on
+		dcm = datacursormode(gcf);
+ 		set(dcm,'UpdateFcn',@getLabel)
+	end
 end
 
 function startSelection(~, ~)
-% Draw selection box
 point1 = get(ax, 'CurrentPoint');
 rbbox; 
 point2 = get(ax, 'CurrentPoint');
 
-% Get the x and y limits of the selection box
-xLimits = [min(point1(1,1), point2(1,1)), max(point1(1,1), point2(1,1))];
-yLimits = [min(point1(1,2), point2(1,2)), max(point1(1,2), point2(1,2))];
-
-% Check which lines are inside the box
-for chanIdx = 1:length(h)
-	xData = get(h(chanIdx), 'XData');
-	yData = get(h(chanIdx), 'YData');
+% Determine if user clicked or clicked and dragged
+if norm(point1(1,1:2) - point2(1,1:2)) < 2
+	xClick = point1(1,1);
+    yClick = point1(1,2);
+    clickedObj = hittest(fig);
 	
-	% Find any points within the x and y limits of the selection box
-	inX = (xData >= xLimits(1)) & (xData <= xLimits(2));
-	inY = (yData >= yLimits(1)) & (yData <= yLimits(2));
+	% Make a text box (similar to datacursor)
+	if isgraphics(clickedObj, 'line')
+		if ~isempty(anno)
+			set(anno,'Visible','off')
+		end
+		
+		tagText = get(clickedObj, 'Tag');
+        txt = {tagText;
+            ['Frequency: ', num2str(xClick)];
+            ['RMS: ', num2str(yClick)];
+        };
+ 
+		% Position on ylog axis
+        axPos = get(ax, 'Position');
+        xlims = get(ax, 'XLim');
+        ylims = get(ax, 'YLim');
+        yNorm = (log10(yClick) - log10(ylims(1))) / (log10(ylims(2)) - log10(ylims(1)));
+		yNorm = axPos(2) + yNorm * axPos(4);
+        xNorm = axPos(1) + (xClick - xlims(1)) / diff(xlims) * axPos(3);
 
-	% If any point of the line is within the selection box, toggle selection
-	if any(inX & inY)
-      set(h(chanIdx), 'UserData', true);
-			set(h(chanIdx), 'Visible','off');
-      ta = get (h(chanIdx),'tag');
-      ind =str2num(ta((strfind(ta,':')+1):end));
-      sel(ind)=true;
-      indices = find(sel);
+        anno = annotation(fig, 'textbox', [xNorm, yNorm, 0.1, 0], ...
+                    'String', txt, ...
+                    'FitBoxToText', 'on', ...
+                    'BackgroundColor', 'w', ...
+                    'EdgeColor', [0.5 0.5 0.5], ...
+                    'Margin', 2);
+	end
+else
+	if ~isempty(anno)
+		set(anno,'Visible','off')
+	end
+	% Get the x and y limits of the selection box
+	xLimits = [min(point1(1,1), point2(1,1)), max(point1(1,1), point2(1,1))];
+	yLimits = [min(point1(1,2), point2(1,2)), max(point1(1,2), point2(1,2))];
+	
+	% Check which lines are inside the box
+	for chanIdx = 1:numChannels
+		xData = get(h(chanIdx), 'XData');
+		yData = get(h(chanIdx), 'YData');
+		
+		% Find any points within the x and y limits of the selection box
+		inX = (xData >= xLimits(1)) & (xData <= xLimits(2));
+		inY = (yData >= yLimits(1)) & (yData <= yLimits(2));
+	
+		% If any point of the line is within the selection box, toggle selection
+		if any(inX & inY)
+			if get(h(chanIdx), 'UserData') == false
+				set(h(chanIdx), 'UserData', true); 
+				set(h(chanIdx), 'Color', [h(chanIdx).Color(1:3), 0]);
+				if isgraphics(g(chanIdx))
+					set(g(chanIdx), 'Visible', 'off');
+				end
+				sel(chanIdx) = true;
+				ta = get (h(chanIdx),'tag');
+      			ind = str2double(ta((strfind(ta,':')+1):end));
+ 				sel(ind) = true;
+      			indices = find(sel);
+			end
+		end
+	end
+	
+	% Exclude selected channels from the median calculation
+	set(p3, 'YData', median(po(:, ~sel), 2));
+	maxSelPo = max(max(po(:,~sel)));
+	minSelPo = min(min(po(:,~sel)));
+	try
+		set(gca,'YLim',[minSelPo,maxSelPo])
+	catch
 	end
 end
-% Exclude selected channels from the median calculation
-set(p3, 'YData', median(po(:, ~sel), 2));
-maxSelPo = max(max(po(:,~sel)));
-minSelPo = min(min(po(:,~sel)));
-set(gca,'YLim',[minSelPo,maxSelPo])
-
 end
 
-end
-
-function txt = getLabel(trash,event)
+function txt = getLabel(~,event)
 pos = get(event,'Position');
 dts = get(event.Target,'Tag');
 txt = {dts,...
-       ['Frequency: ',num2str(pos(1))],...
-     ['RMS: ',num2str(pos(2))]};
+		['Frequency: ',num2str(pos(1))],...
+		['RMS: ',num2str(pos(2))]};
 end
+end
+
+
