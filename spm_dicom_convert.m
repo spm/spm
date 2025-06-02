bf75bb579a9e8a96c8791908336c10940eef1432
John Ashburner <jashburner@gmail.com>
2025-06-02 17:35:38 +0100

    Bug fix: Needs '\\P\\', rather than '\P\' to identify parts of ImageHeader containing '\P\'

--- spm_dicom_convert.m
+++ spm_dicom_convert.m
@@ -1315,7 +1315,7 @@ if isfield(Header,'GE_ImageType')
         end
     end
 else
-    if isfield(Header,'ImageType') && ~isempty(regexp(Header.ImageType,'\P\'))
+    if isfield(Header,'ImageType') && ~isempty(regexp(Header.ImageType,'\\P\\'))
         ImTyp = '-Phase';
     end
 end
