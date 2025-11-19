7d577209fb8aa3951edc9f6e2334d290f779fc13
WillForan <willforan@gmail.com>
2025-11-19 14:38:04 -0500

    test/spm_bids: bidsroot into setup w/tempdir fixture

--- tests/test_spm_bids.m
+++ tests/test_spm_bids.m
@@ -3,43 +3,54 @@ classdef test_spm_bids < matlab.unittest.TestCase
 %   spm_tests('class','unit','test','test_spm_bids')
 %__________________________________________________________________________
 
-% Copyright (C) 2020-2022 Wellcome Centre for Human Neuroimaging
+% Copyright (C) 2025 Wellcome Centre for Human Neuroimaging
 
+properties
+   bidsdir % temporary bids root folder
+end
+
+methods (TestMethodSetup)
+   function setup(testCase)
+      import matlab.unittest.fixtures.TemporaryFolderFixture
+      % per test bids root. matlab removes when test finishes
+      tmp = testCase.applyFixture(TemporaryFolderFixture);
+      testCase.bidsdir = tmp.Folder;
+      % Note: R2023 introduced 'testCase.createTemporaryFolder();'
+      %       more ergonomic than instead of applyFixture().Folder
+
+      % required json at root of BIDS/
+      desc_f = fullfile(testCase.bidsdir,'dataset_description.json');
+      fid = fopen(desc_f,'w');
+      fprintf(fid, '{"Name": "Test", "BIDSVersion": "1.7.0", "Authors": ["None"]}');
+      fclose(fid);
+   end
+end % TestMethodSetup
 
 
 methods (Test)
-
-
 function test_spm_bids_dwi(testCase)
 import matlab.unittest.constraints.*
 
-tmp = spm_platform('tempdir');
-bidsdir = fullfile(tmp,'bids');
-
-% require json at root of BIDS/
-desc_f = fullfile(bidsdir,'dataset_description.json');
-fid = fopen(desc_f,'w');
-fprintf(fid, '{"Name": "Test", "BIDSVersion": "1.7.0", "Authors": ["None"]}');
-fclose(fid);
 
 % create files:
 %  /tmp/bids/sub-1/dwi/sub-1_dir-AP.{nii,json,bvec,bval}
-dwidir = fullfile(bidsdir, 'sub-1','dwi');
+dwidir = fullfile(testCase.bidsdir, 'sub-1','dwi');
 mkdir(dwidir);
 prefix = 'sub-1_dir-AP_dwi';
+% Note: nii just needs to exist for this test. value/header is not read
 for ext = {'nii','bvec','bval'};
    bids_file = fullfile(dwidir,[prefix '.' ext{1}]);
    fid = fopen(bids_file,'w');
    fprintf(fid,'1'); % any value will do?
    fclose(fid);
 end
-% json handled individually. maybe need to do same for nii?
+% json handled individually
 fid = fopen(fullfile(dwidir,[prefix '.json']),'w');
 fprintf(fid,'{}');
 fclose(fid);
 
 % run BIDS
-BIDS = spm_BIDS(fullfile(tmp,'bids')),
+BIDS = spm_BIDS(testCase.bidsdir);
 
 % confirm we were able to read single dwi
 testCase.verifyEqual(fieldnames(BIDS), {'dir','description','sessions','scans','sess','participants','subjects'}');
