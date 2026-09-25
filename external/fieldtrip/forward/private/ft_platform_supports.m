9e1898660b37842e6fe321e3400a4972cc5a0a30
korbinian90 <1307522+korbinian90@users.noreply.github.com>
2026-09-25 06:05:13 +0000

    Update external/fieldtrip to 7bcde3ff5
    FieldTrip release branch commit: 7bcde3ff5ff0222693d433343f7f04db579a64a9
    Committed: 2026-09-24 15:02:23 +0200
    
    Synced via GitHub Actions (sync-fieldtrip workflow).

--- external/fieldtrip/forward/private/ft_platform_supports.m
+++ external/fieldtrip/forward/private/ft_platform_supports.m
@@ -134,12 +134,7 @@ switch what
     root_dir = fileparts(which('ft_defaults'));
     if ~isempty(root_dir)
       external_stats_dir = fullfile(root_dir, 'external', 'stats');
-      % these files are only used by the other functions in the external/stats directory
-      exclude_mfiles = {
-        'common_size.m'
-        'iscomplex.m'
-        };
-      tf = has_all_functions_in_dir(external_stats_dir, exclude_mfiles);
+      tf = has_all_functions_in_dir(external_stats_dir, {});
     else
       % this probably only works for MATLAB, not for Octave
       tf = ~isempty(ver('stats'));
